c8df0243621c7a94ea927552f54c152643dfdeb2
Pervez Iqbal <pervezfunctor@gmail.com>
2018-11-20 16:01:35 +0530

    make Text responsive

--- src/bulma/Text.tsx
+++ src/bulma/Text.tsx
@@ -3,18 +3,32 @@ import classNames from 'classnames'
 
 type TextSize = '1' | '2' | '3' | '4' | '5' | '6' | '7'
 
+type TextResponsiveSize =
+  | 'none'
+  | 'mobile'
+  | 'tablet'
+  | 'touch'
+  | 'desktop'
+  | 'widescreen'
+  | 'fullhd'
+
 interface TextProps {
   readonly size: TextSize
+  readonly responsiveSize?: TextResponsiveSize
   readonly children: React.ReactChild
   readonly inline?: boolean
 }
 
 export const Text: React.SFC<TextProps> = ({
   size,
+  responsiveSize = 'none',
   children,
   inline = false,
 }: TextProps) => {
-  const cnSize: string = `is-size-${size}`
+  const cnSize: string =
+    responsiveSize === 'none'
+      ? `is-size-${size}`
+      : `is-size-${size}-${responsiveSize}`
   const classes: string = classNames(cnSize)
   return inline ? (
     <span className={classes}>{children}</span>
