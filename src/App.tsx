acc81eb7148c18fc29b20e456a4a04dd983f7954
Pervez Iqbal <pervezfunctor@gmail.com>
2018-11-20 16:22:26 +0530

    add color attribute to Text Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Text } from './bulma/Text'
 
 export const App: React.SFC = () => (
-  <Text size="1" inline={true} responsiveSize="mobile">
+  <Text size="1" inline={true} responsiveSize="mobile" color="info">
     Hello, World!
   </Text>
 )
