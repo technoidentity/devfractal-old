b4af760d9998d06b1a47f470427ebb3e823cd097
Pervez Iqbal <pervezfunctor@gmail.com>
2018-11-20 16:01:35 +0530

    make Text responsive

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Text } from './bulma/Text'
 
 export const App: React.SFC = () => (
-  <Text size="1" inline={true}>
+  <Text size="1" inline={true} responsiveSize="mobile">
     Hello, World!
   </Text>
 )
