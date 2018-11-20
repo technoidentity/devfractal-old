e8d7cdcaea46cc0e202cd1313041e2894adfcc72
Pervez Iqbal <pervezfunctor@gmail.com>
2018-11-20 15:52:33 +0530

    Text can be inline or a block element

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
 import { Text } from './bulma/Text'
 
-export const App: React.SFC = () => <Text size="1">Hello, World!</Text>
+export const App: React.SFC = () => (
+  <Text size="1" inline={true}>
+    Hello, World!
+  </Text>
+)
