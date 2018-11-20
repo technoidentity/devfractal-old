9b577a317bb3f7d89d1374a5b1d625b0848b2ddb
Pervez Iqbal <pervezfunctor@gmail.com>
2018-11-20 15:39:23 +0530

    use classnames package to simplify additions of future bulma modifiers

--- src/bulma/Text.tsx
+++ src/bulma/Text.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import classNames from 'classnames'
 
 type TextSize = '1' | '2' | '3' | '4' | '5' | '6' | '7'
 
@@ -7,6 +8,8 @@ interface TextProps {
   readonly children: React.ReactChild
 }
 
-export const Text: React.SFC<TextProps> = (props: TextProps) => (
-  <h1 className={`is-size-${props.size}`}>{props.children}</h1>
-)
+export const Text: React.SFC<TextProps> = (props: TextProps) => {
+  const size: string = `is-size-${props.size}`
+  const classes: string = classNames(size)
+  return <div className={classes}>{props.children}</div>
+}
