af4122cd13462442abdc17c1c345d503d99dc2b5
Sravani Dande <sravanidnd89@gmail.com>
2018-11-21 15:30:38 +0530

    make Input properties as optional

--- src/bulma/Input.tsx
+++ src/bulma/Input.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import classNames from 'classnames'
+
 type InputColor = 'primary' | 'info' | 'success' | 'warning' | 'danger'
 
 type InputSize = 'small' | 'medium' | 'large'
@@ -9,21 +11,40 @@ type InputStyle = 'rounded'
 type InputState = 'hovered' | 'focused'
 
 interface InputProps {
-  readonly color: InputColor
-  readonly size: InputSize
-  readonly style: InputStyle
-  readonly state: InputState
-  readonly placeHolder: string
-  readonly disabled: boolean
+  readonly color?: InputColor
+  readonly size?: InputSize
+  readonly style?: InputStyle
+  readonly state?: InputState
+  readonly placeholder?: string
+  readonly name: string
+  readonly value?: string
+  readonly disabled?: boolean
 }
 
-export const Input: React.SFC<InputProps> = props => (
-  <input
-    className={`input is-${props.color} is-${props.size} is-${props.style} is-${
-      props.state
-    }`}
-    type="text"
-    placeholder={props.placeHolder}
-    disabled={props.disabled}
-  />
-)
+export const Input: React.SFC<InputProps> = ({
+  color,
+  size,
+  style,
+  state,
+  placeholder,
+  name,
+  value,
+  disabled,
+}) => {
+  const classes: string = classNames('input', {
+    [`is-${color}`]: color,
+    [`is-${size}`]: size,
+    [`is-${style}`]: style,
+    [`is-${state}`]: state,
+  })
+  return (
+    <input
+      className={classes}
+      type="text"
+      placeholder={placeholder}
+      name={name}
+      value={value}
+      disabled={disabled}
+    />
+  )
+}
