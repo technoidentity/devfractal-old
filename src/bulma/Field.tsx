3934e4ea69c9ac8446e1de8fe8da9077a1e4396d
Pervez Iqbal <pervezfunctor@gmail.com>
2018-11-21 18:20:16 +0530

    fields can be grouped and centered

--- src/bulma/Field.tsx
+++ src/bulma/Field.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react'
+import classNames from 'classnames'
 
 export interface FieldProps {
+  readonly grouped?: boolean
+  readonly centered?: boolean
   readonly children: React.ReactNode
 }
 
-export const Field: React.SFC<FieldProps> = ({ children }) => (
-  <div className="field">{children}</div>
-)
+export const Field: React.SFC<FieldProps> = ({
+  children,
+  grouped,
+  centered,
+}) => {
+  const classes: string = classNames('field', {
+    'is-grouped': grouped,
+    'is-grouped-centered': centered,
+  })
+  return <div className={classes}>{children}</div>
+}
