bfd8fb20c9fe2d6a2309ad583e9c3ba3b613851d
Pervez Iqbal <pervezfunctor@gmail.com>
2018-11-22 14:23:05 +0530

    use a single logger function to avoid console.log tslint errors

--- src/bulma/examples/LoginForm.tsx
+++ src/bulma/examples/LoginForm.tsx
@@ -4,31 +4,32 @@ import { Field } from '../Field'
 import { Label } from '../Label'
 import { Button } from '../Button'
 
+const logger: (evt: any) => void = evt => {
+  // tslint:disable no-console
+  console.log('log! ')
+  if (evt && evt.target) {
+    console.log(evt.target.name, ':', evt && evt.target && evt.target.value)
+  }
+  // tslint:enable no-console
+}
+
 export const LoginForm: React.SFC = () => {
   return (
     <div className="section">
       <div className="container">
         <Field>
           <Label>User Name</Label>
-          <Input
-            name="username"
-            type="text"
-            onChange={() => console.log('foo')}
-          />
+          <Input name="username" type="text" onChange={logger} />
         </Field>
         <Field>
           <Label>Password</Label>
-          <Input
-            name="password"
-            type="password"
-            onChange={() => console.log('foo')}
-          />
+          <Input name="password" type="password" onChange={logger} />
         </Field>
         <Field grouped groupCentered>
-          <Button onClick={() => console.log('clicked')} color="info">
+          <Button onClick={logger} color="info">
             Submit
           </Button>
-          <Button onClick={() => console.log('clicked')} color="danger">
+          <Button onClick={logger} color="danger">
             Reset
           </Button>
         </Field>
