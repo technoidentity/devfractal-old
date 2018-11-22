f6dd35a28e43438561b7548677b46fc2e56bb893
Pervez Iqbal <pervezfunctor@gmail.com>
2018-11-22 09:48:12 +0530

    App.tsx showcases all examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,31 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-export const App: React.SFC = () => <h1>Hello, World</h1>
+import { SampleForm } from './bulma/examples/SampleForm'
+import { LoginForm } from './bulma/examples/LoginForm'
+
+export const App: React.SFC = () => (
+  <Router>
+    <div>
+      <div className="content">
+        Examples using Bulma components in devfractal
+        <nav>
+          <ul>
+            <li>
+              <Link to="/sample-form">Sample Form</Link>
+            </li>
+            <li>
+              <Link to="/login-form">Login Form</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="section">
+        <div className="container">
+          <Route path="/sample-form" exact component={SampleForm} />
+          <Route path="/login-form" exact component={LoginForm} />
+        </div>
+      </div>
+    </div>
+  </Router>
+)
