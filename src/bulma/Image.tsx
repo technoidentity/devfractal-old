89603ca20dadef9553193800abda5e8e01fedc35
swathi <swathivalasapalli@gmail.com>
2018-11-22 13:55:22 +0530

    add Image component using bulma

---
+++ src/bulma/Image.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react'
+import classNames from 'classnames'
+
+type FixedSquaredImageSize =
+  | '16x16'
+  | '24x24'
+  | '32x32'
+  | '48x48'
+  | '64x64'
+  | '96x96'
+  | '128x128'
+
+type ResponsiveImageRatio =
+  | 'square'
+  | '1by1'
+  | '5by4'
+  | '3by2'
+  | '5by3'
+  | '16by9'
+  | '2by1'
+  | '3by1'
+  | '4by5'
+  | '3by4'
+  | '2by3'
+  | '3by5'
+  | '9by16'
+  | '1by2'
+  | '1by3'
+
+interface ImageProps {
+  readonly size?: FixedSquaredImageSize
+  readonly responsiveImageRatio?: ResponsiveImageRatio
+  readonly children: React.ReactChild
+}
+
+export const Image: React.SFC<ImageProps> = ({
+  size,
+  children,
+  responsiveImageRatio,
+}) => {
+  const classes: string = classNames('image', {
+    [`is-${size}`]: size,
+    [`is-${responsiveImageRatio}`]: responsiveImageRatio,
+  })
+
+  return <figure className={classes}>{children}</figure>
+}
