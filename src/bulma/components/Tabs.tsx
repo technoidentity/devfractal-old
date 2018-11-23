c5bd59d3e4c2741d8ca910d03ba7376b90e33ade
swathi <swathivalasapalli@gmail.com>
2018-11-23 16:39:57 +0530

    add Tabs component using bulma

---
+++ src/bulma/components/Tabs.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react'
+import classNames from 'classnames'
+
+type TabsSize = 'small' | 'medium' | 'large'
+
+type TabsAlignment = 'centered' | 'right'
+
+interface TabsProps {
+  readonly size?: TabsSize
+  readonly alignment?: TabsAlignment
+  readonly fullWidth?: boolean
+  readonly toggle?: boolean
+  readonly boxed?: boolean
+  readonly children: ReadonlyArray<JSX.Element>
+}
+
+interface TabsItemProps {
+  readonly href: string
+  readonly active?: boolean
+  readonly children: React.ReactChild
+}
+
+export const TabsItem: React.SFC<TabsItemProps> = ({
+  href,
+  active,
+  children,
+}) => {
+  const classes: string = classNames({
+    'is-active': active,
+  })
+  return (
+    <li className={classes}>
+      <a href={href}>{children}</a>
+    </li>
+  )
+}
+
+export const Tabs: React.SFC<TabsProps> = ({
+  size,
+  alignment,
+  fullWidth,
+  toggle,
+  boxed,
+  children,
+}) => {
+  const classes: string = classNames('tabs', {
+    [`is-${size}`]: size,
+    [`is-${alignment}`]: alignment,
+    'is-boxed': boxed,
+    'is-toggle': toggle,
+    'is-fullwidth': fullWidth,
+  })
+  return (
+    <div className={classes}>
+      <ul>
+        {React.Children.map(children, child => (
+          <li>{child}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
