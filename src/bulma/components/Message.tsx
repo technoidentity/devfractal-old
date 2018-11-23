b0aab40e7ec226460061e0e83ca385ad0fcbcf09
swathi <swathivalasapalli@gmail.com>
2018-11-23 16:47:57 +0530

    add Message component using bulma

---
+++ src/bulma/components/Message.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+import classNames from 'classnames'
+
+type MessageColor =
+  | 'dark'
+  | 'primary'
+  | 'link'
+  | 'info'
+  | 'success'
+  | 'warning'
+  | 'danger'
+
+type MessageSize = 'normal' | 'small' | 'medium' | 'large'
+
+interface MessageProps {
+  readonly color?: MessageColor
+  readonly size?: MessageSize
+  readonly children: React.ReactNode
+}
+
+interface MessageHeaderProps {
+  readonly children: React.ReactNode
+}
+
+interface MessageBodyProps {
+  readonly children: React.ReactChild
+}
+export const Message: React.SFC<MessageProps> = ({ size, color, children }) => {
+  const classes: string = classNames('message', {
+    [`is-${size}`]: size,
+    [`is-${color}`]: color,
+  })
+  return <article className={classes}>{children}</article>
+}
+
+export const MessageHeader: React.SFC<MessageHeaderProps> = ({ children }) => {
+  return <div className={`message-header`}>{children}</div>
+}
+
+export const MessageBody: React.SFC<MessageBodyProps> = ({ children }) => {
+  return <div className={`message-body`}>{children}</div>
+}
