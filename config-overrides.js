997ae6ea438a5af3850a1acd3f446f44b9a90898
Pervez Iqbal <pervezfunctor@gmail.com>
2018-11-27 17:07:44 +0530

    add storybook

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,6 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader')
+const path = require('path')
+const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin')
 
 /* config-overrides.js */
 module.exports = function override(config, env) {
