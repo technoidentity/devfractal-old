64e90f3d38c8c0cf67e9deba09f5378ca8b52462
swathi <swathivalasapalli@gmail.com>
2018-11-28 14:15:23 +0530

    add Image example

---
+++ src/bulma/examples/ImageExample.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { Image } from '../Image'
+
+export const ImageExample: React.SFC = () => (
+  <Image size="128x128">
+    <img src="https://bulma.io/images/placeholders/128x128.png" />
+  </Image>
+)
