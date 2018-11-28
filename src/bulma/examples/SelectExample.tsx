d645f74148bc9ee46c6c832a6d4d999b6ce6d37d
sravani dande <sravanidnd89@gmail.com>
2018-11-28 13:35:51 +0530

    add Select example

---
+++ src/bulma/examples/SelectExample.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+import { Select } from '../Select'
+
+export const SelectExample: React.SFC = () => (
+  <Select>
+    <option>Select dropdown</option>
+    <option>With options</option>
+  </Select>
+)
