cb3f8c9611f8aa48340057616016433249db9cf1
swathi <swathivalasapalli@gmail.com>
2018-11-28 17:22:29 +0530

    add ModalCard to Modal component

--- src/bulma/components/Modal.tsx
+++ src/bulma/components/Modal.tsx
@@ -6,17 +6,29 @@ interface ModalProps {
   readonly children: React.ReactNode
 }
 
-// TODO: we can place any other Bulma Elements you want in ModalContent
 interface ModalContentProps {
   readonly children: React.ReactNode
 }
 
-export const Modal: React.SFC<ModalProps> = ({ active, children }) => {
-  const classes: string = classNames('modal', {
-    'is-active': active,
-  })
-  return <div className={classes}>{children}</div>
-}
+export const ModalCard: React.SFC = ({ children }) => (
+  <div className="modal-card">{children}</div>
+)
+
+export const ModalCardHead: React.SFC = ({ children }) => (
+  <header className="modal-card-head">{children}</header>
+)
+
+export const ModalCardTitle: React.SFC = ({ children }) => (
+  <p className="modal-card-title">{children}</p>
+)
+
+export const ModalCardBody: React.SFC = ({ children }) => (
+  <section className="modal-card-body">{children}</section>
+)
+
+export const ModalCardFoot: React.SFC = ({ children }) => (
+  <footer className="modal-card-foot">{children}</footer>
+)
 export const ModalBackground: React.SFC = () => (
   <div className="modal-background" />
 )
@@ -26,3 +38,10 @@ export const ModalContent: React.SFC<ModalContentProps> = ({ children }) => (
 )
 
 export const ModalClose: React.SFC = () => <button className="modal-close" />
+
+export const Modal: React.SFC<ModalProps> = ({ active, children }) => {
+  const classes: string = classNames('modal', {
+    'is-active': active,
+  })
+  return <div className={classes}>{children}</div>
+}
