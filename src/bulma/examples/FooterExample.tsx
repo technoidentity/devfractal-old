13a54202a8e639980408e5541a47c3d552b3e38d
sravani dande <sravanidnd89@gmail.com>
2018-11-28 15:28:49 +0530

    add Footer example

---
+++ src/bulma/examples/FooterExample.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+
+import { Footer } from '../Footer'
+import { Container } from '../Container'
+
+export const FooterExample: React.SFC = () => (
+  <Footer>
+    <Container>
+      <p>
+        <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>
+        . The source code is licensed
+        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The
+        website content is licensed
+        <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+          CC BY NC SA 4.0
+        </a>
+        .
+      </p>
+    </Container>
+  </Footer>
+)
