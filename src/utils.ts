48007ebd99582d6fe659e0d7a8b1d8ea8d8ba665
Technoidentity Solutions Pvt Ltd <venkat@technoidentity.com>
2018-12-01 10:59:54 +0530

    update packages

--- src/utils.ts
+++ src/utils.ts
@@ -3,5 +3,8 @@ import { produce, Draft } from 'immer'
 type Mutable<T> = { -readonly [P in keyof T]: Mutable<T[P]> } // Remove readonly
 
 export function mutative<T>(obj: T, f: (draft: Mutable<T>) => void): T {
-  return produce(obj, f as (this: Draft<T>, draftState: Draft<T>) => void | T)
+  return produce(obj, (f as unknown) as (
+    this: Draft<T>,
+    draftState: Draft<T>,
+  ) => void | T)
 }
