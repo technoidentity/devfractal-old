ff8352c523a82bcc056a0d83591c7b501b3d60e4
Pervez Iqbal <pervezfunctor@gmail.com>
2018-12-01 18:34:35 +0530

    correct forwarding of props for Select

--- src/bulma/Select.tsx
+++ src/bulma/Select.tsx
@@ -3,40 +3,41 @@ import classNames from 'classnames'
 
 type SelectColor = 'primary' | 'info' | 'success' | 'warning' | 'danger'
 
-type SelectStyle = 'rounded'
-
 type SelectSize = 'small' | 'medium' | 'large'
 
-type SelectState = 'hovered' | 'focused'
+type SelectState = 'hovered' | 'focused' | 'loading'
 
-interface SelectProps {
+interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
   readonly color?: SelectColor
-  readonly style?: SelectStyle
-  readonly size?: SelectSize
+  readonly rounded?: boolean
+  readonly selectSize?: SelectSize
   readonly state?: SelectState
-  readonly name?: string
-  readonly onChange?: React.ChangeEventHandler<HTMLSelectElement>
 }
 
 export const Select: React.SFC<SelectProps> = ({
   color,
-  style,
-  size,
+  selectSize,
   state,
-  name,
-  onChange,
   children,
+  rounded,
+  className,
+  ...props
 }) => {
-  const classes: string = classNames('select', {
+  const divClasses: string = classNames('select', {
     [`is-${color}`]: color,
-    [`is-${style}`]: style,
-    [`is-${size}`]: size,
-    [`is-${state}`]: state,
+    [`is-rounded`]: rounded,
+    [`is-${selectSize}`]: selectSize,
   })
+  const classes: string = classNames(
+    {
+      [`is-${state}`]: state,
+    },
+    className,
+  )
   return (
     <div className="control">
-      <div className={classes}>
-        <select onChange={onChange} name={name}>
+      <div className={divClasses}>
+        <select {...props} className={classes}>
           {children}
         </select>
       </div>
--- src/bulma/examples/SelectExample.tsx
+++ src/bulma/examples/SelectExample.tsx
@@ -1,10 +1,40 @@
 import * as React from 'react'
 
 import { Select } from '../Select'
+import { Field } from '../Field'
+import { logger } from './common'
 
 export const SelectExample: React.SFC = () => (
-  <Select>
-    <option>Select dropdown</option>
-    <option>With options</option>
-  </Select>
+  <div>
+    <Field>
+      <Select selectSize="small">
+        <option>Select dropdown</option>
+        <option>With options</option>
+      </Select>
+    </Field>
+    <Field>
+      <Select state="focused" rounded onChange={() => logger('hello world')}>
+        <option>Select dropdown</option>
+        <option>With options</option>
+      </Select>
+    </Field>
+    <Field>
+      <Select color="success" state="hovered">
+        <option>Select dropdown</option>
+        <option>With options</option>
+      </Select>
+    </Field>
+    <Field>
+      <Select color="warning" selectSize="large">
+        <option>Select dropdown</option>
+        <option>With options</option>
+      </Select>
+    </Field>
+    <Field>
+      <Select state="loading">
+        <option>Select dropdown</option>
+        <option>With options</option>
+      </Select>
+    </Field>
+  </div>
 )
