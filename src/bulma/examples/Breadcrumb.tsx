9821d6ab1e6a92c4a435200778d98222072ea1ff
Pervez Iqbal <pervezfunctor@gmail.com>
2018-12-04 15:48:34 +0530

    correct forwarding of props to Breadcrumb Component

--- src/bulma/examples/Breadcrumb.tsx
+++ src/bulma/examples/Breadcrumb.tsx
@@ -3,12 +3,22 @@ import * as React from 'react'
 import { Breadcrumb, BreadcrumbItem } from '../components/Breadcrumb'
 
 export const BreadcrumbExample: React.SFC = () => (
-  <Breadcrumb>
-    <BreadcrumbItem href="#">Bulma</BreadcrumbItem>
-    <BreadcrumbItem href="#" active>
-      Documentation
-    </BreadcrumbItem>
-    <BreadcrumbItem href="#">Components</BreadcrumbItem>
-    <BreadcrumbItem href="#">Breadcrumb</BreadcrumbItem>
-  </Breadcrumb>
+  <div>
+    <Breadcrumb className="has-arrow-separator" size="medium">
+      <BreadcrumbItem>Bulma</BreadcrumbItem>
+      <BreadcrumbItem className="is-active">Documentation</BreadcrumbItem>
+      <BreadcrumbItem>Components</BreadcrumbItem>
+      <BreadcrumbItem>Breadcrumb</BreadcrumbItem>
+    </Breadcrumb>
+    <Breadcrumb
+      className="is-centered is-large"
+      separator="succeeds-separator"
+      aria-label="breadcrumbs"
+    >
+      <BreadcrumbItem>Bulma</BreadcrumbItem>
+      <BreadcrumbItem>Documentation</BreadcrumbItem>
+      <BreadcrumbItem>Components</BreadcrumbItem>
+      <BreadcrumbItem className="is-active">Breadcrumb</BreadcrumbItem>
+    </Breadcrumb>
+  </div>
 )
