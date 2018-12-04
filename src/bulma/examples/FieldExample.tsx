cbad12dc173a93401360c2ed5f467749e756b081
Pervez Iqbal <pervezfunctor@gmail.com>
2018-12-04 15:43:30 +0530

    correct forwarding of props to Field Component

--- src/bulma/examples/FieldExample.tsx
+++ src/bulma/examples/FieldExample.tsx
@@ -2,14 +2,27 @@ import * as React from 'react'
 import { Field } from '../Field'
 import { Input } from '../Input'
 import { Button } from '../Button'
+import { Label } from '../Label'
 
 export const FieldExample: React.SFC = () => (
-  <Field addons addonsCentered>
-    <div className="control">
-      <Input type="text" />
-    </div>
-    <div className="control">
-      <Button color="info">search</Button>
-    </div>
-  </Field>
+  <div>
+    <Field addonsModifier="addons" className="has-addons-centered">
+      <div className="control">
+        <Input type="text" className="is-success" />
+      </div>
+      <div className="control">
+        <Button color="info">search</Button>
+      </div>
+    </Field>
+    <Field>
+      <Label>Name</Label>
+      <div className="control">
+        <Input
+          type="text"
+          placeholder="e.g Alex Smith"
+          className="is-primary"
+        />
+      </div>
+    </Field>
+  </div>
 )
