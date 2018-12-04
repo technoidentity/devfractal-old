1e62c71e24e4d2097251f5e00b154e781e6eddab
Pervez Iqbal <pervezfunctor@gmail.com>
2018-12-04 15:54:50 +0530

    correct forwarding of props to Modal  Component

--- src/bulma/components/Modal.tsx
+++ src/bulma/components/Modal.tsx
@@ -1,42 +1,135 @@
 import * as React from 'react'
 import classNames from 'classnames'
 
-interface ModalProps {
+interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {
   readonly active?: boolean
 }
 
-export const ModalCard: React.SFC = ({ children }) => (
-  <div className="modal-card">{children}</div>
-)
+export const Modal: React.SFC<ModalProps> = ({
+  active,
+  children,
+  className,
+  ...props
+}) => {
+  const classes: string = classNames(
+    'modal',
+    {
+      [`is-active'`]: active,
+    },
+    className,
+  )
+  return (
+    <div {...props} className={classes}>
+      {children}
+    </div>
+  )
+}
 
-export const ModalCardHead: React.SFC = ({ children }) => (
-  <header className="modal-card-head">{children}</header>
-)
+interface ModalCardProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-export const ModalCardTitle: React.SFC = ({ children }) => (
-  <p className="modal-card-title">{children}</p>
-)
+export const ModalCard: React.SFC<ModalCardProps> = ({
+  children,
+  className,
+  ...props
+}) => {
+  const classes: string = classNames('modal-card', className)
+  return (
+    <div {...props} className={classes}>
+      {children}
+    </div>
+  )
+}
 
-export const ModalCardBody: React.SFC = ({ children }) => (
-  <section className="modal-card-body">{children}</section>
-)
+interface ModalHeadProps extends React.HTMLAttributes<HTMLHeadElement> {}
+export const ModalCardHead: React.SFC<ModalHeadProps> = ({
+  children,
+  className,
+  ...props
+}) => {
+  const classes: string = classNames('modal-card-head', className)
+  return (
+    <header {...props} className={classes}>
+      {children}
+    </header>
+  )
+}
 
-export const ModalCardFoot: React.SFC = ({ children }) => (
-  <footer className="modal-card-foot">{children}</footer>
-)
-export const ModalBackground: React.SFC = () => (
-  <div className="modal-background" />
-)
+interface ModalCardTitleProps extends React.HTMLAttributes<HTMLElement> {}
 
-export const ModalContent: React.SFC = ({ children }) => (
-  <div className="modal-content">{children}</div>
-)
+export const ModalCardTitle: React.SFC<ModalCardTitleProps> = ({
+  children,
+  className,
+  ...props
+}) => {
+  const classes: string = classNames('message-card-title', className)
+  return (
+    <p {...props} className={classes}>
+      {children}
+    </p>
+  )
+}
 
-export const ModalClose: React.SFC = () => <button className="modal-close" />
+interface ModalCardBodyProps extends React.HTMLAttributes<HTMLElement> {}
 
-export const Modal: React.SFC<ModalProps> = ({ active, children }) => {
-  const classes: string = classNames('modal', {
-    'is-active': active,
-  })
-  return <div className={classes}>{children}</div>
+export const ModalCardBody: React.SFC<ModalCardBodyProps> = ({
+  children,
+  className,
+  ...props
+}) => {
+  const classes: string = classNames('message-card-body', className)
+  return (
+    <section {...props} className={classes}>
+      {children}
+    </section>
+  )
+}
+
+interface ModalCardFootProps extends React.HTMLAttributes<HTMLElement> {}
+
+export const ModalCardFoot: React.SFC<ModalCardFootProps> = ({
+  children,
+  className,
+  ...props
+}) => {
+  const classes: string = classNames('message-card-foot', className)
+  return (
+    <footer {...props} className={classes}>
+      {children}
+    </footer>
+  )
+}
+
+interface ModalBackgroundProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export const ModalBackground: React.SFC<ModalBackgroundProps> = ({
+  className,
+  ...props
+}) => {
+  const classes: string = classNames('modal-background', className)
+  return <div {...props} className={classes} />
+}
+
+interface ModalContentProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export const ModalContent: React.SFC<ModalContentProps> = ({
+  children,
+  className,
+  ...props
+}) => {
+  const classes: string = classNames('message-content', className)
+  return (
+    <div {...props} className={classes}>
+      {children}
+    </div>
+  )
+}
+
+interface ModalCloseProps extends React.HTMLAttributes<HTMLButtonElement> {}
+
+export const ModalClose: React.SFC<ModalCloseProps> = ({
+  className,
+  ...props
+}) => {
+  const classes: string = classNames('modal-close', className)
+  return <button {...props} className={classes} />
 }
