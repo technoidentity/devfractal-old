332e65a1ac4bfdc80fef591b42ec6d8c8ca8fea3
Sravani Dande <sravanidnd89@gmail.com>
2018-12-08 14:51:19 +0530

    add helpers to checkbox

--- src/bulma/CheckBox.tsx
+++ src/bulma/CheckBox.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
 
-interface CheckBoxProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  readonly readOnly?: boolean
-}
+import classNames from 'classnames'
+import { Helpers, HelpersRemoved, helpersClasses } from './helpers'
+import { removeHelpers } from './helpers'
 
-export const CheckBox: React.SFC<CheckBoxProps> = ({ children, ...props }) => (
-  <div className="control">
-    <label className="checkbox">
-      <input {...props} type="checkbox" />
-      {children}
-    </label>
-  </div>
-)
+interface CheckBoxProps
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    Helpers {}
+
+export const CheckBox: React.SFC<CheckBoxProps> = ({
+  children,
+  className,
+  ...props
+}) => {
+  const propsHelpersRemoved: HelpersRemoved<typeof props> = removeHelpers(props)
+  const classes: string = classNames(
+    'checkbox',
+    className,
+    helpersClasses(props),
+  )
+
+  return (
+    <div className="control">
+      <label className={classes}>
+        <input {...propsHelpersRemoved} {...props} type="checkbox" />
+        {children}
+      </label>
+    </div>
+  )
+}
