a7c250c3aa0877ad04dbd54f4daf676cb85bafc6
Sravani Dande <sravanidnd89@gmail.com>
2018-12-11 15:42:23 +0530

    add helpers to field component

--- src/bulma/Field.tsx
+++ src/bulma/Field.tsx
@@ -1,39 +1,130 @@
 import * as React from 'react'
 import classNames from 'classnames'
+import { Label } from './Label'
+import {
+  Helpers,
+  HelpersRemoved,
+  removeHelpers,
+  helpersClasses,
+} from './helpers'
 
-type FieldGroupModifier =
-  | 'grouped'
-  | 'grouped-centered'
-  | 'grouped-right'
-  | 'grouped-multiline'
-  | 'horizontal'
+type FieldGroupModifier = 'grouped-centered' | 'grouped-right'
 
 type FieldAddonModifier = 'addons' | 'addons-centered' | 'addons-right'
 
-export interface FieldProps extends React.HTMLAttributes<HTMLDivElement> {
+type FieldSize = 'narrow' | 'expanded'
+
+export interface FieldProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    Helpers {
+  readonly grouped?: boolean
+  readonly horizontal?: boolean
+  readonly groupedMultiline?: boolean
+  readonly fieldSize?: FieldSize
   readonly groupModifier?: FieldGroupModifier
   readonly addonsModifier?: FieldAddonModifier
 }
 
 export const Field: React.SFC<FieldProps> = ({
   children,
+  grouped,
+  horizontal,
+  groupedMultiline,
   groupModifier,
+  fieldSize,
   addonsModifier,
   className,
   ...props
 }) => {
+  const propsHelpersRemoved: HelpersRemoved<typeof props> = removeHelpers(props)
   const classes: string = classNames(
     'field',
     {
+      [`is-grouped`]:
+        grouped || horizontal || groupedMultiline || groupModifier,
+      [`is-horizontal`]: horizontal,
+      [`is-${fieldSize}`]: fieldSize,
+      [`is-grouped-multiline`]: groupedMultiline,
       [`is-${groupModifier}`]: groupModifier,
       [`has-${addonsModifier}`]: addonsModifier,
     },
     className,
+    helpersClasses(props),
   )
 
   return (
-    <div {...props} className={classes}>
+    <div {...propsHelpersRemoved} {...props} className={classes}>
       {children}
     </div>
+  )
+}
+
+type FieldLabelSize = 'small' | 'normal' | 'medium' | 'large'
+
+export interface FieldLabelProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    Helpers {
+  readonly fieldLabelSize?: FieldLabelSize
+}
+
+export const FieldLabel: React.SFC<FieldLabelProps> = ({
+  children,
+  fieldLabelSize,
+  className,
+  ...props
+}) => {
+  const propsHelpersRemoved: HelpersRemoved<typeof props> = removeHelpers(props)
+  const classes: string = classNames(
+    'field-label',
+    [`is-${fieldLabelSize}`],
+    className,
+    helpersClasses(props),
+  )
+  return (
+    <div {...propsHelpersRemoved} {...props} className={classes}>
+      <Label>{children}</Label>
+    </div>
+  )
+}
+
+export interface FieldBodyProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    Helpers {}
+
+export const FieldBody: React.SFC<FieldBodyProps> = ({
+  children,
+  className,
+  ...props
+}) => {
+  const propsHelpersRemoved: HelpersRemoved<typeof props> = removeHelpers(props)
+  const classes: string = classNames(
+    'field-body',
+    className,
+    helpersClasses(props),
+  )
+  return (
+    <div {...propsHelpersRemoved} {...props} className={classes}>
+      <div className="field">
+        <div className="control">{children}</div>
+      </div>
+    </div>
+  )
+}
+
+export interface FieldSetProps
+  extends React.FieldsetHTMLAttributes<HTMLFieldSetElement>,
+    Helpers {}
+
+export const FieldSet: React.SFC<FieldSetProps> = ({
+  children,
+  className,
+  ...props
+}) => {
+  const propsHelpersRemoved: HelpersRemoved<typeof props> = removeHelpers(props)
+  const classes: string = classNames(className, helpersClasses(props))
+  return (
+    <fieldset {...propsHelpersRemoved} {...props} className={classes}>
+      <div className="field">{children}</div>
+    </fieldset>
   )
 }
--- src/bulma/examples/FieldExample.tsx
+++ src/bulma/examples/FieldExample.tsx
@@ -1,18 +1,122 @@
 import * as React from 'react'
-import { Field } from '../Field'
+import { Field, FieldLabel, FieldBody } from '../Field'
 import { Input } from '../Input'
 import { Button } from '../Button'
+import { TextArea } from '../TextArea'
 import { Label } from '../Label'
+import { Select } from '../Select'
+import { RadioButton } from '../RadioButton'
 
 export const FieldExample: React.SFC = () => (
   <div>
-    <Field addonsModifier="addons" className="has-addons-centered">
-      <Input type="text" className="is-success" />
-      <Button color="info">search</Button>
+    <Field horizontal>
+      <FieldLabel fieldLabelSize="normal">
+        <Label>From</Label>
+      </FieldLabel>
+      <FieldBody>
+        <Field>
+          <Input type="text" placeholder="Name" />
+        </Field>
+        <Field>
+          <Input
+            color="success"
+            type="email"
+            placeholder="Email"
+            value="alex@smith.com"
+          />
+        </Field>
+      </FieldBody>
     </Field>
-    <Field>
-      <Label>Name</Label>
-      <Input type="text" placeholder="e.g Alex Smith" className="is-primary" />
+
+    <Field horizontal>
+      <FieldLabel />
+      <FieldBody>
+        <Field fieldSize="expanded">
+          <Field addonsModifier="addons">
+            <p className="control">
+              <Button state="static">+44</Button>
+            </p>
+            <p className="control is-expanded">
+              <Input type="tel" placeholder="Your phone number" />
+            </p>
+          </Field>
+          <p className="help">Do not enter the first zero</p>
+        </Field>
+      </FieldBody>
+    </Field>
+
+    <Field horizontal>
+      <FieldLabel fieldLabelSize="normal">
+        <Label>Department</Label>
+      </FieldLabel>
+      <FieldBody>
+        <Field fieldSize="narrow">
+          <div className="control">
+            <Select className="is-fullwidth">
+              <option>Business development</option>
+              <option>Marketing</option>
+              <option>Sales</option>
+            </Select>
+          </div>
+        </Field>
+      </FieldBody>
+    </Field>
+
+    <Field horizontal>
+      <FieldLabel>
+        <Label>Already a member?</Label>
+      </FieldLabel>
+      <FieldBody>
+        <Field>
+          <div className="control">
+            <RadioButton>Yes</RadioButton>
+            <RadioButton>No</RadioButton>
+          </div>
+        </Field>
+      </FieldBody>
+    </Field>
+
+    <Field horizontal>
+      <FieldLabel fieldLabelSize="normal">
+        <Label>Subject</Label>
+      </FieldLabel>
+      <FieldBody>
+        <Field>
+          <div className="control">
+            <Input
+              color="danger"
+              type="text"
+              placeholder="e.g. Partnership opportunity"
+            />
+          </div>
+          <p className="help is-danger">This field is required</p>
+        </Field>
+      </FieldBody>
+    </Field>
+
+    <Field horizontal>
+      <FieldLabel fieldLabelSize="normal">
+        <Label>Question</Label>
+      </FieldLabel>
+      <FieldBody>
+        <Field>
+          <div className="control">
+            <TextArea placeholder="Explain how we can help you" />
+          </div>
+        </Field>
+      </FieldBody>
+    </Field>
+
+    <Field horizontal>
+      <FieldLabel />
+
+      <FieldBody>
+        <Field>
+          <div className="control">
+            <Button color="primary">Send message</Button>
+          </div>
+        </Field>
+      </FieldBody>
     </Field>
   </div>
 )
