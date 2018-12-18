6b6f1e39ea4ffab84825999d4a0201ea0f7a3761
Technoidentity Solutions Pvt Ltd <venkat@technoidentity.com>
2018-12-18 17:08:14 +0530

    refactor CheckBox

--- src/bulma/form/CheckBox.tsx
+++ src/bulma/form/CheckBox.tsx
@@ -1,35 +1,28 @@
 import React from 'react'
 
 import classNames from 'classnames'
-import {
-  CommonHelpers,
-  CommonHelpersRemoved,
-  commonHelpersClasses,
-  removeCommonHelpers,
-} from '../modifiers/commonHelpers'
 
-interface CheckBoxProps
+import { Helpers, helpersClasses, removeHelpers } from '../modifiers'
+
+export interface CheckBoxProps
   extends React.InputHTMLAttributes<HTMLInputElement>,
-    CommonHelpers {}
+    Helpers {}
 
 export const CheckBox: React.SFC<CheckBoxProps> = ({
   children,
   className,
   ...props
 }) => {
-  const propsHelpersRemoved: CommonHelpersRemoved<
-    typeof props
-  > = removeCommonHelpers(props)
   const classes: string = classNames(
     'checkbox',
+    helpersClasses(props),
     className,
-    commonHelpersClasses(props),
   )
 
   return (
     <div className="control">
-      <label className={classes}>
-        <input {...propsHelpersRemoved} type="checkbox" />
+      <label className="checkbox">
+        <input {...removeHelpers(props)} type="checkbox" className={classes} />
         {children}
       </label>
     </div>
