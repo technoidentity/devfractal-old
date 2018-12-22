d2a70cc92ec3ee146f865fd024e1ba1e4c25fba0
Technoidentity Solutions Pvt Ltd <venkat@technoidentity.com>
2018-12-22 16:34:31 +0530

    drop react-hot-loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import React from 'react'
+import * as React from 'react'
 
-import { hot, setConfig } from 'react-hot-loader'
-
-setConfig({
-  ignoreSFC: true,
-  pureSFC: true,
-  pureRender: true,
-})
-
-class App extends React.Component {
-  render(): JSX.Element {
-    return (
-      <div>
-        <input />
-        <h1 style={{ color: 'blue' }}>Hello, React!</h1>
-      </div>
-    )
-  }
-}
+const App: React.SFC = () => (
+  <div>
+    <input />
+    <h1 style={{ color: 'blue' }}>Hello, React!</h1>
+  </div>
+)
 
 // tslint:disable-next-line:no-default-export
-export default hot(module)(App)
+export default App
