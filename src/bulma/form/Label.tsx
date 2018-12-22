c07714a1b728266b931de09cecba2bee2fba28fa
Technoidentity Solutions Pvt Ltd <venkat@technoidentity.com>
2018-12-22 11:53:27 +0530

    fix Label

--- src/bulma/form/Label.tsx
+++ src/bulma/form/Label.tsx
@@ -2,20 +2,26 @@ import * as React from 'react'
 import classNames from 'classnames'
 import { Helpers, helpersClasses, removeHelpers } from '../modifiers'
 
-type Size = 'small' | 'medium' | 'large'
+export type LabelSize = 'small' | 'medium' | 'large'
 
 interface LabelProps
   extends React.LabelHTMLAttributes<HTMLLabelElement>,
     Helpers {
-  readonly size?: Size
+  readonly size?: LabelSize
 }
 
 export const Label: React.SFC<LabelProps> = ({
+  size,
   children,
   className,
   ...props
 }) => {
-  const classes: string = classNames('label', helpersClasses(props), className)
+  const classes: string = classNames(
+    'label',
+    { [`is-${size}`]: size },
+    helpersClasses(props),
+    className,
+  )
 
   return (
     <label {...removeHelpers(props)} className={classes}>
