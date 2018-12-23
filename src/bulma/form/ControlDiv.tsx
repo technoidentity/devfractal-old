fb866b360c62492fc566d5d2eb6117252c6192e5
Technoidentity Solutions Pvt Ltd <venkat@technoidentity.com>
2018-12-23 11:55:13 +0530

    add AllControlHelpers

--- src/bulma/form/ControlDiv.tsx
+++ src/bulma/form/ControlDiv.tsx
@@ -3,19 +3,23 @@ import { Helpers, removeHelpers } from '../modifiers'
 import { removeControlHelpers, ControlHelpers } from './ControlHelpers'
 import { removeIconHelpers, IconHelpers } from './iconHelpers'
 
+export interface AllControlHelpers
+  extends Helpers,
+    ControlHelpers,
+    IconHelpers {}
+
+type ControlType =
+  | 'button'
+  | 'input'
+  | 'select'
+  | 'checkbox'
+  | 'radio'
+  | 'textarea'
+  | 'file'
 interface ControlDivProps
   extends React.AllHTMLAttributes<HTMLElement>,
-    Helpers,
-    ControlHelpers,
-    IconHelpers {
-  readonly as?:
-    | 'button'
-    | 'input'
-    | 'select'
-    | 'checkbox'
-    | 'radio'
-    | 'textarea'
-    | 'file'
+    AllControlHelpers {
+  readonly as?: ControlType
 }
 
 export const ControlDiv: React.SFC<ControlDivProps> = ({
