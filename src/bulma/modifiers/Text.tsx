8df4186fade0076e2f8dbeb3bc584b3460e6deaf
Technoidentity Solutions Pvt Ltd <venkat@technoidentity.com>
2018-12-23 16:15:17 +0530

    add Text component because swathi wants it

---
+++ src/bulma/modifiers/Text.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import { Helpers, classNamesHelper } from '.'
+import { Div } from './Div'
+
+export interface TextProps extends React.HTMLAttributes<HTMLElement>, Helpers {}
+
+export const Text: React.SFC<TextProps> = ({ children, ...props }) => (
+  <Div {...props} className={classNamesHelper(props)}>
+    {children}
+  </Div>
+)
