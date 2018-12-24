002cba6873c4dfcc8fed1fe0d089d2ff8a01e833
swathivalasapalli <swathivalasapalli@gmail.com>
2018-12-24 15:53:30 +0530

    add Link to each Component

--- src/bulma/examples/FrontPage.tsx
+++ src/bulma/examples/FrontPage.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Section, Container, Media } from '../layout'
+import { Section, Container, Media, Hero } from '../layout'
 import { Columns, Column } from '../columns'
 import { MediaLeft, MediaContent } from '../layout/Media'
 import { Title } from '../elements'
@@ -13,95 +13,112 @@ import {
   faCubes,
   faCogs,
 } from '@fortawesome/free-solid-svg-icons'
-export const MainPage: React.SFC = () => (
+import { HeroBody } from '../layout/Hero'
+import { Link } from 'react-router-dom'
+export const FrontPage: React.SFC = () => (
   <>
+    <Hero variant="light">
+      <HeroBody>
+        <Container className="has-text-centered">
+          <Title size="2">Bulma Examples</Title>
+        </Container>
+      </HeroBody>
+    </Hero>
     <Section>
       <Container>
         <Columns multiline>
           <Column>
-            <Media className="notification is-grey">
-              <MediaLeft>
-                <Icon icon={faWpforms} size="2x" textColor="link" />
-              </MediaLeft>
-              <MediaContent>
-                <Title>Form</Title>
-                <SubTitle size="6">
-                  The indispensable form controls designed for maximum clarity
-                </SubTitle>
-              </MediaContent>
-            </Media>
+            <Link to="/Form">
+              <Media className="notification is-light">
+                <MediaLeft>
+                  <Icon icon={faWpforms} size="2x" textColor="link" />
+                </MediaLeft>
+                <MediaContent>
+                  <Title>Form</Title>
+                  <SubTitle size="6">
+                    The indispensable form controls designed for maximum clarity
+                  </SubTitle>
+                </MediaContent>
+              </Media>
+            </Link>
+          </Column>
+          <Column size="one-third">
+            <Link to="/Columns">
+              <Media className="notification is-light">
+                <MediaLeft>
+                  <Icon icon={faColumns} size="2x" textColor="warning" />
+                </MediaLeft>
+                <MediaContent>
+                  <Title>Columns</Title>
+                  <SubTitle size="6">
+                    The power of Flexbox in a simple interface
+                  </SubTitle>
+                </MediaContent>
+              </Media>
+            </Link>
           </Column>
           <Column>
-            <Media className="notification is-grey">
-              <MediaLeft>
-                <Icon icon={faColumns} size="2x" textColor="warning" />
-              </MediaLeft>
-              <MediaContent>
-                <Title>Columns</Title>
-                <SubTitle size="6">
-                  The power of Flexbox in a simple interface
-                </SubTitle>
-              </MediaContent>
-            </Media>
-          </Column>
-          <Column>
-            <Media className="notification is-grey">
-              <MediaLeft>
-                <Icon icon={faWarehouse} size="2x" textColor="success" />
-              </MediaLeft>
-              <MediaContent>
-                <Title>Layout</Title>
-                <SubTitle size="6">
-                  Design the structure of your webpage with these css classes
-                </SubTitle>
-              </MediaContent>
-            </Media>
+            <Link to="/Layout">
+              <Media className="notification is-light">
+                <MediaLeft>
+                  <Icon icon={faWarehouse} size="2x" textColor="success" />
+                </MediaLeft>
+                <MediaContent>
+                  <Title>Layout</Title>
+                  <SubTitle size="6">
+                    Design the structure of your webpage with these css classes
+                  </SubTitle>
+                </MediaContent>
+              </Media>
+            </Link>
           </Column>
         </Columns>
-      </Container>
-    </Section>
-    <Section>
-      <Container>
         <Columns multiline>
           <Column>
-            <Media className="notification is-grey">
-              <MediaLeft>
-                <Icon icon={faCube} size="2x" textColor="danger" />
-              </MediaLeft>
-              <MediaContent>
-                <Title>Elements</Title>
-                <SubTitle size="6">
-                  Essential interface elements that only require a single css
-                  class
-                </SubTitle>
-              </MediaContent>
-            </Media>
+            <Link to="/Elements">
+              <Media className="notification is-light">
+                <MediaLeft>
+                  <Icon icon={faCube} size="2x" textColor="danger" />
+                </MediaLeft>
+                <MediaContent>
+                  <Title>Elements</Title>
+                  <SubTitle size="6">
+                    Essential interface elements that only require a single css
+                    class
+                  </SubTitle>
+                </MediaContent>
+              </Media>
+            </Link>
           </Column>
           <Column>
-            <Media className="notification is-light">
-              <MediaLeft>
-                <Icon icon={faCubes} size="2x" textColor="danger" />
-              </MediaLeft>
-              <MediaContent>
-                <Title>Components</Title>
-                <SubTitle size="6">
-                  Advanced multi-part components with lots of responsibilities
-                </SubTitle>
-              </MediaContent>
-            </Media>
+            <Link to="/Components">
+              <Media className="notification is-light">
+                <MediaLeft>
+                  <Icon icon={faCubes} size="2x" textColor="danger" />
+                </MediaLeft>
+                <MediaContent>
+                  <Title>Components</Title>
+                  <SubTitle size="6">
+                    Advanced multi-part components with lots of responsibilities
+                  </SubTitle>
+                </MediaContent>
+              </Media>
+            </Link>
           </Column>
           <Column>
-            <Media className="notification is-light">
-              <MediaLeft>
-                <Icon icon={faCogs} size="2x" textColor="grey" />
-              </MediaLeft>
-              <MediaContent>
-                <Title>Modifiers</Title>
-                <SubTitle size="6">
-                  An easy-to-read naming system designed for humans
-                </SubTitle>
-              </MediaContent>
-            </Media>
+            <Link to="/Modifiers">
+              <Media className="notification is-light">
+                <MediaLeft>
+                  <Icon icon={faCogs} size="2x" textColor="grey" />
+                </MediaLeft>
+                <MediaContent>
+                  <Title>Modifiers</Title>
+                  <SubTitle size="6">
+                    An easy-to-read naming system designed for humans
+                  </SubTitle>
+                </MediaContent>
+              </Media>
+            </Link>
           </Column>
         </Columns>
       </Container>
