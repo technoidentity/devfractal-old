ab03d6d3bdf8d9ab40daef91fbd951cce4ea7b6a
Sravani Dande <sravanidnd89@gmail.com>
2018-12-25 17:21:40 +0530

    fix Elements Tab

--- src/bulma/front-page/Elements.tsx
+++ src/bulma/front-page/Elements.tsx
@@ -3,19 +3,17 @@ import { Tabs } from '../components'
 import { TabsItem } from '../components/Tabs'
 
 export const Elements: React.SFC = () => (
-  <Tabs to="/elements" size="medium">
-    <TabsItem name="Box" active>
+  <Tabs to="/elements" urlSeparator="-" size="medium">
+    <TabsItem name="box" active>
       Box
     </TabsItem>
-    <TabsItem name="Button">Button</TabsItem>
-    <TabsItem name="Content">Content</TabsItem>
-    <TabsItem name="Delete">Delete</TabsItem>
-    <TabsItem name="Icon">Icon</TabsItem>
-    <TabsItem name="Image">Image</TabsItem>
-    <TabsItem name="Notification">Notification</TabsItem>
+    <TabsItem name="delete">Delete</TabsItem>
+    <TabsItem name="icon">Icon</TabsItem>
+    <TabsItem name="image">Image</TabsItem>
+    <TabsItem name="notification">Notification</TabsItem>
     <TabsItem name="bars">Progress bars</TabsItem>
-    <TabsItem name="Table">Table</TabsItem>
-    <TabsItem name="Tag">Tag</TabsItem>
-    <TabsItem name="Title">Title</TabsItem>
+    <TabsItem name="table">Table</TabsItem>
+    <TabsItem name="tag">Tag</TabsItem>
+    <TabsItem name="title">Title</TabsItem>
   </Tabs>
 )
