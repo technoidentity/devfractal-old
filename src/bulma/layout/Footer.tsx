84af3b7f22d7adb9bf3bef1cee551c46119d0ca0
Sravani Dande <sravanidnd89@gmail.com>
2018-12-27 12:34:36 +0530

    add better helpers to Footer

--- src/bulma/layout/Footer.tsx
+++ src/bulma/layout/Footer.tsx
@@ -1,34 +1,15 @@
-import classNames from 'classnames'
 import React from 'react'
-import {
-  CommonHelpers,
-  commonHelpersClasses,
-  CommonHelpersRemoved,
-  removeCommonHelpers,
-} from '../modifiers/commonHelpers'
+import { classNamesHelper, Div, Helpers } from '../modifiers'
 
-interface FooterProps
-  extends React.HTMLAttributes<HTMLElement>,
-    CommonHelpers {}
+interface FooterProps extends React.HTMLAttributes<HTMLElement>, Helpers {}
 
 // @TODO: children should be Content
-export const Footer: React.SFC<FooterProps> = ({
-  children,
-  className,
-  ...props
-}) => {
-  const propsHelpersRemoved: CommonHelpersRemoved<
-    typeof props
-  > = removeCommonHelpers(props)
-  const classes: string = classNames(
-    'footer',
-    className,
-    commonHelpersClasses(props),
-  )
+export const Footer: React.SFC<FooterProps> = ({ children, ...props }) => {
+  const classes: string = classNamesHelper(props, 'footer')
 
   return (
-    <footer {...propsHelpersRemoved} className={classes}>
+    <Div as="footer" {...props} className={classes}>
       {children}
-    </footer>
+    </Div>
   )
 }
