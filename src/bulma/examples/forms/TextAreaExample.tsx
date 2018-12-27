ca10d7215cf5da0de9172116c5c59b045c042b3f
swathivalasapalli <swathivalasapalli@gmail.com>
2018-12-27 16:55:05 +0530

    improve TextArea example

--- src/bulma/examples/forms/TextAreaExample.tsx
+++ src/bulma/examples/forms/TextAreaExample.tsx
@@ -3,14 +3,71 @@ import { Column, Columns } from '../../columns'
 import { Title } from '../../elements'
 import { TextArea } from '../../form'
 import { Section } from '../../layout'
+import { Text } from '../../modifiers/Text'
 
 export const TextAreaExample: React.SFC = () => (
   <div>
-    <Columns>
-      <Column>
+    <Columns columnCentered>
+      <Column size="three-quarters">
         <Section>
-          <Title>Default Textarea</Title>
+          <Title size="4">Default Textarea</Title>
           <TextArea placeholder="Default textArea" />
+        </Section>
+        <Section>
+          <Title size="4">Colors</Title>
+          <TextArea variant="primary" placeholder="Primary textarea" />
+          <TextArea variant="info" placeholder="Info textarea" />
+          <TextArea variant="success" placeholder="Success textarea" />
+          <TextArea variant="warning" placeholder="Warning textarea" />
+          <TextArea variant="danger" placeholder="Danger textarea" />
+          <TextArea variant="light" placeholder="Light textarea" />
+          <TextArea variant="dark" placeholder="Dark textarea" />
+          <TextArea variant="black" placeholder="Black textarea" />
+        </Section>
+        <Section>
+          <Title size="4">Sizes</Title>
+          <TextArea ctrlSize="small" placeholder="Small textarea" />
+          <TextArea placeholder="Normal textarea" />
+          <TextArea ctrlSize="medium" placeholder="Medium textarea" />
+          <TextArea ctrlSize="large" placeholder="Large textarea" />
+        </Section>
+        <Section>
+          <Title size="4">States</Title>
+          <Text>Normal</Text>
+          <TextArea placeholder="Normal textarea" />
+          <Text>Active</Text>
+          <TextArea state="active" placeholder="Active textarea" />
+          <Text>Hover</Text>
+          <TextArea state="hovered" placeholder="Hovered textarea" />
+          <Text>Focus</Text>
+          <TextArea state="focused" placeholder="Focused textarea" />
+          <Text>Loading</Text>
+          <TextArea loading placeholder="Loading textarea" />
+          <Text>Disabled</Text>
+          <TextArea disabled placeholder="Disabled textarea" />
+        </Section>
+        <Section>
+          <Title size="4">Resize Loading Spinner</Title>
+          <TextArea
+            loading
+            ctrlSize="small"
+            placeholder="Small loading textarea"
+          />
+          <TextArea loading placeholder="Normal loading textarea" />
+          <TextArea
+            loading
+            ctrlSize="medium"
+            placeholder="Medium loading textarea"
+          />
+          <TextArea
+            loading
+            ctrlSize="large"
+            placeholder="Large loading textarea"
+          />
+        </Section>
+        <Section>
+          <Title size="4">Fixed Size</Title>
+          <TextArea fixedSize />
         </Section>
       </Column>
     </Columns>
