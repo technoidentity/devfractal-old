b2c2d6a1436233020e4d2cae8a9d3acd728b3b9c
swathivalasapalli <swathivalasapalli@gmail.com>
2018-12-28 17:53:31 +0530

    add logo and refactor into media components

--- src/bulma/front-page/FrontPage.tsx
+++ src/bulma/front-page/FrontPage.tsx
@@ -15,6 +15,103 @@ import { SubTitle } from '../elements/Title'
 import { Container, Hero, Media, Section } from '../layout'
 import { HeroBody } from '../layout/Hero'
 import { MediaContent, MediaLeft } from '../layout/Media'
+import logo from '../logo.png'
+
+export const FormMedia: React.SFC = () => (
+  <Media className="notification is-light" style={{ display: 'flex' }}>
+    <MediaLeft>
+      <Icon icon={faWpforms} size="2x" textColor="link" />
+    </MediaLeft>
+    <MediaContent>
+      <Title>Form</Title>
+      <SubTitle size="6">
+        The indispensable form controls designed for maximum clarity
+      </SubTitle>
+    </MediaContent>
+  </Media>
+)
+
+export const ColumnsMedia: React.SFC = () => (
+  <Media className="notification is-light">
+    <MediaLeft>
+      <Icon icon={faColumns} size="2x" textColor="warning" />
+    </MediaLeft>
+    <MediaContent>
+      <Title>Columns</Title>
+      <SubTitle size="6">The power of Flexbox in a simple interface</SubTitle>
+    </MediaContent>
+  </Media>
+)
+
+export const LayoutMedia: React.SFC = () => (
+  <Media className="notification is-light">
+    <MediaLeft>
+      <Icon icon={faWarehouse} size="2x" textColor="success" />
+    </MediaLeft>
+    <MediaContent>
+      <Title>Layout</Title>
+      <SubTitle size="6">
+        Design the structure of your webpage with these css classes
+      </SubTitle>
+    </MediaContent>
+  </Media>
+)
+
+export const ElementsMedia: React.SFC = () => (
+  <Media className="notification is-light">
+    <MediaLeft>
+      <Icon icon={faCube} size="2x" textColor="danger" />
+    </MediaLeft>
+    <MediaContent>
+      <Title>Elements</Title>
+      <SubTitle size="6">
+        Essential interface elements that only require a single css class
+      </SubTitle>
+    </MediaContent>
+  </Media>
+)
+
+export const ComponentsMedia: React.SFC = () => (
+  <Media className="notification is-light">
+    <MediaLeft>
+      <Icon icon={faCubes} size="2x" textColor="danger" />
+    </MediaLeft>
+    <MediaContent>
+      <Title>Components</Title>
+      <SubTitle size="6">
+        Advanced multi-part components with lots of responsibilities
+      </SubTitle>
+    </MediaContent>
+  </Media>
+)
+
+export const ModifiersMedia: React.SFC = () => (
+  <Media className="notification is-light">
+    <MediaLeft>
+      <Icon icon={faCogs} size="2x" textColor="grey" />
+    </MediaLeft>
+    <MediaContent>
+      <Title>Modifiers</Title>
+      <SubTitle size="6">
+        An easy-to-read naming system designed for humans
+      </SubTitle>
+    </MediaContent>
+  </Media>
+)
+
+export const CompositesMedia: React.SFC = () => (
+  <Media className="notification is-light">
+    <MediaLeft>
+      <Icon icon={faAccusoft} size="2x" textColor="black-bis" />
+    </MediaLeft>
+    <MediaContent>
+      <Title>Composites</Title>
+      <SubTitle size="6">
+        More complex examples using multiple components
+      </SubTitle>
+    </MediaContent>
+  </Media>
+)
 
 export const MainPageBody: React.SFC = () => (
   <Section>
@@ -22,123 +119,58 @@ export const MainPageBody: React.SFC = () => (
       <Columns multiline>
         <Column>
           <Link to="/form">
-            <Media className="notification is-light">
-              <MediaLeft>
-                <Icon icon={faWpforms} size="2x" textColor="link" />
-              </MediaLeft>
-              <MediaContent>
-                <Title>Form</Title>
-                <SubTitle size="6">
-                  The indispensable form controls designed for maximum clarity
-                </SubTitle>
-              </MediaContent>
-            </Media>
+            <FormMedia />
           </Link>
         </Column>
-        <Column size="one-third">
+        <Column>
           <Link to="/columns">
-            <Media className="notification is-light">
-              <MediaLeft>
-                <Icon icon={faColumns} size="2x" textColor="warning" />
-              </MediaLeft>
-              <MediaContent>
-                <Title>Columns</Title>
-                <SubTitle size="6">
-                  The power of Flexbox in a simple interface
-                </SubTitle>
-              </MediaContent>
-            </Media>
+            <ColumnsMedia />
           </Link>
         </Column>
         <Column>
           <Link to="/layout">
-            <Media className="notification is-light">
-              <MediaLeft>
-                <Icon icon={faWarehouse} size="2x" textColor="success" />
-              </MediaLeft>
-              <MediaContent>
-                <Title>Layout</Title>
-                <SubTitle size="6">
-                  Design the structure of your webpage with these css classes
-                </SubTitle>
-              </MediaContent>
-            </Media>
+            <LayoutMedia />
           </Link>
         </Column>
       </Columns>
       <Columns multiline>
         <Column>
           <Link to="/elements">
-            <Media className="notification is-light">
-              <MediaLeft>
-                <Icon icon={faCube} size="2x" textColor="danger" />
-              </MediaLeft>
-              <MediaContent>
-                <Title>Elements</Title>
-                <SubTitle size="6">
-                  Essential interface elements that only require a single css
-                  class
-                </SubTitle>
-              </MediaContent>
-            </Media>
+            <ElementsMedia />
           </Link>
         </Column>
         <Column>
           <Link to="/components">
-            <Media className="notification is-light">
-              <MediaLeft>
-                <Icon icon={faCubes} size="2x" textColor="danger" />
-              </MediaLeft>
-              <MediaContent>
-                <Title>Components</Title>
-                <SubTitle size="6">
-                  Advanced multi-part components with lots of responsibilities
-                </SubTitle>
-              </MediaContent>
-            </Media>
+            <ComponentsMedia />
           </Link>
         </Column>
         <Column>
           <Link to="/modifiers">
-            <Media className="notification is-light">
-              <MediaLeft>
-                <Icon icon={faCogs} size="2x" textColor="grey" />
-              </MediaLeft>
-              <MediaContent>
-                <Title>Modifiers</Title>
-                <SubTitle size="6">
-                  An easy-to-read naming system designed for humans
-                </SubTitle>
-              </MediaContent>
-            </Media>
+            <ModifiersMedia />
           </Link>
         </Column>
+      </Columns>
+      <Columns>
         <Column>
           <Link to="/composites">
-            <Media className="notification is-light">
-              <MediaLeft>
-                <Icon icon={faAccusoft} size="2x" textColor="black-bis" />
-              </MediaLeft>
-              <MediaContent>
-                <Title>Composites</Title>
-                <SubTitle size="6">
-                  More complex examples using multiple components
-                </SubTitle>
-              </MediaContent>
-            </Media>
+            <CompositesMedia />
           </Link>
         </Column>
+        <Column />
+        <Column />
       </Columns>
     </Container>
   </Section>
 )
 
 export const MainPageHeader: React.SFC = () => (
-  <Hero variant="dark">
+  <Hero variant="light">
     <HeroBody>
       <Container className="has-text-centered">
         <Title>
-          <Link to="/">Devfractal</Link>
+          <Link to="/">
+            <img src={logo} style={{ width: '512px' }} />
+          </Link>
         </Title>
         <SubTitle>React, as simple as Angular!</SubTitle>
       </Container>
