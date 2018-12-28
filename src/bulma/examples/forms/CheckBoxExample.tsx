6b1d6644a6332ad0a344ad39a90ef64df296bcff
Sravani Dande <sravanidnd89@gmail.com>
2018-12-28 15:30:28 +0530

    add examples to Checkbox

--- src/bulma/examples/forms/CheckBoxExample.tsx
+++ src/bulma/examples/forms/CheckBoxExample.tsx
@@ -1,19 +1,39 @@
 import React from 'react'
-import { CheckBox } from '../../form'
+import { Column, Columns } from '../../columns'
+import { Box } from '../../elements'
+import { CheckBox, Field, Label } from '../../form'
+import { Section } from '../../layout'
 import { logger } from '../common'
 
 export const CheckBoxExample: React.SFC = () => (
   <div>
-    <CheckBox readOnly checked>
-      Remember me
-    </CheckBox>
-    <CheckBox
-      onChange={() => {
-        logger('CheckboxExample onChange')
-      }}
-    >
-      i agree <a href="#">terms and conditions</a>
-    </CheckBox>
-    <CheckBox disabled>Save my Changes</CheckBox>
+    <Columns columnCentered>
+      <Column narrow>
+        <Box>
+          <Section>
+            <Label>Normal checkbox</Label>
+            <CheckBox readOnly checked>
+              Remember me
+            </CheckBox>
+          </Section>
+          <Section>
+            <Label>Add links to checkbox</Label>
+            <CheckBox
+              onChange={() => {
+                logger('CheckboxExample onChange')
+              }}
+            >
+              i agree <a href="#">terms and conditions</a>
+            </CheckBox>
+          </Section>
+          <Section>
+            <Field>
+              <Label>Disabled checkbox</Label>
+              <CheckBox disabled>Save my Changes</CheckBox>
+            </Field>
+          </Section>
+        </Box>
+      </Column>
+    </Columns>
   </div>
 )
