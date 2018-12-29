babb4f69932e24c8d07e0a0627ca40485145a9dd
Sravani Dande <sravanidnd89@gmail.com>
2018-12-29 15:44:42 +0530

    small changes to UI for Form

--- src/bulma/examples/forms/RadioButtonExample.tsx
+++ src/bulma/examples/forms/RadioButtonExample.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Column, Columns } from '../../columns'
-import { Box } from '../../elements'
+import { Title } from '../../elements'
 import {
   FieldBody,
   FieldLabel,
-  Label,
   RadioButton,
   RadioButtonGroup,
 } from '../../form'
@@ -16,40 +15,38 @@ export const RadioButtonExample: React.SFC = () => (
   <RadioButtonGroup>
     <Columns columnCentered>
       <Column narrow>
-        <Box>
-          <Section>
-            <Label>Normal radioButton</Label>
-            <FormField horizontal>
-              <FieldLabel>Gender</FieldLabel>
-              <FieldBody>
-                <RadioButton
-                  onChange={() => {
-                    logger('RadiobuttonExample onChange')
-                  }}
-                >
-                  {' '}
-                  Male
-                </RadioButton>
-                <RadioButton> Female</RadioButton>
-              </FieldBody>
-            </FormField>
-          </Section>
-          <Section>
-            <Label>Checked radiobutton</Label>
-            <RadioButton checked readOnly>
-              {' '}
-              Foo
-            </RadioButton>
-            <RadioButton> Bar</RadioButton>
-          </Section>
+        <Section>
+          <Title size="4">Radiobutton</Title>
+          <FormField horizontal>
+            <FieldLabel>Gender</FieldLabel>
+            <FieldBody>
+              <RadioButton
+                onChange={() => {
+                  logger('RadiobuttonExample onChange')
+                }}
+              >
+                {' '}
+                Male
+              </RadioButton>
+              <RadioButton> Female</RadioButton>
+            </FieldBody>
+          </FormField>
+        </Section>
+        <Section>
+          <Title size="4">Checked </Title>
+          <RadioButton checked readOnly>
+            {' '}
+            Foo
+          </RadioButton>
+          <RadioButton> Bar</RadioButton>
+        </Section>
 
-          <Section>
-            <Label>Disabled radioButton</Label>
-            <RadioButton> Going</RadioButton>
-            <RadioButton> Not going</RadioButton>
-            <RadioButton disabled> May be</RadioButton>
-          </Section>
-        </Box>
+        <Section>
+          <Title size="4">Disabled </Title>
+          <RadioButton> Going</RadioButton>
+          <RadioButton> Not going</RadioButton>
+          <RadioButton disabled> May be</RadioButton>
+        </Section>
       </Column>
     </Columns>
   </RadioButtonGroup>
