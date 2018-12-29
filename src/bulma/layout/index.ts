10cc94f66a1c23cd238d7b23de13d44fc6975a8b
Technoidentity Solutions Pvt Ltd <venkat@technoidentity.com>
2018-12-29 15:40:46 +0530

    export * from index.ts for layout

--- src/bulma/layout/index.ts
+++ src/bulma/layout/index.ts
@@ -1,43 +1,7 @@
-export { Container } from './Container'
-export { Footer } from './Footer'
-export { Hero, HeroBody, HeroFoot, HeroHead } from './Hero'
-export { Level } from './Level'
-export { Media, MediaContent, MediaLeft, MediaRight } from './Media'
-export { Section } from './Section'
-export { Tile } from './Tile'
-
-import { ContainerProps as ContainerProps_ } from './Container'
-import { FooterProps as FooterProps_ } from './Footer'
-import {
-  HeroBodyProps as HeroBodyProps_,
-  HeroFootProps as HeroFootProps_,
-  HeroHeadProps as HeroHeadProps_,
-  HeroProps as HeroProps_,
-} from './Hero'
-import {
-  LevelItemProps as LevelItemProps_,
-  LevelProps as LevelProps_,
-} from './Level'
-import {
-  MediaContentProps as MediaContentProps_,
-  MediaLeftProps as MediaLeftProps_,
-  MediaProps as MediaProps_,
-  MediaRightProps as MediaRightProps_,
-} from './Media'
-import { SectionProps as SectionProps_ } from './Section'
-import { TileProps as TileProps_ } from './Tile'
-
-export type ContainerProps = ContainerProps_
-export type FooterProps = FooterProps_
-export type SectionProps = SectionProps_
-export type LevelProps = LevelProps_
-export type LevelItemProps = LevelItemProps_
-export type HeroProps = HeroProps_
-export type HeroBodyProps = HeroBodyProps_
-export type HeroHeadProps = HeroHeadProps_
-export type HeroFootProps = HeroFootProps_
-export type MediaProps = MediaProps_
-export type MediaLeftProps = MediaLeftProps_
-export type MediaRightProps = MediaRightProps_
-export type MediaContentProps = MediaContentProps_
-export type TileProps = TileProps_
+export * from './Container'
+export * from './Footer'
+export * from './Hero'
+export * from './Level'
+export * from './Media'
+export * from './Section'
+export * from './Tile'
