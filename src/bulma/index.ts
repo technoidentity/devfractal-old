d7e4b78d2c43bc2a5c79013d4465d031dc85ba31
Technoidentity Solutions Pvt Ltd <venkat@technoidentity.com>
2018-12-29 15:56:22 +0530

    add index.ts to bulma

---
+++ src/bulma/index.ts
@@ -0,0 +1,7 @@
+export * from './columns'
+export * from './components'
+export * from './elements'
+export * from './layout'
+export * from './modifiers'
+export * from './form'
+export * from './formik'
