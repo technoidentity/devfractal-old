661422284c4def961f5e030bbf06f10ca5bd83e9
swathivalasapalli <swathivalasapalli@gmail.com>
2018-12-29 19:12:25 +0530

    add examples for Image

--- src/bulma/examples/elements/ImageExample.tsx
+++ src/bulma/examples/elements/ImageExample.tsx
@@ -1,16 +1,62 @@
 import React from 'react'
-import { Image } from '../../elements'
+import { Image, Title } from '../../elements'
+import { Section } from '../../layout'
 
 export const ImageExample: React.SFC = () => (
   <>
-    <Image
-      size="128x128"
-      src="https://bulma.io/images/placeholders/128x128.png"
-    />
-    <Image
-      rounded
-      size="128x128"
-      src="https://bulma.io/images/placeholders/128x128.png"
-    />
+    <Section>
+      <Title>Normal Image</Title>
+      <Image
+        size="128x128"
+        src="https://bulma.io/images/placeholders/128x128.png"
+      />
+    </Section>
+    <Section>
+      <Title>Fixed square images</Title>
+      <Section>
+        <Image
+          size="16x16"
+          src="https://bulma.io/images/placeholders/128x128.png"
+        />
+      </Section>
+      <Section>
+        <Image
+          size="24x24"
+          src="https://bulma.io/images/placeholders/128x128.png"
+        />
+      </Section>
+      <Section>
+        <Image
+          size="32x32"
+          src="https://bulma.io/images/placeholders/128x128.png"
+        />
+      </Section>
+      <Section>
+        <Image
+          size="48x48"
+          src="https://bulma.io/images/placeholders/128x128.png"
+        />
+      </Section>
+      <Section>
+        <Image
+          size="64x64"
+          src="https://bulma.io/images/placeholders/128x128.png"
+        />
+      </Section>
+      <Section>
+        <Image
+          size="128x128"
+          src="https://bulma.io/images/placeholders/128x128.png"
+        />
+      </Section>
+    </Section>
+    <Section>
+      <Title>Rounded image</Title>
+      <Image
+        rounded
+        size="96x96"
+        src="https://bulma.io/images/placeholders/128x128.png"
+      />
+    </Section>
   </>
 )
