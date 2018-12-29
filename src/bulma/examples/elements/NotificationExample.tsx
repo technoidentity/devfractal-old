f3d37c7170f97f001e53e6b17294e42ca3282dca
swathivalasapalli <swathivalasapalli@gmail.com>
2018-12-29 19:07:33 +0530

    add examples for Notification component

--- src/bulma/examples/elements/NotificationExample.tsx
+++ src/bulma/examples/elements/NotificationExample.tsx
@@ -1,19 +1,71 @@
 import React from 'react'
-import { Delete, Notification } from '../../elements'
+import { Box, Delete, Notification, Title } from '../../elements'
+import { Section } from '../../layout'
 
 export const NotificationExample: React.SFC = () => (
-  <div>
-    <Notification className="is-info">
-      Bold notification blocks, to alert your users of something Bold
-      notification blocks, to alert your users of something Bold notification
-      blocks, to alert your users of something
-      <Delete />
-    </Notification>
-    <Notification variant="primary">
-      Bold notification blocks, to alert your users of something Bold
-      notification blocks, to alert your users of something Bold notification
-      blocks, to alert your users of something
-      <Delete />
-    </Notification>
-  </div>
+  <>
+    <Section>
+      <Title>Default Notification</Title>
+      <Box>
+        <Notification>
+          Bold notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something
+        </Notification>
+      </Box>
+    </Section>
+    <Section>
+      <Title>Colors</Title>
+      <Box>
+        <Notification variant="primary">
+          Bold notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something
+          <Delete />
+        </Notification>
+        <Notification variant="success">
+          Bold notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something
+          <Delete />
+        </Notification>
+        <Notification variant="warning">
+          Bold notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something
+          <Delete />
+        </Notification>
+        <Notification variant="danger">
+          Bold notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something
+          <Delete />
+        </Notification>
+        <Notification variant="white">
+          Bold notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something
+          <Delete />
+        </Notification>
+        <Notification variant="black">
+          Bold notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something
+          <Delete />
+        </Notification>
+        <Notification variant="dark">
+          Bold notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something
+          <Delete />
+        </Notification>
+        <Notification variant="light">
+          Bold notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something Bold
+          notification blocks, to alert your users of something
+          <Delete />
+        </Notification>
+      </Box>
+    </Section>
+  </>
 )
