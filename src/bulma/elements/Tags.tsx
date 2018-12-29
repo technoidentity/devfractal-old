82e84c4302e41f911b2156a44d03beb797932824
swathivalasapalli <swathivalasapalli@gmail.com>
2018-12-29 17:40:01 +0530

    remove experimental size property for tags

--- src/bulma/elements/Tags.tsx
+++ src/bulma/elements/Tags.tsx
@@ -1,23 +1,14 @@
 import React from 'react'
 import { classNamesHelper, Div, Helpers } from '../modifiers'
 
-type TagsSize = 'medium' | 'large'
-
 export interface TagsProps
   extends React.HTMLAttributes<HTMLDivElement>,
     Helpers {
-  readonly size?: TagsSize
   readonly addons?: boolean
 }
 
-export const Tags: React.SFC<TagsProps> = ({
-  size,
-  addons,
-  children,
-  ...props
-}) => {
+export const Tags: React.SFC<TagsProps> = ({ addons, children, ...props }) => {
   const classes: string = classNamesHelper(props, 'tags', {
-    [`are-${size}`]: size,
     [`has-addons`]: addons,
   })
   return (
