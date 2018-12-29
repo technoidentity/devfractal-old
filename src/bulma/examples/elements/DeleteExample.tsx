4713e5dd6a873eeac35fddf7e4dfa0e7c160504e
swathivalasapalli <swathivalasapalli@gmail.com>
2018-12-29 19:04:24 +0530

    add examples to Delete component

--- src/bulma/examples/elements/DeleteExample.tsx
+++ src/bulma/examples/elements/DeleteExample.tsx
@@ -1,35 +1,59 @@
 import React from 'react'
-import { Message, MessageBody, MessageHeader } from '../../components/Message'
-import { Delete, Notification } from '../../elements'
+import { Message, MessageBody, MessageHeader } from '../../components'
+import { Box, Delete, Notification, Tag, Tags, Title } from '../../elements'
 import { Section } from '../../layout'
 import { logger } from '../common'
 
 export const DeleteExample: React.SFC = () => (
-  <Section>
-    <Notification variant="danger">
-      <Delete
-        className="is-large"
-        onClick={() => {
-          logger('DeleteExample onClick')
-        }}
-      />
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor
-      sit amet, consectetur adipiscing elit
-    </Notification>
-    <Message variant="info">
-      <MessageHeader>
-        Info
+  <>
+    <Section>
+      <Title>Default Delete</Title>
+      <Delete />
+    </Section>
+    <Section>
+      <Title>Sizes</Title>
+      <Box>
+        <Delete size="small" />
+        <Delete />
         <Delete size="medium" />
-      </MessageHeader>
-      <MessageBody>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-        risus mi, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus
-        ac ex sit amet fringilla. Nullam gravida purus diam, et dictum felis
-        venenatis efficitur. Aenean ac eleifend lacus, in mollis lectus. Donec
-        sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id
-        porttitor mi magna a neque. Donec dui urna, vehicula et sem eget,
-        facilisis sodales sem.
-      </MessageBody>
-    </Message>
-  </Section>
+        <Delete size="large" />
+      </Box>
+    </Section>
+    <Section>
+      <Title>Combinations</Title>
+      <Box>
+        <Tags addons>
+          <Tag variant="success">
+            Hello World
+            <Delete />
+          </Tag>
+        </Tags>
+        <Notification variant="danger">
+          <Delete
+            className="is-large"
+            onClick={() => {
+              logger('DeleteExample onClick')
+            }}
+          />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum
+          dolor sit amet, consectetur adipiscing elit
+        </Notification>
+        <Message variant="info">
+          <MessageHeader>
+            Info
+            <Delete size="medium" />
+          </MessageHeader>
+          <MessageBody>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque risus mi, tempus quis placerat ut, porta nec nulla.
+            Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus
+            diam, et dictum felis venenatis efficitur. Aenean ac eleifend lacus,
+            in mollis lectus. Donec sodales, arcu et sollicitudin porttitor,
+            tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui
+            urna, vehicula et sem eget, facilisis sodales sem.
+          </MessageBody>
+        </Message>
+      </Box>
+    </Section>
+  </>
 )
