2c6a456a49a64f7153241ecd1fddbc0e92578981
Technoidentity Solutions Pvt Ltd <venkat@technoidentity.com>
2018-12-29 15:36:11 +0530

    export * from index.ts in elements

--- src/bulma/elements/index.ts
+++ src/bulma/elements/index.ts
@@ -1,51 +1,11 @@
-export { Box } from './Box'
-export { Content } from './Content'
-export { Delete } from './Delete'
-export { Icon } from './Icon'
-export { Image } from './Image'
-export { Notification } from './Notification'
-export { ProgressBar } from './ProgressBar'
-export { Table } from './Table'
-export { Tag } from './Tag'
-export { Tags } from './Tags'
-export { Title, SubTitle } from './Title'
-
-import { BoxProps as BoxProps_ } from './Box'
-import { ContentProps as ContentProps_ } from './Content'
-import { DeleteProps as DeleteProps_ } from './Delete'
-import { ImageProps as ImageProps_ } from './Image'
-import { NotificationProps as NotificationProps_ } from './Notification'
-import { ProgressBarProps as ProgressBarProps_ } from './ProgressBar'
-import {
-  TableBodyProps as TableBodyProps_,
-  TableFootProps as TableFootProps_,
-  TableHeadProps as TableHeadProps_,
-  TableProps as TableProps_,
-  TdProps as TdProps_,
-  ThProps as thProps_,
-  TrProps as TrProps_,
-} from './Table'
-import { TagProps as TagProps_ } from './Tag'
-import { TagsProps as TagsProps_ } from './Tags'
-import {
-  SubTitleProps as SubTitleProps_,
-  TitleProps as TitleProps_,
-} from './Title'
-
-export type BoxProps = BoxProps_
-export type ContentProps = ContentProps_
-export type DeleteProps = DeleteProps_
-export type ImageProps = ImageProps_
-export type NotificationProps = NotificationProps_
-export type ProgressBarProps = ProgressBarProps_
-export type TagProps = TagProps_
-export type TagsProps = TagsProps_
-export type TitleProps = TitleProps_
-export type SubTitleProps = SubTitleProps_
-export type TableProps = TableProps_
-export type TableHeadProps = TableHeadProps_
-export type TableBodyProps = TableBodyProps_
-export type TableFootProps = TableFootProps_
-export type TrProps = TrProps_
-export type TdProps = TdProps_
-export type ThProps = thProps_
+export * from './Box'
+export * from './Content'
+export * from './Delete'
+export * from './Icon'
+export * from './Image'
+export * from './Notification'
+export * from './ProgressBar'
+export * from './Table'
+export * from './Tag'
+export * from './Tags'
+export * from './Title'
