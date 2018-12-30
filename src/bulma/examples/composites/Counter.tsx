e1a8c900d8ce0ba70fa4518c85f447a5bbe09b9b
Sravani Dande <sravanidnd89@gmail.com>
2018-12-30 16:59:19 +0530

    counter is not responsive,simplify it

--- src/bulma/examples/composites/Counter.tsx
+++ src/bulma/examples/composites/Counter.tsx
@@ -1,10 +1,10 @@
 import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
-import { Column, Columns } from '../../columns'
-import { Box, Icon } from '../../elements'
+import { Icon } from '../../elements'
 import { Button } from '../../form'
 import { Section } from '../../layout'
 import { Text } from '../../modifiers'
+import { logger } from '../common'
 
 interface CounterViewProps {
   readonly count: number
@@ -17,48 +17,24 @@ export const CounterView: React.SFC<CounterViewProps> = ({
   onIncrement,
   onDecrement,
 }) => (
-  <Columns columnCentered>
-    <Column narrow>
-      <Section>
-        <Box>
-          <Button variant="primary" size="medium" onClick={onIncrement}>
-            <Icon icon={faPlus} />
-          </Button>
+  <Section>
+    <Button variant="primary" size="medium" onClick={onIncrement}>
+      <Icon icon={faPlus} />
+    </Button>
 
-          <Text textAlignment="centered" textSize="2">
-            {count}
-          </Text>
-          <Button variant="danger" size="medium" onClick={onDecrement}>
-            <Icon icon={faMinus} />
-          </Button>
-        </Box>
-      </Section>
-    </Column>
-  </Columns>
+    <Text as="h1" textSize="2">
+      {count}
+    </Text>
+    <Button variant="danger" size="medium" onClick={onDecrement}>
+      <Icon icon={faMinus} />
+    </Button>
+  </Section>
 )
 
-interface CounterProps {
-  readonly count: number
-}
-
-export class Counter extends React.Component {
-  readonly state: CounterProps = { count: 0 }
-
-  readonly handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 })
-  }
-
-  readonly handleDecrement = () => {
-    this.setState({ count: this.state.count - 1 })
-  }
-
-  render(): JSX.Element {
-    return (
-      <CounterView
-        count={this.state.count}
-        onIncrement={this.handleIncrement}
-        onDecrement={this.handleDecrement}
-      />
-    )
-  }
-}
+export const Counter: React.SFC = () => (
+  <CounterView
+    count={0}
+    onIncrement={() => logger('increment')}
+    onDecrement={() => logger('decrement')}
+  />
+)
