9dae8fd38ad33e54d3a388263c7925a043a2ba0b
Sravani Dande <sravanidnd89@gmail.com>
2018-12-31 15:31:56 +0530

    add better example to Panel

--- src/bulma/examples/components/PanelExample.tsx
+++ src/bulma/examples/components/PanelExample.tsx
@@ -1,37 +1,74 @@
+import {
+  faBook,
+  faCodeBranch,
+  faSearch,
+} from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 import {
   Panel,
   PanelBlock,
   PanelHeading,
+  PanelIcon,
   PanelTabs,
   PanelTabsItem,
 } from '../../components/Panel'
 import { Button, CheckBox, Input } from '../../form'
+import { Section } from '../../layout'
 
 export const PanelExample: React.SFC = () => (
-  <Panel>
-    <PanelHeading>repositories</PanelHeading>
-    <PanelBlock>
-      <Input type="text" />
-    </PanelBlock>
-    <PanelTabs>
-      <PanelTabsItem active>all</PanelTabsItem>
-      <PanelTabsItem>public</PanelTabsItem>
-      <PanelTabsItem>sources</PanelTabsItem>
-      <PanelTabsItem className="active">forks</PanelTabsItem>
-    </PanelTabs>
-    <PanelBlock>bulma</PanelBlock>
-    <PanelBlock active>message</PanelBlock>
-    <PanelBlock>marksheet</PanelBlock>
-    <PanelBlock>miniresetcss</PanelBlock>
-    <PanelBlock>jgthms.github.io</PanelBlock>
-    <PanelBlock>marksheet</PanelBlock>
-    <PanelBlock active>miniresetcss</PanelBlock>
-    <PanelBlock>
-      <CheckBox>Remember me</CheckBox>
-    </PanelBlock>
-    <PanelBlock>
-      <Button variant="primary">reset all filters</Button>
-    </PanelBlock>
-  </Panel>
+  <Section>
+    <Panel>
+      <PanelHeading>repositories</PanelHeading>
+      <PanelBlock>
+        <Input
+          type="text"
+          placeholder="search"
+          ctrlSize="small"
+          leftIcon={faSearch}
+        />
+      </PanelBlock>
+      <PanelTabs>
+        <PanelTabsItem active>all</PanelTabsItem>
+        <PanelTabsItem>public</PanelTabsItem>
+        <PanelTabsItem>sources</PanelTabsItem>
+        <PanelTabsItem className="active">forks</PanelTabsItem>
+      </PanelTabs>
+      <PanelBlock>
+        <PanelIcon icon={faBook} />
+        bulma
+      </PanelBlock>
+
+      <PanelBlock>
+        <PanelIcon icon={faBook} /> marksheet
+      </PanelBlock>
+      <PanelBlock>
+        {' '}
+        <PanelIcon icon={faBook} />
+        miniresetcss
+      </PanelBlock>
+      <PanelBlock>
+        {' '}
+        <PanelIcon icon={faBook} />
+        jgthms.github.io
+      </PanelBlock>
+      <PanelBlock>
+        {' '}
+        <PanelIcon icon={faCodeBranch} />
+        daniellowtw/infboard
+      </PanelBlock>
+      <PanelBlock active>
+        {' '}
+        <PanelIcon icon={faCodeBranch} />
+        mojs
+      </PanelBlock>
+      <PanelBlock>
+        <CheckBox>remember me</CheckBox>
+      </PanelBlock>
+      <PanelBlock>
+        <Button variant="link" fullWidth outlined>
+          reset all filters
+        </Button>
+      </PanelBlock>
+    </Panel>
+  </Section>
 )
