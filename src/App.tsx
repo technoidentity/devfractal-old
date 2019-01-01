110484e1587e7a1f77db93da11a44aedd18dbc34
Pervez Iqbal <pervezfunctor@gmail.com>
2019-01-01 18:35:40 +0530

    use HashRouter, better for serverless

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Docs } from './docs'
 
 export const App: React.SFC = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Docs />
-  </BrowserRouter>
+  </HashRouter>
 )
 
 // tslint:disable-next-line:no-default-export
