c210279613bfdc2d78f141b13bd4a8b09c73381e
Pervez Iqbal <pervezfunctor@gmail.com>
2019-01-01 10:25:00 +0530

    SimpleViewer to view any javascript object

---
+++ src/bulma/simple/SimpleViewer.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Boolean } from 'tcomb'
+import { camelCaseToPhrase } from '../../utils'
+import { Column, Columns } from '../columns'
+import { Box, Title } from '../elements'
+import { CheckBox } from '../form'
+import { Section } from '../layout'
+
+const SimpleHeader: React.SFC<{ readonly objectKey: string }> = ({
+  objectKey,
+}) => <Title size="4">{camelCaseToPhrase(objectKey)}</Title>
+
+const SimpleValue: React.SFC<{ readonly objectValue: string }> = ({
+  objectValue,
+}) =>
+  Boolean.is(objectValue) ? (
+    <CheckBox checked={objectValue} readOnly />
+  ) : (
+    <>{objectValue}</>
+  )
+
+export interface SimpleViewerProps extends React.HTMLAttributes<HTMLElement> {
+  readonly object: { readonly [index: string]: any }
+}
+
+export const SimpleViewer: React.SFC<SimpleViewerProps> = ({ object }) => {
+  return (
+    <Section>
+      <Box>
+        {Object.keys(object).map(key => (
+          <Columns>
+            <Column>
+              <SimpleHeader objectKey={key} />
+            </Column>
+            <Column>
+              <SimpleValue objectValue={object[key]} />
+            </Column>
+          </Columns>
+        ))}
+      </Box>
+    </Section>
+  )
+}
