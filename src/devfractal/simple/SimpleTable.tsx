cf3f1b5c2cdb8e1b9c977580501d7acda2ab4621
Pervez Iqbal <pervezfunctor@gmail.com>
2019-01-02 13:27:44 +0530

    add onRowClicked to SimpleTable and other small improvements

--- src/devfractal/simple/SimpleTable.tsx
+++ src/devfractal/simple/SimpleTable.tsx
@@ -11,24 +11,33 @@ import {
   Tr,
 } from '../elements'
 import { CheckBox } from '../form'
+import { Text } from '../modifiers'
 
-export interface SimpleTableProps extends TableProps {
-  readonly headers: ReadonlyArray<string>
-  readonly values:
-    | ReadonlyArray<{ readonly [index: string]: any }>
-    | (() => Promise<{ readonly [index: string]: any }>)
+export interface RowClickEvent<T> {
+  readonly value: T
 }
 
-export interface RowsProps {
-  readonly headers: ReadonlyArray<string>
-  readonly values: ReadonlyArray<{ readonly [index: string]: any }>
+export interface SimpleTableProps<T> extends TableProps {
+  readonly headers?: ReadonlyArray<string>
+  readonly values: ReadonlyArray<T> | (() => Promise<T>)
+  onRowClicked?(value: RowClickEvent<T>): void
 }
 
-const Rows: React.SFC<RowsProps> = ({ values, headers }) => {
+export interface RowsProps<T> {
+  readonly headers: ReadonlyArray<string>
+  readonly values: ReadonlyArray<T>
+  onRowClicked?(value: RowClickEvent<T>): void
+}
+
+function Rows<T>(props: RowsProps<T>): JSX.Element {
+  const { values, headers, onRowClicked } = props
   return (
     <>
       {values.map((value, i) => (
-        <Tr key={i}>
+        <Tr
+          key={i}
+          onClick={() => onRowClicked && onRowClicked({ value: values[i] })}
+        >
           {headers.map(key => (
             <Td key={key}>
               {Boolean.is(value[key]) ? (
@@ -43,15 +52,19 @@ const Rows: React.SFC<RowsProps> = ({ values, headers }) => {
     </>
   )
 }
-export const SimpleTable: React.SFC<SimpleTableProps> = ({
-  headers,
-  values,
-  ...props
-}) => {
+
+export function SimpleTable<T>(args: SimpleTableProps<T>): JSX.Element {
+  const { headers, values, onRowClicked, ...props } = args
+  if (values.length === 0) {
+    return <Text textSize="4">Currently, no elements available</Text>
+  }
+  const allHeaders: ReadonlyArray<string> =
+    headers || Object.keys(values[0] || {})
+
   const header: JSX.Element = (
     <TableHead>
       <Tr>
-        {headers.map(h => (
+        {allHeaders.map(h => (
           <Th key={h}>{camelCaseToPhrase(h)}</Th>
         ))}
       </Tr>
@@ -68,7 +81,11 @@ export const SimpleTable: React.SFC<SimpleTableProps> = ({
             <Table {...props}>
               {header}
               <TableBody>
-                <Rows values={data} headers={headers} />
+                <Rows
+                  values={data}
+                  headers={allHeaders}
+                  onRowClicked={onRowClicked}
+                />
               </TableBody>
             </Table>
           )
@@ -81,7 +98,7 @@ export const SimpleTable: React.SFC<SimpleTableProps> = ({
     <Table {...props}>
       {header}
       <TableBody>
-        <Rows values={values} headers={headers} />
+        <Rows values={values} headers={allHeaders} />
       </TableBody>
     </Table>
   )
