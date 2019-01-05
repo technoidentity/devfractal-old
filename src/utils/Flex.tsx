ce42fae641a8373525874f4f56b586e6e19714bf
swathivalasapalli <swathivalasapalli@gmail.com>
2019-01-05 15:42:43 +0530

    add Flex component

--- src/utils/Flex.tsx
+++ src/utils/Flex.tsx
@@ -1,11 +1,56 @@
 import React from 'react'
 
+type Display = 'flex' | 'inline-flex'
+type FlexDirection = 'row' | 'column' | 'row-reverse' | 'column-reverse'
+type FlexWrap = 'nowrap' | 'wrap' | 'wrap-reverse'
+type JustifyContent =
+  | 'flex-start'
+  | 'flex-end'
+  | 'center'
+  | 'space-between'
+  | 'space-around'
+  | 'space-evenly'
+
+type AlignItems = 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'baseline'
+
+type AlignContent =
+  | 'flex-start'
+  | 'flex-end'
+  | 'center'
+  | 'space-between'
+  | 'space-around'
+  | 'stretch'
+
 interface FlexProps {
-  readonly direction?: 'row' | 'column'
+  readonly display?: Display
+  readonly flexDirection?: FlexDirection
+  readonly flexWrap?: FlexWrap
+  readonly justifyContent?: JustifyContent
+  readonly alignItems?: AlignItems
+  readonly alignContent?: AlignContent
 }
 
-export const Flex: React.SFC<FlexProps> = ({ direction = 'row', children }) => (
-  <div style={{ display: 'flex', flexDirection: direction as any }}>
-    {children}
-  </div>
-)
+export const Flex: React.SFC<FlexProps> = ({
+  display,
+  flexDirection,
+  flexWrap,
+  justifyContent,
+  alignContent,
+  alignItems,
+  children,
+}) => {
+  return (
+    <div
+      style={{
+        display,
+        flexDirection,
+        flexWrap,
+        justifyContent,
+        alignItems,
+        alignContent,
+      }}
+    >
+      {children}
+    </div>
+  )
+}
