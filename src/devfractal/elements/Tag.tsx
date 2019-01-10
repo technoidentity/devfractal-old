0a502705a36d581243c921447325b4f15bb05c79
Pervez Iqbal <pervez@Pervezs-MacBook-Pro.local>
2019-01-10 17:51:33 +0530

    Tag and Tags in Tag.tsx

--- src/devfractal/elements/Tag.tsx
+++ src/devfractal/elements/Tag.tsx
@@ -43,3 +43,29 @@ export const Tag: React.SFC<TagProps> = ({
     </Div>
   )
 }
+
+type TagsAlignment = 'centered' | 'right'
+
+export interface TagsProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    Helpers {
+  readonly alignment?: TagsAlignment
+  readonly addons?: boolean
+}
+
+export const Tags: React.SFC<TagsProps> = ({
+  alignment,
+  addons,
+  children,
+  ...props
+}) => {
+  const classes: string = classNamesHelper(props, 'tags', {
+    [`is-${alignment}`]: alignment,
+    'has-addons': addons,
+  })
+  return (
+    <Div {...props} className={classes}>
+      {children}
+    </Div>
+  )
+}
