8b152f11ae50fe8520a3f801ee6cff03f023bd92
Pervez Iqbal <pervezfunctor@gmail.com>
2019-01-10 14:01:17 +0530

    try to make Stateful more generic

--- src/devfractal/components/Stateful.tsx
+++ src/devfractal/components/Stateful.tsx
@@ -1,51 +1,51 @@
 import warning from 'tiny-warning'
 import { State } from '../../utils'
 
-export interface SelectionChangeEvent {
+export interface StateChangeEvent<State> {
   readonly name?: string
-  readonly value?: string
+  readonly value?: State
 }
 
 // tslint:disable-next-line: no-empty-interface
-export interface StatefulComponentProps<Value = string> {
-  readonly selected?: Value
-  onSelectionChange?(event: SelectionChangeEvent): void
+export interface StatefulComponentProps<State = {}> {
+  readonly state?: State
+  onStateChange?(event: StateChangeEvent<State>): void
 }
 
-export interface StatefulProps<Value = string> {
-  readonly defaultValue?: Value
-  readonly selected?: Value
+export interface StatefulProps<State = {}> {
+  readonly defaultValue?: State
+  readonly state?: State
   readonly readOnly?: boolean
-  render(props: StatefulComponentProps<Value>): JSX.Element
-  onSelectionChange?(event: SelectionChangeEvent): void
+  render(props: StatefulComponentProps<State>): JSX.Element
+  onStateChange?(event: StateChangeEvent<State>): void
 }
 
-export const Stateful: React.SFC<StatefulProps> = ({
-  defaultValue,
-  render,
-  children,
-  ...props
-}) => {
+export function Stateful<State>(
+  args: StatefulProps<State> & { readonly children: React.ReactNode },
+): JSX.Element {
+  const { defaultValue, render, children, ...props } = args
   warning(
-    !(props.selected && !props.onSelectionChange && !props.readOnly),
+    !(props.state && !props.onStateChange && !props.readOnly),
     "'selected' provided, but not 'onSelectionChange', make this component readOnly.",
   )
 
-  const Component: React.SFC<StatefulComponentProps> = render
+  const Component: (
+    props: StatefulComponentProps<State>,
+  ) => JSX.Element = render
 
-  return props.selected !== undefined ? (
+  return props.state !== undefined ? (
     <Component {...props}>{children}</Component>
   ) : (
     <State
-      initial={props.selected || defaultValue}
+      initial={props.state || defaultValue}
       render={({ value, set }) => (
         <Component
           {...props}
-          selected={value}
-          onSelectionChange={evt => {
+          state={value}
+          onStateChange={evt => {
             set(evt.value)
-            if (props.onSelectionChange) {
-              props.onSelectionChange(evt)
+            if (props.onStateChange) {
+              props.onStateChange(evt)
             }
           }}
         >
