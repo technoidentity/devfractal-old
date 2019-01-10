c4d3f57c2fc8750f989d8df94ef769871b6fe903
Pervez Iqbal <pervezfunctor@gmail.com>
2019-01-10 14:05:40 +0530

    move TableContentLoader to utils

--- src/docs/Routes.tsx
+++ src/docs/Routes.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import TableContentLoader from '../devfractal/elements/TableContentLoader'
 import { SimpleRedirect } from '../utils'
+import TableContentLoader from '../utils/TableContentLoader'
 import {
   BasicColumnExample,
   BoxExample,
