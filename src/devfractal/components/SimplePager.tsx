4732913c35a2c13758d63c6bffcac37306f56204
swathivalasapalli <swathivalasapalli@gmail.com>
2019-01-20 17:23:39 +0530

    add icons to Pagination

--- src/devfractal/components/SimplePager.tsx
+++ src/devfractal/components/SimplePager.tsx
@@ -1,4 +1,11 @@
+import {
+  faArrowLeft,
+  faArrowRight,
+  faStepBackward,
+  faStepForward,
+} from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
+import { Icon } from '../elements'
 import { Button, Field } from '../form'
 import { Section } from '../layout'
 
@@ -16,28 +23,28 @@ export const SimplePagerView: React.SFC<PagerViewProps> = ({
   <Section>
     <Field groupModifier="grouped-right">
       <Button variant="info" onClick={() => onPageChange(1)}>
-        First
+        <Icon icon={faStepBackward} />
       </Button>
       <Button
         variant="info"
         disabled={currentPage === 1}
         onClick={() => onPageChange(currentPage - 1)}
       >
-        Previous
+        <Icon icon={faArrowLeft} />
       </Button>
       <Button
         variant="info"
         disabled={currentPage === maximumPages}
         onClick={() => onPageChange(currentPage + 1)}
       >
-        Next
+        <Icon icon={faArrowRight} />
       </Button>
       <Button
         variant="info"
         disabled={currentPage === maximumPages}
         onClick={() => onPageChange(maximumPages)}
       >
-        Last
+        <Icon icon={faStepForward} />
       </Button>
     </Field>
   </Section>
