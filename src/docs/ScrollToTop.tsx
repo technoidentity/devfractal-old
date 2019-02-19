c01976ba2810c59eecabe2c4ee80b0f31c1cad20
Pervez Iqbal <pervezfunctor@gmail.com>
2019-02-19 19:06:35 +0530

    no checking for ScrollToTop for now

--- src/docs/ScrollToTop.tsx
+++ src/docs/ScrollToTop.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { withRouter } from 'react-router'
 
-class ScrollToTop extends React.Component {
-  componentDidUpdate(prevProps) {
+// tslint:disable
+
+class ScrollToTop extends React.Component<any, any> {
+  componentDidUpdate(prevProps: any) {
     if (this.props.location.pathname !== prevProps.location.pathname) {
       window.scrollTo(0, 0)
     }
