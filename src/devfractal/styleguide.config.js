66a6a4a3d4d9c6eebcf7eb78f92c8339e1f1e4d5
Pervez Iqbal <pervezfunctor@gmail.com>
2019-02-24 15:42:15 +0530

    styleguidist support

---
+++ src/devfractal/styleguide.config.js
@@ -0,0 +1,9 @@
+const { createConfig, babel, typescript } = require('webpack-blocks')
+
+module.exports = {
+  propsParser: require('react-docgen-typescript').withCustomConfig(
+    './tsconfig.json',
+  ).parse,
+  webpackConfig: createConfig([babel(), typescript()]),
+  components: './**/[A-Z]*.tsx',
+}
