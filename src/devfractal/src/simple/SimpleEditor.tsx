1e79f6c6c76944db5c2e1424610a4bd196096918
Pervez Iqbal <pervezfunctor@gmail.com>
2019-02-26 17:02:11 +0530

    Improve SimpleEditor

--- src/devfractal/src/simple/SimpleEditor.tsx
+++ src/devfractal/src/simple/SimpleEditor.tsx
@@ -1,32 +1,49 @@
+import { FormikActions } from 'formik'
 import React from 'react'
 import { Boolean, Number } from 'tcomb'
 import { Box, camelCaseToPhrase, Label, Section } from '../lib'
 import { Simple } from './internal'
 
-export interface SimpleEditorProps {
-  readonly data: { readonly [index: string]: any }
+export interface SimpleEditorProps<T extends object> {
+  readonly data: T
+  onSubmit?(values: T, actions: FormikActions<T>): void
 }
 
-export const SimpleEditor: React.SFC<SimpleEditorProps> = ({ data }) => (
-  <Section>
-    <Box>
-      <Simple.Form initialValues={data}>
-        {Object.keys(data).map(key => (
-          <React.Fragment key={key}>
-            {Boolean.is(data[key]) ? (
-              <>
-                <Label>{camelCaseToPhrase(key)}</Label>
-                <Simple.Checkbox name={key} checked={data[key]} readOnly />
-              </>
-            ) : Number.is(data[key]) ? (
-              <Simple.Number label={camelCaseToPhrase(key)} name={key} />
-            ) : (
-              <Simple.Text label={camelCaseToPhrase(key)} name={key} />
-            )}
-          </React.Fragment>
-        ))}
-        <Simple.FormButtons />
-      </Simple.Form>
-    </Box>
-  </Section>
-)
+export function SimpleEditor<T extends object>({
+  data,
+  onSubmit,
+}: SimpleEditorProps<T>): JSX.Element {
+  return (
+    <Section>
+      <Box>
+        <Simple.Form<T> initialValues={data} onSubmit={onSubmit}>
+          {Object.keys(data).map(key => (
+            <React.Fragment key={key}>
+              {Boolean.is(data[key]) ? (
+                <>
+                  <Simple.Checkbox name={key} checked={data[key]} readOnly>
+                    {' '}
+                    <Label>{camelCaseToPhrase(key)}</Label>
+                  </Simple.Checkbox>
+                </>
+              ) : Number.is(data[key]) ? (
+                <Simple.Number
+                  label={camelCaseToPhrase(key)}
+                  name={key}
+                  readOnly={key === 'id'}
+                />
+              ) : (
+                <Simple.Text
+                  label={camelCaseToPhrase(key)}
+                  name={key}
+                  readOnly={key === 'id'}
+                />
+              )}
+            </React.Fragment>
+          ))}
+          <Simple.FormButtons />
+        </Simple.Form>
+      </Box>
+    </Section>
+  )
+}
