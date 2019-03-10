81c72b248cec660d727df78ea168dacc31300568
sravani dande <sravani@sravanis-MacBook-Pro.local>
2019-03-10 15:51:00 +0530

    add AddTodoForm

---
+++ src/docs/examples/composites/firestore-todo/FSAddTodo.tsx
@@ -0,0 +1,35 @@
+import { FormikActions } from 'formik'
+import React from 'react'
+import { Omit, Section, Simple, Title } from '../../devfractal'
+import { create, FSTodo } from './todoAPI'
+
+export interface AddTodoProps {
+  onAddTodo(todo: Omit<FSTodo, 'id'>, actions: FormikActions<typeof todo>): void
+}
+
+export const FSAddTodoView: React.SFC<AddTodoProps> = ({ onAddTodo }) => {
+  return (
+    <Section>
+      <Title>Add Todo Form</Title>
+      <Simple.Form
+        initialValues={{ title: '', done: false }}
+        onSubmit={onAddTodo}
+      >
+        <Simple.Text name="title" placeholder="title" />
+        <Simple.Checkbox name="done"> Done</Simple.Checkbox>
+        <Simple.FormButtons />
+      </Simple.Form>
+    </Section>
+  )
+}
+
+export const FSAddTodo: React.FC = () => {
+  const handleAddTodo = async (
+    todo: Omit<FSTodo, 'id'>,
+    actions: FormikActions<typeof todo>,
+  ) => {
+    await create(todo)
+    actions.setSubmitting(false)
+  }
+  return <FSAddTodoView onAddTodo={handleAddTodo} />
+}
