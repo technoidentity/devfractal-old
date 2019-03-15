59fc359e20098fb41f8f071a0de3253ff42be9ca
Pervez Iqbal <pervezfunctor@gmail.com>
2019-03-15 12:16:51 +0530

    First attempt at typed Simple form

--- src/devfractal/src/simple/SimpleForm.tsx
+++ src/devfractal/src/simple/SimpleForm.tsx
@@ -81,102 +81,22 @@ function SimpleInput<Values, S extends Schema<any> = StringSchema>(
   )
 }
 
-function SimpleText<Values>(props: GenericInputProps<Values>): JSX.Element {
-  return <SimpleInput<Values> {...props} type="text" schema={string()} />
-}
-
-function SimpleNumber<Values>(
-  props: GenericInputProps<Values, NumberSchema>,
-): JSX.Element {
-  return <SimpleInput schema={number()} {...props} type="number" />
-}
-
-function SimplePassword<Values>(props: GenericInputProps<Values>): JSX.Element {
-  return <SimpleInput schema={string()} {...props} type="password" />
-}
-
-function SimpleEmail<Values>(props: GenericInputProps<Values>): JSX.Element {
-  return <SimpleInput {...props} type="email" schema={string()} />
-}
-
-// @TODO: I think Telephone shouldn't be no?
-function SimpleTelephone<Values>(
-  props: GenericInputProps<Values, NumberSchema>,
-): JSX.Element {
-  return <SimpleInput schema={number()} {...props} type="tel" />
-}
-
-function SimpleUrl<Values>(props: GenericInputProps<Values>): JSX.Element {
-  return <SimpleInput schema={string()} {...props} type="url" />
-}
-
 export interface SimpleCheckboxProps<Values> extends CheckboxFieldProps {
   readonly name: keyof Values & string
   readonly noLabel?: boolean
 }
 
-function SimpleCheckbox<Values>({
-  children,
-  noLabel,
-  ...props
-}: SimpleCheckboxProps<Values>): JSX.Element {
-  return (
-    <Field>
-      <CheckboxField {...props}>
-        {children || (noLabel && ` camelCaseToPhrase(props.name)`)}
-      </CheckboxField>
-      <ErrorField name={props.name} />
-    </Field>
-  )
-}
 export interface SimpleRadioGroupProps<Values> extends RadioFieldProps {
   readonly name: keyof Values & string
-}
-
-function SimpleRadioGroup<Values>({
-  children,
-  ...props
-}: SimpleRadioGroupProps<Values>): JSX.Element {
-  return (
-    <Field>
-      <RadioGroupField {...props}>{children}</RadioGroupField>
-      <ErrorField name={props.name} />
-    </Field>
-  )
 }
 
 export interface SimpleSelectProps<Values> extends SelectFieldProps {
   readonly name: keyof Values & string
 }
 
-function SimpleSelect<Values>({
-  children,
-  ...props
-}: SimpleSelectProps<Values>): JSX.Element {
-  return (
-    <Field>
-      <SelectField {...props}>{children}</SelectField>
-      <ErrorField name={props.name} />
-    </Field>
-  )
-}
-
 export interface SimpleTextAreaProps<Values> extends TextAreaFieldProps {
   readonly name: keyof Values & string
   readonly label: string
-}
-
-function SimpleTextArea<Values>({
-  label,
-  ...props
-}: SimpleTextAreaProps<Values>): JSX.Element {
-  return (
-    <Field>
-      <Label>{label}</Label>
-      <TextAreaField {...props} />
-      <ErrorField name={props.name} />
-    </Field>
-  )
 }
 
 export interface SimpleFormButtonsProps {
@@ -246,37 +166,92 @@ function SimpleForm<Values extends object>({
 }
 
 // tslint:disable-next-line:typedef
-export const Simple = {
-  Form: SimpleForm,
-  FormButtons: SimpleFormButtons,
-  Number: SimpleNumber,
-  Text: SimpleText,
-  Password: SimplePassword,
-  Email: SimpleEmail,
-  Telephone: SimpleTelephone,
-  Checkbox: SimpleCheckbox,
-  Select: SimpleSelect,
-  TextArea: SimpleTextArea,
-  RadioGroup: SimpleRadioGroup,
-  Url: SimpleUrl,
-  Debug: DebugField,
+export function typedForm<Values>() {
+  return {
+    Text: (props: GenericInputProps<Values>): JSX.Element => {
+      return <SimpleInput<Values> {...props} type="text" schema={string()} />
+    },
+    Number: (props: GenericInputProps<Values, NumberSchema>): JSX.Element => {
+      return <SimpleInput schema={number()} {...props} type="number" />
+    },
+
+    Password: (props: GenericInputProps<Values>): JSX.Element => {
+      return <SimpleInput schema={string()} {...props} type="password" />
+    },
+
+    Email: (props: GenericInputProps<Values>): JSX.Element => {
+      return <SimpleInput {...props} type="email" schema={string()} />
+    },
+
+    // @TODO: I think Telephone shouldn't be no?
+    Telephone: (
+      props: GenericInputProps<Values, NumberSchema>,
+    ): JSX.Element => {
+      return <SimpleInput schema={number()} {...props} type="tel" />
+    },
+
+    Url: (props: GenericInputProps<Values>): JSX.Element => {
+      return <SimpleInput schema={string()} {...props} type="url" />
+    },
+
+    Checkbox: ({
+      children,
+      noLabel,
+      ...props
+    }: SimpleCheckboxProps<Values>): JSX.Element => {
+      return (
+        <Field>
+          <CheckboxField {...props}>
+            {children || (noLabel && ` camelCaseToPhrase(props.name)`)}
+          </CheckboxField>
+          <ErrorField name={props.name} />
+        </Field>
+      )
+    },
+
+    RadioGroup: ({
+      children,
+      ...props
+    }: SimpleRadioGroupProps<Values>): JSX.Element => {
+      return (
+        <Field>
+          <RadioGroupField {...props}>{children}</RadioGroupField>
+          <ErrorField name={props.name} />
+        </Field>
+      )
+    },
+
+    Select: ({
+      children,
+      ...props
+    }: SimpleSelectProps<Values>): JSX.Element => {
+      return (
+        <Field>
+          <SelectField {...props}>{children}</SelectField>
+          <ErrorField name={props.name} />
+        </Field>
+      )
+    },
+
+    TextArea: ({
+      label,
+      ...props
+    }: SimpleTextAreaProps<Values>): JSX.Element => {
+      return (
+        <Field>
+          <Label>{label}</Label>
+          <TextAreaField {...props} />
+          <ErrorField name={props.name} />
+        </Field>
+      )
+    },
+  }
 }
 
 // tslint:disable-next-line:typedef
-export function typedForm<Values>() {
-  // Number(props: GenericInputProps<Values, NumberSchema>): JSX.Element
-  // Password(props: GenericInputProps<Values>): JSX.Element
-  // Email(props: GenericInputProps<Values>): JSX.Element
-  // Telephone(props: GenericInputProps<Values>): JSX.Element
-  // Checkbox(props: GenericInputProps<Values>): JSX.Element
-  // Select(props: GenericInputProps<Values>): JSX.Element
-  // TextArea(props: GenericInputProps<Values>): JSX.Element
-  // RadioGroup(props: Props<Values>): JSX.Element
-  // Url(props: UrlProps<Values>): JSX.Element
-  // Debug(props: DebugProps<Values>): JSX.Element
-  return {
-    Text(props: GenericInputProps<Values>): JSX.Element {
-      return <SimpleInput<Values> {...props} type="text" schema={string()} />
-    },
-  }
+export const Simple = {
+  ...typedForm<{ readonly [s: string]: any }>(),
+  Form: SimpleForm,
+  FormButtons: SimpleFormButtons,
+  Debug: DebugField,
 }
