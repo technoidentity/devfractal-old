a7644f74b149034a3c7d303bb9036068c8ce81c3
Pervez Iqbal <pervezfunctor@gmail.com>
2019-03-16 14:30:04 +0530

    use io-ts for Album json-placeholder-forms

--- src/docs/examples/composites/json-placeholder-forms/AlbumForm.tsx
+++ src/docs/examples/composites/json-placeholder-forms/AlbumForm.tsx
@@ -1,4 +1,7 @@
+import * as t from 'io-ts'
 import React from 'react'
+import { emptyFromType } from '../../../../devfractal/src/utils/empty'
+import { typeInvariant } from '../../../../devfractal/src/utils/iotsUtils'
 import {
   consoleSubmit,
   required,
@@ -7,22 +10,25 @@ import {
   Title,
 } from '../../devfractal'
 
-interface AlbumFormValues {
-  readonly title: string
-}
+// tslint:disable typedef
 
-const initialAlbumFormValues: AlbumFormValues = {
-  title: '',
-}
+const AlbumRT = t.readonly(t.type({ title: t.string }))
 
+type AlbumValues = t.TypeOf<typeof AlbumRT>
+
+const initialValues: AlbumValues = emptyFromType(AlbumRT)
+console.log(initialValues)
 export const AlbumForm: React.SFC = () => (
   <Section>
     <Title>Album Form</Title>
     <Simple.Form
-      initialValues={initialAlbumFormValues}
-      onSubmit={consoleSubmit(0)}
+      initialValues={initialValues}
+      onSubmit={(values: AlbumValues) => {
+        typeInvariant(AlbumRT, values)
+        consoleSubmit(0)
+      }}
     >
-      <Simple.Text label="Title" name="title" validations={[required()]} />
+      <Simple.Number label="Title" name="title" validations={[required()]} />
       <Simple.FormButtons />
       <Simple.Debug />
     </Simple.Form>
