9c30f17bbb68588c45b45633ee87226c2ec5bae8
Pervez Iqbal <pervezfunctor@gmail.com>
2019-03-17 10:46:48 +0530

    make component more readable

--- src/devfractal/src/utils/componentUtils.ts
+++ src/devfractal/src/utils/componentUtils.ts
@@ -1,52 +1,13 @@
 import * as t from 'io-ts'
-import {
-  Any,
-  IntersectionC,
-  Mixed,
-  PartialC,
-  Props,
-  ReadonlyC,
-  TypeC,
-} from 'io-ts'
-import { reporter } from 'io-ts-reporters'
 import React from 'react'
-import { warning } from './internal'
+import { typeWarning } from './iotsUtils'
 
-export const optionalProps: <P extends Props>(
-  props: P,
-  name?: string,
-) => ReadonlyC<PartialC<P>> = props => t.readonly(t.partial(props))
-
-export const requiredProps: <P extends Props>(
-  props: P,
-  name?: string,
-) => ReadonlyC<TypeC<P>> = obj => t.readonly(t.type(obj))
-
-export const props: <O extends Props, R extends Props>(
-  optional: O,
-  // @TODO: make 'required' optional to drop requiredProps, optionalProps
-  required: R,
-) => IntersectionC<[ReadonlyC<PartialC<O>>, ReadonlyC<TypeC<R>>]> = (
-  optional,
-  required,
-) =>
-  t.intersection([
-    t.readonly(t.partial(optional)),
-    t.readonly(t.type(required)),
-  ])
-
-export type Properties<C extends Any> = C['_A']
-
-export const component: <
-  T extends Mixed,
-  P extends Properties<T> = Properties<T>
->(
+export const component: <T extends t.Mixed, P extends t.TypeOf<T>>(
+  // displayName?: string,
   propsValue: T,
-  inner: React.ComponentType<P>,
+  inner: React.FC<P>,
 ) => React.FC<P> = (propsValue, inner) => props => {
-  const v: string = reporter(propsValue.decode(props)).join('\n')
-  warning(v === '', v)
-
+  typeWarning(propsValue, props)
   return React.createElement(inner, props)
 }
 
