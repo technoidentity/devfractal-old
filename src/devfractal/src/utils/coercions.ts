e498a1df8d66639d81c01cd7f7a35ced1d089ffb
sravani dande <sravanidnd89@gmail.com>
2019-03-21 15:16:08 +0530

    add rest of the coercion functions closes #146

--- src/devfractal/src/utils/coercions.ts
+++ src/devfractal/src/utils/coercions.ts
@@ -1,10 +1,30 @@
 import { Either } from 'fp-ts/lib/Either'
 import * as t from 'io-ts'
 import { reporter } from 'io-ts-reporters'
-import { IntegerFromString } from 'io-ts-types'
+import {
+  BooleanFromString,
+  DateFromISOString,
+  IntegerFromString,
+  NumberFromString,
+} from 'io-ts-types'
 import { fatal } from './internal'
 
 export const toInt: (from: string) => number = from => {
   const decoded: Either<t.Errors, number> = IntegerFromString.decode(from)
+  return decoded.isRight() ? decoded.value : fatal(reporter(decoded).join('\n'))
+}
+
+export const toBoolean: (from: string) => boolean = from => {
+  const decoded: Either<t.Errors, boolean> = BooleanFromString.decode(from)
+  return decoded.isRight() ? decoded.value : fatal(reporter(decoded).join('\n'))
+}
+
+export const toNumber: (from: string) => number = from => {
+  const decoded: Either<t.Errors, number> = NumberFromString.decode(from)
+  return decoded.isRight() ? decoded.value : fatal(reporter(decoded).join('\n'))
+}
+
+export const toDate: (from: string) => Date = from => {
+  const decoded: Either<t.Errors, Date> = DateFromISOString.decode(from)
   return decoded.isRight() ? decoded.value : fatal(reporter(decoded).join('\n'))
 }
