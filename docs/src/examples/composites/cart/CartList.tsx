66af33c6955a998414739936c6be73df29d6ca16
sravanidande <sravanidnd89@gmail.com>
2019-04-07 15:13:22 +0530

    add CartList

---
+++ docs/src/examples/composites/cart/CartList.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableHead,
+  Td,
+  Th,
+  Title,
+  Tr,
+} from 'technoidentity-devfractal'
+import { Product } from './Product'
+
+interface CartItem {
+  readonly product: Product
+  readonly count: number
+}
+export interface CartItemProps {
+  readonly cartItem: CartItem
+}
+
+export const CartItem: React.SFC<CartItemProps> = ({ cartItem }) => {
+  return (
+    <Tr>
+      <Td>{cartItem.product.id}</Td>
+      <Td>{cartItem.product.title}</Td>
+      <Td>{cartItem.product.price}</Td>
+      <Td>{cartItem.count}</Td>
+    </Tr>
+  )
+}
+
+export interface CartListProps {
+  readonly cartList: ReadonlyArray<CartItem>
+}
+
+export const CartList: React.SFC<CartListProps> = ({ cartList }) => {
+  return (
+    <>
+      <Title>Cart</Title>
+      <Table>
+        <TableHead>
+          <Tr>
+            <Th>id</Th>
+            <Th>title</Th>
+            <Th>price</Th>
+          </Tr>
+        </TableHead>
+        <TableBody>
+          {cartList.map((item, index) => (
+            <CartItem key={index} cartItem={item} />
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  )
+}
