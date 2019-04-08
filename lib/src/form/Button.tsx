841199af7903d6002d23ed23b92a18491c243d97
swathivalasapalli <swathivalasapalli@gail.com>
2019-04-08 11:54:54 +0530

    document properties for Button

--- lib/src/form/Button.tsx
+++ lib/src/form/Button.tsx
@@ -35,16 +35,31 @@ type ButtonState =
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     AllControlHelpers {
+  /**
+   * variant defines the various colors which we can use for Button
+   */
   readonly variant?: ButtonVariant
   /**
    * While the default size is the normal one, the normal modifier exists in case you need to reset the button to its normal size.
    */
   readonly size?: ButtonSize
+
   readonly state?: ButtonState
   readonly fullWidth?: boolean
+
+  /**
+   * The shape of the button will be Rounded rectangle
+   */
   readonly rounded?: boolean
+  /**
+   * The text color becomes the background color , and vice-versa
+   */
   readonly inverted?: boolean
+  /**
+   * While used along with inverted the invert color becomes the text and border colors
+   */
   readonly outlined?: boolean
+
   readonly type?: ButtonType
 }
 
