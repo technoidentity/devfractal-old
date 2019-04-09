bda4728be437641cbdc1cd4fe262cc74e0fb5fef
swathivalasapalli <swathivalasapalli@gail.com>
2019-04-09 11:04:23 +0530

    document properties for Delete

--- lib/src/visual/Delete.tsx
+++ lib/src/visual/Delete.tsx
@@ -6,6 +6,9 @@ type DeleteSize = 'small' | 'medium' | 'large'
 export interface DeleteProps
   extends React.AnchorHTMLAttributes<HTMLAnchorElement>,
     Helpers {
+  /**
+   * Resize the delete element
+   */
   readonly size?: DeleteSize
 }
 
@@ -15,3 +18,6 @@ export const Delete: React.FC<DeleteProps> = ({ size, ...props }) => {
   })
   return <Div as="a" {...props} className={classes} />
 }
+
+// tslint:disable-next-line: no-default-export
+export default Delete
