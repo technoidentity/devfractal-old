45d5d8c60d8389a90987b2bbc5cd63e7dd4a8fe2
swathivalasapalli <swathivalasapalli@gail.com>
2019-04-09 11:01:40 +0530

    minor fix:export default for Control

--- lib/src/form/Control.tsx
+++ lib/src/form/Control.tsx
@@ -6,3 +6,6 @@ export type ControlProps = Omit<ControlWrapperProps, 'noControl'>
 export const Control: React.FC<ControlProps> = ({ children, ...props }) => (
   <ControlWrapper {...props}>{children}</ControlWrapper>
 )
+
+// tslint:disable-next-line: no-default-export
+export default Control
