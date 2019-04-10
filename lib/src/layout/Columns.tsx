4a0d7f52791c948c1679fff78bbe414115038af1
swathivalasapalli <swathivalasapalli@gail.com>
2019-04-10 14:37:57 +0530

    document properties for Columns

--- lib/src/layout/Columns.tsx
+++ lib/src/layout/Columns.tsx
@@ -11,10 +11,25 @@ type ColumnsResponsive =
 export interface ColumnsProps
   extends React.HTMLAttributes<HTMLDivElement>,
     Helpers {
+  /**
+   * Display columns based on each viewport size
+   */
   readonly responsiveness?: ColumnsResponsive
+  /**
+   * To remove space between the columns
+   */
   readonly gapLess?: boolean
+  /**
+   * To align columns vertically
+   */
   readonly verticallyCentered?: boolean
+  /**
+   * To add more column elements than would fit in a single row
+   */
   readonly multiline?: boolean
+  /**
+   * To create horizontal space around column elements
+   */
   readonly columnCentered?: boolean
 }
 
@@ -41,3 +56,6 @@ export const Columns: React.FC<ColumnsProps> = ({
     </Div>
   )
 }
+
+// tslint:disable-next-line: no-default-export
+export default Columns
