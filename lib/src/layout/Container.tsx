32b626d59b57facde1fba8b9aa34daa2fb5f71d3
swathivalasapalli <swathivalasapalli@gail.com>
2019-04-10 14:39:17 +0530

    document properties for Container

--- lib/src/layout/Container.tsx
+++ lib/src/layout/Container.tsx
@@ -4,7 +4,13 @@ import { classNamesHelper, Div, Helpers } from '../lib'
 export interface ContainerProps
   extends React.HTMLAttributes<HTMLDivElement>,
     Helpers {
+  /**
+   * The container width for each display screen
+   */
   readonly breakpoint?: 'widescreen' | 'fullhd'
+  /**
+   * If you don't want to have a maximum width but want to keep the 32px margin on the left and right sides,add fluid
+   */
   readonly fluid?: boolean
 }
 
@@ -25,3 +31,6 @@ export const Container: React.FC<ContainerProps> = ({
     </Div>
   )
 }
+
+// tslint:disable-next-line: no-default-export
+export default Container
