9b466a4bac96c3c3431f425d8d0f5d893219119d
Pervez Iqbal <pervezfunctor@gmail.com>
2019-04-14 01:11:07 +0530

    convert meta to yup validations

---
+++ lib/src/meta/metaToYup.test.ts
@@ -0,0 +1,179 @@
+import { Schema } from 'yup'
+import { metaToYup } from './metaToYup'
+import { ArrayMT, EnumMT, MT, ObjectMT, SimpleMT } from './types'
+
+const noEx: SimpleMT = { kind: 'number' }
+const strEx: SimpleMT = { kind: 'string' }
+const boolEx: SimpleMT = { kind: 'boolean' }
+const dateEx: SimpleMT = { kind: 'date' }
+
+const validate: (value: unknown, schema: Schema<any>) => boolean = (
+  value,
+  schema,
+) => schema.isValidSync(value)
+
+test('number meta', () => {
+  expect(validate(100, metaToYup(noEx))).toBeTruthy()
+  expect(validate('100', metaToYup(noEx))).toBeFalsy()
+})
+
+test('number with refinements', () => {
+  const noREx: SimpleMT = {
+    kind: 'number',
+    refinements: [
+      { kind: 'integer' },
+      { kind: 'min', value: 10 },
+      { kind: 'max', value: 20 },
+    ],
+  }
+  expect(validate(15, metaToYup(noREx))).toBeTruthy()
+  expect(validate(5, metaToYup(noREx))).toBeFalsy()
+  expect(validate(25, metaToYup(noREx))).toBeFalsy()
+})
+
+test('str meta', () => {
+  expect(validate('100', metaToYup(strEx))).toBeTruthy()
+  expect(validate(100, metaToYup(strEx))).toBeFalsy()
+})
+
+test('str meta with refinements', () => {
+  const strREx: SimpleMT = {
+    kind: 'string',
+    refinements: [
+      { kind: 'email' },
+      { kind: 'lowercase' },
+      { kind: 'minLength', value: 10 },
+      { kind: 'maxLength', value: 20 },
+    ],
+  }
+  expect(validate('foobar@gmail.com', metaToYup(strREx))).toBeTruthy()
+  expect(validate('foobaR@gmail.com', metaToYup(strREx))).toBeFalsy()
+  expect(validate('f@g.com', metaToYup(strREx))).toBeFalsy()
+  expect(
+    validate('foooooooooooooooooooooooooooooo@gmai.com', metaToYup(strREx)),
+  ).toBeFalsy()
+})
+
+test('bool meta', () => {
+  expect(validate(true, metaToYup(boolEx))).toBeTruthy()
+  expect(validate(false, metaToYup(boolEx))).toBeTruthy()
+  expect(validate(100, metaToYup(boolEx))).toBeFalsy()
+})
+
+test('date meta', () => {
+  expect(validate(new Date(), metaToYup(dateEx))).toBeTruthy()
+  expect(validate('2000-12-2', metaToYup(dateEx))).toBeFalsy()
+})
+
+test('enum meta', () => {
+  const enumEx: EnumMT = {
+    kind: 'enum',
+    name: 'color',
+    values: ['red', 'green', 'blue'],
+  }
+  expect(validate(100, metaToYup(enumEx))).toBeFalsy()
+  expect(validate('red', metaToYup(enumEx))).toBeTruthy()
+  expect(validate('green', metaToYup(enumEx))).toBeTruthy()
+  expect(validate('blue', metaToYup(enumEx))).toBeTruthy()
+  expect(validate('GREEN', metaToYup(enumEx))).toBeFalsy()
+})
+
+test('array meta', () => {
+  const arrNoEx: ArrayMT = { kind: 'array', of: noEx }
+  expect(validate([], metaToYup(arrNoEx))).toBeTruthy()
+  expect(validate([10, 20], metaToYup(arrNoEx))).toBeTruthy()
+  expect(validate(['10', '20'], metaToYup(arrNoEx))).toBeFalsy()
+})
+
+test('array meta with refinements', () => {
+  const arrNoREx: ArrayMT = {
+    kind: 'array',
+    of: noEx,
+    refinements: [
+      { kind: 'maxLength', value: 6 },
+      { kind: 'minLength', value: 2 },
+    ],
+  }
+  expect(validate([10, 20], metaToYup(arrNoREx))).toBeTruthy()
+  expect(
+    validate([10, 20, 30, 40, 50, 60, 70], metaToYup(arrNoREx)),
+  ).toBeFalsy()
+  expect(validate([], metaToYup(arrNoREx))).toBeFalsy()
+})
+
+test('array with differently typed elements', () => {
+  const arrStrEx: ArrayMT = { kind: 'array', of: strEx }
+  expect(validate([], metaToYup(arrStrEx))).toBeTruthy()
+  expect(validate([10, 20], metaToYup(arrStrEx))).toBeFalsy()
+  expect(validate(['10', '20'], metaToYup(arrStrEx))).toBeTruthy()
+
+  const arrBoolEx: ArrayMT = { kind: 'array', of: boolEx }
+  expect(validate([], metaToYup(arrBoolEx))).toBeTruthy()
+  expect(validate([true, false], metaToYup(arrBoolEx))).toBeTruthy()
+  expect(validate(['10', '20'], metaToYup(arrBoolEx))).toBeFalsy()
+
+  const arrDateEx: ArrayMT = { kind: 'array', of: dateEx }
+  expect(validate([], metaToYup(arrDateEx))).toBeTruthy()
+  expect(validate([new Date(), new Date()], metaToYup(arrDateEx))).toBeTruthy()
+  expect(validate(['10', '20'], metaToYup(arrDateEx))).toBeFalsy()
+})
+
+test('object meta', () => {
+  const objEx: ObjectMT = {
+    kind: 'object',
+    properties: {
+      name: strEx,
+      price: noEx,
+      inStock: boolEx,
+    },
+  }
+  expect(
+    validate({ name: 'iPhone', price: 700, inStock: true }, metaToYup(objEx)),
+  ).toBeTruthy()
+  expect(
+    validate({ name: 'iPhone', price: 700, inStock: 10 }, metaToYup(objEx)),
+  ).toBeFalsy()
+  expect(
+    validate({ name: 'iPhone', price: '700', inStock: 10 }, metaToYup(objEx)),
+  ).toBeFalsy()
+  expect(
+    validate({ name: 100, price: '700', inStock: 10 }, metaToYup(objEx)),
+  ).toBeFalsy()
+})
+
+test('complex meta', () => {
+  const customerMeta: MT = {
+    kind: 'object',
+    properties: {
+      name: strEx,
+      type: {
+        kind: 'enum',
+        name: 'CustomerType',
+        values: ['manager', 'programmer', 'team-leader'],
+      },
+      addresses: {
+        kind: 'array',
+        of: {
+          kind: 'object',
+          properties: {
+            city: strEx,
+            zip: noEx,
+            country: strEx,
+          },
+        },
+      },
+    },
+  }
+
+  // tslint:disable-next-line:typedef
+  const customer = {
+    name: 'foobar',
+    type: 'team-leader',
+    addresses: [
+      { city: 'hyderabad', zip: 500018, country: 'india' },
+      { city: 'chennai', zip: 500038, country: 'india' },
+    ],
+  }
+
+  expect(validate(customer, metaToYup(customerMeta))).toBeTruthy()
+})
---
+++ lib/src/meta/metaToYup.ts
@@ -0,0 +1,147 @@
+import {
+  array,
+  ArraySchema,
+  bool,
+  date,
+  DateSchema,
+  mixed,
+  number,
+  NumberSchema,
+  object,
+  Schema,
+  string,
+  StringSchema,
+} from 'yup'
+import {
+  ArrayRefinements,
+  DateRefinements,
+  MT,
+  NumberRefinements,
+  StringRefinements,
+} from './types'
+
+// tslint:disable typedef switch-default
+
+const toYupNumberRefinements: (
+  schema: NumberSchema,
+  r: NumberRefinements,
+) => NumberSchema = (schema, r) => {
+  switch (r.kind) {
+    case 'integer':
+      return schema.integer()
+
+    case 'max':
+      return schema.max(r.value)
+
+    case 'min':
+      return schema.min(r.value)
+
+    case 'positive':
+      return schema.positive()
+
+    case 'negative':
+      return schema.negative()
+  }
+}
+
+const toYupStringRefinements: (
+  schema: StringSchema,
+  r: StringRefinements,
+) => StringSchema = (schema, r) => {
+  switch (r.kind) {
+    case 'email':
+      return schema.email()
+    case 'url':
+      return schema.url()
+    case 'lowercase':
+      return schema.lowercase()
+    case 'uppercase':
+      return schema.uppercase()
+    case 'maxLength':
+      return schema.max(r.value)
+    case 'minLength':
+      return schema.min(r.value)
+    case 'length':
+      return schema.length(r.value)
+  }
+}
+
+const toYupDateRefinements: (
+  schema: DateSchema,
+  r: DateRefinements,
+) => DateSchema = (schema, r) => {
+  switch (r.kind) {
+    case 'max':
+      return schema.max(r.value)
+    case 'min':
+      return schema.min(r.value)
+  }
+}
+
+const toYupArrayRefinements: (
+  schema: ArraySchema<unknown>,
+  r: ArrayRefinements,
+) => ArraySchema<unknown> = (schema, r) => {
+  switch (r.kind) {
+    case 'maxLength':
+      return schema.max(r.value)
+    case 'minLength':
+      return schema.min(r.value)
+  }
+}
+
+function buildObject(obj: any, f: (key: any) => any): any {
+  const result: any = {}
+  for (const k of Object.keys(obj)) {
+    result[k] = f(k as any)
+  }
+  return result
+}
+
+export const metaToYup: (meta: MT) => Schema<any> = meta => {
+  switch (meta.kind) {
+    case 'number':
+      const ns = number().strict(true)
+      return meta.refinements
+        ? meta.refinements.reduce(toYupNumberRefinements, ns)
+        : ns
+
+    case 'string':
+      const ss = string().strict(true)
+      return meta.refinements
+        ? meta.refinements.reduce(toYupStringRefinements, ss)
+        : ss
+
+    case 'boolean':
+      return bool()
+
+    case 'date':
+      const ds = date().strict(true)
+      return meta.refinements
+        ? meta.refinements.reduce(toYupDateRefinements, ds)
+        : ds
+
+    case 'enum':
+      return string()
+        .strict(true)
+        .oneOf(meta.values as string[])
+
+    case 'union':
+      return mixed()
+        .strict(true)
+        .oneOf(meta.values as any[])
+
+    case 'array':
+      const as = array()
+        .strict(true)
+        .of(metaToYup(meta.of))
+      return meta.refinements
+        ? meta.refinements.reduce(toYupArrayRefinements, as)
+        : as
+
+    case 'object':
+      return object(
+        buildObject(meta.properties, p => metaToYup(meta.properties[p])),
+      ).strict(true)
+  }
+}
