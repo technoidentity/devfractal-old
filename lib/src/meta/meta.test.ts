14ac7ff561785038edfeabfc121fb64259b60cfa
Pervez Iqbal <pervezfunctor@gmail.com>
2019-04-14 01:09:30 +0530

    test meta types

---
+++ lib/src/meta/meta.test.ts
@@ -0,0 +1,168 @@
+import { ArrayMT, EnumMT, MT, ObjectMT, SimpleMT } from './types'
+import { validate } from './validate'
+
+const noEx: SimpleMT = { kind: 'number' }
+const strEx: SimpleMT = { kind: 'string' }
+const boolEx: SimpleMT = { kind: 'boolean' }
+const dateEx: SimpleMT = { kind: 'date' }
+
+test('number meta', () => {
+  expect(validate(noEx, 100)).toBeTruthy()
+  expect(validate(noEx, '100')).toBeFalsy()
+})
+
+test('number with refinements', () => {
+  const noREx: SimpleMT = {
+    kind: 'number',
+    refinements: [
+      { kind: 'integer' },
+      { kind: 'min', value: 10 },
+      { kind: 'max', value: 20 },
+    ],
+  }
+  expect(validate(noREx, 15)).toBeTruthy()
+  expect(validate(noREx, 5)).toBeFalsy()
+  expect(validate(noREx, 25)).toBeFalsy()
+})
+
+test('str meta', () => {
+  expect(validate(strEx, '100')).toBeTruthy()
+  expect(validate(strEx, 100)).toBeFalsy()
+})
+
+test('str meta with refinements', () => {
+  const strREx: SimpleMT = {
+    kind: 'string',
+    refinements: [
+      { kind: 'email' },
+      { kind: 'lowercase' },
+      { kind: 'minLength', value: 10 },
+      { kind: 'maxLength', value: 20 },
+    ],
+  }
+  expect(validate(strREx, 'foobar@gmail.com')).toBeTruthy()
+  expect(validate(strREx, 'foobaR@gmail.com')).toBeFalsy()
+  expect(validate(strREx, 'f@g.com')).toBeFalsy()
+  expect(
+    validate(strREx, 'foooooooooooooooooooooooooooooo@gmai.com'),
+  ).toBeFalsy()
+})
+
+test('bool meta', () => {
+  expect(validate(boolEx, true)).toBeTruthy()
+  expect(validate(boolEx, false)).toBeTruthy()
+  expect(validate(boolEx, 100)).toBeFalsy()
+})
+
+test('date meta', () => {
+  expect(validate(dateEx, new Date())).toBeTruthy()
+  expect(validate(dateEx, '2000-12-2')).toBeFalsy()
+})
+
+test('enum meta', () => {
+  const enumEx: EnumMT = {
+    kind: 'enum',
+    name: 'color',
+    values: ['red', 'green', 'blue'],
+  }
+  expect(validate(enumEx, 100)).toBeFalsy()
+  expect(validate(enumEx, 'red')).toBeTruthy()
+  expect(validate(enumEx, 'green')).toBeTruthy()
+  expect(validate(enumEx, 'blue')).toBeTruthy()
+  expect(validate(enumEx, 'GREEN')).toBeFalsy()
+})
+
+test('array meta', () => {
+  const arrNoEx: ArrayMT = { kind: 'array', of: noEx }
+  expect(validate(arrNoEx, [])).toBeTruthy()
+  expect(validate(arrNoEx, [10, 20])).toBeTruthy()
+  expect(validate(arrNoEx, ['10', '20'])).toBeFalsy()
+})
+
+test('array meta with refinements', () => {
+  const arrNoREx: ArrayMT = {
+    kind: 'array',
+    of: noEx,
+    refinements: [
+      { kind: 'maxLength', value: 6 },
+      { kind: 'minLength', value: 2 },
+    ],
+  }
+  expect(validate(arrNoREx, [10, 20])).toBeTruthy()
+  expect(validate(arrNoREx, [10, 20, 30, 40, 50, 60, 70])).toBeFalsy()
+  expect(validate(arrNoREx, [])).toBeFalsy()
+})
+
+test('array with differently typed elements', () => {
+  const arrStrEx: ArrayMT = { kind: 'array', of: strEx }
+  expect(validate(arrStrEx, [])).toBeTruthy()
+  expect(validate(arrStrEx, [10, 20])).toBeFalsy()
+  expect(validate(arrStrEx, ['10', '20'])).toBeTruthy()
+
+  const arrBoolEx: ArrayMT = { kind: 'array', of: boolEx }
+  expect(validate(arrBoolEx, [])).toBeTruthy()
+  expect(validate(arrBoolEx, [true, false])).toBeTruthy()
+  expect(validate(arrBoolEx, ['10', '20'])).toBeFalsy()
+
+  const arrDateEx: ArrayMT = { kind: 'array', of: dateEx }
+  expect(validate(arrDateEx, [])).toBeTruthy()
+  expect(validate(arrDateEx, [new Date(), new Date()])).toBeTruthy()
+  expect(validate(arrDateEx, ['10', '20'])).toBeFalsy()
+})
+
+test('object meta', () => {
+  const objEx: ObjectMT = {
+    kind: 'object',
+    properties: {
+      name: strEx,
+      price: noEx,
+      inStock: boolEx,
+    },
+  }
+  expect(
+    validate(objEx, { name: 'iPhone', price: 700, inStock: true }),
+  ).toBeTruthy()
+  expect(
+    validate(objEx, { name: 'iPhone', price: 700, inStock: 10 }),
+  ).toBeFalsy()
+  expect(
+    validate(objEx, { name: 'iPhone', price: '700', inStock: 10 }),
+  ).toBeFalsy()
+  expect(validate(objEx, { name: 100, price: '700', inStock: 10 })).toBeFalsy()
+})
+
+test('complex meta', () => {
+  const customerMeta: MT = {
+    kind: 'object',
+    properties: {
+      name: strEx,
+      type: {
+        kind: 'enum',
+        name: 'CustomerType',
+        values: ['manager', 'programmer', 'team-leader'],
+      },
+      addresses: {
+        kind: 'array',
+        of: {
+          kind: 'object',
+          properties: {
+            city: strEx,
+            zip: noEx, // Just an example, so zip as noFalsyne
+            country: strEx,
+          },
+        },
+      },
+    },
+  }
+  // tslint:disable-next-line:typedef
+  const customer = {
+    name: 'foobar',
+    type: 'team-leader',
+    addresses: [
+      { city: 'hyderabad', zip: 500018, country: 'india' },
+      { city: 'chennai', zip: 500038, country: 'india' },
+    ],
+  }
+
+  expect(validate(customerMeta, customer)).toBeTruthy()
+})
---
+++ lib/src/meta/validate.ts
@@ -0,0 +1,171 @@
+import {
+  Array,
+  Boolean,
+  Date as TDate,
+  Number,
+  Object as TObject,
+  String,
+} from 'tcomb'
+import * as yup from 'yup'
+import {
+  ArrayRefinements,
+  DateRefinements,
+  MT,
+  NumberRefinements,
+  StringRefinements,
+} from './types'
+
+// tslint:disable switch-default
+
+const validateNumberRefinements: (
+  r: NumberRefinements,
+  value: number,
+) => boolean = (r, value) => {
+  let s: yup.NumberSchema = yup.number().strict(true)
+  switch (r.kind) {
+    case 'integer':
+      s = s.integer()
+      break
+
+    case 'max':
+      s = s.max(r.value)
+      break
+
+    case 'min':
+      s = s.min(r.value)
+      break
+
+    case 'positive':
+      s = s.positive()
+      break
+
+    case 'negative':
+      s = s.negative()
+  }
+  return s.isValidSync(value)
+}
+
+const validateStringRefinements: (
+  r: StringRefinements,
+  value: string,
+) => boolean = (r, value) => {
+  let s: yup.StringSchema = yup.string().strict(true)
+  switch (r.kind) {
+    case 'email':
+      s = s.email()
+      break
+    case 'url':
+      s = s.url()
+      break
+    case 'lowercase':
+      s = s.lowercase()
+      break
+    case 'uppercase':
+      s = s.uppercase()
+      break
+    case 'maxLength':
+      s = s.max(r.value)
+      break
+    case 'minLength':
+      s = s.min(r.value)
+      break
+    case 'length':
+      s = s.length(r.value)
+  }
+  return s.isValidSync(value)
+}
+
+const validateDateRefinements: (r: DateRefinements, value: Date) => boolean = (
+  r,
+  value,
+) => {
+  let s: yup.DateSchema = yup.date()
+  switch (r.kind) {
+    case 'max':
+      s = s.max(r.value)
+      break
+    case 'min':
+      s = s.min(r.value)
+  }
+  return s.isValidSync(value)
+}
+
+const validateArrayRefinements: (
+  r: ArrayRefinements,
+  value: ReadonlyArray<unknown>,
+) => boolean = (r, value) => {
+  let s: yup.ArraySchema<unknown> = yup.array()
+  switch (r.kind) {
+    case 'maxLength':
+      s = s.max(r.value)
+      break
+    case 'minLength':
+      s = s.min(r.value)
+  }
+  return s.isValidSync(value)
+}
+
+export const validate: (meta: MT, obj: unknown) => boolean = (meta, obj) => {
+  // tslint:disable-next-line:switch-default
+  switch (meta.kind) {
+    case 'number':
+      return (
+        Number.is(obj) &&
+        (meta.refinements === undefined ||
+          meta.refinements.every(r => validateNumberRefinements(r, obj)))
+      )
+
+    case 'string':
+      return (
+        String.is(obj) &&
+        (meta.refinements === undefined ||
+          meta.refinements.every(r => validateStringRefinements(r, obj)))
+      )
+
+    case 'boolean':
+      return Boolean.is(obj)
+
+    case 'date':
+      return (
+        TDate.is(obj) &&
+        (meta.refinements === undefined ||
+          meta.refinements.every(r => validateDateRefinements(r, obj)))
+      )
+
+    case 'enum':
+      return String.is(obj) && meta.values.some(e => e === obj)
+
+    case 'union':
+      return meta.values.some(m => validate(m, obj))
+
+    case 'array':
+      if (!Array.is(obj)) {
+        return false
+      }
+      if (
+        !(
+          meta.refinements === undefined ||
+          meta.refinements.every(e => validateArrayRefinements(e, obj))
+        )
+      ) {
+        return false
+      }
+      for (const e of obj) {
+        if (!validate(meta.of, e)) {
+          return false
+        }
+      }
+      return true
+
+    case 'object':
+      if (!TObject.is(obj)) {
+        return false
+      }
+      for (const k of Object.keys(obj)) {
+        if (!validate(meta.properties[k], obj[k])) {
+          return false
+        }
+      }
+      return true
+  }
+}
