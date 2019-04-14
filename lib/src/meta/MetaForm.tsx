549bd948bd581907e34b8c062ccea29700343a44
Pervez Iqbal <pervezfunctor@gmail.com>
2019-04-14 19:26:10 +0530

    Better MetaForm covers Simple and Enum types

--- lib/src/meta/MetaForm.tsx
+++ lib/src/meta/MetaForm.tsx
@@ -1,109 +1,127 @@
 import { FormikActions } from 'formik'
 import React from 'react'
-import { Label, Simple } from '../lib'
-import { FieldMT, FieldsMT } from './types'
+import { ObjectSchema } from 'yup'
+import { camelCaseToPhrase, Field, Label, Simple } from '../lib'
+import {
+  EnumMT,
+  metaToInitialValues,
+  metaToYup,
+  MT,
+  ObjectMT,
+  SimpleMT,
+} from './index'
+
+interface EditorProps {
+  readonly name: string
+  readonly label?: string
+}
+
+const BooleanEditor: React.FC<EditorProps> = ({ label, name }) => (
+  <Simple.Checkbox name={name}>
+    <Label>{label || camelCaseToPhrase(name)}</Label>
+  </Simple.Checkbox>
+)
+
+const NumberEditor: React.FC<EditorProps> = ({ label, name }) => (
+  <Simple.Number name={name} label={label} />
+)
+
+const TextEditor: React.FC<EditorProps> = ({ label, name }) => (
+  <Simple.Text name={name} label={label} />
+)
+
+const DateEditor: React.FC<EditorProps> = ({ label, name }) => (
+  <Field>
+    <Label>{label || camelCaseToPhrase(name)}</Label>
+    <input className="input" type="date" name={name} />
+  </Field>
+)
+
+interface EnumEditorProps {
+  readonly name: string
+  readonly label?: string
+  readonly meta: EnumMT
+}
+
+const EnumEditor: React.FC<EnumEditorProps> = ({ label, name, meta }) => (
+  <Field>
+    <Label>{label || camelCaseToPhrase(name)}</Label>
+    <Simple.Select name={name}>
+      {meta.values.map(v => (
+        <option key={v} value={v}>
+          {camelCaseToPhrase(v)}
+        </option>
+      ))}
+    </Simple.Select>
+  </Field>
+)
+
+interface SimpleMetaEditorProps {
+  readonly name: string
+  readonly label?: string
+  readonly meta: SimpleMT
+}
+
+const SimpleMetaEditor: React.FC<SimpleMetaEditorProps> = ({
+  label,
+  name,
+  meta: { kind },
+}) => (
+  <>
+    {kind === 'boolean' ? (
+      <BooleanEditor name={name} label={label} />
+    ) : kind === 'number' ? (
+      <NumberEditor name={name} label={label} />
+    ) : kind === 'string' ? (
+      <TextEditor name={name} label={label} />
+    ) : kind === 'date' ? (
+      <DateEditor name={name} label={label} />
+    ) : (
+      <h1>`ERROR: Unknown meta kind: ${kind}`</h1>
+    )}
+  </>
+)
+
+interface ObjectMetaEditorProps {
+  readonly meta: ObjectMT
+}
+
+const ObjectMetaEditor: React.FC<ObjectMetaEditorProps> = ({ meta }) => (
+  <>
+    {Object.keys(meta.properties).map(key => {
+      const props: MT = meta.properties[key]
+
+      return (
+        <React.Fragment key={key}>
+          {props.kind === 'array' ? (
+            <h1>@TODO: Implement array meta editor</h1>
+          ) : props.kind === 'object' ? (
+            <h1>@TODO: Implement nesting object meta</h1>
+          ) : props.kind === 'enum' ? (
+            <EnumEditor name={key} meta={props} />
+          ) : (
+            <SimpleMetaEditor name={key} meta={props} />
+          )}
+        </React.Fragment>
+      )
+    })}
+  </>
+)
 
 export interface MetaFormProps {
-  readonly fields: FieldsMT
-  readonly data: any
+  readonly meta: ObjectMT
   onSubmit?(values: any, actions: FormikActions<any>): void
 }
 
-interface FieldEditorProps {
-  readonly field: FieldMT
-  readonly data: any
-  readonly name: string
-}
-
-const BooleanEditor: React.FC<FieldEditorProps> = ({ data, field }) => (
-  <>
-    <Label>{field.label}</Label>
-    <Simple.Checkbox name={name} checked={data} readOnly noLabel />
-  </>
-)
-
-const NumberEditor: React.FC<FieldEditorProps> = ({ data, field }) => (
-  <>
-    <Label>{field.label}</Label>
-    <Simple.Number name={name} value={data} />
-  </>
-)
-
-const TextEditor: React.FC<FieldEditorProps> = ({ data, field }) => (
-  <>
-    <Label>{field.label}</Label>
-    <Simple.Text name={name} value={data} />
-  </>
-)
-
-const DateEditor: React.FC<FieldEditorProps> = ({ data, field }) => (
-  <>
-    <Label>{field.label}</Label>
-    <input type="date" name={name} value={data} />
-  </>
-)
-
-const EnumEditor: React.FC<FieldEditorProps> = ({ data, field }) => (
-  <>
-    <Label>{field.label}</Label>
-    <Simple.Select name={name} value={data} />
-  </>
-)
-
-const ArrayEditor: React.FC<FieldEditorProps> = ({ field }) => (
-  <>
-    <Label>{field.label}</Label>
-    <h1>Complex Stuff Ahead</h1>
-  </>
-)
-
-const ObjectEditor: React.FC<FieldEditorProps> = ({ field }) => (
-  <>
-    <Label>{field.label}</Label>
-    <h1>Complex Stuff Ahead</h1>
-  </>
-)
-
-export const FieldEditor: React.FC<FieldEditorProps> = props => {
-  const kind: typeof props.field.meta.kind = props.field.meta.kind
-
+export const MetaForm: React.FC<MetaFormProps> = ({ onSubmit, meta }) => {
   return (
-    <>
-      {kind === 'boolean' ? (
-        <BooleanEditor {...props} />
-      ) : kind === 'number' ? (
-        <NumberEditor {...props} />
-      ) : kind === 'string' ? (
-        <TextEditor {...props} />
-      ) : kind === 'date' ? (
-        <DateEditor {...props} />
-      ) : kind === 'enum' ? (
-        <EnumEditor {...props} />
-      ) : kind === 'array' ? (
-        <ArrayEditor {...props} />
-      ) : (
-        <ObjectEditor {...props} />
-      )}
-    </>
-  )
-}
-
-export const MetaForm: React.FC<MetaFormProps> = ({
-  data,
-  onSubmit,
-  fields,
-}) => {
-  return (
-    <Simple.Form initialValues={data} onSubmit={onSubmit}>
-      {Object.keys(fields.fields).map(key => (
-        <React.Fragment key={key} />
-      ))}
+    <Simple.Form
+      initialValues={metaToInitialValues(meta)}
+      validationSchema={metaToYup(meta) as ObjectSchema<any>}
+      onSubmit={onSubmit}
+    >
+      <ObjectMetaEditor meta={meta} />
       <Simple.FormButtons />
     </Simple.Form>
   )
 }
-
-// @TODO: metaToInitialValues
-// @TODO: ArrayField using Formik Array
-// @TODO: Dynamic Form
-// @TODO: Meta Form
