66dc021f63b4599dc4d183bf9f8f11c9d470097d
Pervez Iqbal <pervezfunctor@gmail.com>
2019-04-14 22:57:50 +0530

    meta io-ts types

---
+++ lib/src/meta/iotsTypes.ts
@@ -0,0 +1,102 @@
+import * as t from 'io-ts'
+import { date } from 'io-ts-types'
+import { props, req } from '../utils'
+
+// tslint:disable typedef
+
+const lit = t.literal
+
+export const NumberRefinements = t.union([
+  req({ kind: lit('integer') }),
+  req({ kind: lit('integer') }),
+  req({ kind: lit('positive') }),
+  req({ kind: lit('negative') }),
+  req({ kind: lit('min'), value: t.number }),
+  req({ kind: lit('max'), value: t.number }),
+])
+
+export type NumberRefinements = t.TypeOf<typeof NumberRefinements>
+
+export const StringRefinements = t.union([
+  req({ kind: lit('email') }),
+  req({ kind: lit('url') }),
+  req({ kind: lit('lowercase') }),
+  req({ kind: lit('uppercase') }),
+  req({ kind: lit('length'), value: t.number }),
+  req({ kind: lit('maxLength'), value: t.number }),
+  req({ kind: lit('minLength'), value: t.number }),
+])
+
+export type StringRefinements = t.TypeOf<typeof StringRefinements>
+
+export const DateRefinements = t.union([
+  req({ kind: lit('min'), value: date }),
+  req({ kind: lit('max'), value: date }),
+])
+
+export type DateRefinements = t.TypeOf<typeof DateRefinements>
+
+export const ArrayRefinements = t.union([
+  req({ kind: lit('maxLength'), value: t.number }),
+  req({ kind: lit('minLength'), value: t.number }),
+])
+
+export type ArrayRefinements = t.TypeOf<typeof ArrayRefinements>
+
+export const SimpleMT = t.union([
+  req({ kind: lit('boolean') }),
+  props(
+    { refinements: t.readonlyArray(NumberRefinements) },
+    { kind: lit('number') },
+  ),
+  props(
+    { refinements: t.readonlyArray(StringRefinements) },
+    { kind: lit('string') },
+  ),
+  props(
+    { refinements: t.readonlyArray(DateRefinements) },
+    { kind: lit('date') },
+  ),
+])
+
+export type SimpleMT = t.TypeOf<typeof SimpleMT>
+
+export const EnumMT = props(
+  { name: t.string },
+  { kind: lit('enum'), values: t.readonlyArray(t.string) },
+)
+
+export type EnumMT = t.TypeOf<typeof EnumMT>
+
+export interface ArrayMT {
+  readonly kind: 'array'
+  readonly of: MT
+  readonly refinements?: ReadonlyArray<ArrayRefinements>
+}
+
+export const ArrayMT: t.Type<ArrayMT> = t.recursion('ArrayMT', () =>
+  props(
+    { refinements: t.readonlyArray(ArrayRefinements) },
+    { kind: lit('array'), of: MT },
+  ),
+)
+
+export interface ObjectMT {
+  readonly kind: 'object'
+  readonly name?: string
+  readonly properties: { readonly [prop: string]: MT }
+}
+
+export const ObjectMT: t.Type<ObjectMT> = t.recursion('ObjectMT', () =>
+  props(
+    { name: t.string },
+    { kind: lit('object'), properties: t.readonly(t.record(t.string, MT)) },
+  ),
+)
+
+export const MT: t.Type<SimpleMT | EnumMT | ArrayMT | ObjectMT> = t.recursion(
+  'MT',
+  () => t.union([SimpleMT, EnumMT, ArrayMT, ObjectMT]),
+)
+
+export type MT = t.TypeOf<typeof MT>
