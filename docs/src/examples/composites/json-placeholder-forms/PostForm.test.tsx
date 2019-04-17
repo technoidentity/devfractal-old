0c2967267a48dafaf177ce13ce76019bd4d49454
swathivalasapalli <swathivalasapalli@gail.com>
2019-04-17 17:01:40 +0530

    snapshot test for PostForm Example

---
+++ docs/src/examples/composites/json-placeholder-forms/PostForm.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import renderer, { ReactTestRenderer } from 'react-test-renderer'
+import { PostForm } from './PostForm'
+
+test('Post Form', () => {
+  const component: ReactTestRenderer = renderer.create(<PostForm />)
+  expect(component.toJSON()).toMatchSnapshot()
+})
