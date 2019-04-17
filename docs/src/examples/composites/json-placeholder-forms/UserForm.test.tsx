d47968d712785f626f48573708a09cca472a6bdf
swathivalasapalli <swathivalasapalli@gail.com>
2019-04-17 17:22:59 +0530

    snapshot test for UserForm Example

---
+++ docs/src/examples/composites/json-placeholder-forms/UserForm.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import renderer, { ReactTestRenderer } from 'react-test-renderer'
+import { UserForm } from './UserForm'
+
+test('UserForm Example', () => {
+  const component: ReactTestRenderer = renderer.create(<UserForm />)
+  expect(component.toJSON()).toMatchSnapshot()
+})
