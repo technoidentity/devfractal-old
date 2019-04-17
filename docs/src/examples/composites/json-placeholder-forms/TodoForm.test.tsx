211306f3e347ec4f72c5a698bc8e3216b81b07ad
swathivalasapalli <swathivalasapalli@gail.com>
2019-04-17 17:03:20 +0530

    snapshot test for TodoForm Example

---
+++ docs/src/examples/composites/json-placeholder-forms/TodoForm.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import renderer, { ReactTestRenderer } from 'react-test-renderer'
+import { TodoForm } from './TodoForm'
+
+test('TodoForm Example', () => {
+  const component: ReactTestRenderer = renderer.create(<TodoForm />)
+  expect(component.toJSON()).toMatchSnapshot()
+})
