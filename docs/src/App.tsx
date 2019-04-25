9ece4539c66554260ae9985b16f2150c43cffd2f
Pervez Iqbal <pervezfunctor@gmail.com>
2019-04-25 22:34:58 +0530

    minor fix

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { HashRouter } from 'react-router-dom'
+import { CleaveComponent } from './examples/composites/CleaveComponent'
 import { Routes } from './Routes'
 import { ScrollToTop } from './ScrollToTop'
 
 export const App: React.FC = () => (
   <HashRouter>
     <ScrollToTop>
+      <CleaveComponent />
       <Routes />
     </ScrollToTop>
   </HashRouter>
