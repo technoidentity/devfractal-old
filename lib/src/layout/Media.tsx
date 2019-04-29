44003b50265da31a03c862a0b3dff2a431be8138
sravanidande <sravanidnd89@gmail.com>
2019-04-29 16:50:55 +0530

    baseUrl based imports for layout

--- lib/src/layout/Media.tsx
+++ lib/src/layout/Media.tsx
@@ -1,5 +1,5 @@
+import { classNamesHelper, Div, Helpers } from 'base'
 import React from 'react'
-import { classNamesHelper, Div, Helpers } from '../lib'
 
 export interface MediaProps
   extends React.HTMLAttributes<HTMLElement>,
