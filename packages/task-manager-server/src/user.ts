300dbf77af7ecc6ae2473c8a90d253a95a7423e4
sravanidande <sravanidnd89@gmail.com>
2019-05-22 15:19:49 +0530

    add userSchema

---
+++ packages/task-manager-server/src/user.ts
@@ -0,0 +1,19 @@
+import * as mongoose from 'mongoose'
+
+export interface IUser extends mongoose.Document {
+  readonly name: string
+  readonly email: string
+  readonly password: string
+  readonly confirmPassword: string
+}
+
+export const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, requires: true },
+  password: { type: String, required: true },
+  confirmPassword: { type: String, required: true },
+})
+
+const User = mongoose.model<IUser>('User', userSchema)
+
+export default User
