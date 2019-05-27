7afd2a32740ca88b933a563eb5a5d55d0fa65409
sravanidande <sravanidnd89@gmail.com>
2019-05-27 18:48:55 +0530

    add validation schema for tasks

--- packages/task-manager-server/src/tasks.ts
+++ packages/task-manager-server/src/tasks.ts
@@ -1,5 +1,5 @@
 import * as mongoose from 'mongoose'
-
+import { addDays } from 'date-fns'
 export interface ITask extends mongoose.Document {
   readonly title: string
   readonly description: string
@@ -9,15 +9,44 @@ export interface ITask extends mongoose.Document {
   readonly scheduled: Date
 }
 
-const taskSchema = new mongoose.Schema({
-  title: { type: String },
-  description: { type: String },
+const dateSchema = {
   startsOn: { type: Date },
   deadline: { type: Date },
   completed: { type: Date },
   scheduled: { type: Date },
+}
+
+const dateValidator = (value: typeof dateSchema) => {
+  return value.startsOn < value.deadline
+}
+
+const taskSchema = new mongoose.Schema({
+  title: { type: String, required: true, minlength: 5, maxlength: 100 },
+  description: { type: String, required: true, minlength: 10, maxlength: 200 },
+  dateInfo: {
+    type: dateSchema,
+    validate: {
+      validator: dateValidator,
+      msg: 'date should be greater than startsOn',
+    },
+  },
 })
 
 const Task = mongoose.model<ITask>('Task', taskSchema)
+
+export const task1 = new Task({
+  title: 'zgsdghjs',
+  description: 'JHEdkiw nzdfhsid jszfhdi',
+  dateInfo: {
+    startsOn: addDays(new Date(), 10),
+    deadline: new Date(),
+    completed: new Date(),
+    scheduled: new Date(),
+  },
+})
+
+const error = task1.validateSync()
+
+console.log(error)
 
 export default Task
