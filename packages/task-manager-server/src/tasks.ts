f5a2072d3dbc1fd49b9731ac181dce887ae4e4a5
sravanidande <sravanidnd89@gmail.com>
2019-05-28 17:39:34 +0530

    add schema validations

--- packages/task-manager-server/src/tasks.ts
+++ packages/task-manager-server/src/tasks.ts
@@ -1,34 +1,71 @@
+import { addDays, compareAsc } from 'date-fns'
 import * as mongoose from 'mongoose'
-import { addDays } from 'date-fns'
-export interface ITask extends mongoose.Document {
-  readonly title: string
-  readonly description: string
-  readonly startsOn: Date
-  readonly deadline: Date
-  readonly completed: Date
-  readonly scheduled: Date
-}
 
 const dateSchema = {
-  startsOn: { type: Date },
+  started: { type: Date },
   deadline: { type: Date },
   completed: { type: Date },
   scheduled: { type: Date },
 }
-
-const dateValidator = (value: typeof dateSchema) => {
-  return value.startsOn < value.deadline
+export interface ITask extends mongoose.Document {
+  readonly title: string
+  readonly description: string
+  readonly dateInfo: {
+    readonly started: Date
+    readonly deadline: Date
+    readonly completed: Date
+    readonly scheduled: Date
+  }
 }
 
-const taskSchema = new mongoose.Schema({
+const currentDate: Date = new Date()
+const dateValidator = (value: ITask['dateInfo']): boolean => {
+  return compareAsc(value.deadline, value.started) >= 0
+}
+
+const dateValidator1 = (value: ITask['dateInfo']): boolean => {
+  return compareAsc(value.deadline, value.scheduled) >= 0
+}
+
+const dateValidator2 = (value: ITask['dateInfo']): boolean => {
+  return compareAsc(value.completed, value.started) >= 0
+}
+
+const dateValidator3 = (value: ITask['dateInfo']): boolean => {
+  return compareAsc(value.started, currentDate) >= 0
+}
+
+const dateValidator4 = (value: ITask['dateInfo']): boolean => {
+  return compareAsc(value.scheduled, value.started) >= 0
+}
+
+const taskSchema = new mongoose.Schema<ITask>({
   title: { type: String, required: true, minlength: 5, maxlength: 100 },
   description: { type: String, required: true, minlength: 10, maxlength: 200 },
   dateInfo: {
     type: dateSchema,
-    validate: {
-      validator: dateValidator,
-      msg: 'date should be greater than startsOn',
-    },
+    validate: [
+      {
+        validator: dateValidator,
+        msg: 'deadline date should be after StartsOn date',
+      },
+      {
+        validator: dateValidator1,
+        msg: 'completed date should be after scheduled date',
+      },
+      {
+        validator: dateValidator2,
+        msg: 'completed date should be after StartsOn date',
+      },
+      {
+        validator: dateValidator3,
+        msg: 'date should be equal to or greater than current date',
+      },
+      {
+        validator: dateValidator4,
+        msg: 'date should be equal to or greater than StartsOn date',
+      },
+    ],
   },
 })
 
@@ -38,10 +75,10 @@ export const task1 = new Task({
   title: 'zgsdghjs',
   description: 'JHEdkiw nzdfhsid jszfhdi',
   dateInfo: {
-    startsOn: addDays(new Date(), 10),
-    deadline: new Date(),
-    completed: new Date(),
-    scheduled: new Date(),
+    started: currentDate,
+    deadline: addDays(currentDate, 15),
+    completed: addDays(currentDate, 10),
+    scheduled: addDays(currentDate, 4),
   },
 })
 
