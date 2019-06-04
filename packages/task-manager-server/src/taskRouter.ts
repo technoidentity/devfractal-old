777ecb4e9fb3c151a22dc2a0c4e5df9cdb9b0632
sravanidande <sravanidnd89@gmail.com>
2019-06-04 10:31:52 +0530

    task api router

---
+++ packages/task-manager-server/src/taskRouter.ts
@@ -0,0 +1,61 @@
+import express, { Request, Response } from 'express'
+import { Task } from './taskSchema'
+const app = express()
+const router = express.Router()
+
+router.get('/', async (req: Request, res: Response) => {
+  try {
+    const tasks = await Task.find().exec()
+    res.send(tasks)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+router.get('/:id', async (req: Request, res: Response) => {
+  try {
+    const one = await Task.findById(req.params.id).exec()
+    console.log(one)
+    res.send(one)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const newTask = new Task(req.body)
+    const result = await newTask.save()
+    res.send(result)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+router.put('/:id', async (req: Request, res: Response) => {
+  try {
+    const task = await Task.findById(req.params.id).exec()
+    if (task !== null) {
+      task.set(req.body)
+      const result = await task.save()
+      res.send(result)
+    } else {
+      console.log('task with the given id has not found')
+    }
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+router.delete('/:id', async (req: Request, res: Response) => {
+  try {
+    const one = await Task.deleteOne({ _id: req.params.id }).exec()
+    res.send(one)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+export default router
+
+// users //
