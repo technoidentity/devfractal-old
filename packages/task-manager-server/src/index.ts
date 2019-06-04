6d93402d311b326fa87ecef784494bb06da0a7e0
sravanidande <sravanidnd89@gmail.com>
2019-06-04 10:53:54 +0530

    minor fix

--- packages/task-manager-server/src/index.ts
+++ packages/task-manager-server/src/index.ts
@@ -4,7 +4,6 @@ import express from 'express'
 import * as mongoose from 'mongoose'
 import tasks from './taskRouter'
 import users from './userRouter'
-import { task1 } from './taskSchema'
 
 const port = 9999
 
@@ -32,57 +31,3 @@ const app = (() => {
 
   return app
 })()
-
-// app.get('/users', async (req: Request, res: Response) => {
-//   try {
-//     const users = await User.find().exec()
-//     res.send(users)
-//   } catch (err) {
-//     res.status(500).send(err)
-//   }
-// })
-
-// app.get('/users/:id', async (req: Request, res: Response) => {
-//   try {
-//     const one = await User.findById(req.params.id).exec()
-//     res.send(one)
-//   } catch (err) {
-//     res.status(500).send(err)
-//   }
-// })
-
-// app.post('/users', async (req: Request, res: Response) => {
-//   try {
-//     const newUser = new User(req.body)
-//     const result = await newUser.save()
-//     res.send(result)
-//   } catch (err) {
-//     res.status(500).send(err)
-//   }
-// })
-
-// app.put('/users/:id', async (req: Request, res: Response) => {
-//   try {
-//     const user = await User.findById({ _id: req.params.id }).exec()
-//     if (user !== null) {
-//       user.set(req.body)
-//       const result = await user.save()
-//       res.send({ foo: 'bar' })
-//     } else {
-//       console.log('task with the given id has not found')
-//     }
-//   } catch (err) {
-//     res.status(500).send(err)
-//   }
-// })
-
-// app.delete('/users/:id', async (req: Request, res: Response) => {
-//   try {
-//     const one = await User.deleteOne({ _id: req.params.id }).exec()
-//     res.send(one)
-//   } catch (err) {
-//     res.status(500).send(err)
-//   }
-// })
-
-console.log(task1)
