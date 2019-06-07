b9521421d4d500de9502790ce5a0e9d13c942789
sravanidande <sravanidnd89@gmail.com>
2019-06-07 15:31:03 +0530

    add complted and pending tasks

--- packages/task-manager-server/src/taskRouter.ts
+++ packages/task-manager-server/src/taskRouter.ts
@@ -12,6 +12,28 @@ router.get('/', async (_: Request, res: Response) => {
   }
 })
 
+router.get('/completed', async (_: Request, res: Response) => {
+  try {
+    const completed = await Task.find({
+      'dateInfo.completed': { $exists: true },
+    }).exec()
+    res.send(completed)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+router.get('/pending', async (_: Request, res: Response) => {
+  try {
+    const pendingTasks = await Task.find({
+      'dateInfo.completed': { $exists: false },
+    }).exec()
+    res.send(pendingTasks)
+  } catch (err) {
+    res.send(500).send(err)
+  }
+})
+
 router.get('/:id', async (req: Request, res: Response) => {
   try {
     const one = await Task.findById(req.params.id).exec()
