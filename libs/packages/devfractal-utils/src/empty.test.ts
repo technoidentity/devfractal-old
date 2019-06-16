3c4a8fa8d64b2445196dfec23f29940014fbf9b2
wasay <mawasay83@gmail.com>
2019-06-16 17:57:54 -0500

    add empty tests

--- libs/packages/devfractal-utils/src/empty.test.ts
+++ libs/packages/devfractal-utils/src/empty.test.ts
@@ -1,5 +1,7 @@
 import {
+  array,
   boolean,
+  Int,
   keyof,
   number,
   readonly,
@@ -7,9 +9,35 @@ import {
   string,
   type,
 } from 'io-ts'
+import { date } from 'io-ts-types'
 import { emptyFromType } from './empty'
 
-test('emptyFromType', () => {
+test('primitive values', () => {
+  expect(emptyFromType(number)).toBe(0)
+  expect(emptyFromType(Int)).toBe(0)
+  expect(emptyFromType(string)).toBe('')
+  expect(emptyFromType(boolean)).toBe(false)
+  expect(emptyFromType(date)).toEqual(expect.any(Date))
+  expect(emptyFromType(keyof({ red: 1, blue: 2, green: 3 }))).toEqual('red')
+})
+
+test('array', () => {
+  expect(emptyFromType(array(number))).toEqual([])
+  expect(emptyFromType(readonlyArray(number))).toEqual([])
+})
+
+test('object', () => {
+  expect(emptyFromType(type({ x: string, y: number }))).toEqual({
+    x: '',
+    y: 0,
+  })
+  expect(emptyFromType(readonly(type({ x: string, y: number })))).toEqual({
+    x: '',
+    y: 0,
+  })
+})
+
+test('nested object and array', () => {
   expect(
     emptyFromType(
       readonly(
@@ -17,7 +45,7 @@ test('emptyFromType', () => {
           i: number,
           s: string,
           b: boolean,
-          // d: date,
+          d: date,
           a: readonlyArray(boolean),
           e: keyof({
             foo: 1,
@@ -31,16 +59,20 @@ test('emptyFromType', () => {
         }),
       ),
     ),
-  ).toMatchInlineSnapshot(`
-Object {
-  "a": Array [],
-  "b": false,
-  "e": "foo",
-  "i": 0,
-  "o": Object {
-    "fizz": Array [],
-  },
-  "s": "",
-}
-`)
+  ).toMatchInlineSnapshot(
+    { d: expect.any(Date) },
+    `
+    Object {
+      "a": Array [],
+      "b": false,
+      "d": Any<Date>,
+      "e": "foo",
+      "i": 0,
+      "o": Object {
+        "fizz": Array [],
+      },
+      "s": "",
+    }
+  `,
+  )
 })
--- libs/packages/devfractal-utils/src/empty.ts
+++ libs/packages/devfractal-utils/src/empty.ts
@@ -7,95 +7,109 @@ import {
   NumberType,
   Props,
   ReadonlyArrayType,
-  ReadonlyC,
   ReadonlyType,
   StringType,
   TypeC,
   TypeOf,
 } from 'io-ts'
 import { DateType } from 'io-ts-types'
-import t from 'tcomb'
-import { invariant, warning } from './assertions'
-import { nop, keys } from './common'
+import tcomb from 'tcomb'
+import { warning } from './assertions'
+import { buildObject, nop } from './common'
 
-const emptyFromPrimitiveValue: (v: unknown) => any = v => {
-  if (t.Number.is(v)) {
+function emptyFromPrimitiveValue(v: unknown): any {
+  if (tcomb.Number.is(v)) {
     return 0
   }
-  if (t.String.is(v)) {
+  if (tcomb.String.is(v)) {
     return ''
   }
-  if (t.Boolean.is(v)) {
+  if (tcomb.Boolean.is(v)) {
     return false
   }
-  if (t.Nil.is(v)) {
+  if (tcomb.Nil.is(v)) {
     return v
   }
-  if (t.Date.is(v)) {
+  if (tcomb.Date.is(v)) {
     return new Date()
   }
-  if (t.Function.is(v)) {
+  if (tcomb.Function.is(v)) {
     return nop
   }
   warning(false, `Unsupported value ${v}`)
 }
 
-const emptyFromObjectValue: <T extends Object>(value: T) => T = value => {
-  const draft: any = {}
-  for (const k of keys(value)) {
-    draft[k] = emptyFromPrimitiveValue(value[k])
-  }
-  return draft
+function emptyFromObjectValue<T extends Object>(value: T): T {
+  return buildObject(value, (_, v) => emptyFromPrimitiveValue(v))
 }
 
 export const emptyFromValue: <T>(value: T) => T = value => {
-  if (t.Array.is(value)) {
+  if (tcomb.Array.is(value)) {
     // may be if array contains object and such, use empty one those?
     return []
   }
-  if (t.Object.is(value)) {
+  if (tcomb.Object.is(value)) {
     return emptyFromObjectValue(value)
   }
   return emptyFromPrimitiveValue(value)
 }
 
-export function emptyFromType<T extends Props>(
-  typeValue: ReadonlyC<TypeC<T>>,
-  id?: keyof T,
-): TypeOf<typeof typeValue> {
-  const props: T = typeValue.type.props
-  const draft: any = {}
-
-  keys(props).forEach(prop => {
-    if (prop !== id) {
-      if (props[prop] instanceof NumberType || props[prop].name === 'Int') {
-        draft[prop] = 0
-      } else if (props[prop] instanceof StringType) {
-        draft[prop] = ''
-      } else if (props[prop] instanceof BooleanType) {
-        draft[prop] = false
-        // @TODO: handle array and object
-      } else if (props[prop] instanceof DateType) {
-        draft[prop] = new Date()
-      } else {
-        const v: Mixed = props[prop]
-        invariant(
-          !(v instanceof InterfaceType || v instanceof ArrayType),
-          `Everything must be readonly: ${v.name}`,
-        )
-        if (v instanceof KeyofType) {
-          draft[prop] = Object.keys(v.keys)[0]
-        } else if (v instanceof ReadonlyType) {
-          draft[prop] = emptyFromType(v, undefined)
-        } else if (v instanceof ReadonlyArrayType) {
-          draft[prop] = []
-        } else {
-          throw new Error(`Unknown type ${props[prop]}`)
-        }
-      }
-    }
-  })
-
-  // warnProps(typeValue, value)
-  return draft
+function emptyFromPrimitive<T extends Mixed>(typeValue: T): TypeOf<T> {
+  if (typeValue.name === 'Int' || typeValue instanceof NumberType) {
+    return 0
+  }
+  if (typeValue instanceof StringType) {
+    return ''
+  }
+  if (typeValue instanceof BooleanType) {
+    return false
+  }
+  if (typeValue instanceof DateType) {
+    return new Date()
+  }
+  if (typeValue instanceof KeyofType) {
+    return Object.keys(typeValue.keys)[0]
+  }
+  throw new Error(`Unsupported type: ${typeValue.name}`)
 }
+
+function emptyFromObject<T extends Props>(
+  typeValue: TypeC<T>,
+): TypeOf<TypeC<T>> {
+  return buildObject(typeValue.props, emptyFromType)
+}
+
+export function emptyFromType<T extends Mixed>(typeValue: T): TypeOf<T> {
+  if (typeValue instanceof ReadonlyType) {
+    return emptyFromObject(typeValue.type)
+  }
+  if (typeValue instanceof InterfaceType) {
+    return emptyFromObject(typeValue)
+  }
+  if (
+    typeValue instanceof ReadonlyArrayType ||
+    typeValue instanceof ArrayType
+  ) {
+    return []
+  }
+  return emptyFromPrimitive(typeValue)
+}
+
+// console.log(
+//   emptyFromType(
+//     t.readonly(
+//       t.type({
+//         x: t.number,
+//         d: date,
+//         e: t.keyof({ foo: 0, bar: 0 }),
+//         a: t.readonlyArray(t.string),
+//         o: t.readonly(t.type({ x: t.number, y: t.number })),
+//         o2: t.readonly(
+//           t.type({
+//             fizz: t.readonly(t.type({ buzz: t.boolean })),
+//           }),
+//         ),
+//       }),
+//     ),
+//   ),
+// )
