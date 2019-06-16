6b33d80672fb7b8ef01d1183bc7b5111e3c8e958
wasay <mawasay83@gmail.com>
2019-06-16 17:39:41 -0500

    fix meta to json schema

--- libs/packages/devfractal-meta-core/src/metaToJsonSchema.ts
+++ libs/packages/devfractal-meta-core/src/metaToJsonSchema.ts
@@ -1,111 +1,113 @@
-// import { buildObject, keys } from '@technoidentity/utils'
-// import { JSONSchema7, JSONSchema7Definition } from 'json-schema'
-// import {
-//   ArrayRefinements,
-//   Mixed,
-//   NumberRefinements,
-//   StringRefinements,
-// } from './meta'
+import { buildObject, keys } from '@technoidentity/utils'
+import { JSONSchema7, JSONSchema7Definition } from 'json-schema'
+import {
+  ArrayRefinements,
+  Mixed,
+  NumberRefinements,
+  StringRefinements,
+} from './meta'
 
-// // tslint:disable no-object-mutation
+// tslint:disable no-object-mutation
 
-// function toJsonSchemaNumberRefinements(r: NumberRefinements): JSONSchema7 {
-//   const schema: JSONSchema7 = {}
+function toJsonSchemaNumberRefinements(r: NumberRefinements): JSONSchema7 {
+  const schema: JSONSchema7 = {}
 
-//   if (r.integer) {
-//     schema.type = 'integer'
-//   }
-//   if (r.positive) {
-//     schema.minimum = 0
-//   }
-//   if (r.negative) {
-//     schema.exclusiveMaximum = 0
-//   }
-//   if (r.max) {
-//     schema.maximum = r.max
-//   }
-//   if (r.min) {
-//     schema.minimum = r.min
-//   }
+  if (r.integer) {
+    schema.type = 'integer'
+  }
+  if (r.sign === 'positive') {
+    schema.minimum = 0
+  }
+  if (r.sign === 'negative') {
+    schema.exclusiveMaximum = 0
+  }
+  if (r.max) {
+    schema.maximum = r.max
+  }
+  if (r.min) {
+    schema.minimum = r.min
+  }
 
-//   return schema
-// }
+  return schema
+}
 
-// function toJsonSchemaStringRefinements(r: StringRefinements): JSONSchema7 {
-//   const schema: JSONSchema7 = {}
+function toJsonSchemaStringRefinements(r: StringRefinements): JSONSchema7 {
+  const schema: JSONSchema7 = {}
 
-//   if (r.email) {
-//     schema.format = 'email'
-//   }
-//   if (r.url) {
-//     schema.format = 'uri'
-//   }
-//   if (r.maxStringLength) {
-//     schema.maxLength = r.maxStringLength
-//   }
-//   if (r.minStringLength) {
-//     schema.minLength = r.minStringLength
-//   }
-//   if (r.length) {
-//     schema.maxLength = r.length
-//   }
+  if (r.base === 'email') {
+    schema.format = 'email'
+  }
+  if (r.base === 'url') {
+    schema.format = 'uri'
+  }
+  if(r.strLength) {
+    if (r.strLength.max) {
+        schema.maxLength = r.strLength.max
+    }
+    if (r.strLength.min) {
+        schema.minLength = r.strLength.min
+    }
+    if (r.strLength.fixed) {
+        schema.maxLength = r.strLength.fixed
+    }
+  }
 
-//   return schema
-// }
+  return schema
+}
 
-// function toJsonSchemaArrayRefinements(r: ArrayRefinements): JSONSchema7 {
-//   const schema: JSONSchema7 = {}
+function toJsonSchemaArrayRefinements(r: ArrayRefinements): JSONSchema7 {
+  const schema: JSONSchema7 = {}
 
-//   if (r.maxArrayLength) {
-//     schema.maxItems = r.maxArrayLength
-//   }
-//   if (r.minArrayLength) {
-//     schema.minItems = r.minArrayLength
-//   }
+  if (r.maxArrayLength) {
+    schema.maxItems = r.maxArrayLength
+  }
+  if (r.minArrayLength) {
+    schema.minItems = r.minArrayLength
+  }
 
-//   return schema
-// }
+  return schema
+}
 
-// export function metaToJsonSchema(meta: Mixed): JSONSchema7Definition {
-//   switch (meta.kind) {
-//     case 'number':
-//       return meta.refinements
-//         ? { type: 'number', ...toJsonSchemaNumberRefinements(meta.refinements) }
-//         : { type: 'number' }
+export function metaToJsonSchema(meta: Mixed): JSONSchema7Definition {
+  switch (meta.kind) {
+    case 'number':
+      return meta.refinements
+        ? { type: 'number', ...toJsonSchemaNumberRefinements(meta.refinements) }
+        : { type: 'number' }
 
-//     case 'string':
-//       return meta.refinements
-//         ? { type: 'string', ...toJsonSchemaStringRefinements(meta.refinements) }
-//         : { type: 'string' }
+    case 'string':
+      return meta.refinements
+        ? { type: 'string', ...toJsonSchemaStringRefinements(meta.refinements) }
+        : { type: 'string' }
 
-//     case 'boolean':
-//       return { type: 'boolean' }
+    case 'boolean':
+      return { type: 'boolean' }
 
-//     case 'date':
-//       return { type: 'string', format: 'date' }
+    case 'date':
+      return { type: 'string', format: 'date' }
 
-//     case 'enum':
-//       return { type: 'string', enum: [...meta.values] }
+    case 'enum':
+      return { type: 'string', enum: [...meta.values] }
 
-//     case 'array':
-//       return meta.refinements
-//         ? {
-//             type: 'array',
-//             items: metaToJsonSchema(meta.of),
-//             ...toJsonSchemaArrayRefinements(meta.refinements),
-//           }
-//         : { type: 'array', items: metaToJsonSchema(meta.of) }
+    case 'array':
+      return meta.refinements
+        ? {
+            type: 'array',
+            items: metaToJsonSchema(meta.of),
+            ...toJsonSchemaArrayRefinements(meta.refinements),
+          }
+        : { type: 'array', items: metaToJsonSchema(meta.of) }
 
-//     case 'object':
-//       const requiredProps: Array<string | number> = keys(
-//         meta.properties,
-//       ).filter(k => !meta.properties[k].optional)
+    case 'object':
+      const requiredProps: Array<string | number> = keys(
+        meta.properties,
+      ).filter(k => !meta.properties[k].optional)
 
-//       return {
-//         type: 'object',
-//         properties: buildObject(meta.properties, metaToJsonSchema),
-//         required: requiredProps as string[],
-//       }
-//   }
-//   return true
-// }
+      return {
+        type: 'object',
+        properties: buildObject(meta.properties, metaToJsonSchema),
+        required: requiredProps as string[],
+      }
+  }
+  return true
+}
