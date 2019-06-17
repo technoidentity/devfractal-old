65dd1b05bc05410695558924ae11a64bb7a57a1f
sravanidande <sravanidnd89@gmail.com>
2019-06-17 16:25:35 +0530

    fix EditTaskForm

--- packages/task-manager/src/App.tsx
+++ packages/task-manager/src/App.tsx
@@ -1,12 +1,16 @@
 import axios from 'axios'
 import React from 'react'
 import {
+  BrowserRouter as Router,
+  Route,
   RouteComponentProps,
 } from 'react-router-dom'
 import { EditTaskForm } from './EditTaskForm'
 import { User } from './SignIn'
-import { TaskForm, TaskValues } from './TaskForm'
+import { TaskForm } from './TaskForm'
 import { TaskList } from './TaskList'
+import { getOneTask, TaskValues } from './TaskValues'
+import { getOneUser } from './user'
 
 const updateData = async (id: string, data: TaskValues) => {
   const result = await axios.put(`http://localhost:9999/tasks/${id}`, data)
@@ -21,12 +25,12 @@ const postUser = (data: User) => {
     .catch(err => console.log(err))
 }
 
-const getOneUser = async () => {
-  return axios
-    .get('http://localhost:9999/tasks/5cf4e807c6b9b813825062a4')
-    .then(res => res.data)
-    .catch(err => console.log(err.message))
-}
+// const getOneUser = async () => {
+//   return axios
+//     .get('http://localhost:9999/tasks/5cf4e807c6b9b813825062a4')
+//     .then(res => res.data)
+//     .catch(err => console.log(err.message))
+// }
 
 // export const App = () => {
 //   return (
@@ -44,7 +48,7 @@ const getOneUser = async () => {
 //   )
 // }
 
-const postData = (data: TaskValues) => {
+export const postData = (data: TaskValues) => {
   return axios.post('http://localhost:9999/tasks', data).then(res => res.data)
 }
 
@@ -57,6 +61,12 @@ export const CreateForm: React.FC<RouteComponentProps> = ({ history }) => {
     />
   )
 }
+
+export const EditTask: React.FC<
+  RouteComponentProps<{ readonly id: string }>
+> = ({ match, history }) => (
+  <EditTaskForm id={match.params.id} onEdit={() => history.push('/')} />
+)
 
 const allList = async () => {
   const result = await axios.get('http://localhost:9999/tasks')
@@ -122,14 +132,15 @@ export const Tasks = () => {
   return <h1>is Loading....</h1>
 }
 
-// export const App: React.FC = () => {
-//   return (
-//     <Router>
-//       <Route exact path="/" component={Tasks} />
-//       <Route path="/add" component={CreateForm} />
-//     </Router>
-//   )
-// }
+export const App: React.FC = () => {
+  return (
+    <Router>
+      <Route exact path="/" component={Tasks} />
+      <Route path="/add" component={CreateForm} />
+      <Route path="/edit/:id" component={EditTask} />
+    </Router>
+  )
+}
 
 const initialValuesOne = {
   title: 'programming',
@@ -142,6 +153,10 @@ const initialValuesOne = {
   },
 }
 
-export const App: React.FC = () => {
-  return <EditTaskForm id="5cf4ed226f2f961489f91e51" />
-}
+getOneUser()
+
+// export const App: React.FC = () => {
+//   return <EditTaskForm id="5cf4ed226f2f961489f91e51" />
+// }
+
+getOneTask()
