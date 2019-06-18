372fb97d46ed589d3cf19289cb13434f1a4c4c26
Pervez Iqbal <pervezfunctor@gmail.com>
2019-06-18 23:40:58 +0530

    use devfractal req, opt for io-ts types

--- packages/task-manager/src/types.ts
+++ packages/task-manager/src/types.ts
@@ -1,53 +1,43 @@
-import axios from 'axios'
-import * as t from 'io-ts'
-import * as tp from 'io-ts-promise'
+import { intersection, string, TypeOf } from 'io-ts'
 import { DateFromISOString } from 'io-ts-types'
+import { opt, req } from 'technoidentity-devfractal'
 
-export const dateInfoReq = t.type({
+const dateInfoRequired = req({
   deadline: DateFromISOString,
   scheduled: DateFromISOString,
 })
 
-export const dateInfoPartial = t.readonly(
-  t.partial({
-    started: DateFromISOString,
-    completed: DateFromISOString,
-  }),
-)
+const dateInfoPartial = opt({
+  started: DateFromISOString,
+  completed: DateFromISOString,
+})
 
-export const taskPartial = t.readonly(
-  t.partial({
-    _id: t.string,
-  }),
-)
+const taskPartial = req({ _id: string })
 
-export const taskReq = t.readonly(
-  t.type({
-    title: t.string,
-    description: t.string,
-    dateInfo: t.intersection([dateInfoReq, dateInfoPartial]),
-  }),
-)
+const taskRequired = req({
+  title: string,
+  description: string,
+  dateInfo: intersection([dateInfoRequired, dateInfoPartial]),
+})
 
-export const TaskRT = t.intersection([taskPartial, taskReq])
-export type Task = t.TypeOf<typeof TaskRT>
+export const TaskRT = intersection([taskPartial, taskRequired])
 
-export const getOneTask = () => {
-  axios
-    .get('http://localhost:9999/tasks/5cf4c3fe67fe360ff25b2014')
-    .then(res => tp.decode(TaskRT, res.data))
-    .then(typeSafeData =>
-      console.log(
-        `${typeSafeData.title} which is scheduled on ${typeSafeData.dateInfo.scheduled}`,
-      ),
-    )
-    .catch(err => console.log(err))
-}
+export type Task = TypeOf<typeof TaskRT>
 
-export const getAllTasks = () => {
-  axios
-    .get('http://localhost:9999/tasks')
-    .then(res => tp.decode(TaskRT, res.data))
-    .then(typeSafeData => console.log(typeSafeData))
-    .catch(err => console.log(err.message))
-}
+// export const getOneTask = () =>
+//   axios
+//     .get('http://localhost:9999/tasks/5cf4c3fe67fe360ff25b2014')
+//     .then(res => tp.decode(TaskRT, res.data))
+//     .then(data =>
+//       console.log(
+//         `${data.title} which is scheduled on ${data.dateInfo.scheduled}`,
+//       ),
+//     )
+//     .catch(err => console.log(err))
+
+// export const getAllTasks = () =>
+//   axios
+//     .get('http://localhost:9999/tasks')
+//     .then(res => tp.decode(TaskRT, res.data))
+//     .then(typeSafeData => console.log(typeSafeData))
+//     .catch(err => console.log(err.message))
