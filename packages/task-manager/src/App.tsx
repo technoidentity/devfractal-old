15c7b4f2591d32f507dc2f98eae1f5e090f426c9
sravanidande <sravanidnd89@gmail.com>
2019-06-18 18:18:21 +0530

    seperate routes from app.tsx

--- packages/task-manager/src/App.tsx
+++ packages/task-manager/src/App.tsx
@@ -1,37 +1,6 @@
-import axios from 'axios'
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  RouteComponentProps,
-} from 'react-router-dom'
-import { Section } from 'technoidentity-devfractal'
-import { EditTaskForm } from './EditTaskForm'
-import { User } from './SignIn'
-import { TaskForm } from './TaskForm'
-import { TaskList } from './TaskList'
-import { allList, completedList, pendingList, postData } from './tasksAPI'
-import { getOneTask, Task } from './types'
-
-const updateData = async (id: string, data: Task) => {
-  const result = await axios.put(`http://localhost:9999/tasks/${id}`, data)
-  return result.data
-}
-
-const postUser = (data: User) => {
-  axios
-    .post('http://localhost:9999/users', data)
-
-    .then(res => console.log(res.data))
-    .catch(err => console.log(err))
-}
-
-// const getOneUser = async () => {
-//   return axios
-//     .get('http://localhost:9999/tasks/5cf4e807c6b9b813825062a4')
-//     .then(res => res.data)
-//     .catch(err => console.log(err.message))
-// }
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { CreateFormRoute, EditTaskFormRoute, TaskListRoute } from './Routes'
 
 // export const App = () => {
 //   return (
@@ -49,79 +18,6 @@ const postUser = (data: User) => {
 //   )
 // }
 
-export const CreateFormRoute: React.FC<RouteComponentProps> = ({ history }) => {
-  return (
-    <Section>
-      <h1 className="title has-text-centered">Create Task</h1>
-      <TaskForm
-        onCreate={data => {
-          postData(data).then(() => history.push('/'))
-        }}
-      />
-    </Section>
-  )
-}
-
-export const EditTaskFormRoute: React.FC<
-  RouteComponentProps<{ readonly id: string }>
-> = ({ match, history }) => (
-  <section className="section">
-    <h1 className="title has-text-centered">Edit Task</h1>
-    <EditTaskForm id={match.params.id} onEdit={() => history.push('/')} />
-  </section>
-)
-
-type ListType = 'all' | 'completed' | 'pending'
-
-export const TaskListRoute = () => {
-  const [type, setType] = React.useState<ListType>('all')
-  const [data, setData] = React.useState<ReadonlyArray<Task> | undefined>(
-    undefined,
-  )
-  const [error, setError] = React.useState<Error | undefined>(undefined)
-
-  React.useEffect(() => {
-    if (type === 'all') {
-      allList()
-        .then(setData)
-        .catch(setError)
-    }
-    if (type === 'completed') {
-      completedList()
-        .then(data => {
-          setData(data)
-        })
-        .catch(setError)
-    }
-    if (type === 'pending') {
-      pendingList()
-        .then(setData)
-        .catch(setError)
-    }
-  }, [type])
-
-  if (error) {
-    return <h1>{error.message}</h1>
-  }
-
-  if (data) {
-    return (
-      <>
-        <TaskList
-          taskList={data}
-          onCompleted={() => {
-            setType('completed')
-          }}
-          onPending={() => {
-            setType('pending')
-          }}
-        />
-      </>
-    )
-  }
-  return <h1>is Loading....</h1>
-}
-
 export const App: React.FC = () => {
   return (
     <Router>
@@ -131,16 +27,3 @@ export const App: React.FC = () => {
     </Router>
   )
 }
-
-const initialValuesOne = {
-  title: 'programming',
-  description: 'learn functional programming to write accurate code',
-  dateInfo: {
-    started: new Date(),
-    deadline: new Date(),
-    scheduled: new Date(),
-    completed: new Date(),
-  },
-}
-
-getOneTask()
