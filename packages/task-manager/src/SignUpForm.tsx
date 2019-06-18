d8f0ffe1d024a7605001bed2bb0afc77034c24fe
Pervez Iqbal <pervezfunctor@gmail.com>
2019-06-19 00:01:17 +0530

    minor changes to SignUpForm

--- packages/task-manager/src/SignUpForm.tsx
+++ packages/task-manager/src/SignUpForm.tsx
@@ -2,21 +2,21 @@ import { Field, Form, Formik, FormikProps } from 'formik'
 import React from 'react'
 import Yup from 'yup'
 
-export interface User {
+export interface SignUpValues {
   readonly name: string
   readonly email: string
   readonly password: string
   readonly confirmPassword: string
 }
 
-export const initialValues = {
+const initialValues: SignUpValues = {
   name: '',
   email: '',
   password: '',
   confirmPassword: '',
 }
 
-const validationSchema = Yup.object().shape({
+const schema = Yup.object().shape({
   name: Yup.string()
     .required()
     .min(2)
@@ -28,37 +28,36 @@ const validationSchema = Yup.object().shape({
   confirmPassword: Yup.string().required(),
 })
 
-const InnerSignUpForm: React.FC<FormikProps<User>> = () => {
-  return (
-    <Form>
-      <label>Name</label>
-      <Field name="name" type="text" />
-      <br />
-      <label>Email</label>
-      <Field name="email" type="email" />
-      <br />
-      <label>Password</label>
-      <Field name="password" type="password" />
-      <br />
-      <label>Confirm Password</label>
-      <Field name="confirmPassword" type="password" />
-      <br />
-      <button type="submit">Submit</button>
-    </Form>
-  )
+const InnerSignUpForm: React.FC<FormikProps<SignUpValues>> = () => (
+  <Form>
+    <label>Name</label>
+    <Field name="name" type="text" />
+    <br />
+    <label>Email</label>
+    <Field name="email" type="email" />
+    <br />
+    <label>Password</label>
+    <Field name="password" type="password" />
+    <br />
+    <label>Confirm Password</label>
+    <Field name="confirmPassword" type="password" />
+    <br />
+    <button type="submit">Submit</button>
+  </Form>
+)
+
+export interface SignUpFormProps {
+  onUserSubmit(values: SignUpValues): void
 }
 
-export interface SignUpFormValues {
-  onUserSubmit(values: User): void
-}
-
-export const SignUpForm: React.FC<SignUpFormValues> = ({ onUserSubmit }) => (
+export const SignUpForm: React.FC<SignUpFormProps> = ({ onUserSubmit }) => (
   <Formik
     initialValues={initialValues}
-    validationSchema={validationSchema}
+    validationSchema={schema}
     onSubmit={(values, actions) => {
       onUserSubmit(values)
       actions.setSubmitting(false)
+      // @TODO: handle the error case
     }}
     component={InnerSignUpForm}
   />
