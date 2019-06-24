bca41164ea9a788203a3a1a9bc8d03b71a0df495
sravanidande <sravanidnd89@gmail.com>
2019-06-24 18:46:01 +0530

    check user is authorized

--- packages/task-manager-server/src/sessionRouter.ts
+++ packages/task-manager-server/src/sessionRouter.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express'
+import { isUserValid } from './index'
 const router = express.Router()
 
 router.get('/', (req: Request, res: Response) => {
@@ -7,17 +8,24 @@ router.get('/', (req: Request, res: Response) => {
 
 router.post('/', async (req: Request, res: Response) => {
   try {
-    ;(req.session as any).loggedIn = true
-    console.log(req.session)
-    return res.sendStatus(201)
+    const userValid = await isUserValid(req.body.name, req.body.password)
+    if (userValid) {
+      ;(req.session as any).loggedIn = true
+
+      return res.sendStatus(201)
+    } else {
+      return res.sendStatus(400)
+    }
   } catch (err) {
     return res.sendStatus(401)
   }
 })
 
-// router.delete('/', async (req: Request, res: Response) => {
-//   ;(req.session as any).loggedIn = undefined
-//   return res.sendStatus(204)
-// })
+router.delete('/', (req: Request, res: Response) => {
+  ;(req.session as any).loggedIn = false
+  console.log(req.session)
+
+  res.sendStatus(200)
+})
 
 export default router
