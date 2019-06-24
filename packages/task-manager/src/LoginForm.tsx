edae9f379f797d2812c7f3ee759fe174389af079
Pervez Iqbal <pervezfunctor@gmail.com>
2019-06-24 10:56:15 +0530

    rename email to password

--- packages/task-manager/src/LoginForm.tsx
+++ packages/task-manager/src/LoginForm.tsx
@@ -4,26 +4,23 @@ import * as yup from 'yup'
 
 interface LoginValues {
   readonly name: string
-  readonly email: string
+  readonly password: string
 }
 
 const schema = yup.object().shape({
   name: yup.string().required(),
 
-  email: yup
-    .string()
-    .email()
-    .required(),
+  password: yup.string().required(),
 })
 
-export const initialValues: LoginValues = { name: '', email: '' }
+export const initialValues: LoginValues = { name: '', password: '' }
 
 const InnerLoginForm: React.FC<FormikProps<LoginValues>> = () => (
   <Form>
     <label>Name</label>
     <Field name="name" type="text" />
-    <label>Email</label>
-    <Field name="email" type="email" />
+    <label>Password</label>
+    <Field name="password" type="password" />
     <button type="submit">Submit</button>
   </Form>
 )
