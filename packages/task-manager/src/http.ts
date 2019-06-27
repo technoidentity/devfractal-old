e9fc57208f28cce873a1a5d74452fe3137c3b5de
Pervez Iqbal <pervezfunctor@gmail.com>
2019-06-27 18:22:13 +0530

    improve http

---
+++ packages/task-manager/src/http.ts
@@ -0,0 +1,101 @@
+import ax, { AxiosInstance, AxiosRequestConfig } from 'axios'
+import { Type } from 'io-ts'
+import { decode } from 'io-ts-promise'
+import { stringify } from 'query-string'
+import { Array, String } from 'tcomb'
+import { chop, invariant, warning } from 'technoidentity-devfractal'
+
+export interface MethodArgs {
+  readonly resource?: string
+  readonly path?: string | ReadonlyArray<string>
+  readonly query?: string | Record<string, any>
+}
+
+function noSlashInvariant(s: string): void {
+  invariant(String.is(s))
+
+  warning(!s.includes('/'), `${s} should not contain "/"`)
+}
+export interface RequestConfig extends AxiosRequestConfig {
+  readonly baseURL: string
+}
+
+function buildResource(resource?: string): string {
+  if (resource !== undefined) {
+    noSlashInvariant(resource)
+    return `/${resource}`
+  }
+  return ''
+}
+
+function buildPath(path?: string | ReadonlyArray<string>): string {
+  if (Array.is(path)) {
+    path.forEach(noSlashInvariant)
+    return `/${path.join('/')}`
+  }
+
+  if (String.is(path)) {
+    noSlashInvariant(path)
+    return `/${path}`
+  }
+
+  return ''
+}
+
+function buildQueryString(query?: string | Object): string {
+  return query === undefined
+    ? ''
+    : `?${String.is(query) ? query : stringify(query)}`
+}
+
+function buildUrl(options: MethodArgs): string {
+  return `${buildResource(options.resource)}${buildPath(
+    options.path,
+  )}${buildQueryString(options.query)}`
+}
+
+// tslint:disable-next-line: typedef
+export function http(config: RequestConfig) {
+  const axios: AxiosInstance = ax.create({
+    ...config,
+    baseURL: chop(config.baseURL, '/'),
+  })
+
+  async function get<I, A>(
+    options: MethodArgs,
+    type: Type<A, A, I>,
+  ): Promise<A> {
+    return axios
+      .get<I>(buildUrl(options))
+      .then(res => res.data)
+      .then(decode(type))
+  }
+
+  async function post<I, A>(
+    options: Omit<MethodArgs, 'query'>,
+    data: I,
+    type: Type<A, A, I>,
+  ): Promise<A> {
+    return axios
+      .post<I>(buildUrl(options), data)
+      .then(res => res.data)
+      .then(decode(type))
+  }
+
+  async function put<I, A>(
+    options: Omit<MethodArgs, 'query'>,
+    data: I,
+    type: Type<A, A, I>,
+  ): Promise<A> {
+    return axios
+      .put<I>(buildUrl(options), data)
+      .then(res => res.data)
+      .then(decode(type))
+  }
+
+  async function del(options: Omit<MethodArgs, 'query'>): Promise<void> {
+    return axios.delete(buildUrl(options))
+  }
+
+  return { get, del, put, post }
+}
