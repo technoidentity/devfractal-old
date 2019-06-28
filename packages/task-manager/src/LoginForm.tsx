c3ae226034be6f3d5eaa30c91a089eea90bc4837
sravanidande <sravanidnd89@gmail.com>
2019-06-28 11:19:53 +0530

    make form center using bulma

--- packages/task-manager/src/LoginForm.tsx
+++ packages/task-manager/src/LoginForm.tsx
@@ -16,46 +16,50 @@ const schema = yup.object().shape({
 export const initialValues: LoginValues = { name: '', password: '' }
 
 const InnerLoginForm: React.FC<FormikProps<LoginValues>> = () => (
-  <Form>
-    <Section>
-      <h1 className="title has-text-centered">Login</h1>
-      <div className="field">
-        <label className="label">Username</label>
-        <div className="control">
-          <Field
-            type="text"
-            name="name"
-            className="input "
-            placeholder="Name"
-          />
-        </div>
-        <div className="help is-danger">
-          <ErrorMessage name="name" />
-        </div>
-      </div>
-      <div className="field">
-        <label className="label">Password</label>
-        <div className="control">
-          <Field
-            type="password"
-            name="password"
-            className="input "
-            placeholder="Password"
-          />
-        </div>
-        <div className="help is-danger">
-          <ErrorMessage name="password" />
-        </div>
-      </div>
-      <div className="field is-grouped is-grouped-centered">
-        <p className="control">
-          <button className="button is-primary" type="submit">
-            Login
-          </button>
-        </p>
-      </div>
-    </Section>
-  </Form>
+  <div className="columns is-centered ">
+    <div className="column is-half">
+      <Form>
+        <Section>
+          <h1 className="title has-text-centered">Login</h1>
+          <div className="field">
+            <label className="label">Username</label>
+            <div className="control">
+              <Field
+                type="text"
+                name="name"
+                className="input "
+                placeholder="Name"
+              />
+            </div>
+            <div className="help is-danger">
+              <ErrorMessage name="name" />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Password</label>
+            <div className="control">
+              <Field
+                type="password"
+                name="password"
+                className="input "
+                placeholder="Password"
+              />
+            </div>
+            <div className="help is-danger">
+              <ErrorMessage name="password" />
+            </div>
+          </div>
+          <div className="field is-grouped is-grouped-centered">
+            <p className="control">
+              <button className="button is-primary" type="submit">
+                Login
+              </button>
+            </p>
+          </div>
+        </Section>
+      </Form>
+    </div>
+  </div>
 )
 
 export interface LoginFormProps {
