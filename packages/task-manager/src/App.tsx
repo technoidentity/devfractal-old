55faed942cb21edbc1445e8d7ae63e2118232f73
sravanidande <sravanidnd89@gmail.com>
2019-06-28 11:25:19 +0530

    add route for signup form

--- packages/task-manager/src/App.tsx
+++ packages/task-manager/src/App.tsx
@@ -4,6 +4,7 @@ import {
   CreateTaskRoute,
   EditTaskRoute,
   LoginRoute,
+  SignupFormRoute,
   TaskListRoute,
 } from './Routes'
 
@@ -25,9 +26,42 @@ import {
 
 export const App: React.FC = () => (
   <Router>
-    <Route exact path="/" component={LoginRoute} />
+    <Route exact path="/" component={SignupFormRoute} />
+    <Route exact path="/login" component={LoginRoute} />
     <Route exact path="/tasks" component={TaskListRoute} />
     <Route path="/add" component={CreateTaskRoute} />
     <Route path="/edit/:id" component={EditTaskRoute} />
   </Router>
 )
+
+// export const App: React.FC = () => {
+//   const postUser = async (data: any) => {
+//     return axios
+//       .post('http://localhost:9999/users', data)
+//       .then(data => console.log(data))
+//       .catch(err => console.log({ error: err.response.data.message }))
+//   }
+//   return <LoginForm onLogin={postUser} />
+// }
+
+// export const App: React.FC = () => {
+//   const [serverError, setServerError] = React.useState<string | undefined>(
+//     undefined,
+//   )
+//   const postTask = async (data: any) => {
+//     return await axios
+//       .post('http://localhost:9999/tasks', data)
+//       .then(data => setServerError(undefined))
+//       .catch(err => setServerError(err.response.data.message))
+//   }
+//   return (
+//     <>
+//       {serverError && (
+//         <article className="message is-danger">
+//           <div className="message-body">{serverError}</div>
+//         </article>
+//       )}
+//       <TaskForm onSubmit={postTask} />
+//     </>
+//   )
+// }
