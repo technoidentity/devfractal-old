10ee852307c7f58338122355ab400dfb43316a1c
Pervez Iqbal <pervezfunctor@gmail.com>
2019-06-30 17:09:06 +0530

    use GET from TaskListRoute

--- packages/task-manager/src/pages/TaskListRoute.tsx
+++ packages/task-manager/src/pages/TaskListRoute.tsx
@@ -1,30 +1,25 @@
 import React from 'react'
 import { getTasks, sessionApi, TaskFilter } from '../api'
-import { ErrorView, Loading, ServerError, useGET, useSubmit } from '../utils'
+import { GET, ServerError, useSubmit } from '../utils'
 import { TaskListView } from '../views'
 
 export const TaskListRoute: React.FC = () => {
   const [filter, setFilter] = React.useState<TaskFilter>('all')
-  const [tasks, error] = useGET(getTasks, filter)
   const [serverError, onLogout] = useSubmit('/', () => sessionApi.del(''))
 
-  if (error) {
-    return <ErrorView error={error} />
-  }
-
-  if (tasks) {
-    return (
-      <>
-        <ServerError error={serverError} />
-        <TaskListView
-          taskList={tasks}
-          // tslint:disable-next-line: no-unnecessary-callback-wrapper
-          onLogout={() => onLogout()}
-          onFilterChange={setFilter}
-        />
-      </>
-    )
-  }
-
-  return <Loading />
+  return (
+    <>
+      <ServerError error={serverError} />
+      <GET asyncFn={getTasks} param={filter}>
+        {data => (
+          <TaskListView
+            taskList={data}
+            // tslint:disable-next-line: no-unnecessary-callback-wrapper
+            onLogout={() => onLogout()}
+            onFilterChange={setFilter}
+          />
+        )}
+      </GET>
+    </>
+  )
 }
