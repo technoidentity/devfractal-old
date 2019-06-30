239dd6bd0f59452940d3993e23161712b753d35a
Pervez Iqbal <pervezfunctor@gmail.com>
2019-06-30 15:26:01 +0530

    drop sessionAPI and userAPI functions, too simple

--- packages/task-manager/src/api/index.ts
+++ packages/task-manager/src/api/index.ts
@@ -1,3 +1,15 @@
-export * from './sessionAPI'
+import { rest, SessionRT, UserRT } from '../utils'
+
 export * from './taskAPI'
-export * from './userAPI'
+
+export const userApi = rest(
+  { baseURL: 'http://localhost:9999', withCredentials: true },
+  UserRT,
+  'users',
+)
+
+export const sessionApi = rest(
+  { baseURL: 'http://localhost:9999', withCredentials: true },
+  SessionRT,
+  'session',
+)
