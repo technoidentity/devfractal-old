46277161d2c6a8021fe60362052726d67df54243
Pervez Iqbal <pervezfunctor@gmail.com>
2019-06-30 15:31:00 +0530

    refactor EditTaskRoute

--- packages/task-manager/src/pages/EditTaskRoute.tsx
+++ packages/task-manager/src/pages/EditTaskRoute.tsx
@@ -1,40 +1,31 @@
 import React from 'react'
 import { RouteComponentProps } from 'react-router'
 import { getTask, updateTask } from '../api'
-import { Task } from '../utils'
+import { GETView, ServerError, Task, useOne, useSubmit } from '../utils'
 import { TaskForm } from '../views'
 
+// @TODO: Use useRouter, fix 'match' type
 export const EditTaskRoute: React.FC<
   RouteComponentProps<{
     readonly id: string
   }>
-> = ({ match, history }) => {
-  const [task, setTask] = React.useState<Task | undefined>(undefined)
-  const [error, setError] = React.useState<Error | undefined>(undefined)
-
+> = ({ match }) => {
   const id = match.params.id
+  const update = (data: Task) => updateTask(id, data)
 
-  React.useEffect(() => {
-    getTask(id)
-      .then(setTask)
-      .catch(setError)
-  }, [id])
+  const [data, error] = useOne(getTask, id)
+  const [serverError, onSubmit] = useSubmit('/tasks', update)
 
   return (
     <section className="section">
       <h1 className="title has-text-centered">Edit Task</h1>
-      {error && (
-        <h1 className="is-text is-size-1 is-danger">{error.message}</h1>
-      )}
-      {task && (
-        <TaskForm
-          onSubmit={data => updateTask(id, data).then(() => history.push('/'))}
-          initial={task}
-        />
-      )}
-      {!error && !task && (
-        <h1 className="is-text is-size-1 is-info">Loading....</h1>
-      )}
+      <ServerError serverError={serverError} />
+      <GETView
+        data={data}
+        error={error}
+        component={TaskForm}
+        onSubmit={onSubmit}
+      />
     </section>
   )
 }
