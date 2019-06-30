23559753dab4054e693d92ac9af7e0c3fc8ed97d
Pervez Iqbal <pervezfunctor@gmail.com>
2019-06-30 15:36:54 +0530

    show server error on logout too?

--- packages/task-manager/src/pages/TaskListRoute.tsx
+++ packages/task-manager/src/pages/TaskListRoute.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { getTasks, sessionApi, TaskFilter } from '../api'
-import { ErrorView, Loading, useMany, useSubmit } from '../utils'
+import { ErrorView, Loading, ServerError, useMany, useSubmit } from '../utils'
 import { TaskListView } from '../views'
 
 export const TaskListRoute: React.FC = () => {
   const [filter, setFilter] = React.useState<TaskFilter>('all')
   const [tasks, error] = useMany(getTasks, filter)
-  const [, onLogout] = useSubmit('/', () => sessionApi.del(''))
+  const [serverError, onLogout] = useSubmit('/', () => sessionApi.del(''))
 
   if (error) {
     return <ErrorView error={error} />
@@ -15,6 +15,7 @@ export const TaskListRoute: React.FC = () => {
   if (tasks) {
     return (
       <>
+        <ServerError serverError={serverError} />
         <TaskListView
           taskList={tasks}
           // tslint:disable-next-line: no-unnecessary-callback-wrapper
