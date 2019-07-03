de766fc7348584bf07120c5c1fb96485c146b4cf
sravanidande <sravanidnd89@gmail.com>
2019-07-03 16:44:25 +0530

    add size attribute to buttons

--- packages/devfractal-components/src/form/ButtonsGroup.tsx
+++ packages/devfractal-components/src/form/ButtonsGroup.tsx
@@ -12,17 +12,20 @@ export interface ButtonsGroupProps
    * To attach buttons together
    */
   readonly addons?: boolean
+  readonly size?: 'small' | 'medium' | 'large'
 }
 
 export const ButtonsGroup: React.FC<ButtonsGroupProps> = ({
   addons,
   alignment,
   children,
+  size,
   ...props
 }) => {
   const classes: string = classNamesHelper(props, 'buttons', {
     [`is-${alignment}`]: alignment,
     [`has-${addons}`]: addons,
+    [`are-${size}`]: size,
   })
 
   return (
