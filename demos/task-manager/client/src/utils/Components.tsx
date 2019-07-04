9e7c9de4f2836075e9f99df62810fa9bf872d311
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-04 13:34:23 +0530

    better ServerError and NotFound

--- demos/task-manager/client/src/utils/Components.tsx
+++ demos/task-manager/client/src/utils/Components.tsx
@@ -1,19 +1,22 @@
 import * as t from 'io-ts'
 import React from 'react'
 import { Message, MessageBody, Text } from 'technoidentity-devfractal'
-import { typeInvariant } from 'technoidentity-utils'
+import { jsonStringify, req, typeInvariant } from 'technoidentity-utils'
 import { useRouter } from './RouterUtils'
 
-const SingleError = t.readonly(t.type({ error: t.string }))
-const Errors = t.readonly(t.type({ errors: t.readonlyArray(t.string) }))
-const ServerError = t.union([t.string, SingleError, Errors])
+const SingleError = req({ error: t.string })
+const Errors = req({ errors: t.readonlyArray(t.string) })
+const ErrorObject = req({ errors: t.record(t.string, t.any) })
 
-type ServerError = t.TypeOf<typeof ServerError>
+export const ServerError = t.union([t.string, SingleError, Errors, ErrorObject])
+
+export type ServerError = t.TypeOf<typeof ServerError>
 
 export interface ServerErrorViewProps {
   readonly error?: ServerError
 }
 
+// @TODO: may be returning string | string[] is a better idea than using join?
 function serverError(error: ServerError): string {
   typeInvariant(ServerError, error)
 
@@ -25,7 +28,17 @@ function serverError(error: ServerError): string {
     return error.error
   }
 
-  return error.errors.join('\n')
+  if (Errors.is(error)) {
+    return error.errors.join('\n')
+  }
+
+  if (ErrorObject.is(error)) {
+    return Object.entries(error)
+      .map(([k, v]) => `${k}: ${v}`)
+      .join('\n')
+  }
+
+  throw new Error(`Unknown error: ${jsonStringify(error)}`)
 }
 
 export const ServerErrorView: React.FC<ServerErrorViewProps> = ({ error }) => (
@@ -45,18 +58,35 @@ export const Loading: React.FC = () => (
 )
 
 export interface ErrorViewProps {
-  readonly error: Error
+  readonly error: Error | ServerError
 }
 
 // @TODO: This must use server error, if error.response.data is not undefined.
-export const ErrorView: React.FC<ErrorViewProps> = ({ error }) => (
-  <Text textSize="1" textColor="danger">
-    {error.message}
-  </Text>
-)
+export const ErrorView: React.FC<ErrorViewProps> = ({ error }) => {
+  const err: any = error
+
+  if (err.response && err.response.data) {
+    return <ServerErrorView error={err.response.data} />
+  }
+
+  if (err.message) {
+    return (
+      <Text textSize="1" textColor="danger">
+        {err.message}
+      </Text>
+    )
+  }
+
+  throw new Error(`Unknown error: ${jsonStringify(err)}`)
+}
 
 export const NotFound: React.FC = () => {
   const { location } = useRouter()
 
-  return <Text>{`path ${location.pathname} did not match any route`}</Text>
+  return (
+    <Text
+      textSize="1"
+      color="danger"
+    >{`path ${location.pathname} did not match any route`}</Text>
+  )
 }
