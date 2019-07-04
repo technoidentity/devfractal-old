e30ac711de0d0f1a6cc145c4edc498b6cfcd95b3
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-04 14:13:30 +0530

    improved rest

--- packages/devfractal-components/src/base/rest.ts
+++ packages/devfractal-components/src/base/rest.ts
@@ -4,12 +4,17 @@ import { Array } from 'tcomb'
 import { typeInvariant } from 'technoidentity-utils'
 import { http as httpAPI, MethodArgs, RequestConfig } from './http'
 
-interface API<I extends Record<string, any>, A extends Record<string, any>> {
-  many(options?: Omit<MethodArgs, 'resource'>): Promise<readonly A[]>
-  one(options: Omit<MethodArgs, 'resource'>): Promise<A>
-  create(data: I, options: Omit<MethodArgs, 'resource'>): Promise<A>
-  get(id: string, options: Omit<MethodArgs, 'resource'>): Promise<A>
-  update(id: string, data: I, options: Omit<MethodArgs, 'resource'>): Promise<A>
+type APIMethodArgs = Omit<MethodArgs, 'resource'>
+interface API<
+  A extends Record<string, any>,
+  I extends Record<string, any> | unknown = unknown
+> {
+  many(options?: APIMethodArgs): Promise<readonly A[]>
+  one(options?: APIMethodArgs): Promise<A>
+  create(data: I, options?: APIMethodArgs): Promise<A>
+  get(id: string, options?: APIMethodArgs): Promise<A>
+  update(id: string, data: I, options?: APIMethodArgs): Promise<A>
+  del(id: string, options?: APIMethodArgs): Promise<void>
 }
 
 function appendId(options: MethodArgs, id: string): MethodArgs {
@@ -19,57 +24,60 @@ function appendId(options: MethodArgs, id: string): MethodArgs {
     } else if (Array.is(draft.path)) {
       draft.path.unshift(id)
     } else {
-      draft.path = [draft.path, id]
+      draft.path = [id, draft.path]
     }
   })
 }
 
-// tslint:disable-next-line: typedef
+interface RestArgs<
+  A extends Record<string, any>,
+  O extends Record<string, any>,
+  I extends Record<string, any> | unknown = unknown
+> extends RequestConfig {
+  readonly resource: string
+  readonly type: Mixed & Type<A, O, I>
+}
+
 export function rest<
-  I extends Record<string, any>,
-  A extends Record<string, any>
->(
-  options: RequestConfig,
-  type: Mixed & Type<A, A, I>,
-  resource: string,
-): API<I, A> {
+  A extends Record<string, any>,
+  O extends Record<string, any>,
+  I extends Record<string, any> | unknown = unknown
+>(args: RestArgs<A, O, I>): API<A, I> {
+  const { resource, type, ...options } = args
+
   const http: ReturnType<typeof httpAPI> = httpAPI(options)
 
-  async function many(
-    options: Omit<MethodArgs, 'resource'>,
-  ): Promise<ReadonlyArray<A>> {
+  async function many(options: APIMethodArgs): Promise<ReadonlyArray<A>> {
     return http.get({ ...options, resource }, readonlyArray(type))
   }
 
-  async function one(options: Omit<MethodArgs, 'resource'>): Promise<A> {
+  async function one(options: APIMethodArgs): Promise<A> {
     return http.get({ ...options, resource }, type)
   }
 
-  async function create(
-    data: I,
-    options: Omit<MethodArgs, 'resource'>,
-  ): Promise<A> {
+  async function create(data: I, options: APIMethodArgs): Promise<A> {
     typeInvariant(type, data)
 
     return http.post({ ...options, resource }, data, type)
   }
 
-  async function get(
-    id: string,
-    options: Omit<MethodArgs, 'resource'>,
-  ): Promise<A> {
+  async function del(id: string, options?: APIMethodArgs): Promise<void> {
+    return http.del(appendId({ ...options, resource }, id))
+  }
+
+  async function get(id: string, options: APIMethodArgs): Promise<A> {
     return one(appendId({ ...options, resource }, id))
   }
 
   async function update(
     id: string,
     data: I,
-    options: Omit<MethodArgs, 'resource'>,
+    options: APIMethodArgs,
   ): Promise<A> {
     typeInvariant(type, data)
 
     return http.put(appendId({ ...options, resource }, id), data, type)
   }
 
-  return { one, many, get, update, create }
+  return { one, many, get, update, create, del }
 }
