5bda646c36ba122a05897638230ff36185f9fbaf
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-04 22:36:53 +0530

    add few more tcomb refinements

--- packages/devfractal-utils/src/tcombRefinements.ts
+++ packages/devfractal-utils/src/tcombRefinements.ts
@@ -4,31 +4,46 @@ import * as t from 'tcomb'
 
 export const Positive = t.refinement(t.Number, n => n >= 0, 'Positive')
 export const Negative = t.refinement(t.Number, n => n <= 0, 'Negative')
-export const Min = (min: number) => t.refinement(t.Number, n => n >= min)
-export const Max = (max: number) => t.refinement(t.Number, n => n <= max)
+export const Min = (min: number) => t.refinement(t.Number, n => n >= min, 'Min')
+export const Max = (max: number) => t.refinement(t.Number, n => n <= max, 'Max')
+
+export const PositiveInt = t.refinement(t.Integer, n => n >= 0, 'PositiveInt')
+export const NegativeInt = t.refinement(t.Integer, n => n <= 0, 'NegativeInt')
+export const MinInt = (min: number) =>
+  t.refinement(t.Integer, n => n >= min, 'MinInt')
+export const MaxInt = (max: number) =>
+  t.refinement(t.Integer, n => n <= max, 'MaxInt')
 
 // eslint-disable-next-line
 const rEmail: RegExp = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i
 // eslint-disable-next-line
 const rUrl: RegExp = /^((https?|ftp):)?\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i
 
-export const Email = t.refinement(t.String, s => rEmail.test(s))
-export const Url = t.refinement(t.String, s => rUrl.test(s))
-export const Lower = t.refinement(t.String, s => s.toLowerCase() === s)
-export const Upper = t.refinement(t.String, s => s.toUpperCase() === s)
+export const Email = t.refinement(t.String, s => rEmail.test(s), 'Email')
+export const Url = t.refinement(t.String, s => rUrl.test(s), 'Url')
+export const Lower = t.refinement(
+  t.String,
+  s => s.toLowerCase() === s,
+  'Lowercase',
+)
+export const Upper = t.refinement(
+  t.String,
+  s => s.toUpperCase() === s,
+  'Uppercase',
+)
 export const Length = (len: number) =>
-  t.refinement(t.String, s => s.length === len)
+  t.refinement(t.String, s => s.length === len, 'StringLength')
 export const MinStringLength = (min: number) =>
-  t.refinement(t.String, s => s.length >= min)
+  t.refinement(t.String, s => s.length >= min, 'MinStringLength')
 export const MaxStringLength = (max: number) =>
-  t.refinement(t.String, s => s.length <= max)
+  t.refinement(t.String, s => s.length <= max, 'MaxStringLength')
 
 export const MinDate = (min: Date) =>
-  t.refinement(t.Date, d => compareAsc(min, d) <= 0)
+  t.refinement(t.Date, d => compareAsc(min, d) <= 0, 'MinDate')
 export const MaxDate = (max: Date) =>
-  t.refinement(t.Date, d => compareAsc(max, d) >= 0)
+  t.refinement(t.Date, d => compareAsc(max, d) >= 0, 'MaxDate')
 
 export const ArrayMinLength = (min: number) =>
-  t.refinement(t.Array, arr => arr.length >= min)
+  t.refinement(t.Array, arr => arr.length >= min, 'ArrayMinLength')
 export const ArrayMaxLength = (max: number) =>
-  t.refinement(t.Array, arr => arr.length <= max)
+  t.refinement(t.Array, arr => arr.length <= max, 'ArrayMaxLength')
