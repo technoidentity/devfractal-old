cbe1891d0a942dae0d41f4afc2387defe679bab1
sravanidande <sravanidnd89@gmail.com>
2019-07-05 16:11:45 +0530

    passing params to TaskListRoute

--- demos/task-manager/client/src/pages/TaskListRoute.tsx
+++ demos/task-manager/client/src/pages/TaskListRoute.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import { RouteComponentProps } from 'react-router-dom'
 import { Get } from 'technoidentity-devfractal'
 import { getTasks, TaskFilter } from '../common'
 import { TaskListView } from '../views'
 
-export const TaskListRoute: React.FC = () => {
-  const [filter, setFilter] = React.useState<TaskFilter>('all')
+export interface TaskListRouteProps {
+  readonly filter: TaskFilter
+}
 
+export const TaskListRoute: React.FC<
+  RouteComponentProps<TaskListRouteProps>
+> = ({ match }: any) => {
+  console.log(match)
+  const filterOne = (match && match.params && match.params.filter) || 'all'
+  console.log(filterOne)
   return (
-    <Get asyncFn={getTasks} deps={[filter]}>
-      {data => <TaskListView taskList={data} onFilterChange={setFilter} />}
+    <Get asyncFn={getTasks} deps={[filterOne]}>
+      {data => <TaskListView taskList={data} />}
     </Get>
   )
 }
