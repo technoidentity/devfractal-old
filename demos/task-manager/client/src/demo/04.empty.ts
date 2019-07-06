a7dc0dcbd0f1d76ab9eeaf74d384893d718fa102
sravanidande <sravanidnd89@gmail.com>
2019-07-06 16:35:37 +0530

    add empty demo

---
+++ demos/task-manager/client/src/demo/04.empty.ts
@@ -0,0 +1,34 @@
+import {
+  boolean,
+  keyof,
+  number,
+  readonly,
+  readonlyArray,
+  string,
+  type,
+} from 'io-ts'
+import { date } from 'io-ts-types'
+import { emptyFromType } from 'technoidentity-utils'
+
+console.log(
+  emptyFromType(
+    readonly(
+      type({
+        i: number,
+        s: string,
+        b: boolean,
+        d: date,
+        a: readonlyArray(boolean),
+        e: keyof({
+          foo: 1,
+          bar: 1,
+        }),
+        o: readonly(
+          type({
+            fizz: readonlyArray(readonly(type({ buzz: boolean }))),
+          }),
+        ),
+      }),
+    ),
+  ),
+)
