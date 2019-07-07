26f68ddd9f3660481c20edc2fa8425e0c3385565
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-07 17:07:51 +0530

    commented out EditTodoForm

---
+++ demos/task-manager/client/src/demo/16.EditTodo.tsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+// import { RouteComponentProps } from 'react-router'
+// import { Put } from 'technoidentity-devfractal'
+// import { taskApi } from '../common'
+// import { Todo } from './11.todo'
+// import { TodoForm } from './12.TodoForm'
+
+// export const EditTodoProps = req({
+//   id:number
+// })
+
+// export const EditTodoRoute: React.FC<
+//   RouteComponentProps<{ readonly id: string }>
+// > = ({ match }) => (
+//   <Put<Todo>
+//     id={match.params.id}
+//     doGet={taskApi.get}
+//     onPut={taskApi.update}
+//     component={TodoForm}
+
+//   />
+// )
