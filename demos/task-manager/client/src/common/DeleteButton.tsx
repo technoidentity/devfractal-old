0c59e24f861e6e7bb38fa18c45873fb8f5f2c1cc
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-08 01:09:40 +0530

    DeleteButton

---
+++ demos/task-manager/client/src/common/DeleteButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from 'technoidentity-devfractal'
+
+export interface DeleteButtonProps {
+  readonly id?: string | number
+  onDelete(id: string): Promise<void>
+}
+
+export const DeleteButton: React.FC<DeleteButtonProps> = ({ id, onDelete }) => {
+  return (
+    <Button variant="danger" onClick={() => id && onDelete(id.toString())}>
+      Delete
+    </Button>
+  )
+}
