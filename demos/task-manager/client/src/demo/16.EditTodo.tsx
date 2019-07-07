e33a223c8ed7792c1940082985592bf4cc26aa14
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-08 01:18:47 +0530

    update devfractal in demo client

--- demos/task-manager/client/src/demo/16.EditTodo.tsx
+++ demos/task-manager/client/src/demo/16.EditTodo.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Put, useMatch } from 'technoidentity-devfractal'
-import { todoApi } from './11.Todo'
+import { Todo, todoApi } from './11.Todo'
 import { TodoForm } from './12.TodoForm'
 
 export const EditTodoRoute: React.FC = () => {
   const { params } = useMatch<{ readonly id: string }>()
 
   return (
-    <Put
+    <Put<Todo>
       id={params.id}
       doGet={todoApi.get}
       onPut={todoApi.update}
