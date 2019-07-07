28187583bf9bdc4c255c4e74814c213e94c40062
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-07 17:06:50 +0530

    add TodoList demo

---
+++ demos/task-manager/client/src/demo/15.TodoList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SimpleAsync } from 'technoidentity-devfractal'
+import { todoApi } from './11.todo'
+import { TodoList } from './14.TodoListView'
+
+export const TodoListRoute: React.FC = () => (
+  <SimpleAsync asyncFn={() => todoApi.many()}>
+    {({ data }) => <TodoList todoList={data} />}
+  </SimpleAsync>
+)
