cfd5f6cc9b1825a78f67dcc0389d1474385cd02f
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-07 10:41:19 +0530

    refactor App into TaskManager

--- demos/task-manager/client/src/App.tsx
+++ demos/task-manager/client/src/App.tsx
@@ -1,29 +1,4 @@
 import React from 'react'
-import { Switch } from 'react-router'
-import { NotFound, Router, SafeRoute as Route } from 'technoidentity-devfractal'
-import {
-  CreateTaskRoute,
-  EditTaskRoute,
-  LoginRoute,
-  SignUpFormRoute,
-  TaskListRoute,
-} from './pages'
-import { AuthProvider } from './utils'
-import { Header } from './views'
+import { TaskManager } from './TaskManager'
 
-export const App: React.FC = () => (
-  <AuthProvider>
-    <Router variant="browser">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={SignUpFormRoute} />
-        <Route exact path="/login" component={LoginRoute} />
-        <Route exact path="/tasks" component={TaskListRoute} />
-        <Route exact path="/tasks/:filter" component={TaskListRoute} />
-        <Route exact path="/add" component={CreateTaskRoute} />
-        <Route exact path="/tasks/:id/edit" component={EditTaskRoute} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
-  </AuthProvider>
-)
+export const App: React.FC = () => <TaskManager />
---
+++ demos/task-manager/client/src/TaskManager.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Switch } from 'react-router'
+import { NotFound, Router, SafeRoute as Route } from 'technoidentity-devfractal'
+import {
+  CreateTaskRoute,
+  EditTaskRoute,
+  LoginRoute,
+  SignUpFormRoute,
+  TaskListRoute,
+} from './pages'
+import { AuthProvider } from './utils'
+import { Header } from './views'
+
+export const TaskManager = () => (
+  <AuthProvider>
+    <Router variant="browser">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={SignUpFormRoute} />
+        <Route exact path="/login" component={LoginRoute} />
+        <Route exact path="/tasks" component={TaskListRoute} />
+        <Route exact path="/tasks/:filter" component={TaskListRoute} />
+        <Route exact path="/add" component={CreateTaskRoute} />
+        <Route exact path="/tasks/:id/edit" component={EditTaskRoute} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  </AuthProvider>
+)
