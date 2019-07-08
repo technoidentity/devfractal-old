adec73a37f8f1c870340ce4911f85c6aebbbdcea
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-08 13:45:03 +0530

    minor changes

--- demos/task-manager/client/src/App.tsx
+++ demos/task-manager/client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TodoApp } from './demo/17.CompleteTodo'
+import { Section, Simple, typedForm } from 'technoidentity-devfractal'
 // import { Router, SafeRoute as Route } from 'technoidentity-devfractal'
 // import { CreateTodoRoute } from './demo/13.CreateTodo'
 // import { TodoListRoute } from './demo/15.TodoListRoute'
@@ -17,4 +17,31 @@ import { TodoApp } from './demo/17.CompleteTodo'
 
 // export const App: React.FC = () => <h1>Hello, World</h1>
 
-export const App = () => <TodoApp />
+// export const App = () => <TodoApp />
+
+// export const App = () => (
+//   <BrowserRouter>
+//     <SimpleCrud
+//       baseURL="http://localhost:3000"
+//       resource="todos"
+//       id="id"
+//       value={Todo}
+//     />
+//   </BrowserRouter>
+// )
+
+const initial = { username: '', password: '' }
+
+export const App = () => {
+  const Typed = typedForm<typeof initial>()
+
+  return (
+    <Section>
+      <Typed.Form initialValues={initial}>
+        <Typed.Text name="username" />
+        <Typed.Password name="password" />
+        <Simple.FormButtons />
+      </Typed.Form>
+    </Section>
+  )
+}
