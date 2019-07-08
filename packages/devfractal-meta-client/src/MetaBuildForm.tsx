e1690163deec5203e72718d061ff614748378c84
wasay <mawasay83@gmail.com>
2019-07-08 06:46:14 -0500

    fix meta builder form

--- packages/devfractal-meta-client/src/MetaBuildForm.tsx
+++ packages/devfractal-meta-client/src/MetaBuildForm.tsx
@@ -2,187 +2,216 @@ import { Form, Formik, FormikActions } from 'formik'
 import React from 'react'
 import {
   CheckBox,
+  CheckboxField,
   Column,
   Columns,
   Container,
-  Input,
-  Select,
+  DateField,
+  InputField,
+  SelectField,
   Simple,
   SimpleArrayField,
   SimpleArrayFieldComponentProps,
   Text,
 } from 'technoidentity-devfractal'
-import { PrimitiveMT } from 'technoidentity-meta-core'
+import { Refinements } from 'technoidentity-meta-core'
 
 export interface MetaBuildFormProps {
   onSubmit(values: any, formikActions: FormikActions<any>): void
 }
 
+type RefinementsKind = 'number' | 'string' | 'date' | 'array'
+
 interface Meta {
   readonly label: string
-  readonly kind: PrimitiveMT['kind']
-  readonly refinements: ReadonlyArray<any>
+  readonly kind: RefinementsKind
+  readonly refinements: Refinements
 }
 
 // tslint:disable-next-line:typedef
 const newMeta: () => Meta = () => ({
   label: '',
   kind: 'number',
-  refinements: [],
+  refinements: {},
 })
 
-const NumberRefinementsSubForm: React.FC = () => {
+interface NumberRefinementsSubFormProps {
+  readonly name: string
+}
+
+const NumberRefinementsSubForm: React.FC<NumberRefinementsSubFormProps> = ({name}) => {
+  const [isSign, setIsSign] = React.useState<boolean>(false)
+  const [isMin, setIsMin] = React.useState<boolean>(false)
+  const [isMax, setIsMax] = React.useState<boolean>(false)
   return (
     <>
       <Columns>
         <Column narrow>
-          <CheckBox />
+          <CheckboxField name={`${name}.integer`} />
         </Column>
         <Column narrow>integer</Column>
         <Column />
       </Columns>
       <Columns>
         <Column narrow>
-          <CheckBox />
+          <CheckBox onChange={evt => setIsSign(evt.target.checked)}/>
         </Column>
-        <Column narrow>positive</Column>
-        <Column />
+        <Column narrow>sign</Column>
+        <Column>
+          {isSign && <SelectField name={`${name}.sign`}>
+            <option value="undefined">Select an option</option>
+            <option value="positive">positive</option>
+            <option value="negative">negative</option>
+          </SelectField>}
+        </Column>
       </Columns>
       <Columns>
         <Column narrow>
-          <CheckBox />
-        </Column>
-        <Column narrow>negative</Column>
-        <Column />
-      </Columns>
-      <Columns>
-        <Column narrow>
-          <CheckBox />
+          <CheckBox onChange={evt => setIsMin(evt.target.checked)}/>
         </Column>
         <Column>minimum</Column>
         <Column>
-          <Input type="number" placeholder="Maximum Value" />
+          {isMin && <InputField type="number" name={`${name}.min`} placeholder="Minimum Value" />}
         </Column>
       </Columns>
       <Columns>
         <Column narrow>
-          <CheckBox />
+          <CheckBox onChange={evt => setIsMax(evt.target.checked)}/>
         </Column>
         <Column>maximum</Column>
         <Column>
-          <Input type="number" placeholder="Minimum Value" />
+          {isMax && <InputField type="number" name={`${name}.max`} placeholder="Maximum Value" />}
         </Column>
       </Columns>
     </>
   )
 }
 
-const StringRefinementsSubForm: React.FC = () => {
+interface StringRefinementsSubFormProps {
+  readonly name: string
+}
+
+const StringRefinementsSubForm: React.FC<StringRefinementsSubFormProps> = ({name}) => {
+  const [isBase, setIsBase] = React.useState<boolean>(false)
+  const [isCase, setIsCase] = React.useState<boolean>(false)
+  const [isFixed, setIsFixed] = React.useState<boolean>(false)
+  const [isMin, setIsMin] = React.useState<boolean>(false)
+  const [isMax, setIsMax] = React.useState<boolean>(false)
   return (
     <>
       <Columns>
         <Column narrow>
-          <CheckBox />
+          <CheckBox onChange={evt => setIsBase(evt.target.checked)}/>
         </Column>
-        <Column narrow>email</Column>
-        <Column />
-      </Columns>
-      <Columns>
-        <Column narrow>
-          <CheckBox />
-        </Column>
-        <Column narrow>url</Column>
-        <Column />
-      </Columns>
-      <Columns>
-        <Column narrow>
-          <CheckBox />
-        </Column>
-        <Column narrow>lowercase</Column>
-        <Column />
-      </Columns>
-      <Columns>
-        <Column narrow>
-          <CheckBox />
-        </Column>
-        <Column narrow>uppercase</Column>
-        <Column />
-      </Columns>
-      <Columns>
-        <Column narrow>
-          <CheckBox />
-        </Column>
-        <Column>length</Column>
+        <Column narrow>base</Column>
         <Column>
-          <Input type="number" placeholder="Exact length" />
+          {isBase && <Simple.Select name={`${name}.base`}>
+            <option value="undefined">Select an option</option>
+            <option value="email">email</option>
+            <option value="url">url</option>
+          </Simple.Select>}
         </Column>
       </Columns>
       <Columns>
         <Column narrow>
-          <CheckBox />
+          <CheckBox onChange={evt => setIsCase(evt.target.checked)}/>
+        </Column>
+        <Column narrow>case</Column>
+        <Column>
+          {isCase && <Simple.Select name={`${name}.case`}>
+            <option value="undefined">Select an option</option>
+            <option value="lower">lower</option>
+            <option value="upper">upper</option>
+          </Simple.Select>}
+        </Column>
+      </Columns>
+      <Columns>
+        <Column narrow>
+          <CheckBox onChange={evt => setIsFixed(evt.target.checked)} />
+        </Column>
+        <Column>fixed</Column>
+        <Column>
+          {isFixed && <InputField type="number" name={`${name}.strLength.fixed`} placeholder="Exact length" />}
+        </Column>
+      </Columns>
+      <Columns>
+        <Column narrow>
+          <CheckBox onChange={evt => setIsMin(evt.target.checked)}/>
         </Column>
         <Column>minimum length</Column>
         <Column>
-          <Input type="number" placeholder="Minimum length" />
+          {isMin && <InputField type="number" name={`${name}.strLength.min`} placeholder="Minimum length" />}
         </Column>
       </Columns>
       <Columns>
         <Column narrow>
-          <CheckBox />
+          <CheckBox onChange={evt => setIsMax(evt.target.checked)}/>
         </Column>
         <Column>maximum length</Column>
         <Column>
-          <Input type="number" placeholder="Maximum Value" />
+          {isMax && <InputField type="number" name={`${name}.strLength.max`} placeholder="Maximum Value" />}
         </Column>
       </Columns>
     </>
   )
 }
 
-const ArrayRefinementsSubForm: React.FC = () => {
+interface ArrayRefinementsSubFormProps {
+  readonly name: string
+}
+
+const ArrayRefinementsSubForm: React.FC<ArrayRefinementsSubFormProps> = ({name}) => {
+  const [isMax, setIsMax] = React.useState<boolean>(false)
+  const [isMin, setIsMin] = React.useState<boolean>(false)
   return (
     <>
       <Columns>
         <Column narrow>
-          <CheckBox />
+          <CheckBox onChange={evt => setIsMax(evt.target.checked)}/>
         </Column>
         <Column>maximum array elements</Column>
         <Column>
-          <Input type="number" placeholder="maximum array elements" />
+          {isMax && <InputField type="number" name={`${name}.maxArrayLength`} placeholder="maximum array elements" />}
         </Column>
       </Columns>
       <Columns>
         <Column narrow>
-          <CheckBox />
+          <CheckBox onChange={evt => setIsMin(evt.target.checked)}/>
         </Column>
         <Column>minimum array elements</Column>
         <Column>
-          <Input type="number" placeholder="minimum array elements" />
+          {isMin && <InputField type="number" name={`${name}.minArrayLength`} placeholder="minimum array elements" />}
         </Column>
       </Columns>
     </>
   )
 }
 
-const DateRefinementsSubForm: React.FC = () => {
+interface DateRefinementsSubFormProps {
+  readonly name: string
+}
+
+const DateRefinementsSubForm: React.FC<DateRefinementsSubFormProps> = ({name}) => {
+  const [isMax, setIsMax] = React.useState<boolean>(false)
+  const [isMin, setIsMin] = React.useState<boolean>(false)
   return (
     <>
       <Columns>
         <Column narrow>
-          <CheckBox />
+          <CheckBox onChange={evt => setIsMax(evt.target.checked)}/>
         </Column>
-        <Column>maximum</Column>
+        <Column>max date</Column>
         <Column>
-          <Input type="number" placeholder="maximum" />
+          {isMax && <DateField name={`${name}.maxDate`} />}
         </Column>
       </Columns>
       <Columns>
         <Column narrow>
-          <CheckBox />
+          <CheckBox onChange={evt => setIsMin(evt.target.checked)}/>
         </Column>
-        <Column>minimum</Column>
+        <Column>min date</Column>
         <Column>
-          <Input type="number" placeholder="minimum" />
+          {isMin && <DateField name={`${name}.minDate`} />}
         </Column>
       </Columns>
     </>
@@ -190,48 +219,44 @@ const DateRefinementsSubForm: React.FC = () => {
 }
 
 interface RefinementsSubFormProps {
-  readonly selectedType: 'number' | 'string' | 'date' | 'array'
+  readonly selectedType: RefinementsKind
+  readonly name: string
 }
 
 const RefinementsSubForm: React.FC<RefinementsSubFormProps> = ({
   selectedType,
+  name
 }) => {
   switch (selectedType) {
     case 'number':
-      return <NumberRefinementsSubForm />
+      return <NumberRefinementsSubForm name={name}/>
     case 'string':
-      return <StringRefinementsSubForm />
+      return <StringRefinementsSubForm name={name}/>
     case 'array':
-      return <ArrayRefinementsSubForm />
+      return <ArrayRefinementsSubForm name={name}/>
     case 'date':
-      return <DateRefinementsSubForm />
+      return <DateRefinementsSubForm name={name}/>
   }
 }
 
 interface FieldProps {
-  readonly refinements: ReadonlyArray<any>
   readonly name: string
+  readonly data: Meta
 }
 
-const AddField: React.FC<FieldProps> = ({ name }) => {
-  const [selectedType, setSelectedType] = React.useState<
-    RefinementsSubFormProps['selectedType']
-  >('number')
+const AddField: React.FC<FieldProps> = ({ name, data }) => {
   return (
     <Container>
       <Simple.Text name={`${name}.label`} label="Label" />
-      <Select
+      <SelectField
         name={`${name}.kind`}
-        onChange={evt => {
-          setSelectedType(evt.target.value as any)
-        }}
       >
         <option value="number">number</option>
         <option value="string">string</option>
         <option value="date">date</option>
         <option value="array">array</option>
-      </Select>
-      <RefinementsSubForm selectedType={selectedType} />
+      </SelectField>
+      <RefinementsSubForm selectedType={data.kind} name={`${name}.refinements`}/>
     </Container>
   )
 }
@@ -246,7 +271,7 @@ const AddFieldComponent: React.FC<SimpleArrayFieldComponentProps<any>> = ({
     <Text textAlignment="centered" textSize="3">
       Add Field
     </Text>
-    <AddField name={name} refinements={data.refinements} />
+    <AddField name={name} data={data}/>
   </>
 )
 
