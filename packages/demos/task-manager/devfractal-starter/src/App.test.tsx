407e7caec6a450167bdcc8a60b1d3936d336409e
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-08 09:11:38 +0530

    initial commit

---
+++ packages/demos/task-manager/devfractal-starter/src/App.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<h1 />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
---
+++ packages/demos/task-manager/devfractal-starter/src/App.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import { TodoApp } from './TodoApp'
+
+export const App = () => <TodoApp />
