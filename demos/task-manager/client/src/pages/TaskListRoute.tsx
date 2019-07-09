e4e4b8def58ae72f0837d988603e138abbe83dc5
sravanidande <sravanidnd89@gmail.com>
2019-07-09 11:34:29 +0530

    fix component using useMatch

--- demos/task-manager/client/src/pages/TaskListRoute.tsx
+++ demos/task-manager/client/src/pages/TaskListRoute.tsx
@@ -1,19 +1,11 @@
 import React from 'react'
-import { RouteComponentProps } from 'react-router-dom'
-import { Get } from 'technoidentity-devfractal'
+import { Get, useMatch } from 'technoidentity-devfractal'
 import { getTasks, TaskFilter } from '../common'
 import { TaskListView } from '../views'
 
-export interface TaskListRouteProps {
-  readonly filter: TaskFilter
-}
-
-export const TaskListRoute: React.FC<
-  RouteComponentProps<TaskListRouteProps>
-> = ({ match }: any) => {
-  console.log(match)
-  const filterOne = (match && match.params && match.params.filter) || 'all'
-  console.log(filterOne)
+export const TaskListRoute: React.FC = () => {
+  const { params } = useMatch<{ readonly filter: TaskFilter }>()
+  const filterOne = (params && params.filter) || 'all'
   return (
     <Get asyncFn={getTasks} deps={[filterOne]}>
       {data => <TaskListView taskList={data} />}
