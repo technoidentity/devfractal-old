486765abddbe33827b2c08a5cc4a5d133e7698da
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-13 00:36:13 +0530

    minor rename

--- packages/devfractal-utils/src/empty.ts
+++ packages/devfractal-utils/src/empty.ts
@@ -26,86 +26,84 @@ import {
 } from 'io-ts'
 import { buildObject, nop } from './common'
 
-function emptyFromPrimitive<T extends Mixed>(typeValue: T): TypeOf<T> {
-  if (typeValue.name === 'Int' || typeValue instanceof NumberType) {
+function emptyFromPrimitive<T extends Mixed>(spec: T): TypeOf<T> {
+  if (spec.name === 'Int' || spec instanceof NumberType) {
     return 0
   }
-  if (typeValue instanceof StringType) {
+  if (spec instanceof StringType) {
     return ''
   }
-  if (typeValue instanceof BooleanType) {
+  if (spec instanceof BooleanType) {
     return false
   }
-  if (typeValue.name === 'Date') {
+  if (spec.name === 'Date') {
     return new Date()
   }
-  if (typeValue instanceof KeyofType) {
-    return Object.keys(typeValue.keys)[0]
+  if (spec instanceof KeyofType) {
+    return Object.keys(spec.keys)[0]
   }
-  if (typeValue instanceof FunctionType) {
+  if (spec instanceof FunctionType) {
     return nop
   }
-  if (typeValue instanceof LiteralType) {
-    return typeValue.value
+  if (spec instanceof LiteralType) {
+    return spec.value
   }
-  if (typeValue instanceof NullType) {
+  if (spec instanceof NullType) {
     // tslint:disable-next-line: no-null-keyword
     return null
   }
-  if (typeValue instanceof UndefinedType || typeValue instanceof VoidType) {
+  if (spec instanceof UndefinedType || spec instanceof VoidType) {
     return undefined
   }
 
-  throw new Error(`Unsupported type: ${typeValue.name}`)
+  throw new Error(`Unsupported type: ${spec.name}`)
 }
 
-function emptyFromObject<T extends Props>(
-  typeValue: TypeC<T>,
-): TypeOf<TypeC<T>> {
-  return buildObject(typeValue.props, empty)
+function emptyFromObject<T extends Props>(spec: TypeC<T>): TypeOf<TypeC<T>> {
+  return buildObject(spec.props, empty)
 }
 
-export function empty<T extends Mixed>(typeValue: T): TypeOf<T> {
+export function empty<T extends Mixed>(spec: T): TypeOf<T> {
   if (
-    typeValue instanceof ExactType ||
-    typeValue instanceof ReadonlyType ||
-    typeValue instanceof RefinementType
+    spec instanceof ExactType ||
+    spec instanceof ReadonlyType ||
+    spec instanceof RefinementType
   ) {
-    return empty(typeValue.type)
+    return empty(spec.type)
   }
 
-  if (typeValue instanceof InterfaceType) {
-    return emptyFromObject(typeValue)
+  if (spec instanceof InterfaceType) {
+    return emptyFromObject(spec)
   }
 
   if (
-    typeValue instanceof ReadonlyArrayType ||
-    typeValue instanceof ArrayType ||
-    typeValue instanceof AnyArrayType
+    spec instanceof ReadonlyArrayType ||
+    spec instanceof ArrayType ||
+    spec instanceof AnyArrayType
   ) {
     return []
   }
 
-  if (typeValue instanceof IntersectionType) {
-    return typeValue.types
+  if (spec instanceof IntersectionType) {
+    return spec.types
       .map(empty)
       .reduce((acc: any, x: any) => ({ ...acc, ...x }))
   }
 
-  if (typeValue instanceof UnionType) {
-    return empty(typeValue.types[0])
+  if (spec instanceof UnionType) {
+    return empty(spec.types[0])
   }
 
-  if (typeValue instanceof PartialType) {
-    return buildObject(typeValue.props, empty)
+  if (spec instanceof PartialType) {
+    return buildObject(spec.props, empty)
   }
-  // if (typeValue instanceof TaggedUnionType) {
-  //   return typeValue.types
+  // if (spec instanceof TaggedUnionType) {
+  //   return spec.types
   //     .map(empty)
   //     .reduce((acc: any, x: any) => ({ ...acc, ...x }))
   // }
-  if (typeValue instanceof TupleType) {
-    return typeValue.types.map(empty)
+  if (spec instanceof TupleType) {
+    return spec.types.map(empty)
   }
-  return emptyFromPrimitive(typeValue)
+  return emptyFromPrimitive(spec)
 }
