658fcd8e2696fc4fc1a2080c8e8325fc01d01521
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-12 10:11:34 +0530

    fix tslint error

--- packages/devfractal-meta-client/src/metaToInitialValues.test.ts
+++ packages/devfractal-meta-client/src/metaToInitialValues.test.ts
@@ -27,6 +27,8 @@ test('initialValues from bool meta', () => {
 test.skip('initialValues from date meta', () => {
   // https://codewithhugo.com/mocking-the-current-date-in-jest-tests/
   jest
+
+    // tslint:disable-next-line: ban-ts-ignore
     // @ts-ignore
     .spyOn(global.Date, 'constructor')
     .mockImplementationOnce(() => new Date('2019-06-19T00:07:19.309Z'))
