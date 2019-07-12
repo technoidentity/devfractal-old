27f218ba1e9655187f4a948d15716dc9a3bd94b2
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-13 00:35:11 +0530

    handle more io-ts types in 'empty'

--- packages/devfractal-utils/src/emptyFromType.ts
+++ packages/devfractal-utils/src/emptyFromType.ts
@@ -1,19 +1,30 @@
 import {
+  AnyArrayType,
   ArrayType,
   BooleanType,
+  ExactType,
+  FunctionType,
   InterfaceType,
+  IntersectionType,
   KeyofType,
+  LiteralType,
   Mixed,
+  NullType,
   NumberType,
+  PartialType,
   Props,
   ReadonlyArrayType,
   ReadonlyType,
+  RefinementType,
   StringType,
+  TupleType,
   TypeC,
   TypeOf,
+  UndefinedType,
+  UnionType,
+  VoidType,
 } from 'io-ts'
-
-import { buildObject } from './common'
+import { buildObject, nop } from './common'
 
 function emptyFromPrimitive<T extends Mixed>(typeValue: T): TypeOf<T> {
   if (typeValue.name === 'Int' || typeValue instanceof NumberType) {
@@ -31,27 +42,70 @@ function emptyFromPrimitive<T extends Mixed>(typeValue: T): TypeOf<T> {
   if (typeValue instanceof KeyofType) {
     return Object.keys(typeValue.keys)[0]
   }
+  if (typeValue instanceof FunctionType) {
+    return nop
+  }
+  if (typeValue instanceof LiteralType) {
+    return typeValue.value
+  }
+  if (typeValue instanceof NullType) {
+    // tslint:disable-next-line: no-null-keyword
+    return null
+  }
+  if (typeValue instanceof UndefinedType || typeValue instanceof VoidType) {
+    return undefined
+  }
+
   throw new Error(`Unsupported type: ${typeValue.name}`)
 }
 
 function emptyFromObject<T extends Props>(
   typeValue: TypeC<T>,
 ): TypeOf<TypeC<T>> {
-  return buildObject(typeValue.props, emptyFromType)
+  return buildObject(typeValue.props, empty)
 }
 
-export function emptyFromType<T extends Mixed>(typeValue: T): TypeOf<T> {
-  if (typeValue instanceof ReadonlyType) {
-    return emptyFromObject(typeValue.type)
+export function empty<T extends Mixed>(typeValue: T): TypeOf<T> {
+  if (
+    typeValue instanceof ExactType ||
+    typeValue instanceof ReadonlyType ||
+    typeValue instanceof RefinementType
+  ) {
+    return empty(typeValue.type)
   }
+
   if (typeValue instanceof InterfaceType) {
     return emptyFromObject(typeValue)
   }
+
   if (
     typeValue instanceof ReadonlyArrayType ||
-    typeValue instanceof ArrayType
+    typeValue instanceof ArrayType ||
+    typeValue instanceof AnyArrayType
   ) {
     return []
+  }
+
+  if (typeValue instanceof IntersectionType) {
+    return typeValue.types
+      .map(empty)
+      .reduce((acc: any, x: any) => ({ ...acc, ...x }))
+  }
+
+  if (typeValue instanceof UnionType) {
+    return empty(typeValue.types[0])
+  }
+
+  if (typeValue instanceof PartialType) {
+    return buildObject(typeValue.props, empty)
+  }
+  // if (typeValue instanceof TaggedUnionType) {
+  //   return typeValue.types
+  //     .map(empty)
+  //     .reduce((acc: any, x: any) => ({ ...acc, ...x }))
+  // }
+  if (typeValue instanceof TupleType) {
+    return typeValue.types.map(empty)
   }
   return emptyFromPrimitive(typeValue)
 }
