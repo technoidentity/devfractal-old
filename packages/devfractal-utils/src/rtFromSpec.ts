cf400376b4260e415772684012e957cbad72bebf
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-14 00:54:59 +0530

    improve rtFromSpec and write tests

--- packages/devfractal-utils/src/rtFromSpec.ts
+++ packages/devfractal-utils/src/rtFromSpec.ts
@@ -2,15 +2,30 @@ import * as iots from 'io-ts'
 import tcomb from 'tcomb'
 import { buildObject } from './common'
 
-function rtFromPrimitiveSpec(
-  spec: iots.Mixed,
-): tcomb.Irreducible<any> | tcomb.Enums {
-  if (spec.name === 'Int') {
-    return tcomb.Integer
-  }
+function rtFromObjectSpec<T extends iots.Props>(
+  spec: iots.TypeC<T>,
+  options: { readonly strict: boolean } = { strict: false },
+): tcomb.Struct<T> {
+  return tcomb.struct(buildObject(spec.props, rtFromSpec), {
+    name: spec.name,
+    strict: options.strict,
+  })
+}
 
+export function rtFromSpec(
+  spec: iots.Mixed,
+):
+  | tcomb.Irreducible<any>
+  | tcomb.Enums
+  | tcomb.Struct<any>
+  | tcomb.Tuple<any>
+  | tcomb.Maybe<any> {
   if (spec instanceof iots.NumberType) {
     return tcomb.Number
+  }
+
+  if (spec.name === 'Int') {
+    return tcomb.Integer
   }
 
   if (spec instanceof iots.StringType) {
@@ -30,43 +45,25 @@ function rtFromPrimitiveSpec(
     return tcomb.Date
   }
 
-  if (spec instanceof iots.KeyofType) {
-    return tcomb.enums.of(Object.keys(spec.keys))
-  }
-
   if (spec instanceof iots.NullType || spec instanceof iots.UndefinedType) {
     return tcomb.Nil
   }
 
-  throw new Error(`Unsupported ${spec.name}`)
-}
+  if (spec instanceof iots.KeyofType) {
+    return tcomb.enums.of(Object.keys(spec.keys))
+  }
 
-function rtFromObjectSpec<T extends iots.Props>(
-  spec: iots.TypeC<T>,
-  options: { readonly strict?: boolean } = { strict: true },
-): tcomb.Struct<T> {
-  return tcomb.struct(buildObject(spec.props, rtFromSpec), {
-    name: spec.name,
-    strict: options.strict,
-  })
-}
+  if (spec instanceof iots.InterfaceType) {
+    return rtFromObjectSpec(spec)
+  }
 
-export function rtFromSpec(
-  spec: iots.Mixed,
-):
-  | tcomb.Irreducible<any>
-  | tcomb.Enums
-  | tcomb.Struct<any>
-  | tcomb.Tuple<any>
-  | tcomb.Maybe<any> {
-  if (
-    spec instanceof iots.ReadonlyType ||
-    spec instanceof iots.ExactType ||
-    spec instanceof iots.RefinementType
-  ) {
+  if (spec instanceof iots.ReadonlyType || spec instanceof iots.ExactType) {
     return rtFromSpec(spec.type)
   }
 
+  if (spec instanceof iots.RefinementType) {
+    return tcomb.refinement(rtFromSpec(spec.type), spec.predicate)
+  }
   if (
     spec instanceof iots.ArrayType ||
     spec instanceof iots.ReadonlyArrayType
@@ -78,22 +75,12 @@ export function rtFromSpec(
     return tcomb.list(tcomb.Any)
   }
 
-  if (spec instanceof iots.InterfaceType) {
-    return rtFromObjectSpec(spec)
-  }
-
   if (spec instanceof iots.IntersectionType) {
-    return spec.types
-      .map(rtFromObjectSpec)
-      .reduce((acc: any, x: any) => ({ ...acc, ...x }))
+    return tcomb.intersection(spec.types.map(rtFromSpec))
   }
 
   if (spec instanceof iots.UnionType) {
-    return rtFromSpec(spec.types[0])
-  }
-
-  if (spec instanceof iots.KeyofType) {
-    return tcomb.enums(spec.keys)
+    return tcomb.union(spec.types.map(rtFromSpec))
   }
 
   if (spec instanceof iots.TupleType) {
@@ -111,28 +98,9 @@ export function rtFromSpec(
     return rtFromObjectSpec(spec.props, { strict: true })
   }
 
-  return rtFromPrimitiveSpec(spec)
+  if (spec instanceof iots.ExactType) {
+    return rtFromObjectSpec(spec.type, { strict: true })
+  }
+
+  throw new Error(`Unsupported ${spec.name}`)
 }
-
-// const io = iots.type({
-//   fizz: iots.type({ x: iots.number, y: iots.number }),
-//   buzz: iots.array(iots.boolean),
-// })
-// const io = iots.type({
-//   fizz: iots.type({ x: iots.string, y: iots.boolean, z: date }),
-//   buzz: iots.array(iots.Int),
-// })
-
-// const io = iots.type({
-//   d: date,
-//   e: iots.keyof({ foo: 0, bar: 0 }),
-//   a: iots.array(iots.string),
-//   o: iots.type({ x: iots.number, y: iots.Int }),
-//   o2: iots.type({
-//     fizz: iots.array(iots.type({ buzz: iots.boolean })),
-//   }),
-// })
-
-// const value = fake(io)
-// const tc = tcombFromRT(io)
-// console.log(tc(value))
