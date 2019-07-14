22949065c94cb68e2bc5c013d492cf66ba549e12
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-14 12:48:38 +0530

    lambda to functions

--- packages/devfractal-utils/src/tcombRefinements.ts
+++ packages/devfractal-utils/src/tcombRefinements.ts
@@ -5,15 +5,21 @@ import * as t from 'tcomb'
 
 export const Positive = t.refinement(t.Number, n => n >= 0, 'Positive')
 export const Negative = t.refinement(t.Number, n => n <= 0, 'Negative')
-export const Min = (min: number) => t.refinement(t.Number, n => n >= min, 'Min')
-export const Max = (max: number) => t.refinement(t.Number, n => n <= max, 'Max')
+export function Min(min: number) {
+  return t.refinement(t.Number, n => n >= min, 'Min')
+}
+export function Max(max: number) {
+  return t.refinement(t.Number, n => n <= max, 'Max')
+}
 
 export const PositiveInt = t.refinement(t.Integer, n => n >= 0, 'PositiveInt')
 export const NegativeInt = t.refinement(t.Integer, n => n <= 0, 'NegativeInt')
-export const MinInt = (min: number) =>
-  t.refinement(t.Integer, n => n >= min, 'MinInt')
-export const MaxInt = (max: number) =>
-  t.refinement(t.Integer, n => n <= max, 'MaxInt')
+export function MinInt(min: number) {
+  return t.refinement(t.Integer, n => n >= min, 'MinInt')
+}
+export function MaxInt(max: number) {
+  return t.refinement(t.Integer, n => n <= max, 'MaxInt')
+}
 
 // eslint-disable-next-line
 const rEmail: RegExp = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i
@@ -32,19 +38,26 @@ export const Upper = t.refinement(
   s => s.toUpperCase() === s,
   'Uppercase',
 )
-export const Length = (len: number) =>
-  t.refinement(t.String, s => s.length === len, 'StringLength')
-export const MinStringLength = (min: number) =>
-  t.refinement(t.String, s => s.length >= min, 'MinStringLength')
-export const MaxStringLength = (max: number) =>
-  t.refinement(t.String, s => s.length <= max, 'MaxStringLength')
+export function Length(len: number) {
+  return t.refinement(t.String, s => s.length === len, 'StringLength')
+}
+export function MinStringLength(min: number) {
+  return t.refinement(t.String, s => s.length >= min, 'MinStringLength')
+}
+export function MaxStringLength(max: number) {
+  return t.refinement(t.String, s => s.length <= max, 'MaxStringLength')
+}
 
-export const MinDate = (min: Date) =>
-  t.refinement(t.Date, d => compareAsc(min, d) <= 0, 'MinDate')
-export const MaxDate = (max: Date) =>
-  t.refinement(t.Date, d => compareAsc(max, d) >= 0, 'MaxDate')
+export function MinDate(min: Date) {
+  return t.refinement(t.Date, d => compareAsc(min, d) <= 0, 'MinDate')
+}
+export function MaxDate(max: Date) {
+  return t.refinement(t.Date, d => compareAsc(max, d) >= 0, 'MaxDate')
+}
 
-export const ArrayMinLength = (min: number) =>
-  t.refinement(t.Array, arr => arr.length >= min, 'ArrayMinLength')
-export const ArrayMaxLength = (max: number) =>
-  t.refinement(t.Array, arr => arr.length <= max, 'ArrayMaxLength')
+export function ArrayMinLength(min: number) {
+  return t.refinement(t.Array, arr => arr.length >= min, 'ArrayMinLength')
+}
+export function ArrayMaxLength(max: number) {
+  return t.refinement(t.Array, arr => arr.length <= max, 'ArrayMaxLength')
+}
