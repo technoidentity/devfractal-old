f476b96ebbd6505eab768fd0e686681d335afd57
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-14 13:06:59 +0530

    improve empty test

--- packages/devfractal-utils/src/empty.test.ts
+++ packages/devfractal-utils/src/empty.test.ts
@@ -21,65 +21,101 @@ import {
 import { date } from 'io-ts-types/lib/date'
 import { empty } from './empty'
 
-test('primitive values', () => {
-  expect(empty(number)).toBe(0)
-  expect(empty(Int)).toBe(0)
-  expect(empty(string)).toBe('')
-  expect(empty(boolean)).toBe(false)
-  expect(empty(date)).toEqual(expect.any(Date))
-  expect(empty(keyof({ red: 1, blue: 2, green: 3 }))).toEqual('red')
-})
+// tslint:disable no-null-keyword typedef
 
-test('array', () => {
-  expect(empty(array(number))).toEqual([])
-  expect(empty(readonlyArray(number))).toEqual([])
-})
-
-test('object', () => {
-  expect(empty(type({ x: string, y: number }))).toEqual({
-    x: '',
-    y: 0,
+describe('empty from spec', () => {
+  test('primitive values', () => {
+    expect(empty(number)).toBe(0)
+    expect(empty(Int)).toBe(0)
+    expect(empty(string)).toBe('')
+    expect(empty(boolean)).toBe(false)
+    expect(empty(date)).toEqual(expect.any(Date))
   })
-  expect(empty(readonly(type({ x: string, y: number })))).toEqual({
-    x: '',
-    y: 0,
-  })
-})
 
-test('nested object and array', () => {
-  expect(
-    empty(
-      readonly(
-        partial({
-          int: Int,
-          number,
-          string,
-          boolean,
-          date,
-          any,
-          array: readonlyArray(boolean),
-          enum: keyof({ foo: 1, bar: 1 }),
-          exact: exact(type({ a: number, b: string })),
-          readonly: readonly(type({ x: number })),
-          readonlyArray: readonlyArray(type({ x: number })),
-          interface: readonly(
-            strict({
-              fizz: readonlyArray(readonly(type({ buzz: boolean }))),
-              buzz: array(type({ buzz: boolean })),
-            }),
-          ),
-          intersection: intersection([
-            type({ x: number }),
-            strict({ y: literal('hello'), z: nullType }),
-          ]),
-          union: union([number, string]),
-          tuple: tuple([number, string]),
-        }),
+  test('enum', () => {
+    expect(empty(keyof({ red: 1, blue: 2, green: 3 }))).toEqual('red')
+  })
+
+  test('array', () => {
+    expect(empty(array(number))).toEqual([])
+    expect(empty(readonlyArray(number))).toEqual([])
+  })
+
+  test('union', () => {
+    expect(empty(union([string, boolean]))).toEqual('')
+    expect(empty(union([boolean, string]))).toEqual(false)
+  })
+
+  test('tuple', () => {
+    expect(
+      empty(tuple([string, boolean, type({ x: number, y: Int })])),
+    ).toEqual(['', false, { x: 0, y: 0 }])
+  })
+
+  test('object', () => {
+    expect(empty(type({ x: string, y: number }))).toEqual({
+      x: '',
+      y: 0,
+    })
+
+    expect(empty(readonly(type({ x: string, y: number })))).toEqual({
+      x: '',
+      y: 0,
+    })
+
+    expect(empty(partial({ int: Int, number }))).toEqual({ int: 0, number: 0 })
+    expect(empty(strict({ int: Int, number }))).toEqual({ int: 0, number: 0 })
+    expect(empty(exact(type({ int: Int, number })))).toEqual({
+      int: 0,
+      number: 0,
+    })
+  })
+
+  test('intersection', () => {
+    expect(
+      empty(
+        intersection([
+          type({ x: number }),
+          strict({ y: literal('hello'), z: nullType }),
+        ]),
       ),
-    ),
-  ).toMatchInlineSnapshot(
-    { date: expect.any(Date) },
-    `
+    ).toEqual({ x: 0, y: 'hello', z: null })
+  })
+
+  test('nested object and array', () => {
+    expect(
+      empty(
+        readonly(
+          partial({
+            int: Int,
+            number,
+            string,
+            boolean,
+            date,
+            any,
+            array: readonlyArray(boolean),
+            enum: keyof({ foo: 1, bar: 1 }),
+            exact: exact(type({ a: number, b: string })),
+            readonly: readonly(type({ x: number })),
+            readonlyArray: readonlyArray(type({ x: number })),
+            interface: readonly(
+              strict({
+                fizz: readonlyArray(readonly(type({ buzz: boolean }))),
+                buzz: array(type({ buzz: boolean })),
+              }),
+            ),
+            intersection: intersection([
+              type({ x: number }),
+              strict({ y: literal('hello'), z: nullType }),
+            ]),
+            union: union([number, string]),
+            tuple: tuple([number, string]),
+          }),
+        ),
+      ),
+    ).toMatchInlineSnapshot(
+      { date: expect.any(Date) },
+      `
     Object {
       "any": "",
       "array": Array [],
@@ -113,5 +149,6 @@ test('nested object and array', () => {
       "union": 0,
     }
   `,
-  )
+    )
+  })
 })
