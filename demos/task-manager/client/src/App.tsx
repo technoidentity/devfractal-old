3e91d6bdc74b250513ea0274afef350a54ac55b7
sravanidande <sravanidnd89@gmail.com>
2019-07-17 17:02:05 +0530

    check TaskManager

--- demos/task-manager/client/src/App.tsx
+++ demos/task-manager/client/src/App.tsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import { Router, SafeRoute as Route } from 'technoidentity-devfractal'
-import { CreateTodoRoute } from './demo/13.CreateTodo'
-import { TodoListRoute } from './demo/15.TodoListRoute'
-import { EditTodoRoute } from './demo/16.EditTodo'
+import React from 'react';
+import { TaskManager } from './TaskManager';
 
-export const App: React.FC = () => (
-  <Router variant="browser">
-    <Route exact path="/" component={TodoListRoute} />
-    <Route exact path="/todos/add" component={CreateTodoRoute} />
-    <Route exact path="/todos/:id/edit" component={EditTodoRoute} />
-  </Router>
-)
+// export const App: React.FC = () => (
+//   <Router variant="browser">
+//     <Route exact path="/" component={TodoListRoute} />
+//     <Route exact path="/todos/add" component={CreateTodoRoute} />
+//     <Route exact path="/todos/:id/edit" component={EditTodoRoute} />
+//   </Router>
+// )
 
 // export const App: React.FC = () => <h1>Hello, World</h1>
 
@@ -43,4 +40,4 @@ export const App: React.FC = () => (
 //   )
 // }
 
-// export const App: React.FC = () => <TaskManager />
+export const App: React.FC = () => <TaskManager />
