d665a4b9917a76dd187d6b1582962db1dd05b40c
sravanidande <sravanidnd89@gmail.com>
2019-07-23 12:21:02 +0530

    rename Routes to MainRoute

--- packages/site/src/App.tsx
+++ packages/site/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { HashRouter } from 'react-router-dom'
-import { Routes } from './Routes'
+import { MainRoute } from './MainRoute'
 import { ScrollToTop } from './ScrollToTop'
 
 export const App: React.FC = () => (
   <HashRouter>
     <ScrollToTop>
-      <Routes />
+      <MainRoute />
     </ScrollToTop>
   </HashRouter>
 )
