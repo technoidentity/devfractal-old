e4c6d65d69526ac3a88663ea6baa5f84d29400fe
sravanidande <sravanidnd89@gmail.com>
2019-07-23 12:24:39 +0530

    seperate CompositeRoutes

---
+++ packages/site/src/routes/CompositesRoutes.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Route } from 'react-router'
+import { SimpleRedirect } from 'technoidentity-devfractal'
+import {
+  CardApp,
+  Cart,
+  Counter,
+  FieldsGeneralForm,
+  LoginFormVariants,
+  SimpleFormExample,
+  SimpleTodo,
+  Todo,
+} from 'technoidentity-devfractal-examples'
+
+export const CompositesRoutes: React.FC = () => (
+  <>
+    <SimpleRedirect exact from="/composites" to="/composites/simple-form" />
+    <Route path="/composites/simple-form" exact component={SimpleFormExample} />
+    <Route
+      path="/composites/general-form"
+      exact
+      component={FieldsGeneralForm}
+    />
+    <Route path="/composites/counter" exact component={Counter} />
+    <Route path="/composites/todo" exact component={Todo} />
+    {/* <Route path="/composites/treeview" exact component={TreeViewStructure} /> */}
+    <Route path="/composites/simple-todo" exact component={SimpleTodo} />
+    <Route path="/composites/github-card" exact component={CardApp} />
+    <Route path="/composites/cart-app" exact component={Cart} />
+    <Route
+      path="/composites/login-form-variants"
+      exact
+      component={LoginFormVariants}
+    />
+  </>
+)
