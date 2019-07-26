95d8cc615176252544b0deb1a69e423829f2e324
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-26 12:51:39 +0530

    add project router

---
+++ devfractal/router/src/RouterContext.tsx
@@ -0,0 +1,45 @@
+import { History, Location } from 'history'
+import { Mixed, TypeOf } from 'io-ts'
+import { parse } from 'query-string'
+import React from 'react'
+import { RouteChildrenProps } from 'react-router'
+import { cast, verify } from 'technoidentity-utils'
+// tslint:disable typedef
+
+interface RouterContext extends RouteChildrenProps {
+  setRouteMatched(value: boolean): void
+  getRouteMatched(): boolean
+}
+
+export const RouterContext: React.Context<RouterContext> = React.createContext<
+  RouterContext
+>((undefined as unknown) as RouterContext)
+
+export function useRouter(): RouterContext {
+  const result = React.useContext(RouterContext)
+  verify(result !== null)
+
+  return result
+}
+
+export function useHistory(): History {
+  const { history } = useRouter()
+
+  return history
+}
+
+export function useLocation(): Location {
+  const { location } = useRouter()
+
+  return location
+}
+
+export function useQuery<Spec extends Mixed>(
+  querySpec: Spec,
+): TypeOf<typeof querySpec> {
+  const location = useLocation()
+  const query = parse(location.search)
+  cast(querySpec, query)
+
+  return query
+}
