3a10ff0d901be1ba8864270c0ee4dc20e20e8d55
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-29 16:40:02 +0530

    initial commit

---
+++ packages/demos/task-manager/next-starter/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  globals: { 'ts-jest': { isolatedModules: true } },
+  setupFiles: ['<rootDir>/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+}
---
+++ packages/demos/task-manager/next-starter/pages/Index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import '../sass/styles.scss'
+
+const App = () => <h1 className="is-text is-size-1">Hello, World</h1>
+
+export default App
