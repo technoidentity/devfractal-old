dc7cdf714dac1162c14272ff8f09252909e6af31
Pervez Iqbal <pervezfunctor@gmail.com>
2019-07-31 18:38:04 +0530

    add io-ts types

---
+++ packages/demos/task-manager/diya-project/src/views/common.ts
@@ -0,0 +1,40 @@
+import { Int, keyof, string, TypeOf } from 'io-ts'
+import { date } from 'io-ts-types/lib/date'
+import { req } from 'technoidentity-utils'
+
+const Shift = keyof({
+  morning: true,
+  evening: true,
+})
+
+const Status = keyof({
+  active: true,
+  inActive: true,
+})
+
+type Shift = TypeOf<typeof Shift>
+
+type Status = TypeOf<typeof Status>
+
+export const SingleDriverDetails = req({
+  name: string,
+  lastActive: date,
+  shift: Shift,
+  status: Status,
+})
+
+const Group = keyof({
+  Retail: true,
+  Cargo: true,
+})
+
+type Group = TypeOf<typeof Group>
+
+export const SingleBatteryDetails = req({
+  name: string,
+  id: string,
+  group: Group,
+  remainingCycles: Int,
+  lastCharged: date,
+  status: Status,
+})
