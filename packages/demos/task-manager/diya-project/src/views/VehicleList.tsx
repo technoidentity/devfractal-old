d12fb54e0be1e1ecf8ed7baf0f466d409932d52c
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-02 14:15:24 +0530

    add Link to Vehiclelist component

--- packages/demos/task-manager/diya-project/src/views/VehicleList.tsx
+++ packages/demos/task-manager/diya-project/src/views/VehicleList.tsx
@@ -1,7 +1,8 @@
 import { readonlyArray, TypeOf } from 'io-ts'
 import React from 'react'
-import { component, SimpleTable } from 'technoidentity-devfractal'
-import { req } from 'technoidentity-utils'
+import { Link } from 'react-router-dom'
+import { ButtonsGroup, component, SimpleTable } from 'technoidentity-devfractal'
+import { fake, req } from 'technoidentity-utils'
 import { SingleVehicleDetails } from './common'
 
 const SingleVehicleProps = req({ singleVehicleDetails: SingleVehicleDetails })
@@ -14,6 +15,28 @@ const VehicleListProps = req({
 
 type VehicleListProps = TypeOf<typeof VehicleListProps>
 
-export const VehicleList = component(VehicleListProps, ({ vehicleList }) => (
-  <SimpleTable data={vehicleList} striped />
-))
+export const VehicleListForm = component(
+  VehicleListProps,
+  ({ vehicleList }) => (
+    <>
+      <ButtonsGroup alignment="right">
+        <Link to="/vehicles/add" className="button is-primary">
+          Add Vehicle
+        </Link>
+      </ButtonsGroup>
+      <SimpleTable data={vehicleList} striped />
+    </>
+  ),
+)
+
+export const multipleVehicles = (n: Number) => {
+  const vehicles = []
+  for (let i = 0; i < n; i += 1) {
+    vehicles.push(fake(SingleVehicleDetails))
+  }
+  return vehicles
+}
+
+export const VehicleList: React.FC = () => (
+  <VehicleListForm vehicleList={multipleVehicles(10)} />
+)
