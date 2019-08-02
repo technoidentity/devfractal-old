f300dfa61ed64067eb46d9dba34e8d73c2d57336
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-02 14:16:40 +0530

    small changes in menu component

--- packages/demos/task-manager/diya-project/src/views/MenuComponent.tsx
+++ packages/demos/task-manager/diya-project/src/views/MenuComponent.tsx
@@ -9,7 +9,6 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 import {
-  Box,
   Icon,
   Menu,
   MenuItem,
@@ -19,51 +18,59 @@ import {
 
 export const MenuComponent: React.FC = () => (
   <Section>
-    <Box>
-      <Menu>
-        <MenuList>
-          <MenuItem>
-            <a href="#!">
-              <Icon icon={faUsers} /> Drivers
-            </a>
-          </MenuItem>
-          <MenuItem>
-            <a href="#!">
-              <Icon icon={faBus} /> Vehicles
-            </a>
-          </MenuItem>
-          <MenuItem>
-            <a href="#!">
-              <Icon icon={faCarBattery} /> Battery
-            </a>
-          </MenuItem>
-          <MenuItem>
-            <a href="#!">
-              <Icon icon={faUserFriends} /> Clients
-            </a>
-          </MenuItem>
-          <MenuItem>
-            <a href="#!">
-              <Icon icon={faMapMarked} /> Geofences
-            </a>
-          </MenuItem>
-          <MenuItem>
-            <a href="#!">
-              <Icon icon={faUsers} /> Users
-            </a>
-          </MenuItem>
-          <MenuItem>
-            <a href="#!">
-              <Icon icon={faBell} /> Alerts
-            </a>
-          </MenuItem>
-          <MenuItem>
-            <a href="#!">
-              <Icon icon={faPaperPlane} /> Reports
-            </a>
-          </MenuItem>
-        </MenuList>
-      </Menu>
-    </Box>
+    <Menu
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 's',
+      }}
+    >
+      <MenuList>
+        {/* <MenuItem>
+            <Image src={diyaLogo} alt="diyaImage" size="64x64" />
+          </MenuItem> */}
+
+        <MenuItem>
+          <a href="/drivers">
+            <Icon icon={faUsers} /> Drivers
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a href="/vehicles">
+            <Icon icon={faBus} /> Vehicles
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a href="/batteries">
+            <Icon icon={faCarBattery} /> Battery
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a href="#!">
+            <Icon icon={faUserFriends} /> Clients
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a href="#!">
+            <Icon icon={faMapMarked} /> Geofences
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a href="#!">
+            <Icon icon={faUsers} /> Users
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a href="#!">
+            <Icon icon={faBell} /> Alerts
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a href="#!">
+            <Icon icon={faPaperPlane} /> Reports
+          </a>
+        </MenuItem>
+      </MenuList>
+    </Menu>
   </Section>
 )
