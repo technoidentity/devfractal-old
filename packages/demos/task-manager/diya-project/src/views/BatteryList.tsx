42b47831b2cd0e90bf47d3607dfe9b11b29a336a
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-02 14:11:39 +0530

    add Link to battery list component

--- packages/demos/task-manager/diya-project/src/views/BatteryList.tsx
+++ packages/demos/task-manager/diya-project/src/views/BatteryList.tsx
@@ -1,7 +1,8 @@
 import { readonlyArray, TypeOf } from 'io-ts'
 import React from 'react'
-import { component, SimpleTable } from 'technoidentity-devfractal'
-import { req } from 'technoidentity-utils'
+import { Link } from 'react-router-dom'
+import { ButtonsGroup, component, SimpleTable } from 'technoidentity-devfractal'
+import { fake, req } from 'technoidentity-utils'
 import { SingleBatteryDetails } from './common'
 
 export const SingleBatteryProps = req({
@@ -16,6 +17,28 @@ const BatteryListProps = req({
 
 type BatteryListProps = TypeOf<typeof BatteryListProps>
 
-export const BatteryList = component(BatteryListProps, ({ batteryList }) => (
-  <SimpleTable data={batteryList} striped />
-))
+export const BatteryListForm = component(
+  BatteryListProps,
+  ({ batteryList }) => (
+    <>
+      <ButtonsGroup alignment="right">
+        <Link to="/batteries/add" className="button is-primary">
+          Add Battery
+        </Link>
+      </ButtonsGroup>
+      <SimpleTable data={batteryList} striped />
+    </>
+  ),
+)
+
+export const multipleBatteries = (n: Number) => {
+  const batteries = []
+  for (let i = 0; i < n; i += 1) {
+    batteries.push(fake(SingleBatteryDetails))
+  }
+  return batteries
+}
+
+export const BatteryList: React.FC = () => (
+  <BatteryListForm batteryList={multipleBatteries(10)} />
+)
