721f7be1fa27628cdea1e5d2107703e6090ad58a
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-02 18:30:51 +0530

    improve fakeJSONServer

--- packages/dev-utils/src/index.ts
+++ packages/dev-utils/src/index.ts
@@ -1,17 +1,19 @@
 import * as io from 'io-ts'
 import * as server from 'json-server'
-import { buildObject, fake } from 'technoidentity-utils'
-
-const port = process.env.PORT || 3000
+import { buildObject, fake, range } from 'technoidentity-utils'
 
 export function startFakeJSONServer<
   T extends Record<string, io.Type<any, any>>
->(faker: T) {
+>(faker: T, port = process.env.PORT || 5555, eachCount: number = 10) {
   server
     .create()
     .use(server.defaults())
-    .use(server.router(buildObject(faker, v => fake(io.readonlyArray(v)))))
+    .use(
+      server.router(
+        buildObject(faker, v => range(eachCount).map(_ => fake(v))),
+      ),
+    )
     .listen(port, () => {
-      console.log('fake JSON Server is running')
+      console.log(`fake JSON Server is running at port: ${port}`)
     })
 }
