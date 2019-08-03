e8e71a1c2f022781223f0a9a60f51b17462413bc
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-03 23:11:19 +0530

    improve ManyQuery and add toJSONServerQuery

---
+++ packages/devfractal/api/src/rest.test.ts
@@ -0,0 +1,42 @@
+import * as t from 'io-ts'
+import { opt, req } from 'technoidentity-utils'
+import { manyQuery, ManyQuerySpec, toJSONServerQuery } from './rest'
+
+// tslint:disable typedef
+
+export interface ManyQuery<C extends t.Props>
+  extends t.TypeOf<ManyQuerySpec<C>> {}
+
+it('toJSONServerQuery', () => {
+  const User = req({
+    name: t.string,
+    age: t.number,
+    address: opt({ street: t.string, city: t.string }),
+  })
+
+  const userQuery: ManyQuerySpec<typeof User.type.props> = manyQuery(
+    User.type.props,
+  )
+
+  const query: ManyQuery<typeof User.type.props> = {
+    filter: { name: 'foo', age: 20 },
+    asc: ['name'],
+    desc: ['age'],
+    fullText: 'p',
+    range: { current: 0, limit: 10 },
+    embed: 'address',
+  }
+
+  expect(toJSONServerQuery(userQuery, query)).toMatchInlineSnapshot(
+    `"_limit=10&_order=asc,desc&_page=0&_sort=name,age&age=20&embed=address&name=foo&q=p"`,
+  )
+
+  const query2: ManyQuery<typeof User.type.props> = {
+    asc: ['name', 'age'],
+    range: { start: 0, end: 10 },
+  }
+
+  expect(toJSONServerQuery(userQuery, query2)).toMatchInlineSnapshot(
+    `"_end=10&_order=asc,asc&_sort=name,age&_start=0"`,
+  )
+})
--- packages/devfractal/api/src/rest.ts
+++ packages/devfractal/api/src/rest.ts
@@ -1,29 +1,80 @@
 import { produce } from 'immer'
 import * as t from 'io-ts'
-import { Array } from 'tcomb'
+import { stringify } from 'query-string'
 import { cast, opt } from 'technoidentity-utils'
 import { http as httpAPI, MethodArgs, RequestConfig } from './http'
-
 // tslint:disable typedef
 
-// Currently will not support intersection etc...
-// export function ManyQuery<C extends t.Props>(codec: t.ReadonlyC<t.TypeC<C>>) {
-//   return opt({
-//     page: t.number,
-//     limit: t.number,
-//     asc: t.keyof(codec.type.props), 
-//     desc: t.keyof(codec.type.props),
-//   })
-// }
+const Page = opt({ current: t.number, limit: t.number })
+const Slice = opt({ start: t.number, end: t.number, limit: t.number })
 
-export function ManyQuery<C extends t.Props>(codec: t.ReadonlyC<t.TypeC<C>>) {
+// const Operators = opt({
+//   gt: t.number,
+//   lt: t.number,
+//   gte: t.number,
+//   lte: t.number,
+//   ne: NumOrStr,
+//   like: t.string,
+// })
+
+export type ManyQuerySpec<C extends t.Props> = t.ReadonlyC<
+  t.PartialC<{
+    readonly filter: t.PartialC<C>
+    // tslint:disable-next-line: readonly-array
+    readonly range: t.UnionC<[typeof Page, typeof Slice]>
+    readonly asc: t.ReadonlyArrayC<t.KeyofC<C>>
+    readonly desc: t.ReadonlyArrayC<t.KeyofC<C>>
+    readonly fullText: t.StringC
+    // readonly operators: t.RecordC<t.KeyofC<C>, typeof Operators>
+    readonly embed: t.KeyofC<C>
+  }>
+>
+
+export function manyQuery<C extends t.Props>(props: C): ManyQuerySpec<C> {
   return opt({
-    _page: t.number,
-    _limit: t.number,
-    _sort: t.keyof(codec.type.props),
-    _order: t.keyof({asc:true,desc:true}),
-    
+    filter: t.partial(props),
+    range: t.union([Page, Slice]),
+    asc: t.readonlyArray(t.keyof(props)),
+    desc: t.readonlyArray(t.keyof(props)),
+    fullText: t.string,
+    // operators: t.record(keys(codec), Operators),
+    embed: t.keyof(props),
   })
+}
+
+export type ManyQuery<C extends t.Props> = t.TypeOf<ManyQuerySpec<C>>
+
+export function toJSONServerQuery<C extends t.Props>(
+  codec: ManyQuerySpec<C>,
+  query: ManyQuery<C>,
+): string {
+  cast(codec, query)
+
+  const { range } = query
+  const page = Page.is(range)
+    ? { _page: range.current, _limit: range.limit }
+    : {}
+
+  const slice = Slice.is(range)
+    ? {
+        _start: range.start,
+        _end: range.end,
+        _limit: range.limit,
+      }
+    : {}
+
+  const { asc, desc } = query
+  const _sort = (asc || []).concat(desc || [])
+
+  const _order = (asc || [])
+    .map(_ => 'asc')
+    .concat((desc || []).map(_ => 'desc'))
+
+  const { filter, fullText: q, embed } = query
+  return stringify(
+    { ...filter, ...page, ...slice, _sort, _order, q, embed },
+    { arrayFormat: 'comma' },
+  )
 }
 
 type APIMethodArgs = Omit<MethodArgs, 'resource'>
