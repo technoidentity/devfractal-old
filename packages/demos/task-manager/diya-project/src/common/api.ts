406071c1052e7ed193a6b4875336896dcbd82e3f
sravanidande <sravanidnd89@gmail.com>
2019-08-05 15:29:19 +0530

    add API for battery and vehicle

--- packages/demos/task-manager/diya-project/src/common/api.ts
+++ packages/demos/task-manager/diya-project/src/common/api.ts
@@ -1,5 +1,5 @@
 import { rest } from 'technoidentity-devfractal'
-import { Driver } from './models'
+import { Battery, Driver, Vehicle } from './models'
 
 export const apiOptions = {
   baseURL: 'http://localhost:5555',
@@ -8,5 +8,17 @@ export const apiOptions = {
 export const driverAPI = rest({
   resource: 'drivers',
   type: Driver,
+  ...apiOptions,
+})
+
+export const batteryAPI = rest({
+  resource: 'batteries',
+  type: Battery,
+  ...apiOptions,
+})
+
+export const vehicleAPI = rest({
+  resource: 'vehicles',
+  type: Vehicle,
   ...apiOptions,
 })
