9812d079803e6150d47b279a07e64d1dde28baa6
sravanidande <sravanidnd89@gmail.com>
2019-08-08 16:34:13 +0530

    add Create user route

---
+++ packages/demos/task-manager/diya-project/src/pages/CreateUserRoute.tsx
@@ -0,0 +1,15 @@
+import { omit } from 'lodash-es'
+import React from 'react'
+import { Post, Section } from 'technoidentity-devfractal'
+import { User, userAPI } from '../common'
+import { UserForm } from '../views'
+
+export const CreateUserRoute: React.FC = () => (
+  <>
+    <Post<User>
+      onPost={values => userAPI.create(omit(values, 'id'))}
+      redirectPath="/users"
+      component={UserForm}
+    />
+  </>
+)
