278626a53310b37ac923ed98bf9af70f6f84716d
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-08 16:33:45 +0530

    move getProps to iotsUtils

--- packages/utils/src/iotsUtils.ts
+++ packages/utils/src/iotsUtils.ts
@@ -65,3 +65,41 @@ export function props<O extends t.Props, R extends t.Props>(
 // tslint:enable readonly-array
 
 export const lit: typeof t.literal = t.literal
+
+import * as t from 'io-ts'
+
+// tslint:disable readonly-array array-type readonly-keyword
+export interface HasPropsReadonly
+  extends t.ReadonlyType<HasProps, any, any, any> {}
+
+export interface HasPropsIntersection  // tslint:disable-next-line: readonly-array
+  extends t.IntersectionType<
+    [HasProps, HasProps, ...Array<HasProps>],
+    any,
+    any,
+    any
+  > {}
+
+export type HasPropsOnType = HasPropsReadonly | t.ExactType<any, any, any, any>
+
+export type HasPropsOnProps =
+  | t.InterfaceType<any, any, any, any>
+  | t.PartialType<any, any, any, any>
+
+export type HasProps = HasPropsIntersection | HasPropsOnProps | HasPropsOnType
+
+export function getProps<T extends t.Mixed>(codec: T & HasProps): t.Props {
+  switch (codec._tag) {
+    case 'ReadonlyType':
+    case 'ExactType':
+      return getProps(codec.type)
+    case 'InterfaceType':
+    case 'PartialType':
+      return codec.props
+    case 'IntersectionType':
+      return codec.types.reduce<t.Props>(
+        (props, type) => ({ ...props, ...getProps(type) }),
+        {},
+      )
+  }
+}
