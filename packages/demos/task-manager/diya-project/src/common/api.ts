b64165aed14235a2696d100638db46e3d8e53fb4
sravanidande <sravanidnd89@gmail.com>
2019-08-08 15:38:42 +0530

    add clientAPI

--- packages/demos/task-manager/diya-project/src/common/api.ts
+++ packages/demos/task-manager/diya-project/src/common/api.ts
@@ -1,5 +1,5 @@
 import { rest } from 'technoidentity-devfractal'
-import { Battery, Driver, Vehicle } from './models'
+import { Battery, Client, Driver, User, Vehicle } from './models'
 
 export const apiOptions = {
   baseURL: 'http://localhost:9999',
@@ -13,3 +13,7 @@ export const batteryAPI = rest(Battery, {
   resource: 'batteries',
   ...apiOptions,
 })
+
+export const clientAPI = rest(Client, { resource: 'clients', ...apiOptions })
+
+// export const userAPI = rest(User, { resource: 'users', ...apiOptions })
