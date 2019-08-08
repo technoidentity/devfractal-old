f038b7a3ac2e26bc435f424961fa5b85c7f0a42c
sravanidande <sravanidnd89@gmail.com>
2019-08-08 16:33:27 +0530

    add users to faker

--- packages/demos/task-manager/diya-project/src/db.ts
+++ packages/demos/task-manager/diya-project/src/db.ts
@@ -1,11 +1,12 @@
 import { startFakeJSONServer } from 'technoidentity-dev-utils'
-import { Battery, Client, Driver, Vehicle } from './common'
+import { Battery, Client, Driver, User, Vehicle } from './common'
 
 const faker = {
   drivers: Driver,
   vehicles: Vehicle,
   batteries: Battery,
   clients: Client,
+  users: User,
 }
 
 startFakeJSONServer(faker, 9999, 10)
