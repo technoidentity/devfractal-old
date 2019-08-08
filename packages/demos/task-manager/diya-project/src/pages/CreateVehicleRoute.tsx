141a69c41324f400b48fed616ae036ed82dbe8b6
sravanidande <sravanidnd89@gmail.com>
2019-08-08 16:34:26 +0530

    small fix

--- packages/demos/task-manager/diya-project/src/pages/CreateVehicleRoute.tsx
+++ packages/demos/task-manager/diya-project/src/pages/CreateVehicleRoute.tsx
@@ -1,13 +1,15 @@
 import { omit } from 'lodash-es'
 import React from 'react'
-import { Post } from 'technoidentity-devfractal'
+import { Post, Section } from 'technoidentity-devfractal'
 import { Vehicle, vehicleAPI } from '../common'
 import { VehicleForm } from '../views'
 
 export const CreateVehicleRoute: React.FC = () => (
-  <Post<Vehicle>
-    redirectPath="/vehicles"
-    onPost={values => vehicleAPI.create(omit(values, 'id'))}
-    component={VehicleForm}
-  />
+  <Section>
+    <Post<Vehicle>
+      redirectPath="/vehicles"
+      onPost={values => vehicleAPI.create(omit(values, 'id'))}
+      component={VehicleForm}
+    />
+  </Section>
 )
