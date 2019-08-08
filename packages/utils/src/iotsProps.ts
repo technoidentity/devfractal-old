06297567d416aa986462d15f86962cd9d772a7aa
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-08 16:24:22 +0530

    I can't implement PropsOf, I am dumb!

--- packages/utils/src/iotsProps.ts
+++ packages/utils/src/iotsProps.ts
@@ -1,62 +1,26 @@
 import * as t from 'io-ts'
 
+// tslint:disable readonly-array array-type readonly-keyword
 export interface HasPropsReadonly
   extends t.ReadonlyType<HasProps, any, any, any> {}
 
 export interface HasPropsIntersection  // tslint:disable-next-line: readonly-array
-  extends t.IntersectionType<HasProps[], any, any, any> {}
+  extends t.IntersectionType<
+    [HasProps, HasProps, ...Array<HasProps>],
+    any,
+    any,
+    any
+  > {}
 
-export type HasProps =
-  | HasPropsIntersection
-  | HasPropsReadonly
-  | t.ExactType<any, any, any, any>
+export type HasPropsOnType = HasPropsReadonly | t.ExactType<any, any, any, any>
+
+export type HasPropsOnProps =
   | t.InterfaceType<any, any, any, any>
   | t.PartialType<any, any, any, any>
 
-type OfProps<
-  T extends
-    | t.InterfaceType<any, any, any, any>
-    | t.PartialType<any, any, any, any>
-> = T['props']
+export type HasProps = HasPropsIntersection | HasPropsOnProps | HasPropsOnType
 
-// tslint:disable readonly-keyword readonly-array
-export type OfIntersection<
-  CS extends [HasProps, HasProps, ...HasProps[]]
-> = CS extends {
-  length: 2
-}
-  ? Of<CS[0]> & Of<CS[1]>
-  : CS extends {
-      length: 3
-    }
-  ? Of<CS[0]> & Of<CS[1]> & Of<CS[2]>
-  : CS extends {
-      length: 4
-    }
-  ? Of<CS[0]> & Of<CS[1]> & Of<CS[2]> & Of<CS[3]>
-  : CS extends {
-      length: 5
-    }
-  ? Of<CS[0]> & Of<CS[1]> & Of<CS[2]> & Of<CS[3]> & Of<CS[4]>
-  : unknown
-
-// tslint:enable readonly-keyword readonly-array
-
-type OfType<
-  T extends HasPropsReadonly | t.ExactType<any, any, any, any>
-> = T['type']['props']
-
-type Of<T extends HasProps> = T extends
-  | t.InterfaceType<any, any, any, any>
-  | t.PartialType<any, any, any, any>
-  ? OfProps<T>
-  : T extends HasPropsReadonly | t.ExactType<any, any, any, any>
-  ? OfType<T>
-  : unknown
-
-export function getProps<T extends t.Mixed>(
-  codec: T & HasProps,
-): Of<typeof codec> {
+export function getProps<T extends t.Mixed>(codec: T & HasProps): t.Props {
   switch (codec._tag) {
     case 'ReadonlyType':
     case 'ExactType':
@@ -65,7 +29,7 @@ export function getProps<T extends t.Mixed>(
     case 'PartialType':
       return codec.props
     case 'IntersectionType':
-      return codec.types.reduce<any>(
+      return codec.types.reduce<t.Props>(
         (props, type) => ({ ...props, ...getProps(type) }),
         {},
       )
