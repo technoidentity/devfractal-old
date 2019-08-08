ad184aa6b40e5ebcfe8321fac447c2ced79c452a
sravanidande <sravanidnd89@gmail.com>
2019-08-08 18:59:37 +0530

    make edit working

--- packages/demos/task-manager/diya-project/src/views/Actions.tsx
+++ packages/demos/task-manager/diya-project/src/views/Actions.tsx
@@ -1,35 +1,23 @@
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
-import { TypeOf } from 'io-ts'
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { string, TypeOf } from 'io-ts'
 import React from 'react'
-import { Button, Icon, Section } from 'technoidentity-devfractal'
-import { fn, req } from 'technoidentity-utils'
+import { Link } from 'react-router-dom'
+import { Icon } from 'technoidentity-devfractal'
+import { req } from 'technoidentity-utils'
 
 const ActionsProps = req({
-  onEdit: fn<() => void>(),
-  onDelete: fn<() => void>(),
+  editUrl: string,
 })
 
 type ActionsProps = TypeOf<typeof ActionsProps>
 
-export const Actions: React.FC<ActionsProps> = ({ onEdit, onDelete }) => (
-  <Section>
-    <>
-      <Button
-        onClick={() => {
-          onEdit()
-        }}
-      >
-        <Icon icon={faTrashAlt} />
-        Delete
-      </Button>
-      <Button
-        onClick={() => {
-          onDelete()
-        }}
-      >
-        <Icon icon={faTrashAlt} />
-        Delete
-      </Button>
-    </>
-  </Section>
+export const Actions: React.FC<ActionsProps> = ({ editUrl }) => (
+  <>
+    <Link to={editUrl}>
+      <Icon icon={faEdit} />
+    </Link>
+    <Link to="">
+      <Icon icon={faTrash} />
+    </Link>
+  </>
 )
