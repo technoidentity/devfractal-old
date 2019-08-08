50a9b5a9ba7a4a2427e6382c51dfb51db34c6a08
sravanidande <sravanidnd89@gmail.com>
2019-08-08 19:00:08 +0530

    add edit action for userList

--- packages/demos/task-manager/diya-project/src/views/UserList.tsx
+++ packages/demos/task-manager/diya-project/src/views/UserList.tsx
@@ -3,8 +3,9 @@ import { readonlyArray, TypeOf } from 'io-ts'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Get, SimpleTable } from 'technoidentity-devfractal'
-import { req } from 'technoidentity-utils'
+import { fn, req } from 'technoidentity-utils'
 import { User, userAPI } from '../common'
+import { Actions } from './Actions'
 import { StaticPagination } from './Pagination'
 
 export const UserFormProps = req({
@@ -13,17 +14,25 @@ export const UserFormProps = req({
 
 type UserFormProps = TypeOf<typeof UserFormProps>
 
-export const UserListForm = component(UserFormProps, ({ userList }) => (
-  <>
-    <ButtonsGroup alignment="right">
-      <Link to="/users/add" className="button is-primary">
-        Add user
-      </Link>
-    </ButtonsGroup>
-    <SimpleTable data={userList} />
-  </>
-))
-
+export const UserListForm = component(UserFormProps, ({ userList }) => {
+  return (
+    <>
+      <ButtonsGroup alignment="right">
+        <Link to="/users/add" className="button is-primary">
+          Add user
+        </Link>
+      </ButtonsGroup>
+      <SimpleTable data={userList} headers={['userName', 'role', 'Actions']}>
+        {(key, value) =>
+          key === 'Actions' ? (
+            <Actions editUrl={`users/${value.id}/edit`} />
+          ) : // tslint:disable-next-line: no-null-keyword
+          null
+        }
+      </SimpleTable>
+    </>
+  )
+})
 export const UserListTable = component(UserFormProps, ({ userList }) => (
   <Section>
     <Title size="4" textColor="info">
