f722dc8cf94be0bd88757e674c0e2d19e16b4f84
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-09 11:25:26 +0530

    add test for toQuery

--- packages/devfractal/api/src/query.test.ts
+++ packages/devfractal/api/src/query.test.ts
@@ -1,23 +1,23 @@
 import * as t from 'io-ts'
 import { opt, props } from 'technoidentity-utils'
-import { Query, toJSONServerQuery } from './query'
+import { Query, toJSONServerQuery, toQuery } from './query'
 
 // tslint:disable typedef
 
-it('toJSONServerQuery', () => {
-  const User = props(
-    {
-      id: t.number,
-      name: t.string,
-    },
-    {
-      age: t.number,
-      address: opt({ street: t.string, city: t.string }),
-    },
-  )
+const User = props(
+  {
+    id: t.number,
+    name: t.string,
+  },
+  {
+    age: t.number,
+    address: opt({ street: t.string, city: t.string }),
+  },
+)
 
-  type User = t.TypeOf<typeof User>
+type User = t.TypeOf<typeof User>
 
+describe('query', () => {
   const query: Query<User> = {
     filter: { name: 'foo', age: 20 },
     asc: ['name'],
@@ -27,16 +27,28 @@ it('toJSONServerQuery', () => {
     embed: 'address',
   }
 
-  expect(toJSONServerQuery(User, query)).toMatchInlineSnapshot(
-    `"_limit=10&_order=asc,desc&_page=0&_sort=name,age&age=20&embed=address&name=foo&q=p"`,
-  )
-
   const query2: Query<User> = {
     asc: ['name', 'age'],
     range: { start: 0, end: 10 },
   }
 
-  expect(toJSONServerQuery(User, query2)).toMatchInlineSnapshot(
-    `"_end=10&_order=asc,asc&_sort=name,age&_start=0"`,
-  )
+  it('toJSONServerQuery', () => {
+    expect(toJSONServerQuery(User, query)).toMatchInlineSnapshot(
+      `"_limit=10&_order=asc,desc&_page=0&_sort=name,age&age=20&embed=address&name=foo&q=p"`,
+    )
+
+    expect(toJSONServerQuery(User, query2)).toMatchInlineSnapshot(
+      `"_end=10&_order=asc,asc&_sort=name,age&_start=0"`,
+    )
+  })
+
+  it('toQuery', () => {
+    expect(toQuery(User, query)).toMatchInlineSnapshot(
+      `"age=20&asc=name&current=0&desc=age&embed=address&limit=10&name=foo&q=p"`,
+    )
+
+    expect(toQuery(User, query2)).toMatchInlineSnapshot(
+      `"asc=name,age&end=10&start=0"`,
+    )
+  })
 })
