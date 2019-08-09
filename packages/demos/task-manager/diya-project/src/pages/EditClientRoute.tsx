4771b43e67536e2376a340f904a88176a2605b7c
sravanidande <sravanidnd89@gmail.com>
2019-08-09 11:45:10 +0530

    add edit client route form

---
+++ packages/demos/task-manager/diya-project/src/pages/EditClientRoute.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Put, useMatch } from 'technoidentity-devfractal'
+import { Client, clientAPI, Params } from '../common'
+import { ClientForm } from '../views'
+
+export const EditClientRoute: React.FC = () => {
+  const { params } = useMatch(Params)
+
+  return (
+    <Put<Client>
+      id={params.id}
+      doGet={clientAPI.get}
+      onPut={clientAPI.update}
+      component={ClientForm}
+      redirectPath="/clients"
+    />
+  )
+}
