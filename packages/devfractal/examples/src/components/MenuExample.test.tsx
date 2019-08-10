a38f4fb5360dc6d052964f224d00294f2132cf4d
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-10 10:25:03 +0530

    update menu test

--- packages/devfractal/examples/src/components/MenuExample.test.tsx
+++ packages/devfractal/examples/src/components/MenuExample.test.tsx
@@ -1,8 +1,13 @@
+import { Router, SafeRoute as Route } from 'devfractal-router'
 import React from 'react'
 import renderer, { ReactTestRenderer } from 'react-test-renderer'
 import { MenuExample } from './MenuExample'
 
 test('Menu Example', () => {
-  const component: ReactTestRenderer = renderer.create(<MenuExample />)
+  const component: ReactTestRenderer = renderer.create(
+    <Router variant="hash">
+      <Route to="/" component={MenuExample} />
+    </Router>,
+  )
   expect(component.toJSON()).toMatchSnapshot()
 })
