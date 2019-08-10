1d4700838ca9a1fc9f7a65a812d45fb3f08363a0
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-10 15:19:37 +0530

    add Patch component

---
+++ packages/devfractal/api/src/Patch.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { SubmitAction } from './common'
+import { Get } from './Get'
+import { ServerError } from './ServerError'
+import { useSubmitRedirect } from './useSubmit'
+
+export interface PatchComponentProps<T> {
+  readonly initial?: T
+  readonly onSubmit: SubmitAction<T>
+}
+
+export interface PatchProps<T, ID extends keyof T> {
+  readonly redirectPath?: string
+  readonly id: T[ID]
+  doGet(id: T[ID]): Promise<T>
+  onPatch(id: T[ID], values: Partial<T>): Promise<T>
+  readonly component: React.FC<PatchComponentProps<T>>
+}
+
+export function Patch<T, ID extends keyof T>({
+  id,
+  redirectPath,
+  doGet,
+  onPatch,
+  component: Component,
+}: PatchProps<T, ID>): JSX.Element {
+  async function update(data: T): Promise<T> {
+    return onPatch(id, data)
+  }
+
+  const { serverError, onSubmit } = useSubmitRedirect(update, redirectPath)
+
+  return (
+    <>
+      <ServerError error={serverError} />
+      <Get asyncFn={doGet} deps={[id]}>
+        {(data: T) => <Component initial={data} onSubmit={onSubmit} />}
+      </Get>
+    </>
+  )
+}
