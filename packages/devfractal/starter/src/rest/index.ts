7e95926908e9bcc69c67e29eb9b7193dcadd0119
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-11 15:03:06 +0530

    tslint fix

--- packages/devfractal/starter/src/rest/index.ts
+++ packages/devfractal/starter/src/rest/index.ts
@@ -1,7 +1,3 @@
-import { API } from 'devfractal-api'
-import { HasProps, Mixed, TypeOf } from 'io-ts'
-import React from 'react'
-
 export * from './SimpleGet'
 export * from './SimplePost'
 export * from './SimplePut'
