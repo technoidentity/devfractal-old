8796c3f992321352010fc595499784015ffab2ea
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-11 14:10:37 +0530

    export Simple Get/Post/Put

---
+++ packages/devfractal/starter/src/rest/index.ts
@@ -0,0 +1,7 @@
+import { API } from 'devfractal-api'
+import { HasProps, Mixed, TypeOf } from 'io-ts'
+import React from 'react'
+
+export * from './SimpleGet'
+export * from './SimplePost'
+export * from './SimplePut'
