937afbd5e7c403b4edea80a8fb465bac3d144e64
sravanidande <sravanidnd89@gmail.com>
2019-08-13 15:56:40 +0530

    add MenuComponent Route component

---
+++ packages/demos/task-manager/diya-project/src/views/MenuComponentRoute.tsx
@@ -0,0 +1,43 @@
+import {
+  faBookReader,
+  faBus,
+  faLocationArrow,
+  faMapMarked,
+  faMoneyBill,
+  faNotesMedical,
+  faStickyNote,
+  faUser,
+  faUsers,
+} from '@fortawesome/free-solid-svg-icons'
+import { Menu, MenuItem, MenuList } from 'devfractal-ui'
+import { Icon, Section } from 'devfractal-ui-core'
+import React from 'react'
+
+export const MenuComponentRoute: React.FC = () => (
+  <Section>
+    <Menu>
+      <MenuList
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <MenuItem href="/employees">
+          <Icon icon={faUsers} /> Employee
+        </MenuItem>
+        <MenuItem href="/evs">
+          <Icon icon={faBus} /> EVS Assigned
+        </MenuItem>
+        <MenuItem href="/planRoute">
+          <Icon icon={faLocationArrow} /> Plan Route
+        </MenuItem>
+        <MenuItem href="/invoices">
+          <Icon icon={faStickyNote} /> Invoices
+        </MenuItem>
+        <MenuItem href="/viewTrips">
+          <Icon icon={faMapMarked} /> View Trips
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  </Section>
+)
