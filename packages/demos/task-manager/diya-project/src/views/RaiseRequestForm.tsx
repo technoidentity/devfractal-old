0623ea65948b1aea5dc0d923e8cec49bcfc68324
sravanidande <sravanidnd89@gmail.com>
2019-08-13 15:57:52 +0530

    add Raise request form

---
+++ packages/demos/task-manager/diya-project/src/views/RaiseRequestForm.tsx
@@ -0,0 +1,43 @@
+import { FormikActions } from 'formik'
+import { TypeOf } from 'io-ts'
+import React from 'react'
+import { component, Section, Simple, Title } from 'technoidentity-devfractal'
+import { empty, fn, props } from 'technoidentity-utils'
+import { Evs } from '../common'
+
+const RaiseRequestFormProps = props(
+  { initial: Evs },
+  {
+    onSubmit: fn<(values: Evs, Actions: FormikActions<Evs>) => Promise<void>>(),
+  },
+)
+
+type RaiseRequestFormProps = TypeOf<typeof RaiseRequestFormProps>
+
+const initialValues = empty(Evs)
+
+export const RaiseRequestForm = component(
+  RaiseRequestFormProps,
+  ({ initial, onSubmit }) => (
+    <>
+      <Title size="4">Raise Request</Title>
+      <Section>
+        <Simple.Form
+          initialValues={initial || initialValues}
+          onSubmit={onSubmit}
+        >
+          <Simple.Select name="addtionalEVsRequired">
+            <option>select</option>
+          </Simple.Select>
+          <Simple.Select name="frequency">
+            <option value="once">Once</option>
+            <option value="weekly">Weekly</option>
+            <option value="monthly">Monthly</option>
+            <option value="yearly">yearly</option>
+          </Simple.Select>
+          <Simple.FormButtons />
+        </Simple.Form>
+      </Section>
+    </>
+  ),
+)
