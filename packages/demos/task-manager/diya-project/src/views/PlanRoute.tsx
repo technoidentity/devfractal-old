b689e0d87c56c12a95a3483f918471f0b9541655
sravanidande <sravanidnd89@gmail.com>
2019-08-13 15:57:08 +0530

    add PlanRoute component

---
+++ packages/demos/task-manager/diya-project/src/views/PlanRoute.tsx
@@ -0,0 +1,37 @@
+import { Get } from 'devfractal-api'
+import { SimpleTable } from 'devfractal-simple'
+import { component, Section, Title } from 'devfractal-ui-core'
+import { readonlyArray, TypeOf } from 'io-ts'
+import React from 'react'
+import { req } from 'technoidentity-utils'
+import { PlanRoute, planRouteAPI } from '../common'
+import { Actions } from '../views'
+
+const PlanRouteProps = req({ data: readonlyArray(PlanRoute) })
+
+type PlanRouteProps = TypeOf<typeof PlanRouteProps>
+
+export const PlanRouteView: React.FC<PlanRouteProps> = ({ data }) => (
+  <SimpleTable
+    data={data}
+    headers={['customerName', 'address', 'contactNumber', 'status', 'Actions']}
+  >
+    {(key, values) =>
+      // tslint:disable-next-line: no-null-keyword
+      key === 'Actions' ? <Actions editUrl={`plans/${values.ID}/edit`} /> : null
+    }
+  </SimpleTable>
+)
+
+export const PlanRouteList = component(PlanRouteProps, ({ data }) => (
+  <Section>
+    <Title size="4">Plan Route</Title>
+    <PlanRouteView data={data} />
+  </Section>
+))
+
+export const PlanRouteMap = () => (
+  <Get asyncFn={() => planRouteAPI.many()}>
+    {data => <PlanRouteList data={data} />}
+  </Get>
+)
