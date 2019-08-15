03b5cf207d17f14052d1519ce0e2627fbca12592
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-15 13:47:04 +0530

    add HeadTitle

---
+++ packages/demos/task-manager/diya-project/src/common/HeadTitle.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Title } from 'technoidentity-devfractal'
+export const HeadTitle: React.FC = ({ children }) => (
+  <Title size="4" textColor="info">
+    {children}
+  </Title>
+)
