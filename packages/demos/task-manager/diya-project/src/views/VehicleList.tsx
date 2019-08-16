0242c7a9c4b586fd49644cf745386a2166ca093f
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-16 17:54:42 +0530

    use listComponent in VehicleList

--- packages/demos/task-manager/diya-project/src/views/VehicleList.tsx
+++ packages/demos/task-manager/diya-project/src/views/VehicleList.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { component, Section } from 'technoidentity-devfractal'
-import { links, listProps, Vehicle } from '../common'
+import { Section } from 'technoidentity-devfractal'
+import { links, listComponent, Vehicle } from '../common'
 import {
   CreateLink,
   CrudTable,
@@ -8,32 +8,27 @@ import {
   StaticPagination,
 } from '../components'
 
-const VehicleListProps = listProps(Vehicle)
-
 const vehicleLinks = links('vehicles')
 
-export const VehicleList = component(
-  VehicleListProps,
-  ({ data: vehicleList }) => (
-    <Section>
-      <HeadTitle>Vehicles</HeadTitle>
+export const VehicleList = listComponent(Vehicle, ({ data: vehicleList }) => (
+  <Section>
+    <HeadTitle>Vehicles</HeadTitle>
 
-      <CreateLink to={vehicleLinks.create}>Add Vehicle</CreateLink>
+    <CreateLink to={vehicleLinks.create}>Add Vehicle</CreateLink>
 
-      <CrudTable
-        data={vehicleList}
-        headers={[
-          'name',
-          'numberPlate',
-          'group',
-          'nextService',
-          'insuranceDue',
-          'vehicleStatus',
-        ]}
-        editLink={v => vehicleLinks.edit(v.id)}
-      />
+    <CrudTable
+      data={vehicleList}
+      headers={[
+        'name',
+        'numberPlate',
+        'group',
+        'nextService',
+        'insuranceDue',
+        'vehicleStatus',
+      ]}
+      editLink={v => vehicleLinks.edit(v.id)}
+    />
 
-      <StaticPagination />
-    </Section>
-  ),
-)
+    <StaticPagination />
+  </Section>
+))
