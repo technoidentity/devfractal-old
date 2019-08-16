8c5d8ab8c4844f15c278eed2cf2ce9e6ed8b2828
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-16 17:54:04 +0530

    fix formComponent, add listComponent

--- packages/demos/task-manager/diya-project/src/common/formComponent.ts
+++ packages/demos/task-manager/diya-project/src/common/formComponent.ts
@@ -1,24 +1,48 @@
 import { FormikActions } from 'formik'
 import { Mixed, TypeOf } from 'io-ts'
 import React from 'react'
-import { component } from 'technoidentity-devfractal'
-import { empty } from 'technoidentity-utils'
-import { formProps } from './crud'
+import { cast, empty } from 'technoidentity-utils'
+import { formProps, listProps } from './crud'
 
-interface FormProps<Spec extends Mixed> {
+interface InnerFormProps<Spec extends Mixed> {
   readonly initial: TypeOf<Spec>
+  readonly edit: boolean
   onSubmit(
     values: TypeOf<Spec>,
     actions: FormikActions<TypeOf<Spec>>,
   ): Promise<void>
 }
 
+type FormProps<Spec extends Mixed> = Omit<InnerFormProps<Spec>, 'edit'> & {
+  readonly initial?: InnerFormProps<Spec>['initial']
+}
+
 // tslint:disable-next-line: typedef
 export function formComponent<Spec extends Mixed>(
   spec: Spec,
-  inner: React.FC<FormProps<Spec>>,
-) {
-  return component(formProps(spec), props =>
-    React.createElement(inner, { initial: empty(spec), ...props }),
-  )
+  inner: React.FC<InnerFormProps<Spec>>,
+): React.FC<FormProps<Spec>> {
+  return (props: FormProps<TypeOf<Spec>>) => {
+    cast(formProps(spec), props)
+    return React.createElement(inner, {
+      initial: empty(spec),
+      edit: props.initial !== undefined,
+      ...props,
+    })
+  }
+}
+
+interface ListProps<Spec extends Mixed> {
+  readonly data: readonly TypeOf<Spec>[]
+}
+
+// tslint:disable-next-line: typedef
+export function listComponent<Spec extends Mixed>(
+  spec: Spec,
+  inner: React.FC<ListProps<Spec>>,
+): React.FC<ListProps<Spec>> {
+  return (props: ListProps<Spec>) => {
+    cast(listProps(spec), props)
+    return React.createElement(inner, props)
+  }
 }
