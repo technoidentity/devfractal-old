39811effc659dc07ea6710b66465e02a81ea254e
sravanidande <sravanidnd89@gmail.com>
2019-08-20 14:24:28 +0530

    replace SimplePost with CrudRoutes

--- packages/demos/task-manager/diya-project/src/pages/GeoFenceRoutes.tsx
+++ packages/demos/task-manager/diya-project/src/pages/GeoFenceRoutes.tsx
@@ -1,27 +1,15 @@
 import React from 'react'
-import { SimpleGet, SimplePost } from 'technoidentity-devfractal'
 import { geoFenceAPI } from '../common'
-import { paths } from '../crud'
+import { CrudRoutes, paths } from '../crud'
 import { GeoFenceForm, GeoFenceList } from '../views'
-
-const { create, list } = paths('geo_fences')
-
-const GeoFenceRoute = () => (
-  <SimplePost
-    path={create}
-    api={geoFenceAPI}
-    component={GeoFenceForm}
-    redirectPath={list}
-  />
-)
-
-const GeoFenceListRoute = () => (
-  <SimpleGet path={list} component={GeoFenceList} api={geoFenceAPI} />
-)
 
 export const GeoFenceRoutes = () => (
   <>
-    <GeoFenceListRoute />
-    <GeoFenceRoute />
+    <CrudRoutes
+      api={geoFenceAPI}
+      resource="geo_fences"
+      formComponent={GeoFenceForm}
+      listComponent={GeoFenceList}
+    />
   </>
 )
