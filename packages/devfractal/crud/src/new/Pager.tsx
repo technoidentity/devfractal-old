c3d9a9eb43e4db18dead1cf4bb8d1c3d0e8c8069
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-21 12:18:30 +0530

    SplitRoutedPager

--- packages/devfractal/crud/src/new/Pager.tsx
+++ packages/devfractal/crud/src/new/Pager.tsx
@@ -1,17 +1,12 @@
-import { useHistory, useLocation } from 'devfractal-router'
 import { Pagination, PaginationNext, PaginationPrevious } from 'devfractal-ui'
-import { IntFromString } from 'io-ts-types/lib/IntFromString'
-import { stringify } from 'query-string'
 import React from 'react'
-import { opt } from 'technoidentity-utils'
-import { useQuery } from './useQuery'
 
-interface PagerViewProps {
+export interface PagerProps {
   readonly page: number
   onPageChange(n: number): void
 }
 
-const PagerView: React.FC<PagerViewProps> = ({ page, onPageChange }) => (
+export const Pager: React.FC<PagerProps> = ({ page, onPageChange }) => (
   <Pagination alignment="centered">
     <PaginationPrevious
       onClick={() => {
@@ -42,22 +37,3 @@ const PagerView: React.FC<PagerViewProps> = ({ page, onPageChange }) => (
     </PaginationList> */}
   </Pagination>
 )
-
-export const RoutedPager: React.FC = () => {
-  const { pathname } = useLocation()
-  const { push } = useHistory()
-
-  // tslint:disable-next-line: typedef
-  const query = useQuery(opt({ page: IntFromString }))
-
-  const page: number = query.page ? query.page : 1
-
-  return (
-    <PagerView
-      page={page}
-      onPageChange={page => {
-        push(`${pathname}?${stringify({ ...query, page })}`)
-      }}
-    />
-  )
-}
---
+++ packages/devfractal/crud/src/new/RoutedPager.tsx
@@ -0,0 +1,26 @@
+import { useHistory, useLocation } from 'devfractal-router'
+import { IntFromString } from 'io-ts-types/lib/IntFromString'
+import { stringify } from 'query-string'
+import React from 'react'
+import { opt } from 'technoidentity-utils'
+import { Pager } from './Pager'
+import { useQuery } from './useQuery'
+
+export const RoutedPager: React.FC = () => {
+  const { pathname } = useLocation()
+  const { push } = useHistory()
+
+  // tslint:disable-next-line: typedef
+  const query = useQuery(opt({ page: IntFromString }))
+
+  const page: number = query.page ? query.page : 1
+
+  return (
+    <Pager
+      page={page}
+      onPageChange={page => {
+        push(`${pathname}?${stringify({ ...query, page })}`)
+      }}
+    />
+  )
+}
