727f6548f6e9207045cc71e7dad15f296795dc6d
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-21 17:12:49 +0530

    buttonlink can be aligned and sized

--- packages/devfractal/crud/src/new/ButtonLink.tsx
+++ packages/devfractal/crud/src/new/ButtonLink.tsx
@@ -1,21 +1,42 @@
-import { ButtonProps, classNames } from 'devfractal-ui-core'
+import {
+  ButtonProps,
+  ButtonsGroup,
+  ButtonsGroupProps,
+  classNames,
+} from 'devfractal-ui-core'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 export interface ButtonLinkProps {
   readonly to: string
   readonly variant?: ButtonProps['variant']
+  readonly alignment?: ButtonsGroupProps['alignment']
+  readonly size?: ButtonProps['size']
 }
 
 export const ButtonLink: React.FC<ButtonLinkProps> = ({
   variant,
+  alignment,
+  size,
   to,
   children,
-}) => (
-  <Link
-    to={to}
-    className={classNames('button', { [`is-${variant}`]: variant })}
-  >
-    {children}
-  </Link>
-)
+}) => {
+  // tslint:disable-next-line: typedef
+  const link = (
+    <Link
+      to={to}
+      className={classNames('button', {
+        [`is-${variant}`]: variant,
+        [`is-${size}`]: size,
+      })}
+    >
+      {children}
+    </Link>
+  )
+
+  return alignment ? (
+    <ButtonsGroup alignment={alignment}>{link}</ButtonsGroup>
+  ) : (
+    link
+  )
+}
