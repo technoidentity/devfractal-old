69ffb83e67202bcdc766519a288a7ed352cab82e
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-22 10:07:22 +0530

    update configs for diya

--- packages/demos/diya/styleguide.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  propsParser: require('react-docgen-typescript').withCustomConfig(
-    './tsconfig.json',
-  ).parse,
-  components: 'src/**/[A-Z]*.tsx',
-  webpackConfig: require('react-scripts/config/webpack.config.js'),
-}
