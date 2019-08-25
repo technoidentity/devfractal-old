825ce2aaf5ad95eb44a8ff01a45ae5572e2d11df
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-25 10:23:17 +0530

    fix issues with TodoAppV1

--- packages/demos/presentation/src/todo/TodoAppV1.tsx
+++ packages/demos/presentation/src/todo/TodoAppV1.tsx
@@ -1,14 +1,20 @@
 import { Form, Formik } from 'formik'
+import { type } from 'io-ts'
+import { IntFromString } from 'io-ts-types/lib/IntFromString'
 import React from 'react'
 import {
+  Button,
   CheckboxField,
   DateField,
+  Field,
   Get,
   InputField,
+  Label,
   Post,
   Put,
   SubmitAction,
   Title,
+  useMatch,
 } from 'technoidentity-devfractal'
 import { empty } from 'technoidentity-utils'
 import { Todo, todoAPI } from './common'
@@ -21,9 +27,23 @@ interface TodoFormProps {
 
 const TodoFormInner = () => (
   <Form>
+    <Label>Title</Label>
     <InputField type="text" name="title" />
-    <CheckboxField name="done" />
+
+    <Label>Scheduled</Label>
     <DateField name="scheduled" />
+
+    <Label>Done</Label>
+    <CheckboxField name="done" />
+
+    <Field groupModifier="grouped-centered">
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+      <Button variant="danger" type="reset">
+        Reset
+      </Button>
+    </Field>
   </Form>
 )
 
@@ -43,20 +63,21 @@ const CreateTodo = () => (
   />
 )
 
-interface EditTodoProps {
-  readonly id: number
+const EditTodo: React.FC = () => {
+  const {
+    params: { id },
+  } = useMatch(type({ id: IntFromString }))
+
+  return (
+    <Put<Todo, 'id'>
+      id={id}
+      doGet={todoAPI.get}
+      onPut={todoAPI.update}
+      component={TodoForm}
+      redirectTo="/todos"
+    />
+  )
 }
-
-const EditTodo: React.FC<EditTodoProps> = ({ id }) => (
-  <Put<Todo, 'id'>
-    id={id}
-    doGet={todoAPI.get}
-    onPut={todoAPI.update}
-    component={TodoForm}
-    redirectTo="/todos"
-  />
-)
-
 const TodoList = () => (
   <>
     <Title textAlignment="centered">Todo List</Title>
