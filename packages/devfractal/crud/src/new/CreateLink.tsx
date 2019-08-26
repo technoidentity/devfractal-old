9bbc237fdc3a8276fc26ae9d24acc7f9f4d50ff1
sravanidande <sravanidnd89@gmail.com>
2019-08-26 18:24:00 +0530

    fix CreateLink imports

--- packages/devfractal/crud/src/new/CreateLink.tsx
+++ packages/devfractal/crud/src/new/CreateLink.tsx
@@ -1,11 +1,6 @@
+import { ButtonsGroup, ButtonsGroupProps } from 'devfractal-ui-core'
 import React from 'react'
-import {
-  ButtonLink,
-  ButtonLinkProps,
-  ButtonsGroup,
-  ButtonsGroupProps,
-} from 'technoidentity-devfractal'
-
+import { ButtonLink, ButtonLinkProps } from './ButtonLink'
 export interface CreateLinkProps extends ButtonLinkProps {
   readonly alignment?: ButtonsGroupProps['alignment']
 }
