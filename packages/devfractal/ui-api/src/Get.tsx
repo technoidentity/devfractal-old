5ad6a9097e711386a3eb70e635401329497ae0fa
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-27 10:10:03 +0530

    Get takes component

--- packages/devfractal/ui-api/src/Get.tsx
+++ packages/devfractal/ui-api/src/Get.tsx
@@ -1,30 +1,34 @@
 import { AsyncResult, useGet } from 'devfractal-api'
 import React from 'react'
-import { AnyTuple } from 'typelevel-ts'
 import { ErrorView, Loading } from './Components'
 
-// tslint:disable readonly-array
-export interface GetProps<T extends {}, P extends AnyTuple> {
+// tslint:disable readonly-array no-unbound-method
+export interface GetProps<T extends {}, P extends any[]> {
   readonly deps?: P | []
+  readonly component?: React.FC<{ readonly data: T; fetchAgain?(): void }>
   // You should pass a global function, not a closure. Pass all deps to 'deps' instead.
-  asyncFn(...params: P | []): Promise<T>
+  asyncFn(...params: P): Promise<T>
   children(data: T, fetchAgain: () => void): JSX.Element
 }
 
-export function Get<T extends {}, P extends AnyTuple>({
+export function Get<T extends {}, P extends any[]>({
   asyncFn,
   deps = [],
+  component: Component,
   children,
 }: GetProps<T, P>): JSX.Element {
-  const result: AsyncResult<T> = useGet(asyncFn, ...(deps || []))
+  const result: AsyncResult<T> = useGet(asyncFn, ...(deps as P))
 
   if (result.state === 'failure') {
     return <ErrorView error={result.error} />
   }
 
   if (result.state === 'success') {
-    // tslint:disable-next-line: no-unbound-method
-    return children(result.data, result.refresh)
+    return Component ? (
+      <Component data={result.data} fetchAgain={result.refresh} />
+    ) : (
+      children(result.data, result.refresh)
+    )
   }
 
   return <Loading />
