177b7012c9a411a22713159de79ece257a3f27e1
Pervez Iqbal <pervezfunctor@gmail.com>
2019-08-30 12:26:55 +0530

    spec: initial commit

---
+++ packages/spec/src/checked.test.ts
@@ -0,0 +1,91 @@
+import * as t from 'io-ts'
+import { checked, checkedAsync, range } from 'technoidentity-utils'
+
+// tslint:disable typedef
+it('checked', () => {
+  const checkedAdd = checked(
+    [t.number, t.number, t.number],
+    t.number,
+    (x, y, z) => x + y + z,
+  )
+
+  expect(checkedAdd(100, 100, 100)).toEqual(300)
+  expect(() => checkedAdd(100, '100' as any, 100)).toThrow()
+
+  const checkedTimes = checked([t.string, t.number], t.string, (s, n) =>
+    range(n).reduce((acc, _) => acc + s, ''),
+  )
+
+  expect(checkedTimes('hello', 3)).toEqual('hellohellohello')
+  expect(() => checkedTimes(5 as any, 5)).toThrow()
+  expect(() => checkedTimes('hello', '5' as any)).toThrow()
+
+  const Point = t.readonly(t.type({ x: t.number, y: t.number }))
+
+  const checkedMoveBy = checked(
+    [Point, t.number, t.number],
+    Point,
+    (pt, dx, dy) => {
+      return { x: pt.x + dx, y: pt.y + dy }
+    },
+  )
+  expect(checkedMoveBy({ x: 1, y: 2 }, 1, 1)).toEqual({ x: 2, y: 3 })
+  expect(() => checkedMoveBy({ x: 1, y: 2 }, false as any, 1)).toThrow()
+  expect(() => checkedMoveBy({ x: 1, y: 2 }, 1, false as any)).toThrow()
+  expect(() => checkedMoveBy({ x: 1, y: 'hello' as any }, 1, 1)).toThrow()
+  expect(() => checkedMoveBy({ x: 'world' as any, y: 2 }, 1, 1)).toThrow()
+
+  // @TODO: make checked work with decoders like DateFromIsoString
+
+  // const checkedDateInc = checked(
+  //   [DateFromISOString],
+  //   DateFromISOString,
+  //   s => addDays(new Date(s), 1).toISOString(),
+  // )
+  // const date = new Date().toISOString()
+  // expect(checkedDateInc(date)).toEqual(addDays(date, 1).toISOString())
+})
+
+// tslint:disable no-floating-promises
+
+it('checkedAsync', async () => {
+  const checkedAdd = checkedAsync(
+    [t.number, t.number, t.number],
+    t.number,
+    async (x, y, z) => x + y + z,
+  )
+
+  await expect(checkedAdd(100, 100, 100)).resolves.toEqual(300)
+  await expect(checkedAdd(100, '100' as any, 100)).rejects.toThrow()
+
+  const checkedTimes = checkedAsync(
+    [t.string, t.number],
+    t.string,
+    async (s, n) => range(n).reduce((acc, _) => acc + s, ''),
+  )
+
+  await expect(checkedTimes('hello', 3)).resolves.toEqual('hellohellohello')
+  await expect(checkedTimes(5 as any, 5)).rejects.toThrow()
+  await expect(checkedTimes('hello', '5' as any)).rejects.toThrow()
+
+  const Point = t.readonly(t.type({ x: t.number, y: t.number }))
+
+  const checkedMoveBy = checkedAsync(
+    [Point, t.number, t.number],
+    Point,
+    async (pt, dx, dy) => ({ x: pt.x + dx, y: pt.y + dy }),
+  )
+  await expect(checkedMoveBy({ x: 1, y: 2 }, 1, 1)).resolves.toEqual({
+    x: 2,
+    y: 3,
+  })
+
+  await expect(checkedMoveBy({ x: 1, y: 2 }, false as any, 1)).rejects.toThrow()
+  await expect(checkedMoveBy({ x: 1, y: 2 }, 1, false as any)).rejects.toThrow()
+  await expect(
+    checkedMoveBy({ x: 1, y: 'hello' as any }, 1, 1),
+  ).rejects.toThrow()
+  await expect(
+    checkedMoveBy({ x: 'world' as any, y: 2 }, 1, 1),
+  ).rejects.toThrow()
+})
---
+++ packages/spec/src/fn.test.ts
@@ -0,0 +1,20 @@
+import { number, TypeOf } from 'io-ts'
+import { fn } from './fn'
+import { cast, props } from './iotsUtils'
+
+// tslint:disable typedef no-empty
+
+test('fn', () => {
+  const Point = props(
+    { distance: fn<() => number>() },
+    { x: number, y: number },
+  )
+
+  type Point = TypeOf<typeof Point>
+
+  expect(() => cast(Point, { x: 1, y: 2 })).not.toThrowError()
+  expect(() => cast(Point, { x: 1, y: 2, distance: 3 })).toThrowError()
+  expect(() =>
+    cast(Point, { x: 1, y: 2, distance: () => {} }),
+  ).not.toThrowError()
+})
