71d848a52fe079fed955296a19f81da1a126401d
sravanidande <sravanidnd89@gmail.com>
2019-09-06 12:39:02 +0530

    add missing fields and interfaces

--- packages/devfractal/examples/src/composites/SimpleFormExample.tsx
+++ packages/devfractal/examples/src/composites/SimpleFormExample.tsx
@@ -1,16 +1,36 @@
+// import { maxLength, min, required } from 'devfractal-forms'
 import { Simple } from 'devfractal-simple'
 import { Radio, Section } from 'devfractal-ui-core'
+import { keyof, TypeOf } from 'io-ts'
 import React from 'react'
+import { ISODate } from 'technoidentity-utils'
 import { number, object, ObjectSchema, string } from 'yup'
+
+// tslint:disable-next-line: typedef
+const Position = keyof({
+  associate: true,
+  assistant: true,
+})
+
+type Position = TypeOf<typeof Position>
+
+// tslint:disable-next-line: typedef
+const Gender = keyof({
+  female: true,
+  male: true,
+})
+
+type Gender = TypeOf<typeof Gender>
 
 interface SimpleValues {
   readonly username: string
   readonly password: string
   readonly email: string
   readonly tel: number
+  readonly date: ISODate
   readonly remember: boolean
-  readonly gender: string
-  readonly select: string
+  readonly gender: Gender
+  readonly select: Position
   readonly message: string
   readonly age: number
 }
@@ -19,10 +39,11 @@ const initialValues: SimpleValues = {
   username: '',
   password: '',
   email: '',
+  date: new Date(),
   tel: 0,
   remember: false,
   gender: 'male',
-  select: '',
+  select: 'assistant',
   message: '',
   age: 0,
 }
@@ -49,12 +70,13 @@ export const SimpleFormExample: React.FC = () => (
       <Simple.Password label="Password" name="password" />
       <Simple.Email label="Email" name="email" />
       <Simple.Telephone label="Telephone" name="tel" />
+      <Simple.Date name="dateOfBirth" />
       <Simple.Checkbox name="remember"> Remember Me</Simple.Checkbox>
       <Simple.RadioGroup name="gender" defaultValue="female">
         <Radio value="female"> Female</Radio>
         <Radio value="male"> Male</Radio>
       </Simple.RadioGroup>
-      <Simple.Select name="select">
+      <Simple.Select name="position">
         <option value="associate">associate</option>
         <option value="assistant">assistant</option>
       </Simple.Select>
