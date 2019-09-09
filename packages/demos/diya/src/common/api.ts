c9af3683aed35ad29ededb02f7d298057d9b26d4
sravanidande <sravanidnd89@gmail.com>
2019-09-09 16:49:09 +0530

    add api for AssignVehicle and AssignBattery forms

--- packages/demos/diya/src/common/api.ts
+++ packages/demos/diya/src/common/api.ts
@@ -3,7 +3,9 @@ import { Mixed } from 'technoidentity-spec'
 import { HasProps } from 'technoidentity-utils'
 import {
   AdManager,
+  AssignBattery,
   AssignDriver,
+  AssignVehicle,
   Battery,
   Client,
   Driver,
@@ -40,3 +42,5 @@ export const geoFenceAPI = api(GeoFence, 'geo_fences')
 export const tripAPI = api(Trip, 'trips')
 export const adManagerAPI = api(AdManager, 'adManagers')
 export const assignDriverAPI = api(AssignDriver, 'assignDriver')
+export const assignVehicleAPI = api(AssignVehicle, 'assignVehicle')
+export const assignBatteryAPI = api(AssignBattery, 'assignBattery')
