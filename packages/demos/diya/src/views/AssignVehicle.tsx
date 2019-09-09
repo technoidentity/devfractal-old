5ae080e34f4e40c2659604c8920cbace85dcaf91
sravanidande <sravanidnd89@gmail.com>
2019-09-09 16:55:58 +0530

    add AssignVehicle form

---
+++ packages/demos/diya/src/views/AssignVehicle.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import 'react-datepicker/dist/react-datepicker.css'
+import {
+  Column,
+  Columns,
+  component,
+  Create,
+  formProps,
+  Section,
+  Simple,
+} from 'technoidentity-devfractal'
+import { empty } from 'technoidentity-utils'
+import { AssignVehicle, assignVehicleAPI } from '../common'
+import { HeadTitle } from '../components'
+
+const AssignVehicleFormProps = formProps(AssignVehicle)
+
+export const AssignVehicleForm = component(
+  AssignVehicleFormProps,
+  ({ onSubmit, initial = empty(AssignVehicle) }) => (
+    <>
+      <Section>
+        <HeadTitle>Assign </HeadTitle>
+        <Simple.Form initialValues={initial} onSubmit={onSubmit}>
+          <Columns>
+            <Column>
+              <Simple.Text name="client" />
+              <Simple.Text name="batteryID" label="Battery ID" />
+              <Simple.Text name="driver" />
+            </Column>
+            <Column>
+              <Simple.Date name="startDate" />
+              <Simple.Date name="endDate" />
+              <Simple.Select name="contractType">
+                <option value="">Select</option>
+                <option value="Weekly">Weekly</option>
+                <option value="Monthly">Monthly</option>
+              </Simple.Select>
+            </Column>
+          </Columns>
+          <Simple.FormButtons />
+        </Simple.Form>
+      </Section>
+    </>
+  ),
+)
+
+export const AssignVehicleRoute = () => (
+  <Create
+    api={assignVehicleAPI}
+    form={AssignVehicleForm}
+    path="/assignVehicle/:id"
+    redirectTo="/vehicles"
+  />
+)
