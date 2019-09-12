23a610bbd380d9f5043390b1d40ce1aa6abef545
nandigam chandu <nandigamchandu@gmail.com>
2019-09-12 12:12:01 +0530

    add fetchsuggestions function

--- packages/demos/diya/src/components/Search.tsx
+++ packages/demos/diya/src/components/Search.tsx
@@ -1,7 +1,34 @@
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import { Icon } from 'devfractal-ui-core'
+import { Button, Icon } from 'devfractal-ui-core'
+import { Mixed, readonlyArray } from 'io-ts'
 import React from 'react'
 import Autosuggest from 'react-autosuggest'
+import { http as httpAPI } from 'technoidentity-devfractal'
+
+const http: ReturnType<typeof httpAPI> = httpAPI({
+  baseURL: 'http://localhost:9999',
+})
+
+export const fetchSuggestions = async <Spec extends Mixed>(
+  value: string,
+  searchBy: string,
+  resource: string,
+  spec: Spec,
+) => {
+  const inputLength = value.length
+  try {
+    const suggestions = await http.get(
+      {
+        resource,
+        query: `${searchBy}_like=^${value}`,
+      },
+      readonlyArray(spec),
+    )
+    return inputLength === 0 ? [] : suggestions
+  } catch (err) {
+    throw new Error(err)
+  }
+}
 
 export interface SearchProps<T> {
   readonly searchBy: string
@@ -40,14 +67,20 @@ export function Search<T>({
         inputProps={{
           placeholder: `Type a ${searchBy}`,
           value: state.value,
+          type: 'search',
           onChange: (_, { newValue }) =>
             setState({ ...state, value: newValue }),
         }}
       />
-
-      <button type="button" onClick={() => onSearch(state.value)}>
+      <Button
+        variant="danger"
+        size="small"
+        state="hovered"
+        type="button"
+        onClick={() => onSearch(state.value)}
+      >
         <Icon icon={faSearch} />
-      </button>
+      </Button>
     </>
   )
 }
