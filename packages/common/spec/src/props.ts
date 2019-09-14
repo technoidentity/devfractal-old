3fc1c1c424fd719816657d84e648530bf9d64835
Pervez Iqbal <pervezfunctor@gmail.com>
2019-09-15 00:46:13 +0530

    getProps; improvements to props handling v3

--- packages/common/spec/src/props.ts
+++ packages/common/spec/src/props.ts
@@ -16,18 +16,24 @@ import { omit, pick } from 'lodash-es'
 
 // tslint:disable no-class no-parameter-properties readonly-array typedef no-this
 
-class ObjType<A, O, I> extends Type<A, O, I> {}
+class PropsBase<P extends Props, A, O, I> extends Type<A, O, I> {
+  readonly _P!: P
+}
 
-class ReqType<R extends Props, A, O, I> extends ObjType<A, O, I> {
+export type AnyPropsBase = PropsBase<any, any, any, any>
+
+export type PropsOf<T extends AnyPropsBase> = T['_P']
+
+class ReqType<R extends Props, A, O, I> extends PropsBase<R, A, O, I> {
   readonly _tag: 'ReqType' = 'ReqType'
   constructor(readonly props: R, spec: Type<A, O, I>, name: string) {
     super(name, spec.is, spec.validate, spec.encode)
   }
 }
 
-type RS<R extends Props> = ReadonlyC<TypeC<R>>
-type AR<R extends Props> = TypeOf<RS<R>>
-type OR<R extends Props> = OutputOf<RS<R>>
+type SR<R extends Props> = ReadonlyC<TypeC<R>>
+type AR<R extends Props> = TypeOf<SR<R>>
+type OR<R extends Props> = OutputOf<SR<R>>
 
 export interface ReqTypeC<R extends Props>
   extends ReqType<R, AR<R>, OR<R>, unknown> {}
@@ -36,16 +42,16 @@ export function req<R extends Props>(required: R, name?: string): ReqTypeC<R> {
   return new ReqType(required, readonly(type(required)), name || 'ReqType')
 }
 
-export class OptType<P extends Props, A, O, I> extends ObjType<A, O, I> {
+export class OptType<P extends Props, A, O, I> extends PropsBase<P, A, O, I> {
   readonly _tag: 'OptType' = 'OptType'
   constructor(readonly props: P, spec: Type<A, O, I>, name: string) {
     super(name, spec.is, spec.validate, spec.encode)
   }
 }
 
-type PS<P extends Props> = ReadonlyC<PartialC<P>>
-type AP<P extends Props> = TypeOf<PS<P>>
-type OP<P extends Props> = OutputOf<PS<P>>
+type SP<P extends Props> = ReadonlyC<PartialC<P>>
+type AP<P extends Props> = TypeOf<SP<P>>
+type OP<P extends Props> = OutputOf<SP<P>>
 
 export interface OptTypeC<P extends Props>
   extends OptType<P, AP<P>, OP<P>, unknown> {}
@@ -54,14 +60,14 @@ export function opt<R extends Props>(optional: R, name?: string): OptTypeC<R> {
   return new OptType(optional, readonly(partial(optional)), name || 'OptType')
 }
 
-export class PropsType<
+export class BothType<
   R extends Props,
   P extends Props,
   A,
   O,
   I
-> extends ObjType<A, O, I> {
-  readonly _tag: 'PropsType' = 'PropsType'
+> extends PropsBase<R & P, A, O, I> {
+  readonly _tag: 'BothType' = 'BothType'
 
   constructor(
     readonly required: R,
@@ -76,53 +82,53 @@ export class PropsType<
     return { ...this.required, ...this.optional }
   }
 }
-
-type IS<R extends Props, P extends Props> = IntersectionC<
+// @TODO: may be not use intersection?
+type SI<R extends Props, P extends Props> = IntersectionC<
   [ReadonlyC<TypeC<R>>, ReadonlyC<PartialC<P>>]
 >
-type AI<R extends Props, P extends Props> = TypeOf<IS<R, P>>
-type OI<R extends Props, P extends Props> = OutputOf<IS<R, P>>
+type AI<R extends Props, P extends Props> = TypeOf<SI<R, P>>
+type OI<R extends Props, P extends Props> = OutputOf<SI<R, P>>
 
-export interface PropsTypeC<R extends Props, P extends Props>
-  extends PropsType<R, P, AI<R, P>, OI<R, P>, unknown> {}
+export interface BothTypeC<R extends Props, P extends Props>
+  extends BothType<R, P, AI<R, P>, OI<R, P>, unknown> {}
 
-export function props<R extends Props, P extends Props>(
+export function both<R extends Props, P extends Props>(
   required: R,
   optional: P,
   name?: string,
-): PropsTypeC<R, P> {
-  return new PropsType(
+): BothTypeC<R, P> {
+  return new BothType(
     required,
     optional,
     intersection([readonly(type(required)), readonly(partial(optional))]),
-    name || 'PropsType',
+    name || 'BothType',
   )
 }
 
-export function propsPick<
+export function bothPick<
   R extends Props,
   P extends Props,
   KR extends keyof R,
   KP extends keyof P
 >(
-  spec: PropsTypeC<R, P>,
+  spec: BothTypeC<R, P>,
   reqKeys: readonly KR[],
   optKeys: readonly KP[],
-): PropsTypeC<Pick<R, KR>, Pick<P, KP>> {
-  return props(pick(spec.required, reqKeys), pick(spec.optional, optKeys))
+): BothTypeC<Pick<R, KR>, Pick<P, KP>> {
+  return both(pick(spec.required, reqKeys), pick(spec.optional, optKeys))
 }
 
-export function propsOmit<
+export function bothOmit<
   R extends Props,
   P extends Props,
   KR extends keyof R,
   KP extends keyof P
 >(
-  spec: PropsTypeC<R, P>,
+  spec: BothTypeC<R, P>,
   reqKeys: readonly KR[],
   optKeys: readonly KP[],
-): PropsTypeC<Omit<R, KR>, Omit<P, KP>> {
-  return props(omit(spec.required, reqKeys), omit(spec.optional, optKeys))
+): BothTypeC<Omit<R, KR>, Omit<P, KP>> {
+  return both(omit(spec.required, reqKeys), omit(spec.optional, optKeys))
 }
 
 export function reqPick<R extends Props, KR extends keyof R>(
@@ -141,7 +147,6 @@ export function reqOmit<R extends Props, KR extends keyof R>(
 
 export function optPick<P extends Props, KP extends keyof P>(
   spec: OptTypeC<P>,
-
   optKeys: readonly KP[],
 ): OptTypeC<Pick<P, KP>> {
   return opt(pick(spec.props, optKeys))
@@ -154,31 +159,11 @@ export function optOmit<P extends Props, KP extends keyof P>(
   return opt(omit(spec.props, optKeys))
 }
 
-export function reqToOpt<P extends Props>(spec: ReqTypeC<P>): OptTypeC<P> {
-  return opt(spec.props)
-}
-
-export function propsToOpt<R extends Props, P extends Props>(
-  spec: PropsTypeC<R, P>,
-): OptTypeC<P & R> {
-  return opt(spec.props)
-}
-
-export function optToReq<P extends Props>(spec: OptTypeC<P>): ReqTypeC<P> {
-  return req(spec.props)
-}
-
-export function propsToReq<R extends Props, P extends Props>(
-  spec: PropsTypeC<R, P>,
-): ReqTypeC<P & R> {
-  return req(spec.props)
-}
-
-export function toProps<R extends Props, P extends Props>(
+export function reqOptToBoth<R extends Props, P extends Props>(
   req: ReqTypeC<R>,
   opt: OptTypeC<P>,
-): PropsTypeC<R, P> {
-  return props(req.props, opt.props)
+): BothTypeC<R, P> {
+  return both(req.props, opt.props)
 }
 
 export function reqCombine<R extends Props, R2 extends Props>(
@@ -188,23 +173,59 @@ export function reqCombine<R extends Props, R2 extends Props>(
   return req({ ...r.props, ...r2.props })
 }
 
-export function OptCombine<P extends Props, P2 extends Props>(
+export function optCombine<P extends Props, P2 extends Props>(
   o: OptTypeC<P>,
   o2: OptTypeC<P2>,
 ): OptTypeC<P & P2> {
   return opt({ ...o.props, ...o2.props })
 }
 
-export function propsCombine<
+export function bothCombine<
   R extends Props,
   R2 extends Props,
   P extends Props,
   P2 extends Props
->(p: PropsTypeC<R, P>, p2: PropsTypeC<R2, P2>): PropsTypeC<R & R2, P & P2> {
-  return props(
+>(p: BothTypeC<R, P>, p2: BothTypeC<R2, P2>): BothTypeC<R & R2, P & P2> {
+  return both(
     { ...p.required, ...p2.required },
     { ...p.optional, ...p2.optional },
   )
+}
+
+export function isOpt(spec: AnyPropsBase): spec is OptTypeC<any> {
+  return spec instanceof OptType
+}
+
+export function isReq(spec: AnyPropsBase): spec is ReqTypeC<any> {
+  return spec instanceof ReqType
+}
+
+export function isBoth(spec: AnyPropsBase): spec is BothTypeC<any, any> {
+  return spec instanceof BothType
+}
+
+export function getProps<Spec extends AnyPropsBase>(
+  spec: Spec,
+): PropsOf<typeof spec> {
+  if (isOpt(spec) || isReq(spec)) {
+    return spec.props
+  }
+  if (isBoth(spec)) {
+    return { ...spec.required, ...spec.optional }
+  }
+  throw new Error(`Unsupported type: ${spec.name}`)
+}
+
+export function toOpt<Spec extends AnyPropsBase>(
+  spec: Spec,
+): OptTypeC<PropsOf<typeof spec>> {
+  return opt(getProps(spec))
+}
+
+export function toReq<Spec extends AnyPropsBase>(
+  spec: Spec,
+): ReqTypeC<PropsOf<typeof spec>> {
+  return req(getProps(spec))
 }
 
 // const r = req({ x: number, y: string })
