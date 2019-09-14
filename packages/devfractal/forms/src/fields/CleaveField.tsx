6b4b85f685b6bcb5c21043035d4960c68b7e6a7c
Sravani Dande <sravanidnd89@gmail.com>
2019-09-14 13:37:24 +0530

    add specific cleave fields like CleaveTime

--- packages/devfractal/forms/src/fields/CleaveField.tsx
+++ packages/devfractal/forms/src/fields/CleaveField.tsx
@@ -26,3 +26,70 @@ export type CleaveInputFieldProps = CleaveInputProps & FieldConfig
 export const CleaveInputField: React.FC<CleaveInputFieldProps> = props => (
   <Field {...props} component={CleaveInputInner} />
 )
+
+export interface CleaveTimeFieldProps
+  extends Omit<CleaveInputFieldProps, 'options'> {
+  readonly timePattern?: CleaveInputFieldProps['options']['timePattern']
+}
+
+export const CleaveTimeField: React.FC<CleaveTimeFieldProps> = ({
+  timePattern = ['h', 'm'],
+  ...props
+}) => <CleaveInputField {...props} options={{ time: true, timePattern }} />
+
+export interface CleaveDateFieldProps
+  extends Omit<CleaveInputFieldProps, 'options'> {
+  readonly datePattern?: CleaveInputFieldProps['options']['datePattern']
+  readonly delimiter?: CleaveInputFieldProps['options']['delimiter']
+}
+
+export const CleaveDateField: React.FC<CleaveDateFieldProps> = ({
+  datePattern = ['y', 'm', 'd'],
+  delimiter = '/',
+  ...props
+}) => (
+  <CleaveInputField
+    {...props}
+    options={{ date: true, datePattern, delimiter }}
+  />
+)
+
+export interface CleavePhoneFieldProps
+  extends Omit<CleaveInputFieldProps, 'options'> {
+  readonly phoneRegionCode: CleaveInputFieldProps['options']['phoneRegionCode']
+}
+
+export const CleavePhoneField: React.FC<CleavePhoneFieldProps> = ({
+  phoneRegionCode = 'IN',
+  ...props
+}) => <CleaveInputField {...props} options={{ phone: true, phoneRegionCode }} />
+
+export interface CleaveNumeralFieldProps
+  extends Omit<CleaveInputFieldProps, 'options'> {
+  readonly numeralThousandsGroupStyle: CleaveInputFieldProps['options']['numeralThousandsGroupStyle']
+}
+
+export const CleaveNumeralField: React.FC<CleaveNumeralFieldProps> = ({
+  numeralThousandsGroupStyle = 'thousand',
+  ...props
+}) => (
+  <CleaveInputField
+    {...props}
+    options={{ numeral: true, numeralThousandsGroupStyle }}
+  />
+)
+
+export interface CleaveCreditCardProps
+  extends Omit<CleaveInputFieldProps, 'options'> {
+  readonly onCreditCardTypeChanged?: CleaveInputFieldProps['options']['onCreditCardTypeChanged']
+}
+
+export const CleaveCreditCardField: React.FC<CleaveCreditCardProps> = ({
+  onCreditCardTypeChanged,
+  ...props
+}) => (
+  <CleaveInputField
+    options={{ creditCard: true, onCreditCardTypeChanged }}
+    {...props}
+  />
+)
