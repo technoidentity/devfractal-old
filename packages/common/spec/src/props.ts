bdb497c73d84dbc1d452ceaeff01ac8b942475fe
Pervez Iqbal <pervezfunctor@gmail.com>
2019-09-15 15:12:31 +0530

    minor changes

--- packages/common/spec/src/props.ts
+++ packages/common/spec/src/props.ts
@@ -14,13 +14,14 @@ import {
 } from 'io-ts'
 import { omit, pick } from 'lodash-es'
 
-// tslint:disable no-class no-parameter-properties readonly-array typedef no-this
+// tslint:disable no-class no-parameter-properties
+// tslint:disable no-this readonly-array typedef
 
 class PropsBase<P extends Props, A, O, I> extends Type<A, O, I> {
   readonly _P!: P
 }
 
-export type AnyPropsBase = PropsBase<any, any, any, any>
+export type AnyPropsBase<P extends Props = any> = PropsBase<P, any, any, any>
 
 export type PropsOf<T extends AnyPropsBase> = T['_P']
 
@@ -82,10 +83,9 @@ export class BothType<
     return { ...this.required, ...this.optional }
   }
 }
+
 // @TODO: may be not use intersection?
-type SI<R extends Props, P extends Props> = IntersectionC<
-  [ReadonlyC<TypeC<R>>, ReadonlyC<PartialC<P>>]
->
+type SI<R extends Props, P extends Props> = IntersectionC<[SR<R>, SP<P>]>
 type AI<R extends Props, P extends Props> = TypeOf<SI<R, P>>
 type OI<R extends Props, P extends Props> = OutputOf<SI<R, P>>
 
@@ -111,11 +111,11 @@ export function bothPick<
   KR extends keyof R,
   KP extends keyof P
 >(
-  spec: BothTypeC<R, P>,
+  { required, optional }: BothTypeC<R, P>,
   reqKeys: readonly KR[],
   optKeys: readonly KP[],
 ): BothTypeC<Pick<R, KR>, Pick<P, KP>> {
-  return both(pick(spec.required, reqKeys), pick(spec.optional, optKeys))
+  return both(pick(required, reqKeys), pick(optional, optKeys))
 }
 
 export function bothOmit<
@@ -124,39 +124,39 @@ export function bothOmit<
   KR extends keyof R,
   KP extends keyof P
 >(
-  spec: BothTypeC<R, P>,
+  { required, optional }: BothTypeC<R, P>,
   reqKeys: readonly KR[],
   optKeys: readonly KP[],
 ): BothTypeC<Omit<R, KR>, Omit<P, KP>> {
-  return both(omit(spec.required, reqKeys), omit(spec.optional, optKeys))
+  return both(omit(required, reqKeys), omit(optional, optKeys))
 }
 
 export function reqPick<R extends Props, KR extends keyof R>(
-  spec: ReqTypeC<R>,
+  { props }: ReqTypeC<R>,
   reqKeys: readonly KR[],
 ): ReqTypeC<Pick<R, KR>> {
-  return req(pick(spec.props, reqKeys))
+  return req(pick(props, reqKeys))
 }
 
 export function reqOmit<R extends Props, KR extends keyof R>(
-  spec: ReqTypeC<R>,
+  { props }: ReqTypeC<R>,
   reqKeys: readonly KR[],
 ): ReqTypeC<Omit<R, KR>> {
-  return req(omit(spec.props, reqKeys))
+  return req(omit(props, reqKeys))
 }
 
 export function optPick<P extends Props, KP extends keyof P>(
-  spec: OptTypeC<P>,
+  { props }: OptTypeC<P>,
   optKeys: readonly KP[],
 ): OptTypeC<Pick<P, KP>> {
-  return opt(pick(spec.props, optKeys))
+  return opt(pick(props, optKeys))
 }
 
 export function optOmit<P extends Props, KP extends keyof P>(
-  spec: OptTypeC<P>,
+  { props }: OptTypeC<P>,
   optKeys: readonly KP[],
 ): OptTypeC<Omit<P, KP>> {
-  return opt(omit(spec.props, optKeys))
+  return opt(omit(props, optKeys))
 }
 
 export function reqOptToBoth<R extends Props, P extends Props>(
@@ -216,6 +216,42 @@ export function getProps<Spec extends AnyPropsBase>(
   throw new Error(`Unsupported type: ${spec.name}`)
 }
 
+export function getReqProps<Spec extends AnyPropsBase>(
+  spec: Spec,
+): PropsOf<typeof spec> {
+  if (isOpt(spec)) {
+    return {}
+  }
+
+  if (isReq(spec)) {
+    return spec.props
+  }
+
+  if (isBoth(spec)) {
+    return spec.required
+  }
+
+  throw new Error(`Unsupported type: ${spec.name}`)
+}
+
+export function getOptProps<Spec extends AnyPropsBase>(
+  spec: Spec,
+): PropsOf<typeof spec> {
+  if (isOpt(spec)) {
+    return spec.props
+  }
+
+  if (isReq(spec)) {
+    return {}
+  }
+
+  if (isBoth(spec)) {
+    return spec.optional
+  }
+
+  throw new Error(`Unsupported type: ${spec.name}`)
+}
+
 export function toOpt<Spec extends AnyPropsBase>(
   spec: Spec,
 ): OptTypeC<PropsOf<typeof spec>> {
@@ -228,25 +264,51 @@ export function toReq<Spec extends AnyPropsBase>(
   return req(getProps(spec))
 }
 
+// export function propsPick<Spec extends AnyPropsBase>(spec: Spec) {
+//   return pick(getProps(spec))
+// }
+
+// export function propsOmit<Spec extends AnyPropsBase>(spec: Spec) {
+//   return omit(getProps(spec))
+// }
+
+// export function combine<Spec extends AnyPropsBase, Spec2 extends AnyPropsBase>(
+//   spec: Spec,
+//   spec2: Spec2,
+// ) {
+//   if (isReq(spec) && isReq(spec2)) {
+//     return req({ ...spec.props, ...spec2.props })
+//   }
+
+//   if (isOpt(spec) && isOpt(spec2)) {
+//     return opt({ ...spec.props, ...spec2.props })
+//   }
+
+//   return both(
+//     { ...getReqProps(spec), ...getReqProps(spec2) },
+//     { ...getOptProps(spec), ...getOptProps(spec2) },
+//   )
+// }
+
 // const r = req({ x: number, y: string })
 // type R = TypeOf<typeof r>
 
 // const o = opt({ x: number, y: string })
 // type O = TypeOf<typeof o>
 
-// const p = props({ x: number }, { y: string })
+// const p = both({ x: number }, { y: string })
 // type P = TypeOf<typeof p>
 
-// const oo = reqToOpt(r)
+// const oo = toOpt(r)
 // type OO = TypeOf<typeof oo>
 
-// const rr = optToReq(o)
+// const rr = toReq(o)
 // type RR = TypeOf<typeof rr>
 
-// const pp = propsToReq(p)
+// const pp = toReq(p)
 // type PP = TypeOf<typeof pp>
 
-// const pp2 = propsToOpt(p)
+// const pp2 = toOpt(p)
 // type PP2 = TypeOf<typeof pp2>
 
 // const pir = reqPick(r, ['x'])
@@ -255,7 +317,7 @@ export function toReq<Spec extends AnyPropsBase>(
 // const pio = optPick(o, ['y'])
 // type PIO = TypeOf<typeof pio>
 
-// const pip = propsPick(p, ['x'], ['y'])
+// const pip = bothPick(p, ['x'], ['y'])
 // type PIP = TypeOf<typeof pip>
 
 // const oir = reqOmit(r, ['x'])
@@ -264,10 +326,10 @@ export function toReq<Spec extends AnyPropsBase>(
 // const oio = optOmit(o, ['y'])
 // type OIO = TypeOf<typeof oio>
 
-// const oip = propsOmit(p, [], ['y'])
+// const oip = bothOmit(p, [], ['y'])
 // type OIP = TypeOf<typeof oip>
 
-// const p = props({ x: number }, { y: string })
-// const p2 = props({ a: number }, { b: string })
-// const p3 = propsCombine(p, p2)
+// const p1 = both({ x: number }, { y: string })
+// const p2 = both({ a: number }, { b: string })
+// const p3 = bothCombine(p, p2)
 // type P3 = TypeOf<typeof p3>
