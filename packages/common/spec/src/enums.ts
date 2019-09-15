5a584c3be8ca8b9606c3d9c94004f929c939fd45
Pervez Iqbal <pervezfunctor@gmail.com>
2019-09-15 20:07:02 +0530

    enums for io-ts

---
+++ packages/common/spec/src/enums.ts
@@ -0,0 +1,34 @@
+import { failure, identity, string, success, Type } from 'io-ts'
+
+// tslint:disable no-class readonly-array
+
+export class EnumType<D extends string[], A> extends Type<A> {
+  readonly _tag: 'KeyofType' = 'KeyofType'
+
+  constructor(
+    // tslint:disable-next-line: no-parameter-properties
+    readonly keys: D,
+    name: string,
+    is: EnumType<D, A>['is'],
+    validate: EnumType<D, A>['validate'],
+    encode: EnumType<D, A>['encode'],
+  ) {
+    super(name, is, validate, encode)
+  }
+}
+
+export interface EnumC<D extends string[]> extends EnumType<D, D[number]> {}
+
+export function enums<T extends string[]>(name: string, ...keys: T): EnumC<T> {
+  function is(u: unknown): u is T[number] {
+    return string.is(u) && keys.includes(u)
+  }
+
+  return new EnumType(
+    keys,
+    name,
+    is,
+    (u, c) => (is(u) ? success(u) : failure(u, c)),
+    identity,
+  )
+}
