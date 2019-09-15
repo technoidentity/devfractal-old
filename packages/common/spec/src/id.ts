54c64e0de7bf1497a4f8013754d3415fc8bf5eb1
Pervez Iqbal <pervezfunctor@gmail.com>
2019-09-15 23:30:26 +0530

    improve ID

--- packages/common/spec/src/id.ts
+++ packages/common/spec/src/id.ts
@@ -1,15 +1,19 @@
-import { brand, Branded, Mixed, TypeOf } from 'io-ts'
+import { brand, BrandC, Branded, Int, Mixed, string, TypeOf } from 'io-ts'
 
-/* eslint-disable-next-line @typescript-eslint/interface-name-prefix */
-export interface IDBrand {
-  readonly ID: unique symbol
-}
+// tslint:disable typedef
 
-// tslint:disable-next-line: typedef eslint-disable
-export function id<T extends Mixed>(inner: T) {
+export function id<T extends Mixed>(
+  inner: T,
+): BrandC<T, { readonly id: symbol }> {
   return brand(
     inner,
-    (n): n is Branded<TypeOf<T>, IDBrand> => inner.is(n),
+    (n): n is Branded<TypeOf<T>, { readonly id: unique symbol }> => inner.is(n),
     'id',
   )
 }
+
+export const IntID = id(Int)
+export type IntID = TypeOf<typeof IntID>
+
+export const StrID = id(string)
+export type StrID = TypeOf<typeof IntID>
