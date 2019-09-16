3aac67e550b95df74e981b214b57269506368759
nandigam chandu <nandigamchandu@gmail.com>
2019-09-16 15:03:05 +0530

    modify MenuBurger with bulma navbar-burger

--- packages/demos/diya/src/components/MenuBurger.tsx
+++ packages/demos/diya/src/components/MenuBurger.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { classNames } from 'technoidentity-devfractal'
 export const MenuBurger: React.FC<any> = ({ isMinimal, onClick }) => (
-  <div
+  <a
     className={classNames('menu-burger', { minimal: isMinimal })}
     onClick={onClick}
   >
-    <div className="burger-lines" />
-  </div>
+    <span />
+    <span />
+    <span />
+  </a>
 )
