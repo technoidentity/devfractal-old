48d8bb1f88510591c1947da58ea88e2b4a62106f
Pervez Iqbal <pervezfunctor@gmail.com>
2019-09-16 22:17:38 +0530

    better name for id

--- packages/common/spec/src/id.ts
+++ packages/common/spec/src/id.ts
@@ -4,16 +4,17 @@ import { brand, BrandC, Branded, Int, Mixed, string, TypeOf } from 'io-ts'
 
 export function id<T extends Mixed>(
   inner: T,
+  name?: string,
 ): BrandC<T, { readonly id: symbol }> {
   return brand(
     inner,
     (n): n is Branded<TypeOf<T>, { readonly id: unique symbol }> => inner.is(n),
-    'id',
+    name || `id<${inner.name}`,
   )
 }
 
-export const IntID = id(Int)
+export const IntID = id(Int, 'IntID')
 export type IntID = TypeOf<typeof IntID>
 
-export const StrID = id(string)
+export const StrID = id(string, 'StrID')
 export type StrID = TypeOf<typeof IntID>
