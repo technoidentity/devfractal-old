aeffe707fb8868ef2e5771c9efa0440b0e0f9a76
Pervez Iqbal <pervezfunctor@gmail.com>
2019-09-16 15:26:22 +0530

    minor fix

--- packages/demos/diya/src/components/MenuBurger.tsx
+++ packages/demos/diya/src/components/MenuBurger.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { classNames } from 'technoidentity-devfractal'
 export const MenuBurger: React.FC<any> = ({ isMinimal, onClick }) => (
   <a
+    href="#!"
     className={classNames('menu-burger', { minimal: isMinimal })}
     onClick={onClick}
   >
