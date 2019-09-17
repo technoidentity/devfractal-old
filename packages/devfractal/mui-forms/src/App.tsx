71ad35dd726fe97eb1031268e830d6613ce8fbf9
Gopikrishna <gopikrishna@Gopikrishnas-iMac.local>
2019-09-17 10:54:31 +0530

    Add formikwitmui form

--- packages/devfractal/mui-forms/src/App.tsx
+++ packages/devfractal/mui-forms/src/App.tsx
@@ -1,14 +1,9 @@
-import Button from '@material-ui/core/Button'
-import { ThreeDRotation } from '@material-ui/icons'
 import React from 'react'
 import { render } from 'react-dom'
 import 'typeface-roboto'
+import { FormikWithMuiForm } from './form/FormikWithMuiForm'
 
 const App: React.FC = () => {
-  return (
-    <Button variant="contained" color="primary">
-      <ThreeDRotation />
-    </Button>
-  )
+  return <FormikWithMuiForm />
 }
 render(<App />, document.getElementById('root'))
