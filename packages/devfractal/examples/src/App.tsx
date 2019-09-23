000dc898d3f8b847e26f8f768d0fdafd26ef123d
sravanidande <sravanidnd89@gmail.com>
2019-09-23 15:38:05 +0530

    Route for Firestore Todo App

--- packages/devfractal/examples/src/App.tsx
+++ packages/devfractal/examples/src/App.tsx
@@ -1,6 +1,19 @@
 import 'bulma/css/bulma.css'
+import { Route, Router } from 'devfractal-router'
 import React from 'react'
 import { render } from 'react-dom'
-import { FSAddForm } from './composites/firestore-todo/views'
+import {
+  FSAddForm,
+  FSEditForm,
+  FsList,
+} from './composites/firestore-todo/views'
 
-render(<FSAddForm />, document.getElementById('root'))
+export const App: React.FC = () => (
+  <Router variant="browser">
+    <Route exact path="/list" component={FsList} />
+    <Route exact path="/list/add" component={FSAddForm} />
+    <Route exact path="/list/:id/edit" component={FSEditForm} />
+  </Router>
+)
+
+render(<App />, document.getElementById('root'))
