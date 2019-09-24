126df469ab54d47f3b21ed4d9219ab413d27b2ca
sravanidande <sravanidnd89@gmail.com>
2019-09-24 14:04:28 +0530

    make FirestoreAPI generic

--- packages/devfractal/examples/src/composites/firestore-todo/todoAPI.ts
+++ packages/devfractal/examples/src/composites/firestore-todo/todoAPI.ts
@@ -1,14 +1,23 @@
-import { boolean, cast, req, string, TypeOf } from 'technoidentity-utils'
+import {
+  AnyObj,
+  boolean,
+  cast,
+  req,
+  string,
+  TypeOf,
+} from 'technoidentity-utils'
 import { db } from './firestoreNew'
 
 // tslint:disable typedef
 
-export interface FSAPI {
-  many(): Promise<ReadonlyArray<FSTodo>>
-  one(id: string): Promise<FSTodo>
-  create(todo: Omit<FSTodo, 'id'>): Promise<FSTodo>
-  replace(todo: FSTodo): Promise<FSTodo>
-  del(id: string): Promise<void>
+export interface FSAPI<Spec extends AnyObj, ID extends keyof TypeOf<Spec>> {
+  many(): Promise<ReadonlyArray<TypeOf<Spec>>>
+  one(id: TypeOf<Spec>[ID]): Promise<TypeOf<Spec>>
+  create(todo: Omit<TypeOf<Spec>, ID>): Promise<TypeOf<Spec>>
+  replace(todo: TypeOf<Spec>): Promise<TypeOf<Spec>>
+  del(id: TypeOf<Spec>[ID]): Promise<void>
+  readonly idKey: ID
+  readonly spec: Spec
 }
 
 export const FSTodo = req({
@@ -19,28 +28,30 @@ export const FSTodo = req({
 
 export type FSTodo = TypeOf<typeof FSTodo>
 
-export function fsRest(
-  // spec: Spec,
-  // idKey: ID /* = 'id' as any */,
+export function fsRest<Spec extends AnyObj, ID extends keyof TypeOf<Spec>>(
+  spec: Spec,
+  idKey: ID /* = 'id' as any */,
   resource: string,
 ): // toQuery: (spec: Spec, query: APIQuery<TypeOf<Spec>>) => string = toQueryFn,
-FSAPI {
+FSAPI<Spec, ID> {
   const res = db.collection(resource)
 
-  const createTodo = (doc: firebase.firestore.DocumentSnapshot): FSTodo => {
+  const createTodo = (
+    doc: firebase.firestore.DocumentSnapshot,
+  ): TypeOf<Spec> => {
     const data = doc.data()
     if (data === undefined) {
       throw new Error('todo not found')
     }
-    return cast(FSTodo, { id: doc.id, title: data.title, done: data.done })
+    return cast(spec, { ...doc.data(), [idKey]: doc.id })
   }
 
-  const many: () => Promise<ReadonlyArray<FSTodo>> = async () => {
+  const many: () => Promise<ReadonlyArray<TypeOf<Spec>>> = async () => {
     const snapshot = await res.get()
     return snapshot.docs.map(createTodo)
   }
 
-  const one: (id: string) => Promise<FSTodo> = async id => {
+  const one: (id: TypeOf<Spec>[ID]) => Promise<TypeOf<Spec>> = async id => {
     const doc = await db
       .collection('todos')
       .doc(id)
@@ -49,13 +60,15 @@ FSAPI {
     return createTodo(doc)
   }
 
-  const create: (todo: Omit<FSTodo, 'id'>) => Promise<FSTodo> = async todo => {
+  const create: (
+    todo: Omit<TypeOf<Spec>, ID>,
+  ) => Promise<TypeOf<Spec>> = async todo => {
     const ref = await res.add(todo)
     const doc = await ref.get()
     return createTodo(doc)
   }
 
-  const replace: (todo: FSTodo) => Promise<FSTodo> = async ({
+  const replace: (todo: TypeOf<Spec>) => Promise<TypeOf<Spec>> = async ({
     id,
     title,
     done,
@@ -67,13 +80,13 @@ FSAPI {
     return createTodo(doc)
   }
 
-  const del: (id: string) => Promise<void> = async id =>
+  const del: (id: TypeOf<Spec>[ID]) => Promise<void> = async id =>
     db
       .collection('todos')
       .doc(id)
       .delete()
 
-  return { many, one, create, replace, del }
+  return { many, one, create, replace, del, spec, idKey }
 }
 
-export const fsTodoAPI = fsRest('todos')
+export const fsTodoAPI = fsRest(FSTodo, 'id', 'todos')
