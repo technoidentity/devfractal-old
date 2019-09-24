6ebfca9b3f2a815e78f4b9a02aab6d75116e20d2
sravanidande <sravanidnd89@gmail.com>
2019-09-24 14:06:03 +0530

    use formComponent for FsAddForm

--- packages/devfractal/examples/src/composites/firestore-todo/views/FireStoreAdd.tsx
+++ packages/devfractal/examples/src/composites/firestore-todo/views/FireStoreAdd.tsx
@@ -1,43 +1,23 @@
+import { formComponent } from 'devfractal-crud'
 import { Simple } from 'devfractal-simple'
 import { useSubmitRedirect } from 'devfractal-ui-api'
 import { Section } from 'devfractal-ui-core'
-import { FormikActions } from 'formik'
 import React from 'react'
 import { FSTodo, fsTodoAPI } from '../todoAPI'
 
-interface FSAddFormInnerProps {
-  fsAddTodo(todo: FSTodo, actions: FormikActions<Omit<FSTodo, 'id'>>): void
-}
-
-const initialValues: Omit<FSTodo, 'id'> = {
-  title: '',
-  done: false,
-}
-
-const FSAddFormInner: React.FC<FSAddFormInnerProps> = ({ fsAddTodo }) => (
+export const FSAddFormView = formComponent(FSTodo, ({ initial, onSubmit }) => (
   <Section>
-    <Simple.Form onSubmit={fsAddTodo} initialValues={initialValues}>
+    <Simple.Form onSubmit={onSubmit} initialValues={initial}>
       <Simple.Text name="title" />
       <Simple.Checkbox name="done">Done</Simple.Checkbox>
       <Simple.FormButtons />
       <Simple.Debug />
     </Simple.Form>
   </Section>
-)
-
-interface FSAddFormViewProps {
-  onCreate(
-    todo: Omit<FSTodo, 'id'>,
-    actions: FormikActions<Omit<FSTodo, 'id'>>,
-  ): void
-}
-
-export const FSAddFormView: React.FC<FSAddFormViewProps> = ({ onCreate }) => (
-  <FSAddFormInner fsAddTodo={onCreate} />
-)
+))
 
 export const FSAddForm: React.FC = () => {
   // tslint:disable-next-line: no-unbound-method
   const { onSubmit } = useSubmitRedirect(fsTodoAPI.create, '/list')
-  return <FSAddFormView onCreate={onSubmit} />
+  return <FSAddFormView onSubmit={onSubmit} />
 }
