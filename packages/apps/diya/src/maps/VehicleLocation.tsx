c698f5a315e68a19320b9e115b8e1ce679ef7b65
nandigam chandu <nandigamchandu@gmail.com>
2019-09-25 14:56:38 +0530

    add vehicleLocation componet

--- packages/apps/diya/src/maps/VehicleLocation.tsx
+++ packages/apps/diya/src/maps/VehicleLocation.tsx
@@ -9,9 +9,10 @@ import {
 import { http as httpAPI } from 'technoidentity-devfractal'
 import { readonlyArray } from 'technoidentity-utils'
 import { VehicleLocation } from '../common'
+import { fakeBaseURL, googleMapApi } from '../config'
 import evIcon from '../images/ev.png'
 
-const http = httpAPI({ baseURL: 'http://localhost:9999' })
+const http = httpAPI({ baseURL: fakeBaseURL })
 
 const Map: React.FC = props => {
   return (
@@ -30,55 +31,63 @@ interface EvPositionState {
   readonly description: string
 }
 
-const WrappedMap = withScriptjs(
-  withGoogleMap(() => {
-    const [state, setState] = React.useState<EvPositionState>()
-    const [evs, setEVs] = React.useState<VehicleLocation[]>([])
-    React.useEffect(() => {
-      const fetchData = async () => {
-        const data = await http.get(
-          { resource: 'vehicles_location' },
-          readonlyArray(VehicleLocation),
-        )
-        setEVs([...data])
-      }
-      fetchData()
-    }, [])
-    return (
-      <Map>
-        {evs &&
-          evs.map((ev: VehicleLocation) => (
-            <Marker
-              key={ev.id}
-              position={{ lat: Number(ev.lat), lng: Number(ev.lng) }}
-              icon={evIcon}
-              onClick={() => {
-                setState({
-                  lat: Number(ev.lat),
-                  lng: Number(ev.lng),
-                  description: ev.description,
-                })
-              }}
-            />
-          ))}
-        {state && (
-          <InfoWindow
-            position={{ lat: state.lat, lng: state.lng }}
-            onCloseClick={() => setState(undefined)}
-          >
-            <div>{state.description}</div>
-          </InfoWindow>
-        )}
-      </Map>
-    )
-  }),
-)
+interface EvLocationsProps {
+  readonly resource: string
+}
+
+const EvLocations: React.FC<EvLocationsProps> = ({ resource }) => {
+  const [state, setState] = React.useState<EvPositionState>()
+  const [evs, setEVs] = React.useState<VehicleLocation[]>([])
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const data = await http.get({ resource }, readonlyArray(VehicleLocation))
+      setEVs([...data])
+    }
+    fetchData()
+  }, [])
+  return (
+    <Map>
+      {evs &&
+        evs.map((ev: VehicleLocation) => (
+          <Marker
+            key={ev.id}
+            position={{
+              lat: Number(ev.lat),
+              lng: Number(ev.lng),
+            }}
+            icon={evIcon}
+            onClick={() => {
+              setState({
+                lat: Number(ev.lat),
+                lng: Number(ev.lng),
+                description: ev.description,
+              })
+            }}
+          />
+        ))}
+      {state && (
+        <InfoWindow
+          position={{
+            lat: state.lat,
+            lng: state.lng,
+          }}
+          onCloseClick={() => setState(undefined)}
+        >
+          <div>{state.description}</div>
+        </InfoWindow>
+      )}
+    </Map>
+  )
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(EvLocations))
 
 export const MapView = () => (
   <WrappedMap
-    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJu6P9EdyC5YfmwzLJzCuDL_26l5Syqx0&v=3.exp&libraries=geometry,drawing,places"
+    resource="vehicles_location"
+    googleMapURL={googleMapApi}
     loadingElement={<div style={{ height: `100%` }} />}
-    containerElement={<div style={{ height: `600px` }} />}
+    containerElement={<section style={{ height: `600px` }} />}
     mapElement={<div style={{ height: `100%` }} />}
   />
 )
