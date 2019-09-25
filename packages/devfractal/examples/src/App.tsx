735aead478f653c6f84afab332ba7218fd114dd1
sravanidande <sravanidnd89@gmail.com>
2019-09-25 16:52:09 +0530

    fix imports

--- packages/devfractal/examples/src/App.tsx
+++ packages/devfractal/examples/src/App.tsx
@@ -2,7 +2,7 @@ import 'bulma/css/bulma.css'
 import { Router } from 'devfractal-router'
 import React from 'react'
 import { render } from 'react-dom'
-import { FSTodoApp } from './composites/firestore-todo/v3/FSTodoApp'
+import { FSTodoApp } from './composites/firestore-todo/v3'
 
 export const App: React.FC = () => (
   <Router variant="browser">
