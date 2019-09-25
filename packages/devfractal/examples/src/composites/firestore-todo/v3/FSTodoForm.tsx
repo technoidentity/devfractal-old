805d883b18ee4114bbae04b34d8f872add8e18c3
sravanidande <sravanidnd89@gmail.com>
2019-09-25 16:53:45 +0530

    add firestore V3

---
+++ packages/devfractal/examples/src/composites/firestore-todo/v3/FSTodoForm.tsx
@@ -0,0 +1,14 @@
+// tslint:disable typedef
+import { formComponent } from 'devfractal-crud'
+import { Simple } from 'devfractal-simple'
+import React from 'react'
+import { FSTodo } from '../common'
+
+export const FSTodoForm = formComponent(FSTodo, ({ initial, onSubmit }) => (
+  <Simple.Form onSubmit={onSubmit} initialValues={initial}>
+    <Simple.Text name="title" />
+    <Simple.Checkbox name="done">Done</Simple.Checkbox>
+    <Simple.FormButtons />
+    <Simple.Debug />
+  </Simple.Form>
+))
---
+++ packages/devfractal/examples/src/composites/firestore-todo/v3/FSTodoItem.tsx
@@ -0,0 +1,36 @@
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { Button, CheckBox, Field, Icon, Td, Tr } from 'devfractal-ui-core'
+import React from 'react'
+import { FSTodo } from '../common'
+
+interface FSTodoItemProps {
+  readonly todo: FSTodo
+  onEdit(id: String): void
+  onDelete(id: String): void
+}
+
+export const FSTodoItem: React.FC<FSTodoItemProps> = ({
+  todo,
+  onEdit,
+  onDelete,
+}) => {
+  return (
+    <Tr>
+      <Td>{todo.id}</Td>
+      <Td>{todo.title}</Td>
+      <Td>
+        <CheckBox checked={todo.done} readOnly />
+      </Td>
+      <Td>
+        <Field grouped>
+          <Button variant="info" onClick={() => onEdit(todo.id)}>
+            <Icon icon={faEdit} />
+          </Button>
+          <Button variant="info" onClick={() => onDelete(todo.id)}>
+            <Icon icon={faTrash} />
+          </Button>
+        </Field>
+      </Td>
+    </Tr>
+  )
+}
