985902debc5a2d0823ba2d33d6936d04bf19cadd
Pervez Iqbal <pervezfunctor@gmail.com>
2019-09-28 11:58:40 +0530

    App.tsx for router

--- packages/devfractal/router/src/App.tsx
+++ packages/devfractal/router/src/App.tsx
@@ -1,4 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Link } from 'react-router-dom'
+import { Router } from './Router'
+import { Route } from './SafeRoute'
+import { SimpleRedirect } from './SimpleRedirect'
 
-render(<h1>hello world</h1>, document.getElementById('root'))
+const Hello: React.FC = () => <Link to="/world">hello world</Link>
+
+const App: React.FC = () => (
+  <Router>
+    <SimpleRedirect from="/" to="/hello" />
+    <Route path="/hello" component={Hello} />
+    <Route path="/world" render={() => <h1>Hello, World!</h1>} />
+  </Router>
+)
+
+render(<App />, document.getElementById('root'))
