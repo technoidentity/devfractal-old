924ece34271090526f8409c3d5622e7f8e0b86a2
nandigam chandu <nandigamchandu@gmail.com>
2019-10-04 12:50:56 +0530

    Fix types issues

--- packages/apps/diya/src/maps/StandaloneMapSearchBox.tsx
+++ packages/apps/diya/src/maps/StandaloneMapSearchBox.tsx
@@ -1,19 +1,19 @@
 import { StandaloneSearchBox } from '@react-google-maps/api'
-import React from 'react'
+import React, { MutableRefObject } from 'react'
 import { Input } from 'technoidentity-devfractal'
 
 export const StandaloneGoogleMapSearchBox = ({
   onPlacesChanged,
 }: {
-  onPlacesChanged(ref: React.MutableRefObject<any>): void
+  onPlacesChanged(ref: MutableRefObject<StandaloneSearchBox>): void
 }) => {
-  const searchBox = React.useRef<any>()
+  const searchBox: MutableRefObject<StandaloneSearchBox> = React.useRef<any>()
   return (
     <StandaloneSearchBox
       ref={searchBox}
       onPlacesChanged={() => onPlacesChanged(searchBox)}
     >
-      <Input type="search" placeholder="Customized your placeholder" />
+      <Input type="search" placeholder="Enter Location Name" />
     </StandaloneSearchBox>
   )
 }
