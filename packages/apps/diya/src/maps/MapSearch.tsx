6e701cb48fd75f363dbfd65776ab3777d5720466
nandigam chandu <nandigamchandu@gmail.com>
2019-10-04 12:39:53 +0530

    refactor mapsearch component

--- packages/apps/diya/src/maps/MapSearch.tsx
+++ packages/apps/diya/src/maps/MapSearch.tsx
@@ -1,12 +1,10 @@
 import { Autocomplete, GoogleMapProps, Marker } from '@react-google-maps/api'
 import React from 'react'
 import { Input, InputProps } from 'technoidentity-devfractal'
-import { googleMapApiKey } from '../config'
-import { defaultMapSettings } from './defaultSettings'
 import { LoadMapApiKey } from './LoadMapApiKey'
 import { Map } from './Map'
 
-interface MapSearchInnerProps extends Omit<InputProps, 'onLoad' | 'onDragEnd'> {
+interface MapSearchProps extends Omit<InputProps, 'onLoad' | 'onDragEnd'> {
   readonly defaultMapSettings: GoogleMapProps
   readonly location: google.maps.LatLngLiteral
   readonly googleMapApiKey: string
@@ -15,7 +13,7 @@ interface MapSearchInnerProps extends Omit<InputProps, 'onLoad' | 'onDragEnd'> {
   onDragEnd?(e: google.maps.MouseEvent): void
 }
 
-export const MapSearchInner: React.FC<MapSearchInnerProps> = props => {
+export const MapSearch: React.FC<MapSearchProps> = props => {
   const {
     googleMapApiKey,
     defaultMapSettings,
@@ -37,39 +35,5 @@ export const MapSearchInner: React.FC<MapSearchInnerProps> = props => {
         {children}
       </Map>
     </LoadMapApiKey>
-  )
-}
-
-export const MapSearch: React.FC = props => {
-  const [location, setLocation] = React.useState<google.maps.LatLngLiteral>({
-    lat: 17.385044,
-    lng: 78.486671,
-  })
-
-  const [places, setPlaces] = React.useState<google.maps.places.Autocomplete>()
-
-  return (
-    <MapSearchInner
-      defaultMapSettings={defaultMapSettings}
-      googleMapApiKey={googleMapApiKey}
-      location={location}
-      onLoad={autocomplete => {
-        setPlaces(autocomplete)
-      }}
-      onPlaceChanged={() => {
-        const geometry =
-          places && places.getPlace() && places.getPlace().geometry
-        if (geometry) {
-          setLocation(geometry.location.toJSON())
-        }
-      }}
-      onDragEnd={e => {
-        setLocation(e.latLng.toJSON())
-      }}
-      type="search"
-      ctrlSize="medium"
-    >
-      {props.children}
-    </MapSearchInner>
   )
 }
