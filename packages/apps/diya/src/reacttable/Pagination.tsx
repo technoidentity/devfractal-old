b4e820b213afa35ac78e97289f5ff4dd342a7e81
Gopikrishna <gopikrishnakathari@gmail.com>
2019-10-11 16:12:48 +0530

    Add pagination Component

---
+++ packages/apps/diya/src/reacttable/Pagination.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { PaginationProps } from './models'
+
+export const Pagination: React.FC<PaginationProps> = ({
+  gotoPage,
+  canPreviousPage,
+  previousPage,
+  nextPage,
+  canNextPage,
+  pageCount,
+  pageIndex,
+  pageOptions,
+  pageSize,
+  setPageSize,
+}) => {
+  return (
+    <div style={{ padding: '0.5rem' }}>
+      <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+        {'<<'}
+      </button>{' '}
+      <button onClick={previousPage} disabled={!canPreviousPage}>
+        {'<'}
+      </button>{' '}
+      <button onClick={nextPage} disabled={!canNextPage}>
+        {'>'}
+      </button>{' '}
+      <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+        {'>>'}
+      </button>{' '}
+      <span>
+        Page{' '}
+        <strong>
+          {pageIndex + 1} of {pageOptions.length}
+        </strong>{' '}
+      </span>
+      <span>
+        | Go to page:{' '}
+        <input
+          type="number"
+          defaultValue={`${pageIndex + 1}`}
+          onChange={e => {
+            const page = e.target.value ? Number(e.target.value) - 1 : 0
+            gotoPage(page)
+          }}
+          style={{ width: '100px' }}
+        />
+      </span>{' '}
+      <select
+        value={pageSize}
+        onChange={e => {
+          setPageSize(Number(e.target.value))
+        }}
+      >
+        {[10, 20, 30, 40, 50].map(pageSize => (
+          <option key={pageSize} value={pageSize}>
+            Show {pageSize}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
