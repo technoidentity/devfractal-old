c6e30c230859d0ff719f5d121af713ab28bd0eaa
Gopikrishna <gopikrishnakathari@gmail.com>
2019-10-11 16:02:46 +0530

    Create Table Component

---
+++ packages/apps/diya/src/reacttable/Table.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import {
+  TableInstance,
+  useFilters,
+  usePagination,
+  UsePaginationValues,
+  useSortBy,
+  useTable,
+  useTableState,
+} from 'react-table'
+import { Filters } from './Filters'
+import { ReactTableProps } from './models'
+import { Pagination } from './Pagination'
+import { ReactTable } from './ReactTable'
+import { generateReactTableData } from './ReactTableData'
+function isPaginated(
+  table: TableInstance | TableInstance & UsePaginationValues,
+): table is TableInstance & UsePaginationValues {
+  return (table as TableInstance & UsePaginationValues).page !== undefined
+}
+
+export const Table: React.FC<ReactTableProps> = ({
+  data,
+  filterOption,
+  headerNames,
+  sorting,
+  pagination,
+}) => {
+  const columns = generateReactTableData({ data, headerNames, filterOption })
+
+  if (data.length > 0) {
+    const tableState = useTableState({ pageIndex: 0, pageSize: 10 })
+
+    const reactTableData = useTable(
+      {
+        columns,
+        data,
+        loading: true,
+        state: tableState,
+      },
+      useFilters,
+      useSortBy,
+      usePagination,
+    )
+
+    if (isPaginated(reactTableData)) {
+      return (
+        <>
+          <Filters {...reactTableData} />
+          <ReactTable {...reactTableData} sorting={sorting} />
+          {pagination ? (
+            <Pagination
+              canPreviousPage={reactTableData.canPreviousPage}
+              canNextPage={reactTableData.canNextPage}
+              pageOptions={reactTableData.pageOptions}
+              pageCount={reactTableData.pageCount}
+              gotoPage={reactTableData.gotoPage}
+              nextPage={reactTableData.nextPage}
+              previousPage={reactTableData.previousPage}
+              setPageSize={reactTableData.setPageSize}
+              pageIndex={reactTableData.pageIndex}
+              pageSize={reactTableData.pageSize}
+            />
+          ) : (
+            <></>
+          )}
+        </>
+      )
+    } else {
+      return <p>error</p>
+    }
+  }
+  return <>No data</>
+}
