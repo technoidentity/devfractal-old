5ab1442261e16f0923c8710f0757421c9593b69f
Gopikrishna <gopikrishnakathari@gmail.com>
2019-10-23 15:34:13 +0530

    add devfractal Select and Input components

--- packages/apps/diya/src/reacttable/ReactTableFilters.tsx
+++ packages/apps/diya/src/reacttable/ReactTableFilters.tsx
@@ -1,46 +1,52 @@
+import { Column, Input, Select } from 'devfractal-ui-core'
 import React from 'react'
 import { Row } from 'react-table'
 import { ColumnFilterProps } from './models'
 
 export const DefaultColumnFilter = ({
-  column: { filterValue, preFilteredRows, setFilter },
+  column: { filterValue, setFilter, Header },
 }: ColumnFilterProps) => {
-  const count = preFilteredRows && preFilteredRows.length
   return (
-    <input
-      value={filterValue || ''}
-      onChange={e => {
-        setFilter(e.target.value)
-      }}
-      placeholder={`Search ${count} records...`}
-    />
+    <Column>
+      <Input
+        value={filterValue || ''}
+        onChange={e => {
+          setFilter(e.target.value)
+        }}
+        placeholder={`Search by ${Header}`}
+      />
+    </Column>
   )
 }
 
 export const SelectColumnFilter = ({
-  column: { filterValue, setFilter, preFilteredRows, id },
+  column: { filterValue, setFilter, preFilteredRows, id, Header },
 }: ColumnFilterProps) => {
   const options = React.useMemo(() => {
     const options = new Set<string[]>()
-    preFilteredRows.forEach((row: Row) => {
-      options.add(row.values[id])
-    })
+    // tslint:disable-next-line:no-unused-expression
+    preFilteredRows &&
+      preFilteredRows.forEach((row: Row) => {
+        options.add(row.values[id])
+      })
     return Array.from(options)
   }, [id, preFilteredRows])
   return (
-    <select
-      value={filterValue}
-      onChange={e => {
-        setFilter(e.target.value)
-      }}
-    >
-      <option value="">All</option>
-      {options &&
-        options.map((option, i) => (
-          <option key={i} value={option}>
-            {option}
-          </option>
-        ))}
-    </select>
+    <Column>
+      <Select
+        value={filterValue}
+        onChange={e => {
+          setFilter(e.target.value)
+        }}
+      >
+        <option value="">{`Filter by ${Header}`}</option>
+        {options &&
+          options.map((option, i) => (
+            <option key={i} value={option}>
+              {option}
+            </option>
+          ))}
+      </Select>
+    </Column>
   )
 }
