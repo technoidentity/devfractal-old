fdfef05055a3139e2ce5d94214b82aba64ca621e
nandigam chandu <nandigamchandu@gmail.com>
2019-10-31 15:14:35 +0530

    add auth functionality

--- packages/apps/diya/src/auth/Auth.tsx
+++ packages/apps/diya/src/auth/Auth.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useAuth } from '../auth/AuthContext'
 import { AuthenticatedApp } from './AuthenticatedApp'
+import { UnAuthenticatedApp } from './UnAuthenticatedApp'
 
 export const Auth: React.FC = () => {
-  // const { user } = useAuth()
+  const { user } = useAuth()
 
-  // return user ? <AuthenticatedApp /> : <UnAuthenticatedApp />
-  return <AuthenticatedApp />
+  return user ? <AuthenticatedApp /> : <UnAuthenticatedApp />
+  // return <AuthenticatedApp />
 }
