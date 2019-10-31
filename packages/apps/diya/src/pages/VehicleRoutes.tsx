e601c82e4027ffbd7a2792b476b52bb30d823e7e
nandigam chandu <nandigamchandu@gmail.com>
2019-10-31 18:03:26 +0530

    replace crud component with api-get component

--- packages/apps/diya/src/pages/VehicleRoutes.tsx
+++ packages/apps/diya/src/pages/VehicleRoutes.tsx
@@ -1,8 +1,42 @@
 import React from 'react'
-import { CrudRoutes } from 'technoidentity-devfractal'
-import { vehicleAPI } from '../common'
-import { VehicleForm, VehicleList } from '../views'
+import {
+  // All,
+  Get,
+  http as httpAPI,
+  // links,
+  // paths,
+  Route,
+} from 'technoidentity-devfractal'
+import { AuthUserInfo, VehicleResponse } from '../common'
+import { baseURL } from '../config'
+import { VehicleList1 } from '../views'
+
+// const ps = paths(vehicleAPI.resource)
+// const ls = links(vehicleAPI.resource)
+
+async function getVehicleList(): Promise<VehicleResponse['data']['rows']> {
+  const userData = localStorage.getItem('loginData')
+  if (userData) {
+    const {
+      data: { token },
+    }: AuthUserInfo = JSON.parse(userData)
+    const http = httpAPI({
+      baseURL,
+      headers: { Authorization: `bearer ${token}` },
+    })
+    const vehicles = await http.get({ resource: 'vehicles' }, VehicleResponse)
+    return vehicles.data.rows
+  }
+  throw Error('Invalid login')
+}
+
+const VehicleListRoute = () => (
+  // <All path={ps.list} api={vehicleAPI} list={VehicleList} />
+  <Get asyncFn={getVehicleList} component={VehicleList1} />
+)
 
 export const VehicleRoutes = () => (
-  <CrudRoutes api={vehicleAPI} form={VehicleForm} list={VehicleList} />
+  // <CrudRoutes api={vehicleAPI} form={VehicleForm} list={VehicleList} />
+  <Route path="/vehicles" render={() => <VehicleListRoute />} />
+  // <VehicleListRoute />
 )
