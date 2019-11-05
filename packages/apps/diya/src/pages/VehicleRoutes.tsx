e05ff5ac2b9f062a3be4075ea5dfd9f362985ae3
nandigam chandu <nandigamchandu@gmail.com>
2019-11-05 12:32:14 +0530

    add integrate vehicle edit api

--- packages/apps/diya/src/pages/VehicleRoutes.tsx
+++ packages/apps/diya/src/pages/VehicleRoutes.tsx
@@ -6,16 +6,19 @@ import {
   http as httpAPI,
   links,
   paths,
+  Put,
   Route,
 } from 'technoidentity-devfractal'
 import {
   AuthUserInfo,
   vehicleAdd,
   vehicleAPI,
+  VehicleEdit as VE,
+  vehicleEditAPI,
   VehicleResponse,
 } from '../common'
 import { baseURL } from '../config'
-import { VehicleList1, VehicleForm } from '../views'
+import { VehicleForm, VehicleList1 } from '../views'
 
 const ps = paths(vehicleAPI.resource)
 const ls = links(vehicleAPI.resource)
@@ -36,6 +39,41 @@ async function getVehicleList(): Promise<VehicleResponse['data']['rows']> {
   throw Error('Invalid login')
 }
 
+async function getVehicle(): Promise<VE['data']> {
+  const userData = localStorage.getItem('loginData')
+  if (userData) {
+    const {
+      data: { token },
+    }: AuthUserInfo = JSON.parse(userData)
+    const http = httpAPI({
+      baseURL,
+      headers: { Authorization: `bearer ${token}` },
+    })
+    const vehicles = await http.get(
+      { resource: 'vehicles', path: 'baaf3208-231f-45f8-9356-08f647dc846f' },
+      VE,
+    )
+    return vehicles.data
+  }
+  throw Error('Invalid login')
+}
+
+async function putVehicle(data: any): Promise<VE['data']> {
+  const userData = localStorage.getItem('loginData')
+  if (userData) {
+    const {
+      data: { token },
+    }: AuthUserInfo = JSON.parse(userData)
+    const http = httpAPI({
+      baseURL,
+      headers: { Authorization: `bearer ${token}` },
+    })
+    const vehicles = await http.put({ resource: 'vehicles' }, data, VE)
+    return vehicles.data
+  }
+  throw Error('Invalid login')
+}
+
 const VehicleListRoute = () => (
   // <All path={ps.list} api={vehicleAPI} list={VehicleList} />
   <Get asyncFn={getVehicleList} component={VehicleList1} />
@@ -50,21 +88,22 @@ const VehicleAdd = () => (
   />
 )
 
-// const VehicleEdit = () => (
-//   <Put
-//     id={vehicleAPI.idKey}
-//     doGet={vehicleAPI.get}
-//     onPut={vehicleAPI.replace}
-//     component={VehicleForm}
-//     redirectTo="/vehicles"
-//   />
-// )
+const VehicleEdit = () => (
+  <Put
+    id={vehicleEditAPI.idKey}
+    doGet={getVehicle}
+    onPut={putVehicle}
+    component={VehicleForm}
+    redirectTo="/vehicles"
+  />
+)
 
 export const VehicleRoutes = () => (
   // <CrudRoutes api={vehicleAPI} form={VehicleForm} list={VehicleList} />
   <>
     <VehicleAdd />
     <Route path="/vehicles" render={() => <VehicleListRoute />} />
+    <Route path={ps.edit} render={() => <VehicleEdit />} />
   </>
   // <VehicleListRoute />
 )
