0900fd57d76c52c10b568df64d1a1841462e794a
nandigam chandu <nandigamchandu@gmail.com>
2019-11-06 15:16:42 +0530

    modify user list table

--- packages/apps/diya/src/views/UserList.tsx
+++ packages/apps/diya/src/views/UserList.tsx
@@ -3,27 +3,41 @@ import {
   CreateLink,
   CrudTable,
   links,
-  listComponent,
+  // listComponent,
   RoutedPager,
   Section,
 } from 'technoidentity-devfractal'
-import { User } from '../common'
+import { UserListResponse } from '../common'
 import { HeadTitle } from '../components'
 
 const userLinks = links('users')
 
-export const UserList = listComponent(User, ({ data: userList }) => (
+export const UserList = ({
+  data,
+}: {
+  readonly data: UserListResponse['data']['rows']
+}) => (
   <Section>
     <HeadTitle>Users</HeadTitle>
     <CreateLink alignment="right" variant="primary" to={userLinks.create}>
       Add user
     </CreateLink>
-    <CrudTable
-      data={userList}
-      select={['userName', 'role', 'address1', 'phone', 'email']}
-      override={{ address1: 'Address' }}
-      editTo={v => userLinks.edit(v.id)}
-    />
+    <CrudTable data={data} editTo={v => userLinks.edit(v.name)} />
     <RoutedPager />
   </Section>
-))
+)
+// export const UserList = listComponent(User, ({ data: userList }) => (
+//   <Section>
+//     <HeadTitle>Users</HeadTitle>
+//     <CreateLink alignment="right" variant="primary" to={userLinks.create}>
+//       Add user
+//     </CreateLink>
+//     <CrudTable
+//       data={userList}
+//       select={['userName', 'role', 'address1', 'phone', 'email']}
+//       override={{ address1: 'Address' }}
+//       editTo={v => userLinks.edit(v.id)}
+//     />
+//     <RoutedPager />
+//   </Section>
+// ))
