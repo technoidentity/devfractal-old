0bd56dbe90990965b19cc30c204c25d5aa2fb350
nandigam chandu <nandigamchandu@gmail.com>
2019-11-09 11:27:58 +0530

    add custom filter to select filter type

--- packages/apps/diya/src/reacttable/utils.ts
+++ packages/apps/diya/src/reacttable/utils.ts
@@ -1,4 +1,17 @@
 import { format } from 'date-fns'
+import matchSorter from 'match-sorter'
+
 export function formatDate(date: Date | undefined): string | undefined {
   return date && format(date, 'yyyy/MM/dd')
+}
+
+export function fuzzyTextFilterFn(
+  rows: readonly any[],
+  id: string,
+  filterValue: string,
+) {
+  return matchSorter(rows, filterValue, {
+    keys: [row => row.values[id]],
+    threshold: matchSorter.rankings.WORD_STARTS_WITH,
+  })
 }
