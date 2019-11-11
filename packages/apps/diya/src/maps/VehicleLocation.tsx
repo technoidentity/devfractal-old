a9860f149fa95290c5f0e4f81242f7eabbdaea91
nandigam chandu <nandigamchandu@gmail.com>
2019-11-11 17:04:50 +0530

    modify EvsLocation component

--- packages/apps/diya/src/maps/VehicleLocation.tsx
+++ packages/apps/diya/src/maps/VehicleLocation.tsx
@@ -1,15 +1,11 @@
 import { InfoWindow, Marker } from '@react-google-maps/api'
 import React from 'react'
-import { http as httpAPI } from 'technoidentity-devfractal'
-import { readonlyArray } from 'technoidentity-utils'
 import { VehicleLocation } from '../common'
-import { fakeBaseURL, googleMapApiKey } from '../config'
+import { googleMapApiKey } from '../config'
 import evIcon from '../images/ev.png'
 import { defaultMapSettings } from './defaultSettings'
 import { LoadMapApiKey } from './LoadMapApiKey'
 import { Map } from './Map'
-
-const http = httpAPI({ baseURL: fakeBaseURL })
 
 interface EvPositionState {
   readonly lat: number
@@ -18,24 +14,15 @@ interface EvPositionState {
 }
 
 interface EvLocationsProps {
-  readonly resource: string
+  readonly data: readonly VehicleLocation[]
 }
 
-const EvLocations: React.FC<EvLocationsProps> = ({ resource }) => {
+const EvLocations: React.FC<EvLocationsProps> = ({ data }) => {
   const [state, setState] = React.useState<EvPositionState>()
-  const [evs, setEVs] = React.useState<VehicleLocation[]>([])
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const data = await http.get({ resource }, readonlyArray(VehicleLocation))
-      setEVs([...data])
-    }
-    // tslint:disable-next-line: no-floating-promises
-    fetchData()
-  }, [resource])
   return (
     <>
-      {evs &&
-        evs.map((ev: VehicleLocation) => (
+      {data &&
+        data.map((ev: VehicleLocation) => (
           <Marker
             key={ev.id}
             position={{
@@ -70,14 +57,16 @@ const EvLocations: React.FC<EvLocationsProps> = ({ resource }) => {
 const LoadComponent = () => <div>...Loading Map</div>
 const ErrorComponent = () => <div>Map cannot be loaded right now, sorry.</div>
 
-export const MapView: React.FC = () => (
+export const MapView: React.FC<{ readonly data: readonly any[] }> = ({
+  data,
+}) => (
   <LoadMapApiKey
     googleMapsApiKey={googleMapApiKey}
     loadComponent={LoadComponent}
     errorComponent={ErrorComponent}
   >
     <Map {...defaultMapSettings}>
-      <EvLocations resource="vehicles_location" />
+      <EvLocations data={data} />
     </Map>
   </LoadMapApiKey>
 )
