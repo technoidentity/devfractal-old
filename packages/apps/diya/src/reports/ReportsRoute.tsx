b38d05a08c9ea8bac4d1f79fb0dbb08d3c009877
Gopikrishna <gopikrishnakathari@gmail.com>
2019-11-11 18:41:14 +0530

    add reportroute component

---
+++ packages/apps/diya/src/reports/ReportsRoute.tsx
@@ -0,0 +1,37 @@
+import { History } from 'history'
+import React from 'react'
+import { Route, useHistory } from 'technoidentity-devfractal'
+import { PerformanceReport } from './batteryreports/PerformanceReport'
+import { TotalAnalysisReport } from './costanalysisreports/TotalAnalysisReport'
+import { ReportScreen } from './ReportScreen'
+
+export const ReportsRoute = () => {
+  const history: History = useHistory()
+  const handleBatteryPerformance = () => {
+    history.push('/reports/batteryperformance')
+  }
+  const handleTotalAnalysisReports = () => {
+    history.push('/reports/totalanalysisreports')
+  }
+  return (
+    <>
+      <Route
+        path="/reports"
+        render={() => (
+          <ReportScreen
+            handleBatteryPerformance={handleBatteryPerformance}
+            handleTotalAnalysisReports={handleTotalAnalysisReports}
+          />
+        )}
+      />
+      <Route
+        path="/reports/batteryperformance"
+        render={() => <PerformanceReport />}
+      />
+      <Route
+        path="/reports/totalanalysisreports"
+        render={() => <TotalAnalysisReport />}
+      />
+    </>
+  )
+}
