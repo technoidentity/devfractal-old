897f19b0a28ff0e8da7b130dbeee5a786b1c8d79
nandigam chandu <nandigamchandu@gmail.com>
2019-11-13 16:09:59 +0530

    modity client table as per model

--- packages/apps/diya/src/views/ClientList.tsx
+++ packages/apps/diya/src/views/ClientList.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
-import {
-  CreateLink,
-  links,
-  listComponent,
-  Section,
-} from 'technoidentity-devfractal'
-import { Client } from '../common'
+import { CreateLink, links, Section } from 'technoidentity-devfractal'
+import { Client, ClientListResponse } from '../common'
 import { HeadTitle } from '../components'
 import { Table } from '../reacttable/Table'
 
 const clientLinks = links('clients')
 
-export const ClientList = listComponent(Client, ({ data: clientList }) => {
-  const tableData = clientList.map(data => ({ ...data, actions: 'actions' }))
+export const ClientList = ({
+  data: clientList,
+}: {
+  readonly data: ClientListResponse['data']['rows']
+}) => {
+  const tableData = clientList.map(data => ({
+    ...data,
+    actions: 'actions',
+  }))
   return (
     <Section>
       <HeadTitle>Clients</HeadTitle>
@@ -29,23 +31,55 @@ export const ClientList = listComponent(Client, ({ data: clientList }) => {
         ]}
         sorting={true}
         pagination={true}
-        headerNames={[
-          'clientName',
-          'contractType',
-          'email',
-          'numberOfEVS',
-          'rateOfEVS',
-          'assignedEVSHistory',
-        ]}
+        headerNames={['name', 'email', 'billingType', 'numberOfEvsOrDrivers']}
         filterOption={[{ columnName: 'clientName', filterType: 'search' }]}
         actions={{
           editTo: id => clientLinks.edit(id),
         }}
         headerLabels={{
-          numberOfEVS: 'No. of EVS',
-          rateOfEVS: 'Rate of EVS',
+          name: 'Client Name',
+          numberOfEvsOrDrivers: 'No. of EVS',
         }}
       />
     </Section>
   )
-})
+}
+
+// export const ClientList = listComponent(Client, ({ data: clientList }) => {
+//   const tableData = clientList.map(data => ({ ...data, actions: 'actions' }))
+//   return (
+//     <Section>
+//       <HeadTitle>Clients</HeadTitle>
+
+//       <CreateLink alignment="right" variant="primary" to={clientLinks.create}>
+//         Add Client
+//       </CreateLink>
+//       <Table
+//         tableData={[
+//           // @TODO: Fix 'id' required/partial later
+//           ...((tableData as unknown) as ReadonlyArray<
+//             Omit<Client, 'id'> & { readonly id: string }
+//           >),
+//         ]}
+//         sorting={true}
+//         pagination={true}
+//         headerNames={[
+//           'clientName',
+//           'contractType',
+//           'email',
+//           'numberOfEVS',
+//           'rateOfEVS',
+//           'assignedEVSHistory',
+//         ]}
+//         filterOption={[{ columnName: 'clientName', filterType: 'search' }]}
+//         actions={{
+//           editTo: id => clientLinks.edit(id),
+//         }}
+//         headerLabels={{
+//           numberOfEVS: 'No. of EVS',
+//           rateOfEVS: 'Rate of EVS',
+//         }}
+//       />
+//     </Section>
+//   )
+// })
