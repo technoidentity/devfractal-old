716e85960eae305ea0bbe8457a602650f07f5a0b
Gopikrishna <gopikrishnakathari@gmail.com>
2019-11-18 17:03:57 +0530

    add react toastify

---
+++ packages/apps/diya/src/components/Message.tsx
@@ -0,0 +1,19 @@
+import { toast, Zoom } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+toast.configure()
+
+export const toastMessage = (message: string) => {
+  message === 'fail'
+    ? toast.success(message, {
+        transition: Zoom,
+        autoClose: 3000,
+        closeButton: false,
+        hideProgressBar: true,
+      })
+    : toast.error(message, {
+        transition: Zoom,
+        autoClose: 3000,
+        closeButton: false,
+        hideProgressBar: true,
+      })
+}
