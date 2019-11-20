81825f832812e7e6a1502ff87e7fe5aa84f564b0
Gopikrishna <gopikrishnakathari@gmail.com>
2019-11-20 17:39:02 +0530

    add diyalogo and background image

--- packages/apps/diya/src/views/LoginForm.tsx
+++ packages/apps/diya/src/views/LoginForm.tsx
@@ -3,13 +3,17 @@ import { Form, Formik, FormikProps } from 'formik'
 import React from 'react'
 import {
   Button,
-  ButtonsGroup,
+  Card,
+  Column,
+  Columns,
   ErrorField,
   Field,
+  Image,
   InputField,
-  Section,
 } from 'technoidentity-devfractal'
 import * as yup from 'yup'
+import diyaLogo from '../images/diyaLogo.png'
+import './LoginForm.css'
 
 export interface LoginValues {
   readonly username: string
@@ -24,39 +28,77 @@ const schema = yup.object().shape({
 const initialValues: LoginValues = { username: '', password: '' }
 
 const InnerLoginForm: React.FC<FormikProps<LoginValues>> = () => (
-  <Section
-    style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+  <div
+    className="hero is-fullheight"
+    style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
   >
-    <Form>
-      <Section>
-        <Field>
-          <InputField
-            type="text"
-            name="username"
-            placeholder="Username"
-            leftIcon={faUser}
+    <Card
+      style={{
+        width: '325px',
+        height: '400px',
+        borderRadius: '10px',
+      }}
+    >
+      <Columns>
+        <Column>
+          <Image
+            src={diyaLogo}
+            alt="diyaImage"
+            size="96x96"
+            style={{ margin: '35px 110px' }}
           />
-          <ErrorField name="username" />
-        </Field>
-        <Field>
-          <InputField
-            type="password"
-            name="password"
-            placeholder="Password"
-            leftIcon={faLock}
-          />
-          <ErrorField name="password" />
-        </Field>
-        <Field>
-          <ButtonsGroup alignment="centered">
-            <Button type="submit" variant="primary">
-              Login
-            </Button>
-          </ButtonsGroup>
-        </Field>
-      </Section>
-    </Form>
-  </Section>
+          <Form
+            style={{
+              marginTop: '100px',
+              paddingLeft: '20px',
+              paddingRight: '20px',
+            }}
+          >
+            <Field>
+              <InputField
+                style={{
+                  borderRadius: '5px',
+                }}
+                type="text"
+                name="username"
+                placeholder="Username"
+                leftIcon={faUser}
+              />
+              <ErrorField name="username" />
+            </Field>
+            <Field>
+              <InputField
+                style={{
+                  borderRadius: '5px',
+                }}
+                type="password"
+                name="password"
+                placeholder="Password"
+                leftIcon={faLock}
+              />
+              <ErrorField name="password" />
+            </Field>
+            <Field>
+              <Button
+                type="submit"
+                variant="primary"
+                fullWidth
+                style={{
+                  borderRadius: '5px',
+                }}
+              >
+                Login
+              </Button>
+            </Field>
+          </Form>
+        </Column>
+      </Columns>
+    </Card>
+  </div>
 )
 
 export interface LoginFormProps {
