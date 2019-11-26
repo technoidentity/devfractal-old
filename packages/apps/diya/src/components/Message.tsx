f77bb503bfcc4440706aa8b04a42bcc52113a928
gopikrishnak <gopikrishnakathari@gmail.com>
2019-11-26 18:00:27 +0530

    refactor toast message

--- packages/apps/diya/src/components/Message.tsx
+++ packages/apps/diya/src/components/Message.tsx
@@ -2,15 +2,15 @@ import { toast, Zoom } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 toast.configure()
 
-export const toastMessage = (message: string) => {
-  message === 'fail'
+export const toastMessage = (code: string, message: string) => {
+  code === 'fail'
     ? toast.error(message, {
         transition: Zoom,
         autoClose: 3000,
         closeButton: false,
         hideProgressBar: true,
       })
-    : message === '401'
+    : code === '401'
     ? toast.error('session expired please login again', {
         transition: Zoom,
         autoClose: 3000,
