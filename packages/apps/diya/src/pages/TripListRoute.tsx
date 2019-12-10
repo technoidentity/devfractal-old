0694e81f2591d28b891e3bc83a6e404975291c21
gopikrishnak <gopikrishnakathari@gmail.com>
2019-12-10 17:31:09 +0530

    add trip list route

--- packages/apps/diya/src/pages/TripListRoute.tsx
+++ packages/apps/diya/src/pages/TripListRoute.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
-import { All, paths } from 'technoidentity-devfractal'
-import { tripAPI } from '../common'
-import { TripList } from '../views'
+import { Get, paths, Route } from 'technoidentity-devfractal'
+import { cargosUrl, tripAPI, TripListResponse } from '../common'
+import { TripDetailsTable } from '../views/TripsDetailsTable'
 
-const ps = paths('trips')
+const ps = paths(tripAPI.resource)
+
+export async function getTripList(): Promise<TripListResponse['data']['rows']> {
+  try {
+    const drivers = await cargosUrl().get(
+      { resource: 'vehicles/assign/clients' },
+      TripListResponse,
+    )
+    return drivers.data.rows
+  } catch (error) {
+    // sessionExpire({ error, setUser, logout, toastMessage })
+    throw Error(error)
+  }
+}
+
+const TripList = () => (
+  <Get asyncFn={getTripList} component={TripDetailsTable} />
+)
 
 export const TripListRoute: React.FC = () => (
-  <All api={tripAPI} path={ps.list} list={TripList} />
+  <Route path={ps.list} render={() => <TripList />} />
 )
