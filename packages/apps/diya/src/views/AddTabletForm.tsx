381ca8afaebaac4aa6dda2e08bb7dd9ac8849905
swathi <swathivalasapalli@gmail.com>
2019-12-11 16:51:41 +0530

    minor fix: change Submit as per required

--- packages/apps/diya/src/views/AddTabletForm.tsx
+++ packages/apps/diya/src/views/AddTabletForm.tsx
@@ -10,51 +10,54 @@ import { getVehicleList } from '../pages'
 
 const tabletLinks = links('tablets')
 
-export const TabletForm = formComponent(TabletData, ({ initial, onSubmit }) => {
-  const { logout, setUser } = useAuth()
-  const [vehicleList, setVehicleList] = useState<
-    VehicleResponse['data']['rows']
-  >([])
-  React.useMemo(async () => {
-    const vehicleList = await getVehicleList({ setUser, logout })
-    setVehicleList(vehicleList)
-  }, [logout, setUser])
-  return (
-    <>
-      <Section>
-        <HeadTitle>Add Tablet</HeadTitle>
-      </Section>
-      <CreateLink alignment="right" variant="primary" to={tabletLinks.list}>
-        Back
-      </CreateLink>
-      <Section>
-        <Simple.Form initialValues={initial} onSubmit={onSubmit}>
-          <Columns>
-            <Column>
-              <Simple.Text
-                label="Android Device ID"
-                name="androidDeviceId"
-                validations={[length(16)]}
-              />
-              <Simple.Select
-                label="Vehicle Id"
-                name="vehicleId"
-                required
-                fullWidth
-              >
-                <option value="">Select Vehicle</option>
-                {vehicleList &&
-                  vehicleList.map(vehicleList => (
-                    <option key={vehicleList.id} value={vehicleList.id}>
-                      {vehicleList.vehicleName}
-                    </option>
-                  ))}
-              </Simple.Select>
-            </Column>
-          </Columns>
-          <Simple.FormButtons submit={'Save'} />
-        </Simple.Form>
-      </Section>
-    </>
-  )
-})
+export const TabletForm = formComponent(
+  TabletData,
+  ({ initial, edit, onSubmit }) => {
+    const { logout, setUser } = useAuth()
+    const [vehicleList, setVehicleList] = useState<
+      VehicleResponse['data']['rows']
+    >([])
+    React.useMemo(async () => {
+      const vehicleList = await getVehicleList({ setUser, logout })
+      setVehicleList(vehicleList)
+    }, [logout, setUser])
+    return (
+      <>
+        <Section>
+          <HeadTitle>Add Tablet</HeadTitle>
+        </Section>
+        <CreateLink alignment="right" variant="primary" to={tabletLinks.list}>
+          Back
+        </CreateLink>
+        <Section>
+          <Simple.Form initialValues={initial} onSubmit={onSubmit}>
+            <Columns>
+              <Column>
+                <Simple.Text
+                  label="Android Device ID"
+                  name="androidDeviceId"
+                  validations={[length(16)]}
+                />
+                <Simple.Select
+                  label="Vehicle Id"
+                  name="vehicleId"
+                  required
+                  fullWidth
+                >
+                  <option value="">Select Vehicle</option>
+                  {vehicleList &&
+                    vehicleList.map(vehicleList => (
+                      <option key={vehicleList.id} value={vehicleList.id}>
+                        {vehicleList.vehicleName}
+                      </option>
+                    ))}
+                </Simple.Select>
+              </Column>
+            </Columns>
+            <Simple.FormButtons submit={edit ? 'Update' : 'Save'} />
+          </Simple.Form>
+        </Section>
+      </>
+    )
+  },
+)
