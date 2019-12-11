20b5364fbfc4a05f6015063b35f178219cd084e5
gopikrishnak <gopikrishnakathari@gmail.com>
2019-12-11 18:25:27 +0530

    use setHeaderText function for update reports headers

--- packages/apps/diya/src/reports/ReportsRoute.tsx
+++ packages/apps/diya/src/reports/ReportsRoute.tsx
@@ -1,17 +1,32 @@
 import { History } from 'history'
 import React from 'react'
 import { Route, useHistory } from 'technoidentity-devfractal'
+import { useAuth } from '../auth/AuthContext'
 import { PerformanceReport } from './batteryreports/PerformanceReport'
+import { AssignedEVsReport } from './clientreports/AssignedEVsReport'
+import { InvoicesReport } from './clientreports/InvoicesReport'
 import { TotalAnalysisReport } from './costanalysisreports/TotalAnalysisReport'
 import { ReportsScreen } from './ReportsScreen'
 
 export const ReportsRoute = () => {
+  const { setHeaderText } = useAuth()
   const history: History = useHistory()
   const handleBatteryPerformance = () => {
-    history.push('/reports/batteryperformance')
+    setHeaderText('Battery Reports > Performance')
+    history.push('/reports/batteryPerformance')
   }
   const handleTotalAnalysisReports = () => {
-    history.push('/reports/totalanalysisreports')
+    setHeaderText('Cost Analysis Report > Total Analysis Report')
+    history.push('/reports/totalAnalysisReport')
+  }
+
+  const handleInvoicesReports = () => {
+    setHeaderText('Client Reports > Invoices Report')
+    history.push('/reports/invoicesReport')
+  }
+  const handleEvsAssignedReports = () => {
+    setHeaderText('Client Reports > Assigned EVs Report')
+    history.push('/reports/assignedEvsReport')
   }
   return (
     <>
@@ -21,16 +36,23 @@ export const ReportsRoute = () => {
           <ReportsScreen
             handleBatteryPerformance={handleBatteryPerformance}
             handleTotalAnalysisReports={handleTotalAnalysisReports}
+            handleInvoicesReports={handleInvoicesReports}
+            handleEvsAssignedReports={handleEvsAssignedReports}
           />
         )}
       />
       <Route
-        path="/reports/batteryperformance"
+        path="/reports/batteryPerformance"
         render={() => <PerformanceReport />}
       />
       <Route
-        path="/reports/totalanalysisreports"
+        path="/reports/totalAnalysisReport"
         render={() => <TotalAnalysisReport />}
+      />
+      <Route path="/reports/invoicesReport" render={() => <InvoicesReport />} />
+      <Route
+        path="/reports/assignedEvsReport"
+        render={() => <AssignedEVsReport />}
       />
     </>
   )
