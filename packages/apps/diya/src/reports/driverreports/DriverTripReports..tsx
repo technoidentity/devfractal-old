4d4fca0c33e3409fd09b69143e45487fe1677234
swathi <swathivalasapalli@gmail.com>
2019-12-12 13:18:30 +0530

    add DriverTripReport

--- packages/apps/diya/src/reports/driverreports/DriverTripReports..tsx
+++ packages/apps/diya/src/reports/driverreports/DriverTripReports..tsx
@@ -1,102 +1,109 @@
-import { faCalendar } from '@fortawesome/free-solid-svg-icons'
 import {
-  Box,
-  Card,
-  CardContent,
-  Column,
-  Columns,
-  Input,
-  Section,
-  Select,
-} from 'devfractal-ui-core'
+  faCalendar,
+  faMapMarker,
+  faUserCircle,
+} from '@fortawesome/free-solid-svg-icons'
+import { Box, Columns, Icon, Input, Select, Text } from 'devfractal-ui-core'
 import React from 'react'
 import { HeadTitle } from '../../components'
 import { Table } from '../../reacttable/Table'
 
-// tslint:disable-next-line:readonly-array
-const fakeData = [
-  {
-    id: '1',
-    noOfCustomers: 10,
-    start: '14/08/2019',
-    end: '14/08/2019',
-    successfulDeliveries: 10,
-    cashCollected: 450000,
-  },
-  {
-    id: '2',
-    noOfCustomers: 12,
-    start: '14/08/2019',
-    end: '14/08/2019',
-    successfulDeliveries: 12,
-    cashCollected: 450000,
-  },
-  {
-    id: '3',
-    noOfCustomers: 13,
-    start: '14/08/2019',
-    end: '14/08/2019',
-    successfulDeliveries: 13,
-    cashCollected: 450000,
-  },
-  {
-    id: '4',
-    noOfCustomers: 14,
-    start: '14/08/2019',
-    end: '14/08/2019',
-    successfulDeliveries: 14,
-    cashCollected: 450000,
-  },
-]
-
-export const DriverTripReport: React.FC = () => (
-  <>
-    <HeadTitle>
-      <Section>Driver Reports > Trip Report</Section>{' '}
-    </HeadTitle>
-    <Card>
-      <Columns>
-        <Column>
-          <Box>
-            <Columns>
-              <Column>Driver-Ramesh</Column>
-              <Column>Client Name: Amazon</Column>
-              <Column>TripID-452</Column>
-              <Column>
-                <Select name="select client">
-                  <option value="select client">select client</option>
-                </Select>
-              </Column>
-              <Column>
-                <Input rightIcon={faCalendar} placeholder="12/12/2019" />
-              </Column>
-            </Columns>
-          </Box>
-          <CardContent>
-            <Table
-              tableData={fakeData}
-              sorting={false}
-              pagination={true}
-              headerNames={[
-                'id',
-                'noOfCustomers',
-                'start',
-                'end',
-                'successfulDeliveries',
-                'cashCollected',
-              ]}
-              headerLabels={{
-                id: 'ID',
-                noOfCustomers: 'No Of Customers',
-                start: 'Start',
-                end: 'End',
-                successfulDeliveries: 'Successful Deliveries',
-                cashCollected: 'Cash Collected',
-              }}
-            />
-          </CardContent>
-        </Column>
+export const DriverTripReport: React.FC = () => {
+  const data = [
+    {
+      id: '1',
+      noOfCustomers: 10,
+      start: '14/08/2019',
+      end: '14/08/2019',
+      successfulDeliveries: 10,
+      cashCollected: 450000,
+    },
+    {
+      id: '2',
+      noOfCustomers: 12,
+      start: '14/08/2019',
+      end: '14/08/2019',
+      successfulDeliveries: 12,
+      cashCollected: 450000,
+    },
+    {
+      id: '3',
+      noOfCustomers: 13,
+      start: '14/08/2019',
+      end: '14/08/2019',
+      successfulDeliveries: 13,
+      cashCollected: 450000,
+    },
+    {
+      id: '4',
+      noOfCustomers: 14,
+      start: '14/08/2019',
+      end: '14/08/2019',
+      successfulDeliveries: 14,
+      cashCollected: 450000,
+    },
+  ]
+  return (
+    <>
+      <Box style={{ backgroundColor: '#87CEFA' }}>
+        <HeadTitle> Driver Reports > Trip Report</HeadTitle>
+      </Box>
+      <Columns style={{ paddingLeft: '40px', marginTop: '10px' }}>
+        <Icon
+          color="#2c7dc0"
+          style={{ marginTop: '-1px', marginLeft: '15px' }}
+          icon={faUserCircle}
+        />
+        <Text style={{ color: '#2c7dc0', marginLeft: '8px' }}>
+          Driver Name -{' '}
+        </Text>{' '}
+        <Text>Ramesh Kumar </Text>
+        <Icon
+          color="#2c7dc0"
+          style={{ marginTop: '-1px', marginLeft: '15px' }}
+          icon={faUserCircle}
+        />
+        <Text style={{ color: '#2c7dc0', marginLeft: '8px' }}>
+          Client Name -{' '}
+        </Text>{' '}
+        <Text>Amazon </Text>
+        <Icon
+          color="#2c7dc0"
+          style={{ marginTop: '-1px', marginLeft: '15px' }}
+          icon={faMapMarker}
+        />
+        <Text style={{ color: '#2c7dc0', marginLeft: '8px' }}>Trip ID -</Text>{' '}
+        <Text style={{ paddingLeft: '10px' }}>452</Text>
+        <Select name="select client" style={{ marginLeft: '40px' }}>
+          <option value="select client">select client</option>
+        </Select>
+        <Input
+          rightIcon={faCalendar}
+          placeholder="12/12/2019"
+          style={{ marginLeft: '40px' }}
+        />
       </Columns>
-    </Card>
-  </>
-)
+      <Table
+        tableData={data}
+        sorting={false}
+        pagination={true}
+        headerNames={[
+          'id',
+          'noOfCustomers',
+          'start',
+          'end',
+          'successfulDeliveries',
+          'cashCollected',
+        ]}
+        headerLabels={{
+          id: 'ID',
+          noOfCustomers: 'No Of Customers',
+          start: 'Start',
+          end: 'End',
+          successfulDeliveries: 'Successful Deliveries',
+          cashCollected: 'Cash Collected',
+        }}
+      />
+    </>
+  )
+}
