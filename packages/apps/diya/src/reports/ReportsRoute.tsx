85f1bc082fe42000d1def69bdf1e422514848370
gopikrishnak <gopikrishnakathari@gmail.com>
2019-12-13 10:33:41 +0530

    remove un used code

--- packages/apps/diya/src/reports/ReportsRoute.tsx
+++ packages/apps/diya/src/reports/ReportsRoute.tsx
@@ -1,7 +1,6 @@
 import { History } from 'history'
 import React from 'react'
 import { Route, useHistory } from 'technoidentity-devfractal'
-import { useAuth } from '../auth/AuthContext'
 import { PerformanceReport } from './batteryreports/PerformanceReport'
 import { BatteryStatusReport } from './batteryreports/StatusReport'
 import { BatteryDayWiseReport } from './batteryreports/StockReport'
@@ -14,47 +13,37 @@ import { DriverTripReport } from './driverreports/DriverTripReports.'
 import { ReportsScreen } from './ReportsScreen'
 
 export const ReportsRoute = () => {
-  const { setHeaderText } = useAuth()
   const history: History = useHistory()
   const handleBatteryPerformance = () => {
-    setHeaderText('Battery Reports > Performance')
     history.push('/reports/batteryPerformance')
   }
   const handleTotalAnalysisReports = () => {
-    setHeaderText('Cost Analysis Report > Total Analysis Report')
     history.push('/reports/totalAnalysisReport')
   }
 
   const handleInvoicesReports = () => {
-    setHeaderText('Client Reports > Invoices Report')
     history.push('/reports/invoicesReport')
   }
   const handleEvsAssignedReports = () => {
-    setHeaderText('Client Reports > Assigned EVs Report')
     history.push('/reports/assignedEvsReport')
   }
 
   const handleDriverDataReport = () => {
-    setHeaderText('Driver Reports > Data Report')
     history.push('/reports/driverDataReport')
   }
   const handleDriverTripReport = () => {
-    setHeaderText('Driver Reports > Trip Report')
     history.push('/reports/driverTripReport')
   }
 
   const handleDriverBehaviourReport = () => {
-    setHeaderText('Driver Reports > Behaviour Report')
     history.push('/reports/driverBehaviourReport')
   }
 
   const handleBatteryDayWiseReport = () => {
-    setHeaderText('Battery Reports > Day wise Report')
     history.push('/reports/batteryDayWiseReport')
   }
 
   const handleBatteryStatusReport = () => {
-    setHeaderText('Battery Reports > Status Report')
     history.push('/reports/batteryStatusReport')
   }
 
