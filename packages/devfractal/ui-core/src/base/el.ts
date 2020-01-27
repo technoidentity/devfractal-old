becf98d6905339f7c1af1a316b3e70cfedb4dee0
Pervez Iqbal <pervezfunctor@gmail.com>
2020-01-27 17:59:44 +0530

    minor change

--- packages/devfractal/ui-core/src/base/el.ts
+++ packages/devfractal/ui-core/src/base/el.ts
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Helpers, removeHelpers } from './helpers'
 
-export interface DivProps
-  extends React.AllHTMLAttributes<HTMLElement>,
-    Helpers {
+export interface ElProps extends React.AllHTMLAttributes<HTMLElement>, Helpers {
   readonly as?: keyof React.ReactHTML
 }
 
-export function El({ as = 'div', className, ...props }: DivProps): JSX.Element {
+export function El({ as = 'div', className, ...props }: ElProps): JSX.Element {
   return React.createElement(as, { ...removeHelpers(props), className })
 }
