6a69363116e2590ee401b2787079ed863a7068f6
sravanidande <sravanidnd89@gmail.com>
2020-01-27 17:27:18 +0530

    use useField in CheckboxField

--- packages/devfractal/forms/src/fields/CheckboxField.tsx
+++ packages/devfractal/forms/src/fields/CheckboxField.tsx
@@ -3,19 +3,11 @@ import { useField } from 'formik'
 import React from 'react'
 import { FormikFieldConfig } from '../types'
 
-export type CheckboxFieldProps = CheckBoxProps &
-  Pick<FormikFieldConfig, 'name' | 'validate'>
+export type CheckboxFieldProps = CheckBoxProps & FormikFieldConfig
 
-export const CheckboxField: React.FC<CheckboxFieldProps> = ({
-  form,
-  children,
-  ...props
-}) => {
-  const [{ value, ...field }] = useField(props)
+export const CheckboxField: React.FC<CheckboxFieldProps> = props => {
+  const [field] = useField(props)
+  const { validate, ...rest } = props
 
-  return (
-    <CheckBox {...props} {...field} checked={value}>
-      {children}
-    </CheckBox>
-  )
+  return <CheckBox {...field} {...rest} />
 }
