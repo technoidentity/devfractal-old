5baa9955adda5411dfeffe166e189f4dd14e837f
sravanidande <sravanidnd89@gmail.com>
2020-01-29 19:17:15 +0530

    use useField for DateMuiField

--- packages/devfractal/mui/src/fields/DateMuiField.tsx
+++ packages/devfractal/mui/src/fields/DateMuiField.tsx
@@ -3,37 +3,29 @@ import {
   KeyboardDatePicker,
   MuiPickersUtilsProvider,
 } from '@material-ui/pickers'
-import { ParsableDate } from '@material-ui/pickers/constants/prop-types'
 import { KeyboardDatePickerProps } from '@material-ui/pickers/DatePicker'
-import { Field as FormikField, FieldProps as FormikFieldProps } from 'formik'
+import { useField } from 'formik'
 import React from 'react'
 import { FormikFieldConfig } from '../types'
-type FormikDateProps = FormikFieldProps<ParsableDate>
-
-const DateMui: (props: FormikDateProps) => JSX.Element = ({
-  form,
-  field,
-  ...props
-}) => (
-  <MuiPickersUtilsProvider utils={DateFnsUtils}>
-    <KeyboardDatePicker
-      {...field}
-      {...props}
-      onChange={event => {
-        form.handleChange(event)
-        form.setFieldValue(field.name, event)
-      }}
-    />
-  </MuiPickersUtilsProvider>
-)
 
 export type DateFieldProps = FormikFieldConfig & KeyboardDatePickerProps
 
 export const DateMuiField: React.FC<DateFieldProps> = ({
   children,
   ...props
-}) => (
-  <FormikField {...props} component={DateMui}>
-    {children}
-  </FormikField>
-)
+}) => {
+  const [field, , helpers] = useField(props as any)
+  const { validate, ...rest } = props
+
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <KeyboardDatePicker
+        {...field}
+        {...rest}
+        onChange={date => {
+          helpers.setValue(date)
+        }}
+      />
+    </MuiPickersUtilsProvider>
+  )
+}
