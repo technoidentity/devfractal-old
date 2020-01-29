fcd7abd0dfe4858d36439fdb6fb679d5dd0d394f
sravanidande <sravanidnd89@gmail.com>
2020-01-29 19:16:32 +0530

    use useField for CheckBoxMuiField

--- packages/devfractal/mui/src/fields/CheckBoxMuiField.tsx
+++ packages/devfractal/mui/src/fields/CheckBoxMuiField.tsx
@@ -1,23 +1,13 @@
 import Checkbox, { CheckboxProps } from '@material-ui/core/Checkbox'
-import { Field as FormikField, FieldProps as FormikFieldProps } from 'formik'
+import { useField } from 'formik'
 import React from 'react'
 import { FormikFieldConfig } from '../types'
 
-type FormikCheckboxProps<V> = FormikFieldProps<V>
-
-const FormikMuiCheckbox: <V>(props: FormikCheckboxProps<V>) => JSX.Element = ({
-  form,
-  field,
-  ...props
-}) => <Checkbox {...field} {...props} />
-
 export type CheckboxFieldProps = FormikFieldConfig & CheckboxProps
 
-export const CheckboxMuiField: React.FC<CheckboxFieldProps> = ({
-  children,
-  ...props
-}) => (
-  <FormikField {...props} component={FormikMuiCheckbox}>
-    {children}
-  </FormikField>
-)
+export const CheckboxMuiField: React.FC<CheckboxFieldProps> = props => {
+  const [field] = useField({ type: 'checkbox', ...(props as any) })
+  const { validate, ...rest } = props
+
+  return <Checkbox {...field} {...rest} />
+}
