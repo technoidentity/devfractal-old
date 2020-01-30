1093f8502bf1ad968bf392c894dd010842cd4644
Pervez Iqbal <pervezfunctor@gmail.com>
2020-01-30 16:47:16 +0530

    rename MuiFields to Fields

--- packages/devfractal/mui/src/fields/TextField.tsx
+++ packages/devfractal/mui/src/fields/TextField.tsx
@@ -1,4 +1,4 @@
-import { TextField } from '@material-ui/core'
+import { TextField as MuiTextField } from '@material-ui/core'
 import { TextFieldProps } from '@material-ui/core/TextField'
 import { useField } from 'formik'
 import React from 'react'
@@ -6,9 +6,9 @@ import { FormikFieldConfig } from '../types'
 
 export type InputFieldProps = FormikFieldConfig & TextFieldProps
 
-export const TextMuiField: React.FC<InputFieldProps> = props => {
+export const TextField: React.FC<InputFieldProps> = props => {
   const [field] = useField(props as any)
   const { validate, ...rest } = props
 
-  return <TextField fullWidth={true} {...field} {...rest} />
+  return <MuiTextField fullWidth={true} {...field} {...rest} />
 }
