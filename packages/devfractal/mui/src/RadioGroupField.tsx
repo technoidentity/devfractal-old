40f6585e95dce58cec6ff9bc254fa3926ef82fe7
sravanidande <sravanidnd89@gmail.com>
2020-02-04 17:03:14 +0530

    add RadioItem

--- packages/devfractal/mui/src/RadioGroupField.tsx
+++ packages/devfractal/mui/src/RadioGroupField.tsx
@@ -1,4 +1,4 @@
-import { RadioGroup } from '@material-ui/core'
+import { FormControlLabel, Radio, RadioGroup } from '@material-ui/core'
 import { RadioGroupProps } from '@material-ui/core/RadioGroup'
 import { useField } from 'formik'
 import React from 'react'
@@ -6,9 +6,28 @@ import { FormikFieldConfig } from './types'
 
 export type RadioProps = FormikFieldConfig & RadioGroupProps
 
+interface RadioContextProps {
+  readonly name: string
+}
+
+const RadioFieldContext: React.Context<RadioContextProps> = React.createContext(
+  { name: '' },
+)
+
 export const RadioGroupField: React.FC<RadioProps> = props => {
   const [field] = useField(props as any)
   const { validate, ...rest } = props
 
-  return <RadioGroup {...field} {...rest} />
+  return (
+    <RadioFieldContext.Provider value={{ name: props.name }}>
+      <RadioGroup {...field} {...rest} />
+    </RadioFieldContext.Provider>
+  )
+}
+
+export const RadioItem: React.FC<any> = ({ label, ...rest }) => {
+  const { name } = React.useContext(RadioFieldContext)
+  return (
+    <FormControlLabel control={<Radio name={name} />} label={label} {...rest} />
+  )
 }
