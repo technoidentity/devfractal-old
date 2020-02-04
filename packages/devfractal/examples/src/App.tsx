dda6c34482a1f52b49a74de10c6b4e55bb0ec34c
sravanidande <sravanidnd89@gmail.com>
2020-02-04 17:16:06 +0530

    minor change

--- packages/devfractal/examples/src/App.tsx
+++ packages/devfractal/examples/src/App.tsx
@@ -1,5 +1,16 @@
+import DateFnsUtils from '@date-io/date-fns'
+import { MuiPickersUtilsProvider } from '@material-ui/pickers'
 import 'bulma/css/bulma.css'
 import React from 'react'
 import { render } from 'react-dom'
+import { FieldsMuiGeneralForm } from './composites/FieldsMuiGeneralForm'
 
-render(<h1>hello world</h1>, document.getElementById('root'))
+export const App: React.FC = () => {
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <FieldsMuiGeneralForm />
+    </MuiPickersUtilsProvider>
+  )
+}
+
+render(<App />, document.getElementById('root'))
