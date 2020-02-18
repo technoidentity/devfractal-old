e63757dd66a7f4d409ee2bcd6a740c871b49bfb5
pervezfunctor <pervezfunctor@gmail.com>
2020-02-18 23:40:12 +0530

    minor change

--- packages/apps/starter/src/App.tsx
+++ packages/apps/starter/src/App.tsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import { APIComponents, Router, UIComponents } from 'technoidentity-devfractal'
 import { TodoApp } from './TodoApp'
 
-export const App = () => <TodoApp />
+export const App = () => (
+  <APIComponents.Provider value={UIComponents}>
+    <Router variant="browser">
+      <TodoApp />
+    </Router>
+  </APIComponents.Provider>
+)
