7fd1edd81af3880ad3a0af7c2b67dfa7f65a35f0
pervezfunctor <pervezfunctor@gmail.com>
2020-03-16 00:23:06 +0530

    add option spec

---
+++ packages/common/utils/src/spec/option.ts
@@ -0,0 +1,47 @@
+import {
+  Any,
+  Mixed,
+  OutputOf,
+  Type,
+  TypeOf,
+  undefined as ioUndefined,
+  UndefinedC,
+  union,
+  UnionC,
+} from 'io-ts'
+
+// tslint:disable readonly-array typedef no-class
+
+export class OptionType<
+  C extends Any,
+  A = any,
+  O = A,
+  I = unknown
+> extends Type<A, O, I> {
+  readonly _tag: 'OptType' = 'OptType'
+  constructor(
+    name: string,
+    is: OptionType<C, A, O, I>['is'],
+    validate: OptionType<C, A, O, I>['validate'],
+    encode: OptionType<C, A, O, I>['encode'],
+    readonly spec: C,
+  ) {
+    super(name, is, validate, encode)
+  }
+}
+
+export interface OptionC<C extends Mixed>
+  extends OptionType<
+    UnionC<[C, UndefinedC]>,
+    TypeOf<UnionC<[C, UndefinedC]>>,
+    OutputOf<UnionC<[C, UndefinedC]>>
+  > {}
+
+export const option = <C extends Mixed>(
+  codec: C,
+  name: string = `Opt<${codec.name}>`,
+): OptionC<C> => {
+  const type = union([codec, ioUndefined])
+
+  return new OptionType(name, type.is, type.validate, type.encode, type)
+}
