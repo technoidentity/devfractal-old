c364194ec1018f03e5c678a0b61b0b9dc0542792
pervezfunctor <pervezfunctor@gmail.com>
2020-03-19 13:31:05 +0530

    comment out variations of obj

--- packages/common/utils/src/spec/propsObj.ts
+++ packages/common/utils/src/spec/propsObj.ts
@@ -1,89 +1,16 @@
-import {
-  intersection,
-  partial,
-  PartialC,
-  Props,
-  readonly,
-  type,
-  TypeC,
-  TypeOf,
-} from 'io-ts'
-import { PickByValue } from 'utility-types'
-import { buildObject } from '../common'
-import { AnyObj, obj, ObjC, ObjType, opt, req } from './obj'
-import { ManyC } from './objMany'
-import { OneC } from './objOne'
-import { OptionC } from './objOption'
+import { Props } from 'io-ts'
+import { Optional, OptionalKeys, RequiredKeys } from 'utility-types'
+import { obj, ObjC, opt, req } from './obj'
 
-type Optional = OptionC<any> | OneC<any> | ManyC<any>
-type OptKeys<P extends Props> = keyof PickByValue<P, Optional>
-type ReqKeys<P extends Props> = keyof Omit<P, OptKeys<P>>
-
-export type OptProps<P extends Props> = Pick<P, OptKeys<P>>
-export type ReqProps<P extends Props> = Pick<P, ReqKeys<P>>
-
-export type PropsC<P extends Props> = ObjC<OptProps<P>, ReqProps<P>>
-
-// tslint:disable typedef
-export function props<P extends Props>(
-  props: P,
-  name?: string,
-): ObjC<OptProps<P>, ReqProps<P>> {
-  const optional = buildObject<any, any>(props, pv =>
-    pv._tag &&
-    (pv._tag === 'OptType' || pv._tag === 'OneType' || pv._tag === 'ManyType')
-      ? pv
-      : undefined,
-  )
-  const required = buildObject<any, any>(props, (v, k) =>
-    k in optional ? undefined : v,
-  )
-
-  const spec = intersection([
-    readonly(partial(optional)),
-    readonly(type(required)),
-  ])
-
-  return new ObjType(spec, name || spec.name) as any
-}
-// tslint:enable typedef
-
-export type PropsTypeOf<P extends Props> = TypeOf<PropsC<P>>
-
-export type PropsReqOf<P extends AnyObj> = TypeOf<
-  TypeC<Pick<P['props'], ReqKeys<P['props']>>>
->
-
-export type PropsOptOf<P extends AnyObj> = TypeOf<
-  PartialC<Pick<P['props'], OptKeys<P['props']>>>
->
-
-export type PropsOptionOf<P extends AnyObj> = TypeOf<
-  PartialC<PickByValue<P['props'], OptionC<any>>>
->
-
-export type PropsOneOf<P extends AnyObj> = TypeOf<
-  PartialC<PickByValue<P['props'], OneC<any>>>
->
-
-export type PropsManyOf<P extends AnyObj> = TypeOf<
-  PartialC<PickByValue<P['props'], ManyC<any>>>
->
-
-type UnWrap<P extends Props> = { readonly [K in keyof P]: P[K]['_A'] }
-export type PropsCompatC<P extends Props> = ObjC<
-  UnWrap<OptProps<P>>,
-  ReqProps<P>
->
-export type WrapOption<P extends Props> = {
-  readonly [K in keyof P]: OptionC<P[K]>
-}
+export type ReqProps<P extends Props> = Pick<P, RequiredKeys<P>>
+export type OptProps<P extends Props> = Pick<P, OptionalKeys<P>>
+export type ObjPropsC<P extends Props> = ObjC<ReqProps<P>, OptProps<P>>
 
 export function propsObj<Opt extends Props, Req extends Props>(
   optional: Opt,
   required: Req,
   name?: string,
-): PropsC<WrapOption<Opt> & Req> {
+): ObjPropsC<Opt & Partial<Req>> {
   return obj(optional, required, name) as any
 }
 
@@ -98,16 +25,77 @@ export function propsObj<Opt extends Props, Req extends Props>(
 export function propsReq<Req extends Props>(
   required: Req,
   name?: string,
-): PropsC<Req> {
+): ObjPropsC<Req> {
   return req(required, name) as any
 }
 
 export function propsOpt<Opt extends Props>(
   optional: Opt,
   name?: string,
-): PropsC<WrapOption<Opt>> {
+): ObjPropsC<Optional<Opt>> {
   return opt(optional, name) as any
 }
+
+// type OptKeys<P extends Props> = keyof PickByValue<P, OptionC<any>>
+// type ReqKeys<P extends Props> = keyof Omit<P, OptKeys<P>>
+
+// export type OptProps<P extends Props> = Pick<P, OptKeys<P>>
+// export type ReqProps<P extends Props> = Pick<P, ReqKeys<P>>
+
+// export type PropsC<P extends Props> = ObjC<OptProps<P>, ReqProps<P>>
+
+// tslint:disable typedef
+// export function props<P extends Props>(
+//   props: P,
+//   name?: string,
+// ): PropsC<OptProps<P>, ReqProps<P>> {
+//   const optional = buildObject<any, any>(props, pv =>
+//     isOption(pv) ? pv.spec : undefined,
+//   )
+
+//   const required = buildObject<any, any>(props, (v, k) =>
+//     k in optional ? undefined : v,
+//   )
+
+//   const spec = intersection([
+//     readonly(partial(optional)),
+//     readonly(type(required)),
+//   ])
+
+//   return obj(optional, required, name || spec.name) as any
+// }
+// tslint:enable typedef
+
+// export type PropsTypeOf<P extends Props> = TypeOf<PropsC<P>>
+
+// export type PropsReqOf<P extends AnyObj> = TypeOf<
+//   TypeC<Pick<P['props'], ReqKeys<P['props']>>>
+// >
+
+// export type PropsOptOf<P extends AnyObj> = TypeOf<
+//   PartialC<Pick<P['props'], OptKeys<P['props']>>>
+// >
+
+// export type PropsOptionOf<P extends AnyObj> = TypeOf<
+//   PartialC<PickByValue<P['props'], OptionC<any>>>
+// >
+
+// export type PropsOneOf<P extends AnyObj> = TypeOf<
+//   PartialC<PickByValue<P['props'], OneC<any>>>
+// >
+
+// export type PropsManyOf<P extends AnyObj> = TypeOf<
+//   PartialC<PickByValue<P['props'], ManyC<any>>>
+// >
+
+// type UnWrap<P extends Props> = { readonly [K in keyof P]: P[K]['_A'] }
+// export type PropsCompatC<P extends Props> = ObjC<
+//   UnWrap<OptProps<P>>,
+//   ReqProps<P>
+// >
+// export type WrapOption<P extends Props> = {
+//   readonly [K in keyof P]: OptionC<P[K]>
+// }
 
 // const Rect = propsObj(
 //   {
